2b5e9c81b35634c6997439a9bfbfec7e3f496f8c
Parham Alvani <parham.alvani@gmail.com>
2015-02-01 01:48:44 +0330

    update script improved.

--- update.bash
+++ update.bash
@@ -6,32 +6,39 @@
 #
 # [] Creation Date : 01-02-2015
 #
-# [] Last Modified : Sun Feb  1 01:13:31 2015
+# [] Last Modified : Sun Feb  1 01:48:37 2015
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 
 # update vimrc file
 if [ -e $HOME/.vimrc ]; then
+	echo "cp $HOME/.vimrc vimrc"
 	cp $HOME/.vimrc vimrc
 fi
 
 # update zshrc file
 if [ -e $HOME/.zshrc ]; then
+	echo "cp $HOME/.zshrc zshrc"
 	cp $HOME/.zshrc zshrc
 fi
 
 # update gdbinit file
 if [ -e $HOME/.gdbinit ]; then
+	echo "cp $HOME/.gdbinit gdbinit"
 	cp $HOME/.gdbinit gdbinit
 fi
 
 # update pinerc file
 if [ -e $HOME/.pinerc ]; then
+	echo "cp $HOME/.pinerc pinerc"
 	cp $HOME/.pinerc pinerc
 fi
 
 # update vim folder
 if [ -d $HOME/.vim ]; then
-	cp -Ru $HOME/.vim Vim
+	echo "rm -Rf Vim"
+	rm -Rf Vim
+	echo "cp -Ru $HOME/.vim Vim"
+	cp -R $HOME/.vim Vim
 fi
