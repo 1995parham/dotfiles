69c0490550f3bfb080f1e77c8b2ef52502d51aa8
Parham Alvani <parham.alvani@gmail.com>
2015-02-01 01:36:04 +0330

    Dummy project improved.
    Former-commit-id: 9b59cbc4f21ca2feff759c7f665b62016bd62186 [formerly f5371f939694fc1855ea1d60ee18bc369297711d] [formerly b950cc0c3a04814d489e05c623d07f664e3be57f [formerly 4b03c9d447d538b6aabae12e5c4dc4ebcfd1ef5f [formerly 4b03c9d447d538b6aabae12e5c4dc4ebcfd1ef5f [formerly 4b03c9d447d538b6aabae12e5c4dc4ebcfd1ef5f [formerly e4755a5baa07427430ad125be0e77355315558a1]]]]]
    Former-commit-id: d4edc3488a5c35f198c35221918af3f2b25da639 [formerly 0b2bc351c8117076da801a0d8d6fdd97deb0ec7c]
    Former-commit-id: 332759d9abf559de6022c2aa4e5fb5c7219f0b34
    Former-commit-id: 3bcb5c579d018469f7c9b9eb209f344c3cc0eda6
    Former-commit-id: c34a6df4d5811524d2310e818fb06ff472265df0
    Former-commit-id: d8bb0d008de401a2ee6168893a149e202a7b8ce7

--- git-update.bash
+++ git-update.bash
@@ -6,9 +6,15 @@
 #
 # [] Creation Date : 01-02-2015
 #
-# [] Last Modified : Sun Feb  1 01:32:24 2015
+# [] Last Modified : Sun Feb  1 01:35:59 2015
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+
+# pulling main project
+echo "start pulling main project..."
 git pull origin master
+
+# pulling sub projects
+echo "start pulling sub projects..."
 git submodule foreach git pull
