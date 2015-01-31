9dd8a2d661f70659701888ff71d0c66c4d39722f
Parham Alvani <parham.alvani@gmail.com>
2015-02-01 01:19:01 +0330

    2 dummy script is added.
    Former-commit-id: 1eb7f33d0b4ca6c5caeb8f2583b6757e95b28b7d [formerly a7fcb58adbbed9acd5844b522e10c8ec9cab82cc] [formerly 934ff89b36cb2b3e8e99cadfceda31c4e58d9fa0 [formerly c78529efd6d1296f6914ec8c4df16b3e93d8430c [formerly c78529efd6d1296f6914ec8c4df16b3e93d8430c [formerly c78529efd6d1296f6914ec8c4df16b3e93d8430c [formerly 52647c154a3caaba235755da21fcc2649a6d31eb]]]]]
    Former-commit-id: b3aaec9145253209a74d3f31ba5ab28c621de2dc [formerly b65ce125fbe7db9ca06a9346a56fe0a4ed86e657]
    Former-commit-id: 50b14d03d7d5a79fc27df088b48c3d16a40192af
    Former-commit-id: c1ef3f98362f0b2382c67a08004d1b96fa84ff74
    Former-commit-id: 24b2b2bff93ccaa1afe7e53fd14351f7aa6967ae
    Former-commit-id: ead5e4274805059071e4e2e3f9985de909e53a83

---
+++ install.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# In The Name Of God
+# ========================================
+# [] File Name : install.bash
+#
+# [] Creation Date : 01-02-2015
+#
+# [] Last Modified : Sun Feb  1 01:16:43 2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+# install vimrc file
+if [ -e $HOME/.vimrc ]; then
+	echo "cp vimrc $HOME/.vimrc"
+	cp vimrc $HOME/.vimrc
+fi
+
+# install zshrc file
+if [ -e $HOME/.zshrc ]; then
+	echo "cp zshrc $HOME/.zshrc"
+	cp zshrc $HOME/.zshrc
+fi
+
+# install gdbinit file
+if [ -e $HOME/.gdbinit ]; then
+	echo "cp gdbinit $HOME/.gdbinit"
+	cp gdbinit $HOME/.gdbinit
+fi
+
+# install pinerc file
+if [ -e $HOME/.pinerc ]; then
+	echo "cp pinerc $HOME/.pinerc"
+	cp pinerc $HOME/.pinerc
+fi
+
+# install vim folder
+if [ -d $HOME/.vim ]; then
+	echo "cp -Ru Vim $HOME/.vim"
+	cp -Ru Vim $HOME/.vim
+fi
