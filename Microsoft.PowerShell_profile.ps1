565a148a0bff200b8c10c1eb50d9395074f21c38
Parham Alvani <parham.alvani@gmail.com>
2015-03-30 14:40:50 +0430

    Powershell profile is added.
    Former-commit-id: 98bf80527821f178a09fe70cd464d17ce9df6575 [formerly 31a7d3e4552fe03d0f042bb6009ddc32ecb9ef7a] [formerly 04ee3f3f916a9dc1d73cbb3650666d2d099f3248 [formerly 19400e2f2af1ab9ec6d15175f701ccb686454d8a [formerly 19400e2f2af1ab9ec6d15175f701ccb686454d8a [formerly 19400e2f2af1ab9ec6d15175f701ccb686454d8a [formerly 0bd837932afe735954dfe331769d74f6d88a7096]]]]]
    Former-commit-id: c690019f4db7954ed27eebbefe952882a7154527 [formerly 1acaad79a97ea6f3748dc129ab552b9daf8bab30]
    Former-commit-id: cbcf51c3296d4fcbf7ea102b5ad49f7893ffcee8
    Former-commit-id: 30f9fc1d5cbd706b05cc6f54835311481d3882ba
    Former-commit-id: aa66cd0643e1c7454d795bbe7c4e8d55e7ea2644
    Former-commit-id: e220476c3111bccfddb8702c5bf41834bb36120d

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+$Host.UI.RawUI.BackgroundColor = "DarkMagenta"
+$Host.UI.RawUI.ForegroundColor = "DarkYellow"
+$Host.UI.RawUI.BufferSize.Height = 3000
+$Host.UI.RawUI.BufferSize.Width = 180
+$Host.UI.RawUI.WindowSize.Width = 180
+$Host.UI.RawUI.WindowSize.Height = 62 
+
+#ErrorForegroundColor    : Red
+#ErrorBackgroundColor    : Black
+#WarningForegroundColor  : Yellow
+#WarningBackgroundColor  : Black
+#DebugForegroundColor    : Yellow
+#DebugBackgroundColor    : Black
+#VerboseForegroundColor  : Yellow
+#VerboseBackgroundColor  : Black
+#ProgressForegroundColor : Yellow
+#ProgressBackgroundColor : DarkCyan
+
+function prompt
+{
+    Write-Host ("PS") -NoNewline
+    Write-Host (" " + $env:USERNAME) -NoNewline -ForegroundColor Yellow 
+    Write-Host (" " + $(Get-Date)) -NoNewline -ForegroundColor Green
+    Write-Host (" " + $(Get-Location)) -ForegroundColor Red
+    Write-Host (" >") -NoNewline
+    return " "
+}
+
+Set-Location $HOME
+Clear-Host
