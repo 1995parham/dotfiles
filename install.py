fac48cbf94f4db2f0a0e7d8ea32c5a871979e654
Parham Alvani <parham.alvani@gmail.com>
2015-11-03 02:39:18 +0330

    Install script updated and changed into python.
    Former-commit-id: da29b6c3463dcb6c5087b6c5c5da6bf5d7734f1a [formerly 50b8b96fcbff12f9f89d3a600f0374728482bbe5] [formerly 54d225c61bad4174d5c4951ef0a78fe41cdabd86 [formerly fdea3a6858545d6f55baa4f134d2ae1e050789fb [formerly fdea3a6858545d6f55baa4f134d2ae1e050789fb [formerly fdea3a6858545d6f55baa4f134d2ae1e050789fb [formerly 2c18d1f191e5d20ca53e6bd3b4ede96149b126c6]]]]]
    Former-commit-id: bfd3dfa47fed7bec09c4d2d5c298c75268dcfd8c [formerly 0d88a90d630a522d4d262bc4aee126c1626ade46]
    Former-commit-id: 7f2e93cb0a28afbbe285b3dedea9084779faf9e6
    Former-commit-id: f818e632bba0bf03f7d7f50d07d475b3263222c8
    Former-commit-id: bc06fdf8f65131fc6d830c4c51a7392b7c3abdd4
    Former-commit-id: 878f271034f24f9054f8634fa037769d9f237b16

---
+++ install.py
@@ -0,0 +1,102 @@
+# In The Name Of God
+# ========================================
+# [] File Name : install.py
+#
+# [] Creation Date : 11/3/15
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+import os
+import shutil
+
+
+def file_linker(module, file):
+    create_link = True
+    if os.path.isfile(os.path.join(home_dir, '.' + file)) or os.path.islink(os.path.join(home_dir, '.' + file)):
+        print('[{0}] {1} already existed'.format(module, file))
+        if input('do you want to remove {} ?[Y/n] '.format(file)) != "Y":
+            create_link = False
+        else:
+            os.remove(os.path.join(home_dir, '.' + file))
+    if create_link:
+        os.symlink(os.path.join(os.path.join(current_dir, module), file), os.path.join(home_dir, '.' + file))
+        print("[{0}] Symbolic link created successfully form {1} in {2}".format(module,
+                                                                                os.path.join(
+                                                                                    os.path.join(current_dir, module),
+                                                                                    file),
+                                                                                os.path.join(home_dir, '.' + file)))
+
+
+def directory_linker(module, directory):
+    create_link = True
+    if os.path.isdir(os.path.join(home_dir, '.' + directory)) or os.path.islink(
+            os.path.join(home_dir, '.' + directory)):
+        print('[{0}] {1} already existed'.format(module, directory))
+    if input('do you want to remove {} ?[Y/n] '.format(directory)) != "Y":
+        create_link = False
+    else:
+        if os.path.islink(os.path.join(home_dir, '.' + directory)):
+            os.remove(os.path.join(home_dir, '.' + directory))
+        elif os.path.isdir(os.path.join(home_dir, '.' + directory)):
+            shutil.rmtree(os.path.join(home_dir, '.' + directory))
+    if create_link:
+        os.symlink(os.path.join(os.path.join(current_dir, module), directory), os.path.join(home_dir, '.' + directory))
+        print("[{0}] Symbolic link created successfully form {1} in {2}".format(module,
+                                                                                os.path.join(
+                                                                                    os.path.join(current_dir, module),
+                                                                                    directory),
+                                                                                os.path.join(home_dir,
+                                                                                             '.' + directory)))
+
+
+###########################
+
+home_dir = os.environ['HOME']
+print("[pre] Home directory found at {}".format(home_dir))
+
+current_dir = os.getcwd()
+print("[pre] Current directory found at {}".format(current_dir))
+
+###########################
+
+# VIM
+module = 'vim'
+print("[{0}] install {0} configuration".format(module))
+
+# .vimrc
+file_linker(module, 'vimrc')
+
+# .vim
+directory_linker(module, 'vim')
+
+print("{:*^80}".format(''))
+
+###########################
+
+# Conf
+module = 'conf'
+print("[{0}] install {0} configuration".format(module))
+
+# .zshrc
+file_linker(module, 'zshrc')
+
+# .dircolors
+file_linker(module, 'dircolors')
+
+print("{:*^80}".format(''))
+
+###########################
+
+# Git
+module = 'git'
+print("[{0}] install {0} configuration".format(module))
+
+# .gitconfig
+file_linker(module, 'gitconfig')
+
+# .gitignore
+file_linker(module, 'gitignore')
+
+print("{:*^80}".format(''))
