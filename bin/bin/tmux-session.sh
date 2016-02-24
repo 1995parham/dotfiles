efde99158d9aaabe8415c6293bbc751fe9fb58ce
Parham Alvani <parham.alvani@gmail.com>
2016-02-24 05:05:04 +0330

    Tmux-session script was improved.
    Former-commit-id: 8ba58c926a62c0f46337ad92b97903031f9486ca [formerly 51010238a3377bf5023dc856ca78487e9beeba62] [formerly 00851a5e49e21f93366e74e1c06df859da04eb5b [formerly 4c0eea0657fc352c4adeda7c121a6f02889a098a [formerly 4c0eea0657fc352c4adeda7c121a6f02889a098a [formerly 4c0eea0657fc352c4adeda7c121a6f02889a098a [formerly 9c2f274c3aa701963ca55182f127e1b52f44f56e]]]]]
    Former-commit-id: 8a13234c54fee061866afa05defc7de431b6f4fe [formerly 1dcd638aed95852f0bc9715c9c8f95ad7460e1de]
    Former-commit-id: 8b3572654079bf56e0e727d8c389ab83da27f994
    Former-commit-id: 4a82d65777c686a82e90e8ec2566f11d6512eac5
    Former-commit-id: d4874ae9463e843384891a5db2d0cece18fe956e
    Former-commit-id: 53469f468f52796f9701e06cb86baf6f1ee7791b

--- bin/bin/tmux-session.sh
+++ bin/bin/tmux-session.sh
@@ -12,55 +12,54 @@
 set -e
 
 dump() {
-  local d=$'\t'
-  tmux list-windows -a -F "#S${d}#W${d}#{pane_current_path}"
+	local d=$'\t'
+	tmux list-windows -a -F "#S${d}#W${d}#{pane_current_path}"
 }
 
 save() {
-  dump > ~/.tmux-session
+	dump > ~/.tmux-session
 }
 
 terminal_size() {
-  stty size 2>/dev/null | awk '{ printf "-x%d -y%d", $2, $1 }'
+	stty size 2>/dev/null | awk '{ printf "-x%d -y%d", $2, $1 }'
 }
 
 session_exists() {
-  tmux has-session -t "$1" 2>/dev/null
+	tmux has-session -t "$1" 2>/dev/null
 }
 
 add_window() {
-  tmux new-window -d -t "$1:" -n "$2" -c "$3"
+	tmux new-window -d -t "$1:" -n "$2" -c "$3"
 }
 
 new_session() {
-  cd "$3" &&
-  tmux new-session -d -s "$1" -n "$2" $4
+	cd "$3" &&
+		tmux new-session -d -s "$1" -n "$2" $4
 }
 
 restore() {
-  tmux start-server
-  local count=0
-  local dimensions="$(terminal_size)"
+	tmux start-server
+	local count=0
+	local dimensions="$(terminal_size)"
 
-  while IFS=$'\t' read session_name window_name dir; do
-    if [[ -d "$dir" && $window_name != "log" && $window_name != "man" ]]; then
-      if session_exists "$session_name"; then
-        add_window "$session_name" "$window_name" "$dir"
-      else
-        new_session "$session_name" "$window_name" "$dir" "$dimensions"
-        count=$(( count + 1 ))
-      fi
-    fi
-  done < ~/.tmux-session
-
-  echo "restored $count sessions"
+	while IFS=$'\t' read session_name window_name dir; do
+		if [[ -d "$dir" && $window_name != "log" && $window_name != "man" ]]; then
+			if session_exists "$session_name"; then
+				add_window "$session_name" "$window_name" "$dir"
+			else
+				new_session "$session_name" "$window_name" "$dir" "$dimensions"
+				count=$(( count + 1 ))
+			fi
+		fi
+	done < ~/.tmux-session
+	echo "restored $count sessions"
 }
 
-case "$1" in
-save | restore )
-  $1
-  ;;
-* )
-  echo "valid commands: save, restore" >&2
-  exit 1
+case "$1"
+	in save | restore )
+		$1
+		;;
+	* )
+		echo "valid commands: save, restore" >&2
+		exit 1
 esac
