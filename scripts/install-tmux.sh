da049060b3aac412772b3984d58dae25c72a2547
Parham Alvani <parham.alvani@gmail.com>
2016-03-17 09:03:15 +0330

    Tmux installation script was added.
    Former-commit-id: 09ec05a085aad4fbe5ceca741ea4785df1513982 [formerly ded23585205e48495eb87d017a7b856fe0c437f7] [formerly 4bed00c2bf488ac820fe9810167ff512b14682eb [formerly 1f2c74af5b4ca1cbac28243782f3f635fa850085 [formerly 1f2c74af5b4ca1cbac28243782f3f635fa850085 [formerly 1f2c74af5b4ca1cbac28243782f3f635fa850085 [formerly 85a965114ed83cbc1746182ba65b43afbc49b4d6]]]]]
    Former-commit-id: b2b3bb6a81ae2f2a0c5c9a292d61e2b98c2c5e0a [formerly 0fec3f30cdedfdc4bcd481c7d190b456f6b20842]
    Former-commit-id: 420f75e5b1c351bbed28ab4d5f9d521cd7e1d8c0
    Former-commit-id: 586b56230c5c349e5e35bf8c62ab2499e3b39980
    Former-commit-id: 1505248542e53d547065946ebe2c6701029e5a2d
    Former-commit-id: 95bc1f2ee8d75cdaa73700c740587acc09527276

---
+++ scripts/install-tmux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : install-tmux.sh
+#
+# [] Creation Date : 17-03-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if [ "$OSTYPE" == "darwin"* ]; then
+	brew install tmux
+else
+	if [ `lsb_release -s -d | gawk -F " " '{print $2}' | gawk -F "." '{print $1}'` -lt 15 ]; then
+		echo "Installing tmux on old ubuntu [before 15.05]"
+		sudo apt-get update
+		sudo apt-get install -y python-software-properties software-properties-common
+		sudo add-apt-repository -y ppa:pi-rho/dev
+		sudo apt-get update
+		sudo apt-get install -y tmux=2.0-1~ppa1
+	else
+		echo "Installing tmux on new ubuntu [15.05 and after]"
+		sudo apt-get install tmux
+	fi
+fi
