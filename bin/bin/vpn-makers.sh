ea395a3f79a21602967cb21cefaf0e101a004517
Parham Alvani <parham.alvani@gmail.com>
2016-04-30 00:58:10 +0430

    Add simple script for connecting to VPN Makers :)
    Former-commit-id: cd6422fbc40ce03526410b84f0ee7a75b9f1a055 [formerly 916626ebb66efe68341411132e1af0b4fec2a93c] [formerly fb8b0ba0a842565fd43a3c014959a5beb8f95323 [formerly 81a63d2b02ea8f238abfcea83ae206ff5679042c [formerly 81a63d2b02ea8f238abfcea83ae206ff5679042c [formerly 81a63d2b02ea8f238abfcea83ae206ff5679042c [formerly 29bc1cce85e6bf626f10d14cc263761d17ece18d]]]]]
    Former-commit-id: 2a67be3314db82c5b2c0d64c9edb6e822df24450 [formerly d93f6619ae7e2502f8f02b3df7a1fc8323bd2d26]
    Former-commit-id: 088ff46ac41e5064b0789ac074f7aeed299ff385
    Former-commit-id: ab56fb8afa9ba14589661954df41652ab4b1f076
    Former-commit-id: aaa710ade313cf1599552d5207979c9970e95a89
    Former-commit-id: 006ec1c3749644b7ce5214aabfa3bfcac7123ee7

---
+++ bin/bin/vpn-makers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# In The Name Of God
+# ========================================
+# [] File Name : vpn-makers.sh
+#
+# [] Creation Date : 30-04-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+echo "Welcome to simple script for connecting to VPN Makers"
+sudo openconnect us.cisadd.com -u iruser318449
