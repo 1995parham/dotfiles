34741c4ef19a25dd88fdccce79dc9b008e4fac5d
Parham Alvani <parham.alvani@gmail.com>
2016-05-07 23:04:38 +0430

    vpn-makers script was improved.
    Former-commit-id: cb27ab700ae738615ecdf1abced75801f82875b4 [formerly 5e3e51ff6fd6cc33a07694f6368642056197f963] [formerly 1fa6a36368f83907552e0b4fa0fcc656e9379eef [formerly 2ece47e8ec272a9c4826e4626ce65d852788915a [formerly 2ece47e8ec272a9c4826e4626ce65d852788915a [formerly 2ece47e8ec272a9c4826e4626ce65d852788915a [formerly a75935093c43f339c647bf9237c7d3f6e9f9783b]]]]]
    Former-commit-id: 6044fa5a7393ad01d27cfc314bdb239f56c8ae5a [formerly 7d118815d9e13e6eacb636f020a4d4af1fbb1df0]
    Former-commit-id: 47904c0c70d708a045b387cee97b5a690253d2b6
    Former-commit-id: b5a9526e0f0948b3f77aeff58713ef30c7edf08c
    Former-commit-id: ca9311eac8cbe760815762bf92eae560aac24576
    Former-commit-id: a29cb3cc10dc18cdefd54a413d31a924583c69ae

--- bin/bin/vpn-makers.sh
+++ bin/bin/vpn-makers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # In The Name Of God
 # ========================================
 # [] File Name : vpn-makers.sh
@@ -8,4 +8,8 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 echo "Welcome to simple script for connecting to VPN Makers"
+if [ "$OSTYPE" == "linux"* ]; then
+	echo >&2 "This script only works on linux based systems :)";
+fi
+command -v openconnect >/dev/null 2>&1 || { echo >&2 "I require openconnect but it's not installed.  Aborting."; exit 1; }
 sudo openconnect us.cisadd.com -u iruser318449
