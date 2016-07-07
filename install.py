9826cc253a22df4947974c7bd34ffc9a3f25483b
Parham Alvani <parham.alvani@gmail.com>
2016-07-08 00:39:39 +0430

    Installation script improvement was completed.
    Former-commit-id: 2b303428ecdf61e21d4ac26b178e28c4c0163f22 [formerly bb5f8d864b327688570546d3d96fac938c7b5e59] [formerly a4015f8566193481008705c1513f00e53072bd9f [formerly 50932df9c04c95b722cdb14194fb2b24bf980291 [formerly 50932df9c04c95b722cdb14194fb2b24bf980291 [formerly 50932df9c04c95b722cdb14194fb2b24bf980291 [formerly 87c1f7b2cd7a1920f3e6c47d5a9e75be1e51df78]]]]]
    Former-commit-id: 0cb2e91b158b70f46a7725124c97a5e2040e902f [formerly a21b8bf31fa44935524be95526d754f075f5db7d]
    Former-commit-id: 7cae1b1d01c0e62dc92a654289dc6335d6c6bfc5
    Former-commit-id: 46ee4be356457fc21c967bf1f02a1723dcf56c9d
    Former-commit-id: 8e5138e26d4e0151f97c39211b9c1bbe1bf41a9a
    Former-commit-id: 27f0247312f09825a31e616d4d436a06df38a65c

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ class DotFile:
     """
 
     def __init__(self, name: str, files: list[str], directories: list[str],
-                 is_customize: bool = False):
+                 is_customize: bool = False, is_hidden: bool = False):
         self.name = name
 
         print("[{0}] install {0} configuration".format(self.name))
@@ -21,17 +21,17 @@ class DotFile:
                 if input("[{0}] do want to install {1} ?[Y/n]".format(
                         self.name, file)) != "Y":
                     continue
-            self.file_linker(file)
+            self.file_linker(file, is_hidden)
 
         for directory in directories:
             if is_customize:
                 if input("[{0}] do want to install {1} ?[Y/n]".format(
                         self.name, directory)) != "Y":
                     continue
-            self.directory_linker(directory)
+            self.directory_linker(directory, is_hidden)
         print("{:*^80}".format(''))
 
-    def file_linker(self, file, is_hidden=True):
+    def file_linker(self, file: str, is_hidden: bool = True):
         dst_file = ('.' if is_hidden else '') + file
         src_file = file
 
@@ -55,7 +55,7 @@ class DotFile:
             print("[{0}] Symbolic link created successfully form {1} in {2}"
                   .format(self.name, src_path, dst_path))
 
-    def directory_linker(self, directory, is_hidden=True):
+    def directory_linker(self, directory: str, is_hidden: bool = True):
         dst_directory = ('.' if is_hidden else '') + directory
         src_directory = directory
 
@@ -101,67 +101,28 @@ if input('[pre] do you want to customize installation ? [Y/n]') == "Y":
 ###########################
 
 # VIM
-DotFile('vim', files=['vimrc'], directories=['vim'])
+DotFile('vim', files=['vimrc'], directories=['vim'],
+        is_customize=do_customization)
 
 ###########################
 
 # Conf
-module = 'conf'
-print("[{0}] install {0} configuration".format(module))
-
-# .zshrc
-file_linker(module, 'zshrc')
-
-# .dircolors
-file_linker(module, 'dircolors')
-
-# .aria2.conf
-directory_linker(module, 'aria2')
-
-# .copyrighter
-directory_linker(module, 'copyrighter')
-
-# .wakatime.cfg
-file_linker(module, 'wakatime.cfg')
-
-# .tmux.conf
-file_linker(module, 'tmux.conf')
-
-# .tmux
-directory_linker(module, 'tmux')
-
-# .pinerc
-file_linker(module, 'pinerc')
-
-# signature
-file_linker(module, 'signature')
-
-# .eslintrc
-file_linker(module, 'eslintrc.json')
-
-print("{:*^80}".format(''))
+DotFile('conf',
+        files=['zshrc', 'dircolors', 'wakatime.cfg', 'tmux.conf',
+               'pinerc', 'signature', 'eslintrc.json'],
+        directories=['copyrighter, aria2', 'tmux'],
+        is_customize=do_customization)
 
 ###########################
 
 # Git
-module = 'git'
-print("[{0}] install {0} configuration".format(module))
-
-# .gitconfig
-file_linker(module, 'gitconfig')
-
-# .gitignore
-file_linker(module, 'gitignore')
-
-print("{:*^80}".format(''))
+DotFile('git', files=['gitconfig', 'gitignore'],
+        directories=[],
+        is_customize=do_customization)
 
 ###########################
 
 # Executable
 module = 'bin'
-print("[{0}] install {0} configuration".format(module))
-
-# bin
-directory_linker(module, 'bin', False)
-
-print("{:*^80}".format(''))
+DotFile('bin', files=[], directories=['bin'],
+        is_hidden=False, is_customize=do_customization)
