6e6c0168e14bba2f6a17932e8d2fbb7ecf808f61
Parham Alvani <parham.alvani@gmail.com>
2016-07-09 18:03:44 +0430

    Bash base installation script was improved.
    Former-commit-id: 1a47a83141f0ea1b7a85b3efd4fe0ee6b3dc28ad [formerly 7c75e2c568831fdf2136647f1b2a2335258039d2] [formerly 85a044f01dc810b55c9d88e5c10fa212a2bb3d6f [formerly 5590b15ecbeb83414c6603a3fa77788d305306b8 [formerly 5590b15ecbeb83414c6603a3fa77788d305306b8 [formerly 5590b15ecbeb83414c6603a3fa77788d305306b8 [formerly 11c08276ab2388e2c26da48b6660ac95d660df71]]]]]
    Former-commit-id: a455378ecdd6f5156326f0041bab1f23179bc533 [formerly 339da588f76352cdcf83be2adfcd0ad48c466f96]
    Former-commit-id: 617659e865d7438eaecfd4ecaf54f209d4e45c4f
    Former-commit-id: 01ae26e1e8e4871895b2eeb13fd32de21f6453dc
    Former-commit-id: b1afb73bb0a0f018657e436aa0ae04f83b51ca4b
    Former-commit-id: 0e111bd1445b8088a3dc5477ed4d0b43cbb5d4fb

--- install.sh
+++ install.sh
@@ -14,11 +14,22 @@ echo "[pre] Home directory found at $HOME"
 
 echo "[pre] Current directory found at $current_dir"
 
-# file_linker
+# Dotfile
+# parameter 1: module name - string
+# parameter 2: file names - array of string
+dotfile() {
+	module=$1
+
+	for file in "${!2}"; do
+		linker $module $file
+	done
+}
+
+# linker
 # parameter 1: module name - string
 # parameter 2: file name - string
 # parameter 3 [default = false]: is hidden file - bool
-file_linker() {
+linker() {
 	module=$1
 	file=$2
 	is_hidden=true
@@ -33,10 +44,10 @@ file_linker() {
 
 	if [ -e $dst_path ] || [ -h $dst_path ]; then
 		echo "[$module] $src_file already existed"
-		read -p "[$module] do you want to remove $dst_file ?[Y/n] " -n 1 delete_confirm; echo
+		read -p "[$module] do you want to remove $dst_path ?[Y/n] " -n 1 delete_confirm; echo
 		echo $delete_confirm
 		if [[ $delete_confirm == "Y" ]]; then
-			rm $dst_path
+			rm -R $dst_path
 			"[$module] $dst_path was removed successfully"
 		else
 			create_link=false
@@ -49,4 +60,6 @@ file_linker() {
 	fi
 }
 
-file_linker "vim" "vimrc"
+#### VIM ####
+files=("vimrc" "vim")
+dotfile "vim" files[@]
