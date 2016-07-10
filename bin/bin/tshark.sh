278baba285267b82eb48c1ec40fc6918b67d7e85
Parham Alvani <parham.alvani@gmail.com>
2016-07-11 03:51:02 +0430

    TShark script was added.
    Former-commit-id: 775c81cfccac5cc458ae25c1f6c7947b3232c018 [formerly 87637a4bb8da9eda69b388e6dfb03d0926bb5f91] [formerly b31e17268c4f7b148e900eecaeac4588902980e7 [formerly 0b42c182f933aebb2850f2386b972f910fcd1e5b [formerly 0b42c182f933aebb2850f2386b972f910fcd1e5b [formerly 0b42c182f933aebb2850f2386b972f910fcd1e5b [formerly fbae328e4a5ea81e160ff1f4693433702cca6e5a]]]]]
    Former-commit-id: a7d32930467ad652e4936723d53e5c51eb9d47bb [formerly ff7fc11902b27d27186a73f4e7a669363b003549]
    Former-commit-id: ae21a40fa9c52b5fae1fca5977626d782aa24b3d
    Former-commit-id: 70f13725517f92aa45dc16bec7f45529ff7e5197
    Former-commit-id: af1f0766ba2af868af9146d558015ceeec4337d2
    Former-commit-id: 90a9ff27cd042be38f83cf885b7d72694687d76a

---
+++ bin/bin/tshark.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : tshark.sh
+#
+# [] Creation Date : 18-11-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+i=0
+while read name details
+do
+	if [ $i -gt 0 ]; then
+		echo "[$i]: $name:"; echo
+		ifconfig $name
+		echo; echo
+		capture_interfaces="$capture_interfaces $name"
+	else
+		echo "[ ]: $name"
+	fi
+	let i=$i+1
+done < <(netstat -in)
+
+oPS3=$PS3
+PS3="Select your capturing interface:"
+select capture_interface in $capture_interfaces
+do
+	if [ ! -z "$capture_interface" ]; then
+		echo "You select $capture_interface"
+		break
+	else
+		echo "$REPLAY is not valid"
+	fi
+done
+PS3=$oPS3
+
+capture_file=/tmp/$(date +"%F_%H:%m:%S")-$capture_interface.pcap
+
+echo "Capturing start"; echo
+sudo tshark -i "$capture_interface" -F pcap -P -w "$capture_file"
+echo; echo "Capturing done"; echo
+
+read -p "Do you want to copy captured file here ?[Y/n]" -n 1 copy_confirm; echo
+if [[ $copy_confirm == "Y" ]]; then
+	sudo chown $USER "$capture_file"
+	cp "$capture_file" .
+	echo "Captured file moved here successfully"
+fi
+echo "Capturing output file is ready for read."
