a5a02e73dcec8e4c274e971e0813012062b181e4
Parham Alvani <parham.alvani@gmail.com>
2016-07-14 10:29:06 +0430

    Version specific tmux configuration loader was added.
    Former-commit-id: 636da8c0c1ce0dae9d61c31d11187f820a798d9b [formerly 8ef6330ddab3ec7cb97dcb2fdc5980d5c29f68ca] [formerly bbc63875b771e72de454bf87791023ff702405f8 [formerly 33def09feb7770bdf63cf07a5eb082f409ab7715 [formerly 33def09feb7770bdf63cf07a5eb082f409ab7715 [formerly 33def09feb7770bdf63cf07a5eb082f409ab7715 [formerly 3b3a29fe8afe893a34c4b60a02a985356d9325d7]]]]]
    Former-commit-id: ab82fb615e686da741864d61b21e615a8e2be716 [formerly e4ce5b58d31cc90cbb00e401f6a490206dace9ac]
    Former-commit-id: fcc30998354be7c6353386b68fbb536e22a38b23
    Former-commit-id: be906f7254649ea8056b6e5125599c7124b29311
    Former-commit-id: 0b7edddfa9553401a9d72e8b529f72fbae6d923d
    Former-commit-id: bfee3f159a6323bacb7e79ce0f3f49e6ad1bc204

---
+++ conf/tmux/loader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : loader.sh
+#
+# [] Creation Date : 14-07-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+tmux_version=`tmux -V | cut -d' ' -f2`
+
+if [[ `echo "$tmux_version > 1.9 " | bc -l` -eq 1 ]]; then
+	tmux source-file ./tmux.config.plugin
+fi
