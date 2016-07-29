07a8d590189c7fb2b86f5e5cee06f8fca9c79ffe
Parham Alvani <parham.alvani@gmail.com>
2016-07-29 21:14:54 +0430

    PHP setup script was added.
    Former-commit-id: 094bf2bd8b3de8a002f53a5f90ac9748cb2bb411 [formerly e43444e62cb3d2728f2d9af6341abd45ba8ab243] [formerly cf3cd0b8fc7401beacb84f40d70604acc0b1c296 [formerly 771050e82d5050e374074165365d234dbc263bdb [formerly 771050e82d5050e374074165365d234dbc263bdb [formerly 771050e82d5050e374074165365d234dbc263bdb [formerly b98bc0ad201d8548e2a3a77a9f797bda4bd9de33]]]]]
    Former-commit-id: b2773f200f35838ca9cd7de52554811d319dae78 [formerly 4d6bfff32d2703e2e664f2a3f7d7665e1e251d41]
    Former-commit-id: 53a8838d9e4a5482e8a7e90ebdcbd093a8369ae5
    Former-commit-id: 8202b167f2444827efb0e2e881c088cb3671bfc3
    Former-commit-id: 05e3e19bfca3358f9c802bd73cc03c8698d196f1
    Former-commit-id: 83238306ab34db492e941a62e3ba8ceb79007fa3

--- scripts/setup/php.sh
+++ scripts/setup/php.sh
@@ -1,4 +1,4 @@
-#!/bin/
+#!/bin/sh
 # In The Name Of God
 # ========================================
 # [] File Name : php.sh
@@ -7,8 +7,10 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+sudo apt install php-cli php-xml
 # setup composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo chown -R $USER:$USER $HOME/.composer
 # setup phpcs globally
 composer global require "squizlabs/php_codesniffer=*"
-phpcs config-set default_standard PSR2
+phpcs --config-set default_standard PSR2
