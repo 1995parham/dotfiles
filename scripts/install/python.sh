78222343d70f7e49d17973788def07c1e6f38ea2
Parham Alvani <parham.alvani@gmail.com>
2016-08-15 00:07:41 +0430

    Python installing script was added.
    Former-commit-id: 1dbf4dd8f13fe30b8f4f5bbd4624054be1abc01c [formerly 04e5bfc69b8530e59760fabfcd02c8c455c16d7f] [formerly a402c3255eb06a0bad5788b3b3b76f97fd684658 [formerly 52b8821f6d506343d4f5594ea16c5ea9cb7272a5 [formerly 52b8821f6d506343d4f5594ea16c5ea9cb7272a5 [formerly 52b8821f6d506343d4f5594ea16c5ea9cb7272a5 [formerly 30284c819841e160e55a15db74c5c8f9b7c8f69a]]]]]
    Former-commit-id: 195074287c4f4137734c5963099ad9e5c9496b6f [formerly 724fb7c32e66204c3204557065d7709a97ccd595]
    Former-commit-id: 4fa5a54760093eed39aec437d55d6db78c5b164b
    Former-commit-id: 66f8e1f924c0243232850b39e809fe01bbb597a4
    Former-commit-id: 7cd0942d18d4e1708d0b465fbc3f58d6d47119b0
    Former-commit-id: fef01c06d0e20c8f10617d1c5cd79d5e08dae5d3

---
+++ scripts/install/python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : python.sh
+#
+# [] Creation Date : 15-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if [ "$OSTYPE" == "darwin"* ]; then
+	wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
+	echo 'PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.5/bin/"' >> ~/.zshrc.local
+
+else
+	sudo apt install python3 python3-venv
+fi
