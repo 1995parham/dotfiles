8e814feabca118080974bfba70727d59cec49e5a
Parham Alvani <parham.alvani@gmail.com>
2016-08-20 02:33:30 +0430

    Ruby environment setup script was added.
    Former-commit-id: 6965f195c0a616aa36d7f0a93da64a18f67990c4 [formerly ddc079a686da75e0b591fd7cf76ec403c71bf460] [formerly 177a0ed3ebf835cc65e85e4d70a5c04dc8d3279a [formerly e3032c2eb775d9dc09ac2770938184a3d99406db [formerly e3032c2eb775d9dc09ac2770938184a3d99406db [formerly e3032c2eb775d9dc09ac2770938184a3d99406db [formerly cd0a23b7d371ef8c9f834f1080822769accd2cf8]]]]]
    Former-commit-id: 35423033a868693a76e2f7c245560d3298f02682 [formerly 6e8a2b0e887a539c3bcf83d9311f033615c1d4ca]
    Former-commit-id: 785e89b5d723b40e072bf605a16a900e8f0fb66e
    Former-commit-id: 1a59923432cd8514004b02fac7967bf22c66ff39
    Former-commit-id: 21cc93fa4fd6b4a798aab2375aa1856b4faaeb48
    Former-commit-id: 5f5f28a360b1a5db8660a6e5ffba89f73760e65d

---
+++ scripts/setup/ruby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : ruby.sh
+#
+# [] Creation Date : 20-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+sudo gem install ruby-lint
