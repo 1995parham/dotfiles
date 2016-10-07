322642495f5474ac199143387eb337986824bc1b
Parham Alvani <parham.alvani@gmail.com>
2016-10-07 19:31:27 +0330

    Ruby installing script was added.
    Former-commit-id: 82932bd5e2c32307751bc0b05337f3a3c0fa0762 [formerly fedd4bf7663dcc55a5ae8a1cb353a4e872523fee] [formerly e6cbc008c055fe6b0fbd9c3fd954761a8762b12d [formerly 83e0af1622c6a54fdbcececb451a2cc349f6faa8 [formerly 83e0af1622c6a54fdbcececb451a2cc349f6faa8 [formerly 83e0af1622c6a54fdbcececb451a2cc349f6faa8 [formerly 499618276ce4ea35dc46d05bc893089ec755ff46]]]]]
    Former-commit-id: bebee34507005b52ce15e4a1d156e67195b8b20a [formerly 0ec127b02da8703bc81d95de002b155e3fe5d2a4]
    Former-commit-id: fad853ae6d4af95f9cc955801b65173646571798
    Former-commit-id: c7289db28a0e690bd8cb3c49b748b164282974bb
    Former-commit-id: b4ab5bdfdfa8dc125ff42ac3d2cb80412dc2d6d3
    Former-commit-id: 288be11aa55118b87e46803c2a48578cfdf57036

---
+++ scripts/install/ruby.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : ruby.sh
+#
+# [] Creation Date : 07-10-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+sudo apt-get install ruby
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update
+sudo apt-get install rvm
