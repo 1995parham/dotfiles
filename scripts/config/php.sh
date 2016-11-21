534eabc024882518ded237541dc258baa93bd27c
Parham Alvani <parham.alvani@gmail.com>
2016-11-22 01:40:39 +0330

    PHP installation and configuration script was improved.
    Former-commit-id: 9f313bebd6093839f58ea65062998196c879c6b9 [formerly 31a4a4beb7cbfe3be1edc904fafd6fb04d06ff46] [formerly 2f19dcd285e01d39e036d122fd6d129f1f46a29d [formerly bfcee14a493f0700340edecff166977adac61978 [formerly bfcee14a493f0700340edecff166977adac61978 [formerly bfcee14a493f0700340edecff166977adac61978 [formerly 377c41a8fd84cd3fc48eeb6916cfabfc7e1408b3]]]]]
    Former-commit-id: 19a5adb536a2ddefb8733a2daab85a07720eabe2 [formerly f9e7b5b54b44a1ee79c3a99173bc5111692b9c0f]
    Former-commit-id: 347cc23bf1a433fa93ee198331f3463218bd8b30
    Former-commit-id: 618223f474fe77f7ba62aa89301fb9ec64cd3c88
    Former-commit-id: 9bdcfa59fd6c493c1fa75ad1d44181a7e662211c
    Former-commit-id: da90585f66538ca56b48fa6054a3b7467427ad5b

--- scripts/config/php.sh
+++ scripts/config/php.sh
@@ -7,18 +7,4 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-sudo apt install php-cli php-xml
 
-# setup composer
-if [ ! -e /usr/local/bin/composer ]; then
-	curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-	sudo chown -R $USER:$USER $HOME/.composer
-fi
-
-# setup phpcs globally
-composer global require "squizlabs/php_codesniffer=*"
-phpcs --config-set default_standard PSR2
-
-# setup phpdoc globally
-sudo apt install php-intl php-xsl
-composer global require "phpdocumentor/phpdocumentor"
---
+++ scripts/php.sh
@@ -0,0 +1,34 @@
+#!/bin/
+# In The Name Of God
+# ========================================
+# [] File Name : php.sh
+#
+# [] Creation Date : 22-11-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if [[ $EUID -ne 0 ]]; then
+	echo "[php] This script must be run as root"
+	exit 1
+fi
+
+echo "[php] Installing PHP"
+
+apt-get install php php-cli php-xml
+
+echo "[php] Installing composer"
+
+if [ ! -e /usr/local/bin/composer ]; then
+	curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+	sudo chown -R $USER:$USER $HOME/.composer
+fi
+
+echo "[php] Installing PHPCS"
+
+composer global require "squizlabs/php_codesniffer=*"
+phpcs --config-set default_standard PSR2
+
+echo "[php] Installing PHPDoc"
+
+apt-get install php-intl php-xsl
+composer global require "phpdocumentor/phpdocumentor"
