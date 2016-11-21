fccf2f070b302195f6775b1b7ea553c0e27c1975
Parham Alvani <parham.alvani@gmail.com>
2016-11-22 01:34:50 +0330

    Improve tmux installation script.

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -7,16 +7,23 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+if [[ $EUID -ne 0 ]]; then
+	echo "[tmux] This script must be run as root"
+	exit 1
+fi
+
+echo "[tmux] Installing tmux 2.x"
+
 if [ "$OSTYPE" == "darwin"* ]; then
 	brew install tmux
 else
 	if [ `lsb_release -s -d | gawk -F " " '{print $2}' | gawk -F "." '{print $1}'` -lt 15 ]; then
 		echo "Installing tmux on old ubuntu [before 15.05]"
-		sudo apt-get update
-		sudo apt-get install -y python-software-properties software-properties-common
-		sudo add-apt-repository -y ppa:pi-rho/dev
-		sudo apt-get update
-		sudo apt-get install -y tmux
+		apt-get update
+		apt-get install -y python-software-properties software-properties-common
+		add-apt-repository -y ppa:pi-rho/dev
+		apt-get update
+		apt-get install -y tmux
 	else
 		echo "Installing tmux on new ubuntu [15.05 and after]"
 		sudo apt-get install tmux
