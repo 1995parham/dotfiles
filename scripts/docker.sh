432637ee1155c36b54650919794e61db2bfd1b4a
Parham Alvani <parham.alvani@gmail.com>
2016-11-22 15:00:32 +0330

    Docker installation script was completed.
    Former-commit-id: 391c2f8852a4d09f3e069d351d31885e7606bd69 [formerly be9d879c7ba93fb3fc84c7b52dc44e373aae3a84] [formerly eaa8932786702cc5597b3f99167d01335aea3c9e [formerly b4ff47824173ca25b063f69f562e90b3d9219469 [formerly b4ff47824173ca25b063f69f562e90b3d9219469 [formerly b4ff47824173ca25b063f69f562e90b3d9219469 [formerly 6c2f4c34c74360672189479711cf182f146c5764]]]]]
    Former-commit-id: e850383ddbe00cb5ebfd5ca67f069d3c6aac6d4a [formerly 1a32abfc0307682b8f299af77103fb297a725596]
    Former-commit-id: 0add6b9e33e67a58221f8dd1f16bfdd27a0efdd8
    Former-commit-id: a3eaf0f2f4d0750c1e7fb01bd8212606b5b634d1
    Former-commit-id: 2505ff3ebc8cb3e946bdd34a371bf11d003760de
    Former-commit-id: 647ae8f7bb38acff950fa509fae5a7f12bf3b75d

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -19,4 +19,14 @@ apt-get install apt-transport-https ca-certificates
 echo "[docker] Add net GPG key"
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+echo "[docker] Add docker apt repository"
+echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" | tee /etc/apt/sources.list.d/docker.list
+
+echo "[docker] Installing docker !"
+apt-get update
+apt-get install docker-engine
+service docker start
+
+echo "[docker] Installing docker-compose"
 curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
