8f83956e93b8f5eea293d74e67a9473d163c2b1c
Parham Alvani <parham.alvani@gmail.com>
2016-11-22 09:40:41 +0330

    First version of docker installation script was added.
    Former-commit-id: 498ad3d25f217a77e6bffe00e339208982edbc00 [formerly 470cf145a802191a6449b31359d882ca51ab3779] [formerly 5c7ca3f898a39b60bb9a2dd492f22354f9084cc5 [formerly 3f57386d506571595c0d7d2b9b44f017378be241 [formerly 3f57386d506571595c0d7d2b9b44f017378be241 [formerly 3f57386d506571595c0d7d2b9b44f017378be241 [formerly 72708b482f36d1aad9c11e9a279c8e96e8a6a7eb]]]]]
    Former-commit-id: 6edaf39d0a460488790a1f9b9d759a1272d5df53 [formerly a5dcca869979d1ec938620c28df0a8633c3cfdf3]
    Former-commit-id: 1c90f5ba2179924dd13ff6d2793e2ae1c55b2ea2
    Former-commit-id: 96f510c2caa1be74668d8aa810369926364ceee4
    Former-commit-id: fce9d85d46b375fe621cc5a45fca3127f5db9006
    Former-commit-id: c2a8ccf7edd03441505c5d547ff9a141c0fa7891

---
+++ scripts/docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : docker.sh
+#
+# [] Creation Date : 22-11-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if [[ $EUID -ne 0 ]]; then
+	echo "[docker] This script must be run as root"
+	exit 1
+fi
+
+echo "[docker] Installing tools for apt repository management"
+apt-get update
+apt-get install apt-transport-https ca-certificates
+
+echo "[docker] Add net GPG key"
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
