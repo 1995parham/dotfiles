f3b1b23f023f9d4603d5a198852d12c640ecd7a6
Parham Alvani <parham.alvani@gmail.com>
2016-11-23 11:57:46 +0330

    Web installation script was improved.
    Former-commit-id: d592424f5d7188ff3efba0fb778772dcb636df51 [formerly 682a1d3e24386f118982a4288c07f853f606d708] [formerly 70b8928043dab3f06f2b1e0f23e0fffaa654a322 [formerly cb4cc9b09ae88bde5f6e5a352ca60a6b3808cefe [formerly cb4cc9b09ae88bde5f6e5a352ca60a6b3808cefe [formerly cb4cc9b09ae88bde5f6e5a352ca60a6b3808cefe [formerly c2da4d4986ada821b1b2f353e4d814229fdf2c5b]]]]]
    Former-commit-id: a19ec1e83f12c5f30ace677837a1387ae657a11a [formerly fd5f6f221d4f7801b73dd61f65f2a1b8e2435972]
    Former-commit-id: 4d7d3d8e406f449a1a270abf35b0900bc9b70e2a
    Former-commit-id: 5a84393b7762abec7144ea1b10ae3002096c298a
    Former-commit-id: 9a91211911e6ae1a6c4480cd5dece54eea8d2771
    Former-commit-id: d0be7a2288865303cd6bf53376abd51c4484d3b0

--- scripts/web.sh
+++ scripts/web.sh
@@ -22,3 +22,5 @@ npm install -g grunt
 
 echo "[web] Installing yeoman - yeoman helps you to kickstart new projects,"
 npm install -g yo
+npm install -g generator-angular
+npm install -g generator-karma
