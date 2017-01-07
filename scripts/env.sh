7da4a44d67236c1def8b2d857249c07fcbce7a3a
Parham Alvani <parham.alvani@gmail.com>
2017-01-07 04:53:30 +0330

    environment setup script was added.

---
+++ scripts/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : env.sh
+#
+# [] Creation Date : 07-01-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+if [ "$OSTYPE" == "darwin"* ]; then
+	brew install zsh
+	brew install ctags
+else
+	if [[ $EUID -ne 0 ]]; then
+		echo "[env] This script must be run as root"
+		exit 1
+	fi
+
+	apt-get install zsh ctags
+fi
+
