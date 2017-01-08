a4d2d558cc95f11e97f378a9bab46f1298943913
Parham Alvani <parham.alvani@gmail.com>
2017-01-08 04:15:16 +0330

    First version of go environment setup script was added.
    Former-commit-id: 5ad238bd6bdb47508556044f56f148bfb552e5d9 [formerly ed138341c17e60218ae060ca55bc0f6289b549e3] [formerly 51e8b7ca6ba8121c46564a20a47735c91bcaf795 [formerly 50e1ab713e9b1a5fb8dc4f68f8f5aba8c3c1619b [formerly 50e1ab713e9b1a5fb8dc4f68f8f5aba8c3c1619b [formerly 50e1ab713e9b1a5fb8dc4f68f8f5aba8c3c1619b [formerly b5007e31d6d1e5b95581beac7c015979a79dc7ba]]]]]
    Former-commit-id: e8ab2e9df22685387c1041607309bedfcabfb541 [formerly 7b4d55385bdacd3e9f6c2552b15eba3f4e9e9c98]
    Former-commit-id: d57fa7ed3fe2220dfdd0f6e9bc94aa466b65343f
    Former-commit-id: 5420bc1eb33c3bd07162db705c609f06bc4f6e4d
    Former-commit-id: 6c2954a10ce35f4c193ea5effb7c0b3fff4db93e
    Former-commit-id: b5fa1db99f2876a7a5f6c48eb9ef8e46dc974aa6

---
+++ scripts/go.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : go.sh
+#
+# [] Creation Date : 07-01-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+echo "[go] Create go directory structure"
+if [ ! -d $HOME/Documents/Go ]; then
+	mkdir $HOME/Documents/Go/bin
+	mkdir $HOME/Documents/Go/src
+	mkdir $HOME/Documents/Go/lib
+fi
+
+go get -u github.com/jstemmer/gotags
+go get -u github.com/nsf/gocode
+go get -u github.com/garyburd/go-explorer/src/getool
+go get -u golang.org/x/tools/cmd/goimports
