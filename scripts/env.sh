687ad7db742aa6eb862dc921da9c3bb7bf5773b3
Parham Alvani <parham.alvani@gmail.com>
2017-01-10 08:26:04 +0330

    Environment setup script was improved.

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,17 +7,15 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+if [[ $EUID -ne 0 ]]; then
+	echo "[env] This script must be run as root"
+	exit 1
+fi
+
 
 if [ "$OSTYPE" == "darwin"* ]; then
-	brew install zsh
-	brew install ctags
-	brew install vim
+	brew install zsh ctags vim tmux
 else
-	if [[ $EUID -ne 0 ]]; then
-		echo "[env] This script must be run as root"
-		exit 1
-	fi
-
-	apt-get install zsh ctags vim
+	apt-get install zsh ctags vim tmux
 fi
 
