bf7c23ec8dedb166e92a0e1ddd4ad6eee18aed95
Parham Alvani <parham.alvani@gmail.com>
2017-04-06 19:59:56 +0430

    Asses folder was removed due to existence in parent.

--- docs/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-function GetURLParameter(key) {
-   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
-   var r=[], m;
-   while ((m=re.exec(document.location.search)) != null) r.push(m[1]);
-   return r;
-}
