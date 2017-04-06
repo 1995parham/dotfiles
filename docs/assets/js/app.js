f4c8319ea0130d854d09ca0f15e1010ff79afcea
Parham Alvani <parham.alvani@gmail.com>
2017-04-06 18:47:19 +0430

    Improve dotfiles github page.

---
+++ docs/assets/js/app.js
@@ -0,0 +1,6 @@
+function GetURLParameter(key) {
+   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+   var r=[], m;
+   while ((m=re.exec(document.location.search)) != null) r.push(m[1]);
+   return r;
+}
