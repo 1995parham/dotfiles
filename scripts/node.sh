5444440d5fa037405866465ecfffdd9bc5321bde
Parham Alvani <parham.alvani@gmail.com>
2017-04-22 22:16:15 +0430

    Node installation script was improved.
    Former-commit-id: 0206fd46eacc94ce957083cf906ec9a4f4d04b35 [formerly e601c394edf6634a2c3e8124fa6e752a2675f938] [formerly b04ec316cc58323a125b49d4f325713aada5ddfc [formerly ecb49da4782541c4cbcf5841af3056a6c0ea0b16 [formerly ecb49da4782541c4cbcf5841af3056a6c0ea0b16 [formerly ecb49da4782541c4cbcf5841af3056a6c0ea0b16 [formerly 24b6288f102aa62c79dc0352288914b0af8bddc2]]]]]
    Former-commit-id: f646e92d89692ecdd6666d77a085ab04c98078e8 [formerly 0deb0b08a588d47ea80f49f50da192810e1a27ca]
    Former-commit-id: 1ff98105227ad43c100d4a2473dff7adaa36c20b
    Former-commit-id: 688bd0c090509ab484f24b9536cb617dfbbe38c4
    Former-commit-id: 80efc91f79972a415a4cefd9df368273af06c292
    Former-commit-id: 0d82db14088e3e51088511c57ea6297dfd80d622

--- scripts/node.sh
+++ scripts/node.sh
@@ -7,16 +7,20 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-if [[ $EUID -ne 0 ]]; then
-	echo "[node] This script must be run as root"
-	exit 1
-fi
-
 echo "[node] Installing Node 6.x"
 
-if [ "$OSTYPE" == "darwin"* ]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "[node] Darwin"
+
 	brew install node
 else
+	echo "[node] Linux"
+
+	if [[ $EUID -ne 0 ]]; then
+		echo "[node] This script must be run as root"
+		exit 1
+	fi
+
 	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 	apt-get install -y nodejs
 	ln -s /usr/bin/nodejs /usr/bin/node
