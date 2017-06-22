aa4ea696fc3e807c669d6032a49965aa6856076f
Parham Alvani <parham.alvani@gmail.com>
2017-06-22 16:06:44 +0430

    Again ...
    Former-commit-id: 85f7a0962ea8d8f06279cedb91535189f3151435 [formerly 16cbf4851839521a0e3fa3cbf2c1819b79648085] [formerly 05168766f0b2c2550e3565316cd6e733a51fcafe [formerly f9e8fc4f93d61fb2bda87bc3a0e8df379fa5ace7 [formerly f9e8fc4f93d61fb2bda87bc3a0e8df379fa5ace7 [formerly f9e8fc4f93d61fb2bda87bc3a0e8df379fa5ace7 [formerly d89022ba6ae1f9e2c82ff3183be4dfb56d0a59a5]]]]]
    Former-commit-id: 1f8a8b6249779c350a9cdfaf16d6f40289ef0d9b [formerly 2791883ef5cca28175871651812c5d939d707d41]
    Former-commit-id: 6f607239316895bc9aef86de0d96768d66735ab5
    Former-commit-id: 08909da7602bacf84a3be1d7183715b2288dab7b
    Former-commit-id: 091a71ff7f0395bcf19752691b2f14b1908c9020
    Former-commit-id: e4e16a2c77ebffb14575c61c353f12cd4133d597

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -5,11 +5,11 @@ Write-Output "[profile] Powershell script location: $PSScriptRoot"
 Write-Output "[profile] create profile configuration"
 New-Item -Path $profile -ItemType "file" -Force
 Remove-Item $profile
-if (Get-Command ls -errorAction SilentlyContinue)
+if (Get-Command ln -errorAction SilentlyContinue)
 {
 	Write-Output "[profile] symbolic link :)"
 	ln -s "$PSScriptRoot/Microsoft.PowerShell_profile.ps1" $profile
 } else {
 	Write-Output "[profile] hard copy :("
-        Copy-Item "$PSScriptRoot/Microsoft.PowerShell_profile.ps1"  $profile
+  Copy-Item "$PSScriptRoot/Microsoft.PowerShell_profile.ps1"  $profile
 }
