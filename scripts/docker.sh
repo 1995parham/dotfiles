cbae796c8c0d7155de4662bf2122e9724921e6c4
Parham Alvani <parham.alvani@gmail.com>
2017-12-23 23:18:26 +0330

    Better docker repository but awful proxy.
    Former-commit-id: bdbd7aca8714e2e7ab95f67789edce7f7a70a30d [formerly 3b262583d98e1745a5f263c7b88dc57b2e4d26ec] [formerly cf090bccc09965636b0dab5db1c81283c5db367a [formerly 6a51c6ec298457ac915f4b257bbf350ca1d4a529 [formerly 6a51c6ec298457ac915f4b257bbf350ca1d4a529 [formerly 6a51c6ec298457ac915f4b257bbf350ca1d4a529 [formerly 0cc4c60ad2954aaf9a6812b39f9c03393eadc01e]]]]]
    Former-commit-id: aa7b5bcb055f6fb38b454a0d8b4674336f107ed2 [formerly 6656c029059bb3a3b8a3c1fec7c619bbfea5adcd]
    Former-commit-id: f66768f1fcaf0f398cb1154e4bab88b36fd64bd0
    Former-commit-id: fb2560197570f25f2e70bb2c5e0b1b4e1d3abb8f
    Former-commit-id: 56c4f857c1cf497306d125b30a13fe865dba365c
    Former-commit-id: 6c99ebc23981d7ec7d91fda9def9febe1c8a4d2b

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -12,8 +12,11 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+echo "[docker] HTTP Proxy, for those who lives in Iran"
+export http_proxy=http://fod.backtory.com:8118/
+
 echo "[docker] Installing tools for apt repository management"
-apt-get update
+apt-get -q update
 apt-get install apt-transport-https ca-certificates
 
 echo "[docker] Add net GPG key"
@@ -26,22 +29,18 @@ add-apt-repository \
 	stable"
 
 echo "[docker] Installing docker"
-apt-get update
+apt-get -q update
 apt-get install docker-ce
 echo "[docker] The Docker daemon starts automatically."
 
 echo "[docker] Docker in futherland"
-cat > /etc/docker/daemon.json << "EOF"
-
-{
-	"registry-mirrors": [
-		"http://repo.docker.ir:5000"
-	],
-	"userland-proxy": false
-}
-
+mkdir -p /etc/systemd/system/docker.service.d
+cat > /etc/systemd/system/docker.service.d/http-proxy << "EOF"
+[Service]
+Environment="HTTP_PROXY=http://fod.backtory.com:8118/"
 EOF
-service docker restart
+systemctl daemon-reload
+systemctl restart docker
 
 compose_vr=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 compose_vl=$(docker-compose version --short)
@@ -51,3 +50,6 @@ if [ "${compose_vl}" != "${compose_vr}" ]; then
 	curl -L "https://github.com/docker/compose/releases/download/${compose_vr}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 fi
+
+echo "[docker] HTTP Proxy, for those who lives in Iran"
+unset http_proxy
