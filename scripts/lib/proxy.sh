a1e5e4fa7e230a799196f59f265d99992b299fdd
Parham Alvani <parham.alvani@gmail.com>
2018-06-28 20:12:43 +0430

    Let's move to vpnm.
    Former-commit-id: 0ff5433853a5e8a0865afc3c3e6825288050b2a9 [formerly c43367d8cc378dbab7e789a290d36357a17e376a] [formerly b804ccec19bf9b662158b57f05c02f8a7183cb8b [formerly 0aee99f5761c851d34a1f59b6f27a5d1098e34e9 [formerly 0aee99f5761c851d34a1f59b6f27a5d1098e34e9 [formerly 0aee99f5761c851d34a1f59b6f27a5d1098e34e9 [formerly 0fe6069653b0ff0b7b06491105a7b79236245175]]]]]
    Former-commit-id: 5c0501cab5a8faee7ec2be1d6240790692fc3ab4 [formerly e3008125c97eec1c91f6d4dd539ee1f72b55b7f9]
    Former-commit-id: af3178f580a2b79adf002a5cc42f44856078660e
    Former-commit-id: 5d5fbdb0d153996279450986040dc810ba610597
    Former-commit-id: 8916cb929b04dd06f260d520d4474da491a3019e
    Former-commit-id: 0bf5111d815034f42fe4e90563645d1b3c5edea9

--- scripts/lib/proxy.sh
+++ scripts/lib/proxy.sh
@@ -9,16 +9,22 @@
 # =======================================
 
 proxy_start() {
+
+        local host="us.mybestport.com:443"
+        local user="ir273269"
+        local password
+
         dig +short myip.opendns.com @resolver1.opendns.com
 
         # ssh -fTN -L 38080:127.0.0.1:38080 $parham_usvs
         echo "[proxy] Welcome to proxy script"
+        printf "[proxy] Destination: %s:?@%s\n" $user $host
         printf '%s: ' 'Password'
         read -s password
-        export {http,https,ftp}_proxy=sam:$password@ssl.gates.ga:50505
+        export {http,https,ftp}_proxy="$user:$password@$host"
 
         if [ $EUID -eq 0 ]; then
-                echo "Acquire::http::proxy \"http://sam:$password@ssl.gates.ga:50505/\";" > /etc/apt/apt.conf.d/95proxies
+                echo "Acquire::http::proxy \"http://$user:$password@$host/\";" > /etc/apt/apt.conf.d/95proxies
         fi
 
         echo
