e597da54c08eef8fbdd1f15c9ef116c3610a15d3
Parham Alvani <parham.alvani@gmail.com>
2018-07-17 21:46:22 +0430

    Adds sample script.
    Former-commit-id: b4ac6f6115755e4e826aca8a4fdfed05282b1d56 [formerly 3c37de87203846ccca5cb4b31bda61fb746336fb] [formerly 733435659f37ea90405656e9af3220f423a7b16e [formerly a23357a6f7e748754db598fc4f62457a795a3746 [formerly a23357a6f7e748754db598fc4f62457a795a3746 [formerly a23357a6f7e748754db598fc4f62457a795a3746 [formerly a5115e709d389375b2157c41301824c300802ae1]]]]]
    Former-commit-id: 6637ed46ae8ec9dc1ab13cdd9b379bf32cca282e [formerly 09c064a426e2164ebf1918fee2363606a8ca70ff]
    Former-commit-id: 91189c6add84601f9bd561465edd6c406f11606a
    Former-commit-id: 512bca42610ddf7866ce18ad33259eff237d20e9
    Former-commit-id: aedf0f1ee8950bda8006bcf6e2ce679b9f21ac25
    Former-commit-id: 62353e5abc6e0a6afa88fa06844c297ca4a81eff

---
+++ scripts/sample.sh
@@ -0,0 +1,18 @@
+#!/bin/
+# In The Name of God
+# ========================================
+# [] File Name : sample.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage sample"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+}
