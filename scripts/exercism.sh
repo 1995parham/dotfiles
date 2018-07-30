6af20cf82817725a875cecc7624b0c54d0e6f983
Parham Alvani <parham.alvani@gmail.com>
2018-07-30 06:46:39 +0430

    Adds exercism download script.
    Former-commit-id: 32c381d2383d16ea69a6bae96a12af28b320826f [formerly 6b8766dd7fa6337b9f415c7078953f6208f865ea] [formerly df2258f1cc36df5536e3446a4f630b5835f4f8fc [formerly 6a4e384e518209a0704bebf359bb2458bdc2165c [formerly 6a4e384e518209a0704bebf359bb2458bdc2165c [formerly 6a4e384e518209a0704bebf359bb2458bdc2165c [formerly 0a40c4df8ce742ad99262157e0320367ff0a84a7]]]]]
    Former-commit-id: 613460733340299ec642290af143f167c33c3998 [formerly 3a0d33a34959ac2c883df73542b7d0c7e94dad6c]
    Former-commit-id: e5731eed8348c8882fb938a7b9fa1ccccc97077c
    Former-commit-id: 8f4c2336edffde0fa7b5b540c6f7bebee104117a
    Former-commit-id: afa79393441a87785c5cc971efede2718d048013
    Former-commit-id: f0e8ba1c15e842babf67da92a35386c7ee7682c9

---
+++ scripts/exercism.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : exercism.sh
+#
+# [] Creation Date : 30-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+usage() {
+        echo "usage: exercism"
+}
+
+main() {
+        message "exercism" "Getting version information"
+        exercism_v=$(curl -s https://api.github.com/repos/exercism/cli/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+        message "exercism" "Downloading exercism $exercism_v"
+        curl -L -# "https://github.com/exercism/cli/releases/download/${exercism_v}/exercism-linux-64bit.tgz" -o exercism.tgz
+        tar -vfxz exercism.tgz
+}
