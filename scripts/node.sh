eff5aab7526c02d3e90ea7e1ff81e3094d713c90
Parham Alvani <parham.alvani@gmail.com>
2018-08-03 18:43:04 +0430

    Brand new node installation script.

--- scripts/node.sh
+++ scripts/node.sh
@@ -8,23 +8,31 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 version="10.x"
-echo "[node] Installing Node $version"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "[node] Darwin"
+usage() {
+        echo "usage: node"
+}
 
-	brew install node
-	brew install yarn
-else
-	echo "[node] Linux"
 
-	curl -sL https://deb.nodesource.com/setup_$version | sudo -E bash -
-	sudo apt-get install -y nodejs
-	ln -s /usr/bin/nodejs /usr/bin/node
-fi
+node-install() {
+        message "node" "Installing Node $version"
 
-echo "[node] Installing PM2 - the industry-standard task runner"
-npm install -g pm2
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+	        echo "[node] Darwin"
 
-echo "[node] Installing Newman - Modern software is built on APIs. Postman helps you develop APIs faster."
-npm install -g newman
+	        brew install node
+	        brew install yarn
+        else
+	        echo "[node] Linux"
+
+	        curl -sL https://deb.nodesource.com/setup_$version | sudo -E bash -
+	        sudo apt-get install -y nodejs
+	        ln -s /usr/bin/nodejs /usr/bin/node
+        fi
+
+        message "node" "$(node -v)"
+}
+
+main() {
+        node-install
+}
