e92253702b9f3b57490ccdcacfb61e5aefb07d20
Parham Alvani <parham.alvani@gmail.com>
2018-09-13 03:19:40 +0430

    Adds over-the-air-installation :joy:

---
+++ over-the-air-installation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : over-the-air-installation.sh
+#
+# [] Creation Date : 13-09-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+if ! [ -x "$(command -v git)" ]; then
+  echo "git is not installed."
+  exit 1
+fi
+
+echo "Create directory structure"
+mkdir $HOME/Documents
+mkdir $HOME/Downloads
+mkdir -p $HOME/Documents/Git/parham
+mkdir -p $HOME/Documents/Git/others
+
+echo "Clone https://github.com/1995parham/dotfiles"
+cd $HOME/Documents/Git/parham
+git clone https://github.com/1995parham/dotfiles
+cd dotfiles
