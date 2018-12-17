1ed3611e5f0bfd376500166c580b5ba1f9f52ac9
Parham Alvani <parham.alvani@gmail.com>
2018-12-17 20:45:50 +0330

    Adds travis installation script.
    Former-commit-id: fcfa03eb8c0c84806d38550861c8c6d0d029b73c [formerly d588717ec5c384fe975e740ca8cd9b08ea3c2776] [formerly bb021195414a18b0bc147466952afd6cfa4d40b8 [formerly 1f4cf23c40a045d5c06f86f384a4c904f61a0471 [formerly 1f4cf23c40a045d5c06f86f384a4c904f61a0471 [formerly 1f4cf23c40a045d5c06f86f384a4c904f61a0471 [formerly 059290300e956ff51f8a1997d0e8c11e155ae63b]]]]]
    Former-commit-id: 5ae94874e2e57c2357a63b50d8968ac36fb22291 [formerly a7ed79d5ff115cddc1686577bb43f46da2fcdb6d]
    Former-commit-id: f7fd76f194b492c05a25726efe11ca11e762e891
    Former-commit-id: 9745a0456a5b0a86cfefe23fa018703246de9aec
    Former-commit-id: 26b52116f7c93c4998421462e3713535ebe4a258
    Former-commit-id: b013d5d61b25c89d3055f9ba64d33d66fa72b822

---
+++ scripts/travis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : travis.sh
+#
+# [] Creation Date : 17-12-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: travis"
+        echo "install travis command line client and ruby library"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+
+        if $(hash gem 2>/dev/null); then
+	        gem install travis -v 1.8.9 --no-rdoc --no-ri
+                travis version
+        else
+                echo "please install ruby first"
+        fi
+
+}
