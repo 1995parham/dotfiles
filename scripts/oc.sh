d7ab07cc1a856ab351c29bcf28de8b20b642d8ff
Parham Alvani <parham.alvani@gmail.com>
2019-03-18 00:46:48 +0330

    Adds linux installation for openshift cli.

--- scripts/oc.sh
+++ scripts/oc.sh
@@ -15,6 +15,14 @@ usage() {
 oc-install() {
 	if [[ $OSTYPE == "linux-gnu" ]]; then
 		message "oc" "Linux"
+
+                message "oc" "Upstall openshift-cli from github"
+	        oc_vr=$(curl -s https://api.github.com/repos/openshift/origin/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+                message "oc" "Dowloading from github"
+                url=$(curl -s https://api.github.com/repos/openshift/origin/releases/latest | grep -E "https://github.com/openshift/origin/releases/download/${oc_vr}/openshift-origin-client-tools-${oc_vr}-[[:alnum:]]{7}-linux-64bit.tar.gz" | cut -d\" -f4)
+                curl -L $url | tar -xvz --wildcards --no-anchored '*oc*' | sudo tee /usr/local/bin/oc > /dev/null
+                sudo chmod +x /usr/local/bin/oc
 	else
 		message "forticlient" "Darwin"
 
