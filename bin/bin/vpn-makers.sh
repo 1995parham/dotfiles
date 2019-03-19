b0d38a5f163f087fa2245401be0c49b18f383eec
Parham Alvani <parham.alvani@gmail.com>
2019-03-19 09:51:17 +0330

    vpn-makers works on osx too.
    Former-commit-id: cfa7d0c7e6b8112f458e980202fa873bf8f9a120 [formerly 076e8527b760bc5320f021ba8a9888ccb7c271a5] [formerly 68a6d549be8be45b88215220b5c7cd206cfed108 [formerly c51d3a3a6e3192a19f30281eaa124799c3892895 [formerly c51d3a3a6e3192a19f30281eaa124799c3892895 [formerly c51d3a3a6e3192a19f30281eaa124799c3892895 [formerly 72cf7df1dd00c15df37da9a1c162d7385d1756d6]]]]]
    Former-commit-id: 4be786690dfa200d5acdfdd751395fd762516460 [formerly 7c28f2e05f8d1074bd1b8403b91abaf4ceae11be]
    Former-commit-id: 0a2b7d5f9952d472a74d48c8337f33c3d5f6b70b
    Former-commit-id: 2201435bc3090ed16d1d0d780d3cdbd81222544d
    Former-commit-id: 18063a3fee7a166c3298d16273e20cfb34a38cf6
    Former-commit-id: cdc44318a54fb5ba6cc811dac4165230d4365398

--- bin/bin/vpn-makers.sh
+++ bin/bin/vpn-makers.sh
@@ -8,9 +8,5 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 echo "Welcome to simple script for connecting to VPN Makers"
-if [[ "$OSTYPE" != "linux-gnu" ]]; then
-	echo >&2 "This script only works on linux based systems :)";
-        exit
-fi
 command -v openconnect >/dev/null 2>&1 || { echo >&2 "I require openconnect but it's not installed.  Aborting."; exit 1; }
 sudo openconnect us4.cisadd2.com -u ir273269
