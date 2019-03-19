77459ebc09359ffb273f9d85853506d70b04df63
Parham Alvani <parham.alvani@gmail.com>
2019-03-19 10:06:14 +0330

    Updates vpn-makers script.

--- bin/bin/vpn-makers.sh
+++ bin/bin/vpn-makers.sh
@@ -7,6 +7,50 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-echo "Welcome to simple script for connecting to VPN Makers"
-command -v openconnect >/dev/null 2>&1 || { echo >&2 "I require openconnect but it's not installed.  Aborting."; exit 1; }
-sudo openconnect us4.cisadd2.com -u ir273269
+
+usage() {
+        echo "$(basename $0) [-h] [-o]"
+        echo "-h use Parham's home user of VPN Makers [ir273269]"
+        echo "-o use Parham's out-of-home user of VPN Makers (default) [ir536722]"
+        echo "-u server url that can be found on 'https://vpnmakers.com/servers?p=cisco'"
+}
+
+connect() {
+        username=$1
+        url=$2
+
+        echo "---"
+        echo "create connection to $username@$url"
+        echo "---"
+
+        sudo openconnect $url -u $username
+}
+
+main() {
+        echo "Welcome to simple script for connecting to VPN Makers"
+
+        if ! hash openconnect 2>/dev/null; then
+                echo "This script uses VPN Makers cisco protocl and requires 'openconnect'"
+                exit
+        fi
+
+        username="ir536722"
+        url="us4.cisadd2.com"
+        while getopts 'hou:' argv; do
+                case $argv in
+                        o)
+                                username="ir536722"
+                                ;;
+                        h)
+                                username="ir273269"
+                                ;;
+                        u)
+                                url=$OPTARG
+                                ;;
+                esac
+        done
+
+        connect $username $url
+}
+
+main $@
