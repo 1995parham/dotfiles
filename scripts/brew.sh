321db24a2bd4cd7f64ad35c9f677b0716f94482d
Parham Alvani <parham.alvani@gmail.com>
2019-05-20 19:49:50 +0430

    Adds brew installation script.
    Former-commit-id: 06c407bdd8107a563cba1a506945f99622ab37bf [formerly 429960b6de6b41b9abd26ecb0dcab917024b70c9] [formerly 063a1cadb5c94f52bbfcf8a37878450136aca976 [formerly f564402081b50f91f5df679e1fb4c52db60a58ab [formerly f564402081b50f91f5df679e1fb4c52db60a58ab [formerly f564402081b50f91f5df679e1fb4c52db60a58ab [formerly 42baad5c871d6b65cb85cb4782e5c1543bb61fb6]]]]]
    Former-commit-id: d3709e94371ad3198deaecbb4b9d4ab3c3a8bec0 [formerly e6d59ad1f204f66afaca21c28f9d3b27103b4486]
    Former-commit-id: 49a4617c59f27d0bd55bf03b6c1ecf6d0cb181db
    Former-commit-id: 42528dd08ac4b2377c93a9f328d0dd8447ae1303
    Former-commit-id: 43dbfacaa5dce3620f685c93cda946b534a28db0
    Former-commit-id: db5a75c90bf80d1d2b457acac882f51dd670c8aa

---
+++ scripts/brew.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : sample.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: brew"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        else
+                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+        fi
+}
