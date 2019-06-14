e085c49e322d2b1418f7c606f3de821674e23b67
Parham Alvani <parham.alvani@gmail.com>
2019-06-14 20:14:06 +0000

    chore: Node Installation Script Check Package Manager for Installation
    Former-commit-id: 91fc2f59d82c9479b6bc71305d5e5a4ebda82f62 [formerly 4f8fc0a03be123655309324c4daca006527166d5] [formerly b2b7d8c3fb23a6e77cfe00c8e865d59bf5371d1f [formerly a114adaea7035dda1facfeba9d5f16c3a847ba4a [formerly a114adaea7035dda1facfeba9d5f16c3a847ba4a [formerly a114adaea7035dda1facfeba9d5f16c3a847ba4a [formerly 19ae2872768d81492dbd09b70163cde620a50063]]]]]
    Former-commit-id: 4825017a37661c0dc3824b42503b115533ccd01b [formerly 3f5d1f4310368aeddce34424d1812f10bb58e3be]
    Former-commit-id: e6acf81310c15b607ff3757f9f8f9d8df93f86ca
    Former-commit-id: c5a2544ac54f2e5cc8c59736bf6689628b897d79
    Former-commit-id: bc2385da6325a6762fa3eb223d83c861985b9089
    Former-commit-id: a26dc03649eaedd217d1fb1640718508caf71f41

--- scripts/node.sh
+++ scripts/node.sh
@@ -17,13 +17,13 @@ usage() {
 node-install() {
         message "node" "Installing Node $version"
 
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-	        echo "[node] Darwin"
+        if hash brew 2>/dev/null; then
+	        echo "[node] with brew"
 
 	        brew install node
 	        brew install yarn
         else
-	        echo "[node] Linux"
+	        echo "[node] with apt"
 
 	        curl -sL https://deb.nodesource.com/setup_$version | sudo -E bash -
 	        sudo apt-get install -y nodejs
