538fcc475cba22c6d016b12a978993732884e32c
Parham Alvani <parham.alvani@gmail.com>
2019-12-05 21:33:45 +0330

    feat: We are Ghermezi
    Former-commit-id: 36029b521a367bb96d773262fd29e5a92b18a81e [formerly cbf2ab5c52bcf1fc63cc12a3a7e76caec0bb21ba] [formerly 734044bf6eaf92c4dad75868bc340511e3b472a1 [formerly 3fada23707f3c9b036162fafd2dcc7f49957f5c3 [formerly 3fada23707f3c9b036162fafd2dcc7f49957f5c3 [formerly 3fada23707f3c9b036162fafd2dcc7f49957f5c3 [formerly ec243cff841b7fdb2344e2524c815673e2860e5b]]]]]
    Former-commit-id: 6279ea0ebce6498e42e8f37b673d3b68302f536b [formerly 06340f9536b14aaf868d7834ce4e23ceedeabd14]
    Former-commit-id: f7b4691e769551d5d12dfb9bdb128e741ade6717
    Former-commit-id: 4c2d014be109c7cec44347b0d137011111714bb8
    Former-commit-id: 3705af7bb5a2442a2176b0572cb82fcbd43e56c2
    Former-commit-id: 147c5881aff3c8f49c1313828cfd3f49bbba0192

--- bin/bin/vpn-makers.sh
+++ bin/bin/vpn-makers.sh
@@ -9,10 +9,7 @@
 # =======================================
 
 usage() {
-        echo "$(basename $0) [-h] [-o]"
-        echo "-h use Parham's home user of VPN Makers [ir273269]"
-        echo "-o use Parham's out-of-home user of VPN Makers (default) [ir536722]"
-        echo "-u server url that can be found on 'https://vpnmakers.com/servers?p=cisco'"
+        echo "$(basename $0)"
 }
 
 connect() {
@@ -27,28 +24,15 @@ connect() {
 }
 
 main() {
-        echo "Welcome to simple script for connecting to VPN Makers"
+        echo "Welcome to simple script for connecting to Ghermezi Openconnect"
 
         if ! hash openconnect 2>/dev/null; then
-                echo "This script uses VPN Makers cisco protocl and requires 'openconnect'"
+                echo "This script requires 'openconnect'"
                 exit
         fi
 
-        username="ir536722"
-        url="us4.cisadd2.com"
-        while getopts 'hou:' argv; do
-                case $argv in
-                        o)
-                                username="ir536722"
-                                ;;
-                        h)
-                                username="ir273269"
-                                ;;
-                        u)
-                                url=$OPTARG
-                                ;;
-                esac
-        done
+        username="parham"
+        url="oc.ghermezi.xyz:4443"
 
         connect $username $url
 }
