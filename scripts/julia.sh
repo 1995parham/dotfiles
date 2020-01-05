84c1f8ae7017f8b751c534a5a917ab291545b4dc
Parham Alvani <parham.alvani@gmail.com>
2020-01-05 20:42:00 +0330

    feat: Make Julia Script Even Better
    Former-commit-id: 9acb039b1b097ec3c1503dc61fdc55b7e9386e69 [formerly 736e69a44feda6696502c945b722606d9f10fbea] [formerly 99a7c1f3c28787ecd0704530e523eab4d93d88e6 [formerly 123d6ba3f03b4e529de26251fb096d2c5fee4cfd [formerly 123d6ba3f03b4e529de26251fb096d2c5fee4cfd [formerly 123d6ba3f03b4e529de26251fb096d2c5fee4cfd [formerly 2fa0b1e253f0e33119f92113272ab5409759d18a]]]]]
    Former-commit-id: 641270214f5eb664cb554e0ff2d1b37e659a5cbf [formerly 0e5ca703120cec55aa4a7aa480c89c6453e6a5fd]
    Former-commit-id: 0714f967b91e8d2cbdc596881d650962d7e91cd1
    Former-commit-id: 496ab13d15bd1ddb22b2f097fe4d753fe1223367
    Former-commit-id: e7e9c4b5e3a67d4b5afa05d2954ced3d75484950
    Former-commit-id: b889bd82d1e681c36a03400510bba5b671b07704

--- scripts/julia.sh
+++ scripts/julia.sh
@@ -8,14 +8,17 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 
-version='1.2.0'
+version='1.3.1'
 
 usage() {
         echo "usage: julia"
 }
 
 download_julia() {
-        local_version=$(julia --version | cut -d ' ' -f 3)
+        local_version="-"
+        if hash julia 2>/dev/null; then
+                local_version=$(julia --version | cut -d ' ' -f 3)
+        fi
         if [ $local_version == $version ]; then
                 message "julia" "you have installed julia ${local_version}"
                 return
@@ -23,7 +26,13 @@ download_julia() {
 
         message "julia" "install ${version}"
         curl -L https://julialang-s3.julialang.org/bin/linux/x64/${version%.*}/julia-${version}-linux-x86_64.tar.gz | tar xvfz -
+        if [ -e /opt/julia ]; then
+                sudo rm -r -f /opt/julia
+        fi
         sudo mv julia-${version} /opt/julia
+        if [ -e /usr/local/bin/julia ]; then
+                sudo rm -r -f /usr/local/bin/julia
+        fi
         sudo ln -s /opt/julia/bin/julia /usr/local/bin/julia
 }
 
