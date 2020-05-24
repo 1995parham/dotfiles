28309555842ffe7e0d88857f5fa8bd099983790d
Parham Alvani <parham.alvani@gmail.com>
2020-05-24 08:35:50 +0430

    feat: Merge Kubeval Installation to OC
    Former-commit-id: 36e5acf0d20d8b4162da3e677adb4dc65467c06b [formerly 8cd7393b19440e19da34b17e28dd536a8da93fa7] [formerly adf45f4976dbf1dc9ace556e67baa03ef37d6573 [formerly f8837c7533bfb8ef628b6765cf19381c991b1f59 [formerly f8837c7533bfb8ef628b6765cf19381c991b1f59 [formerly f8837c7533bfb8ef628b6765cf19381c991b1f59 [formerly bf566e0fa9c3f09b84b12531209531780e83f018]]]]]
    Former-commit-id: 318491e1048b34cf2fe48d0a82a44dd6cfcc355e [formerly f1dabb0ed240dd5b0720ef2af8d212cb07e85874]
    Former-commit-id: 3f0b149a322ebab7fe6de382d56eb03342a97024
    Former-commit-id: 6266b12a985709931d4bf563bb08066c732fb059
    Former-commit-id: 05b859d449d9a1802291efae211c49f4d9604688
    Former-commit-id: 81bfab79ca08979e2bf75f9a3e6760b5a024f1a6

--- scripts/oc.sh
+++ scripts/oc.sh
@@ -12,6 +12,24 @@ usage() {
         echo "usage: oc"
 }
 
+kubeval-upstall() {
+        message "kubeval" "Upstall kubeval from github"
+	kubeval_vr=$(curl -s https://api.github.com/repos/instrumenta/kubeval/releases/latest | grep 'tag_name' | cut -d\" -f4)
+        kubeval_vl=''
+        if hash kubeval 2> /dev/null; then
+                kubeval_vl=$(kubeval --version | grep Version | cut -d' ' -f2)
+        fi
+
+        if [[ $kubeval_vr != $kubeval_vl ]]; then
+                message "kubeval" "Dowloading from github"
+                curl -L https://github.com/instrumenta/kubeval/releases/download/${kubeval_vr}/kubeval-$(uname -s | awk '{print tolower($0)}')-amd64.tar.gz | tar -zxOf - kubeval | sudo tee /usr/local/bin/kubeval > /dev/null
+                sudo chmod +x /usr/local/bin/kubeval
+        fi
+
+        message "kubeval" "$(kubeval --version)"
+}
+
+
 kube-install() {
         message "oc" "Install kubectl from brew"
         brew install kubernetes-cli
@@ -34,5 +52,6 @@ oc-install() {
 
 main() {
         kube-install
+        kubeval-upstall
         oc-install
 }
