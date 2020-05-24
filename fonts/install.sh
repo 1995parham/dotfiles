74960dde64d0fc94718c0676d4f990b13338b552
Parham Alvani <parham.alvani@gmail.com>
2020-05-24 18:32:44 +0430

    feat: Better Font Directory Structure
    Former-commit-id: 2f53a3e8172e9eea6a28bdfb8886712870771d01 [formerly 1b2cb0bc9b5a303227838467111000af8bcd2850] [formerly f6cc5d6bb51e543a9e8ea009f459a4dc98c5258a [formerly 4b305cfe52c72a4ca9c19ea45673a33e9aa15ee6 [formerly 4b305cfe52c72a4ca9c19ea45673a33e9aa15ee6 [formerly 4b305cfe52c72a4ca9c19ea45673a33e9aa15ee6 [formerly ea0ddca1f766828b4ec4388fcc7e2fc4460c871d]]]]]
    Former-commit-id: ac0d9f8ef49b768e5269cd9db263edb4f1b69971 [formerly ec9e7c4993e108fdb962017e06e76230b03edc14]
    Former-commit-id: 72989f68a5e1776ca6edb872425908073ee49bea
    Former-commit-id: 9b51dcacc8ac5607ac7fa640bd19bd9d6d8733ca
    Former-commit-id: 04cb2290417db03d01c5ebf73e72ad0e6cfbd2d2
    Former-commit-id: 85b75d61274b333bd484b1054c32e8e2a4477e91

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# Set source and target directories
+# set source and target directories
 powerline_fonts_dir=$( cd "$( dirname "$0" )" && pwd )
 
 find_command="find \"$powerline_fonts_dir\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
 
 if [[ $(uname) == 'Darwin' ]]; then
-  # MacOS
   font_dir="$HOME/Library/Fonts"
 else
-  # Linux
   font_dir="$HOME/.local/share/fonts"
   mkdir -p $font_dir
 fi
 
-# Copy all fonts to user fonts directory
+# copy all fonts to user fonts directory
 echo "Copying fonts..."
 eval $find_command | xargs -0 -I % cp "%" "$font_dir/"
 eval $find_command | xargs -0 -I % basename "%"
 
-# Reset font cache on Linux
+# reset font cache on Linux
 if command -v fc-cache @>/dev/null ; then
     echo "Resetting font cache, this may take a moment..."
     fc-cache -f $font_dir
