2a91337753b6187e276ad0cdb220e33c5fdc355c
Parham Alvani <parham.alvani@gmail.com>
2020-07-07 02:37:56 +0430

    fix: Correct govc Installation Script
    Former-commit-id: 5e1bd0b2c5873d43dcde76f504139b3499d8bea5 [formerly ecf97ffba6d4439128e6317d24c039540e9963d9] [formerly 6c1bccb8768b5be41791476ca606d4250f9a0c08 [formerly 819d5923e5376ba4813e1475541bd3950718fc5a [formerly 819d5923e5376ba4813e1475541bd3950718fc5a [formerly 819d5923e5376ba4813e1475541bd3950718fc5a [formerly f66bb68f0bcb077b5a55a51a5962dd8956226fb8]]]]]
    Former-commit-id: 2d941c28f60ef6603dfb8f7fff7d64dae22181b9 [formerly c0be5a23b8b95c07dfb8ed717f1f87e5c4113fb9]
    Former-commit-id: 75685c88575fe85f6947943e06cdd02b98964d06
    Former-commit-id: f54be2d44ee3c4f4f5b6b5918bb4c80ae989df5f
    Former-commit-id: a404e1ca7dace297b58d2d9ed15df6b8c5a247d9
    Former-commit-id: 791d48d229749941791bc96ad94d73f4bf94f943

--- scripts/govc.sh
+++ scripts/govc.sh
@@ -15,12 +15,12 @@ usage() {
 
 govc-upstall() {
         message "govc" "Upstall govc from github"
-	govc_vr=$(curl -s https://api.github.com/repos/vmware/govmomi/releases/latest | grep 'tag_name' | cut -d\" -f4)
-        govc version -require=$(echo $govc_vr | cut -dv -f2) 2> /dev/null
+        govc_vr=$(curl -s https://api.github.com/repos/vmware/govmomi/releases/latest | grep 'tag_name' | cut -d\" -f4)
+        hash govc 2> /dev/null && govc version -require=$(echo $govc_vr | cut -dv -f2) 2> /dev/null
 
         if [ $? -ne 0 ]; then
                 message "govc" "Dowloading from github"
-                sudo curl -L https://github.com/vmware/govmomi/releases/download/${govc_vr}/govc_$(uname -s | awk '{print tolower($0)}')_amd64.gz | gunzip > /usr/local/bin/govc
+                curl -L https://github.com/vmware/govmomi/releases/download/${govc_vr}/govc_$(uname -s | awk '{print tolower($0)}')_amd64.gz | gunzip | sudo tee /usr/local/bin/govc > /dev/null
                 sudo chmod +x /usr/local/bin/govc
         fi
 
