f93c1020ce3d32728d2851b1e559b3e2d4b66df3
Parham Alvani <parham.alvani@gmail.com>
2020-07-23 16:21:53 +0430

    feat: Improve Brew Installation Script

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -9,17 +9,32 @@
 # =======================================
 
 usage() {
-        echo "usage: brew"
+        echo "usage: brew [-p]"
+        echo "-p: install .profile or .bash_profile for using brew"
 }
 
 main() {
+        install_profile=false
+
         # Reset optind between calls to getopts
         OPTIND=1
+        while getopts "p" argv; do
+	        case $argv in
+		        p)
+                                install_profile=true
+			        ;;
+	        esac
+        done
 
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        else
-                bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        if [[ "$OSTYPE" == "linux"* ]]; then
                 sudo apt-get install build-essential curl file git
+
+                if [ $install_profile = true ]; then
+                        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+                        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+                        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+                        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+                fi
         fi
 }
