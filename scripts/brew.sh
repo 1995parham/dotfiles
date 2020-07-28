c0556aea04c49546c2d947b7f3f79b356bdcd008
Parham Alvani <parham.alvani@gmail.com>
2020-07-28 09:23:55 +0430

    fix: Correct Brew Installation Message
    Former-commit-id: 3dbfa0cf8afcd42a56b332bfeaa298cbd12f603f [formerly a00344ac7714a7c1883b808b6fe4ace95f19d667] [formerly 01f7e5c04bd20a3d748afca970f468bddb8193f3 [formerly e3dcc8217f1b32ddacc9c3d5f5abb3828b8900d1 [formerly e3dcc8217f1b32ddacc9c3d5f5abb3828b8900d1 [formerly e3dcc8217f1b32ddacc9c3d5f5abb3828b8900d1 [formerly 2a049d6a9235df93601c4dc8c91296024cb7dfe5]]]]]
    Former-commit-id: 6d60af65d823eeebf0bc6ef5ff9ca4437b42944a [formerly 86b2fd86e886cd6b6cf6ff2fdfcb4da8eeac9940]
    Former-commit-id: 9910be4ae5cc6c7cc457868cc1d7eaa16f1e5e52
    Former-commit-id: ee44a36d0661fff8d1e402597bdfaa8bd71e3f2f
    Former-commit-id: 580cf3663e3827eb0d7ccbae6b6cc2df18f12fd7
    Former-commit-id: 1c8d818a5f25934f8ab625071bd58897e72fd744

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -32,7 +32,7 @@ main() {
                 sudo apt-get install build-essential
 
                 if [ $install_profile = true ]; then
-                        message "brew" "add Homebrew to your PATH in"
+                        message "brew" "add Homebrew to your PATH:"
                         test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
                         test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
                         test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
