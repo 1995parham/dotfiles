a03d6c38d91db1ac019477d1e5cc79d50cde3db1
Parham Alvani <parham.alvani@gmail.com>
2020-07-28 10:11:48 +0430

    feat: Complete Go Installation Script
    Former-commit-id: 00455fafbfe34cdef112f9157d492d50a1848659 [formerly ff1c9d8c91aed599fd74c819ea90269c0132b293] [formerly 5709d97ec97ab04d78b72253875abbe8717bf862 [formerly 8ad6eb55be43609ca079c2cf17c8f9ca64963d29 [formerly 8ad6eb55be43609ca079c2cf17c8f9ca64963d29 [formerly 8ad6eb55be43609ca079c2cf17c8f9ca64963d29 [formerly 17adbab104388cc9ccd4e3671f089932114ed659]]]]]
    Former-commit-id: f308d1191422c9d3a273bb9c681e65caf34033ec [formerly 52b8276881d3a6e8ef59a36c49cc87ada9a2ed92]
    Former-commit-id: 5789da42bf52632cb18d4ca3b8a9d37f8cd305cf
    Former-commit-id: bb38dca56a0b030941281500f1d9bb3f2a823220
    Former-commit-id: 1af3cc35220322aa03e59d5628aa154f95c42771
    Former-commit-id: 36f2ef67cf78600b0b7fd28b1bed7716c66e3482

--- scripts/go.sh
+++ scripts/go.sh
@@ -8,17 +8,31 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 usage() {
-	echo "usage: go"
+        echo "usage: go"
 }
 
 go-install() {
-	message "go" "Installing go"
-
-        brew install go
+        if brew ls --versions go > /dev/null; then
+                message "go" "upgrading go"
+                brew upgrade go
+        else
+                message "go" "installing go"
+                brew install go
+        fi
 
         message "go" "$(go version)"
 
-	message "go" "Create go directory structure"
+        if brew ls --versions go > /dev/null; then
+                message "go" "upgrading golangci-lint"
+                brew upgrade golangci/tap/golangci-lint
+        else
+                message "go" "installing golangci-lint"
+                brew install golangci/tap/golangci-lint
+        fi
+
+        message "go" "$(golangci-lint --version)"
+
+        message "go" "create go directory structure"
         local gopath
         gopath=$HOME/Documents/Go
         for dir in bin pkg src mod; do
@@ -28,32 +42,33 @@ go-install() {
 }
 
 go-install-package() {
-	go get -u $1
+        message "go" "goproxy ${GOPROXY} $1"
+        go get -v $1
 
-	if [ $? -eq 0 ]; then
-		message "go" "$1 installation succeeded"
-	else
-		message "go" "$1 installation failed"
-	fi
+        if [ $? -eq 0 ]; then
+                message "go" "$1 installation succeeded"
+        else
+                message "go" "$1 installation failed"
+        fi
 }
 
 go-install-packages() {
-        message "go" "Fetch some good and useful go packages"
+        message "go" "fetch some good and useful go packages"
 
-	# Go Tools
-	message "go" "Go Tools"
+        # Go Tools
+        message "go" "go tools"
         # Linters Runner for Go. 5x faster than gometalinter. Nice colored output.
-	go-install-package "github.com/golangci/golangci-lint/cmd/golangci-lint"
+        # go-install-package "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
-	# Go Debugger
+        # Go Debugger
         # vim-go adds dlv since v1.17 (March 27, 2018)
-	# message "go" "Go Debugger [delve]"
-	# go-install-package "github.com/derekparker/delve/cmd/dlv"
+        # message "go" "Go Debugger [delve]"
+        # go-install-package "github.com/derekparker/delve/cmd/dlv"
 
-	message "go" "Install binary requirements of vim-go"
+        message "go" "install binary requirements of vim-go"
         # please consider that this script runs in bash so it cannot see
         # aliases that are defined in zsh
-        hash nvim &> /dev/null || nvim +GoUpdateBinaries +qall
+        hash nvim &> /dev/null && nvim +GoUpdateBinaries --headless +qall
 }
 
 
