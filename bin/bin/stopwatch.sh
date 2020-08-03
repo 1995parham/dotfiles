0b5c1634cbdc7db2c1545b00a2583369e7e17b68
Parham Alvani <parham.alvani@gmail.com>
2020-08-03 11:55:37 +0430

    feat: Better Exit Code Handling in Stopwatch

--- bin/bin/stopwatch.sh
+++ bin/bin/stopwatch.sh
@@ -8,6 +8,6 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 python3 -m termdown -a $@
-if [ $? -ne 0 ]; then
+if [ $? -lt 0  ]; then
         python3 -m pip install termdown
 fi
