82b185d3cf5bf41d79923b09f7058a00f6100c70
Parham Alvani <parham.alvani@gmail.com>
2020-08-06 03:36:34 +0430

    feat: Complete Gopass Installation Script
    Former-commit-id: 7b226af30c9a9b1a90bc32ca383f10e47274b6d2 [formerly 889d046a986bcfd593011f0fc9e575eccf6c6a5a] [formerly 340d386c531ba9060967ecb73fffe64f375c8ff4 [formerly 4f01907f90f51c40629292b39710ca96a17d4f75 [formerly 4f01907f90f51c40629292b39710ca96a17d4f75 [formerly 4f01907f90f51c40629292b39710ca96a17d4f75 [formerly 6685a9f5966efd85db54fc8479544bba0a54d8f3]]]]]
    Former-commit-id: 1e0fe55da10feca566506360e54e0d2b4cd79c04 [formerly f0f9234970a2d711ffd91bdedadd8c99507445c5]
    Former-commit-id: 5c41c15b0ebafa260888b7becf626b82cc469574
    Former-commit-id: 30dba605a42971467ad9d007e15dbe47a9ec88f9
    Former-commit-id: 4d5870e5e0dd39f3c782c914957da079f2b634e3
    Former-commit-id: 7eee611a10ffdc926319f70502c915f15f4551cd

--- scripts/gopass.sh
+++ scripts/gopass.sh
@@ -18,10 +18,11 @@ gopass-upstall() {
         gopass_vr=$(curl -s https://api.github.com/repos/gopasspw/gopass/releases/latest | grep 'tag_name' | cut -d\" -f4)
         gopass_vl=''
         if hash gopass &> /dev/null; then
-                gopass_vl=$(gopass version | grep Version | cut -d' ' -f2)
+                gopass_vl=$(gopass version | grep gopass | cut -d' ' -f2 | sed 's/\+.*//')
         fi
 
-        if [[ $gopass_vr != $gopass_vl ]]; then
+        message "gopass" "github: ${gopass_vr#v}, local: $gopass_vl"
+        if [[ ${gopass_vr#v} != $gopass_vl ]]; then
                 message "gopass" "Dowloading from github"
                 curl -L https://github.com/gopasspw/gopass/releases/download/${gopass_vr}/gopass_${gopass_vr#v}_linux_amd64.deb > gopass.deb
                 sudo dpkg -i gopass.deb
