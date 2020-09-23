62c0c23123ca8aeb9cafa57fec051a28be2336c0
Parham Alvani <parham.alvani@gmail.com>
2020-09-23 08:52:18 +0330

    feat: Add Package Based Fonts
    Former-commit-id: b475b8acd6a83de026e7fb7ff0061b074b8ff046 [formerly 2757965f717118eed8bf6e1a8916a2842d339869] [formerly be9d898bee14a6b56b06771e8f2ecc433d65581d [formerly 5891013ff261f675a3c83dedc9f77bbc371db461 [formerly 5891013ff261f675a3c83dedc9f77bbc371db461 [formerly 5891013ff261f675a3c83dedc9f77bbc371db461 [formerly b30702614c7a2f6a454792a4fb4bb5a025628d05]]]]]
    Former-commit-id: e093594f6ce4a2394ef52a5335ccf363ec0d68c1 [formerly 522d0eff9f8d3ff7497b94bf0c8c6cb4f300d307]
    Former-commit-id: 57661b5b3c9c79d5089059a67ef6eaaf5bbf116b
    Former-commit-id: 812d89bb2d7f9083dfd2e09590924a146b478ece
    Former-commit-id: f18d71b7f2f323bef5b483f9f579e521b37b6312
    Former-commit-id: adf3a601448c8a2a6765048a2e91f0ece9dd51dc

---
+++ scripts/font.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : sample.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: font"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+
+        if [[ "$(command -v apt)" ]]; then
+                sudo apt-get install fonts-roboto
+        elif [[ "$(command -v pacman)" ]]; then
+                sudo pacman -Syu noto-fonts-emoji ttf-roboto
+        fi
+}
