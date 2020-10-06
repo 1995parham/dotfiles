5295a3f656ed8ce837303b7752e55683b13e1568
Parham Alvani <parham.alvani@gmail.com>
2020-10-06 14:20:38 +0330

    feat: Improve Node Installation Script
    Former-commit-id: f353442747854bc9c7498924052e2d1f717a05bb [formerly 003b84b07d81964244278a5195de853184214caa] [formerly 7f793bb1995811df6872248814bbad504c61afbb [formerly 896c6c4c5ad6f081d4ac2395b75429592a093d11 [formerly 896c6c4c5ad6f081d4ac2395b75429592a093d11 [formerly 896c6c4c5ad6f081d4ac2395b75429592a093d11 [formerly ae052f77a0ed4bc9ad8ef1c38acaa821e965b7cc]]]]]
    Former-commit-id: c1d8c733326202672772a9273db67cfe584838a8 [formerly adebd52326dca201491b57894e612ae3f9e8ac9e]
    Former-commit-id: 35dc3f19a836cc40f70508a39878432fd7c7d403
    Former-commit-id: b5c3fbcad30a2d93ebab5e23f5305632636e50a0
    Former-commit-id: 941fd7b48d3b07697277b1f52de4ce0672b4fa80
    Former-commit-id: b65392cb6c81ffa5eae7c1834d6f04261170b61f

--- scripts/node.sh
+++ scripts/node.sh
@@ -13,11 +13,21 @@ usage() {
 
 
 node-install() {
-        message "node" "Installing Node $version"
+        message "node" "Installing Node from brew"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+                message "node" "Darwin"
 
-        echo "[node] with brew"
+                brew install node
+        else
+                message "node" "Linux"
+                if [[ "$(command -v apt)" ]]; then
+                        echo "TODO"
+                elif [[ "$(command -v pacman)" ]]; then
+                        message "node" "install node with pacman"
+                        sudo pacman -Syu --noconfirm --needed nodejs npm
+                fi
+        fi
 
-        brew install node
 
         message "node" "$(node -v)"
 }
