050ce8cd95f54782649ddf739b74af61c2778d8e
Parham Alvani <parham.alvani@gmail.com>
2020-11-18 08:12:50 +0330

    feat: Add Firefox Script
    Former-commit-id: acaa12b3a2b7e8e373e31c8a1a98b3b0ee414e81 [formerly b34a877cf359b148b0da9a46bacdd84ac51cd598] [formerly ebf5ab8539765fbb399312966d145ccae8fefc79 [formerly 9362cfa740e462a3a94e419ffbe16e0e35d1fcc3 [formerly 9362cfa740e462a3a94e419ffbe16e0e35d1fcc3 [formerly 9362cfa740e462a3a94e419ffbe16e0e35d1fcc3 [formerly b2893e90f69446691f181114ffc60c80ab8f1a3a]]]]]
    Former-commit-id: 85ef8c44f32fd24bd1c8ae85cf1530fb1a8c754e [formerly 4746c45407b06e4318cdb4232826081e2db5abee]
    Former-commit-id: f7bb756a6d2416fcfc8d4509aea42465af2f7224
    Former-commit-id: a76da8628e3022561eac4da8bc1719d5bdbe9b7d
    Former-commit-id: 3594005664f0e1f5a18b73260825b95b07f3a31a
    Former-commit-id: e346440419fe21ec797758f35e650d302ef8903a

---
+++ scripts/firefox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : firefox.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: firefox"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+
+        sudo pacman -Syu --noconfirm --needed firefox
+}
