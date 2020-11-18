9ae3b9b2cdcd81301a6090cd23920f5404a55322
Parham Alvani <parham.alvani@gmail.com>
2020-11-18 07:51:43 +0330

    feat: Move Configfile Function to start.sh
    Former-commit-id: 0ddda534d32796bfbda45c44b994fb64fff2c89f [formerly 5cfb9ec0df4ff45da3251017749f69a38486bf7a] [formerly cfea6bcf10b9da3ac7164c06e5d3d561ad00c9be [formerly db05fe4e887ae7484a5f054e95467b80cbae503c [formerly db05fe4e887ae7484a5f054e95467b80cbae503c [formerly db05fe4e887ae7484a5f054e95467b80cbae503c [formerly 77a3437b5c7595edf6def1257d59101d996f8bc6]]]]]
    Former-commit-id: 68a2558c76eb6d233101690e10ea93e9b8053362 [formerly d5348bea963f4a0948ec448b9ce24200647af3e5]
    Former-commit-id: ae0aeb8f96377ceb83850675f4578b7f5d694b97
    Former-commit-id: b126fc622142eaa407bfad7353e3574ca6cf89fa
    Former-commit-id: c357765279a7c7cbbc299b9c144ccd689150d3d5
    Former-commit-id: 0f0899f89ec0640602f37ea2b1a45f6a57a004d5

--- scripts/snapp.sh
+++ scripts/snapp.sh
@@ -1,9 +1,9 @@
-#!/bin/
+#!/bin/bash
 # In The Name of God
 # ========================================
-# [] File Name : sample.sh
+# [] File Name : snapp.sh
 #
-# [] Creation Date : 17-07-2018
+# [] Creation Date : 17-11-2020
 #
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
@@ -11,72 +11,6 @@
 usage() {
         echo "usage: snapp"
         echo "install snapp corporation mail/calender/contacts"
-}
-
-# Creates a config file that resides in the `.config` directory, and provides a soft link for it.
-# parameter 1: module name - string
-# parameter 2: file name - string
-configfile() {
-        local module=$1
-        local src_file=$2
-
-        if [ ! -e "$HOME/.config" ]; then
-                mkdir "$HOME/.config"
-        fi
-
-        if [ ! -z $src_file ]; then
-                local src_path="$current_dir/snapp/$module/$src_file"
-                local dst_file="$module/$src_file"
-
-                if [ ! -d "$HOME/.config/$module" ]; then
-                        mkdir "$HOME/.config/$module"
-                fi
-        else
-                src_file=$module
-                local src_path="$current_dir/snapp/$module"
-                local dst_file="$module"
-        fi
-        local dst_path="$HOME/.config/$dst_file"
-
-        linker $module $src_path $dst_path
-}
-
-# linker
-# parameter 1: module name - string
-# parameter 2: source path - string
-# parameter 3: destination path - string
-linker() {
-        local module=$1
-        local src_path=$2
-        local dst_path=$3
-
-        local create_link=true
-
-        if [ -e $dst_path ] || [ -L $dst_path ]; then
-                message "$module" "$dst_path already existed"
-
-                if [[ $src_path = $(readlink $dst_path) ]]; then
-                        message "$module" "$dst_path is a correct link"
-                        create_link=false
-                        return
-                fi
-
-                if [[ $yes_to_all == 0 ]]; then
-                        read -p "[$module] do you want to remove $dst_path ?[Y/n] " -n 1 delete_confirm; echo
-                fi
-
-                if [[ $delete_confirm == "Y" ]] || [[ $yes_to_all == 1 ]]; then
-                        rm -R $dst_path
-                        message "$module" "$dst_path was removed successfully"
-                else
-                        create_link=false
-                fi
-        fi
-
-        if $create_link; then
-                ln -s $src_path $dst_path
-                message "$module" "Symbolic link created successfully from $src_path to $dst_path"
-        fi
 }
 
 main() {
@@ -89,8 +23,8 @@ main() {
         systemctl --user enable davmail@snapp
         systemctl --user start davmail@snapp
 
-        configfile davmail
-        configfile khal
-        configfile vdirsyncer
-        configfile mutt
+        configfile davmail "" snapp
+        configfile khal "" snapp
+        configfile vdirsyncer "" snapp
+        configfile mutt "" snapp
 }
