0e6c104a58d7d0912ea0b57e709c8ff613c44ff4
Parham Alvani <parham.alvani@gmail.com>
2020-11-18 14:12:39 +0330

    feat: Add i3 Module
    Former-commit-id: cd4722e7c084f91fccce3bccff65564a7ec33ade [formerly 339a1bf1a92e1c0791648b04c807b1b594b06a9f] [formerly ff34ecce090d41e82f116b6877fcafbe396ee6f1 [formerly c4b482a2c5a5d7a14d754f02d3be4498b8487db6 [formerly c4b482a2c5a5d7a14d754f02d3be4498b8487db6 [formerly c4b482a2c5a5d7a14d754f02d3be4498b8487db6 [formerly 721432fd625884b3fd9a470a91b58958e412deaa]]]]]
    Former-commit-id: 8020e8310d7790852b823bb4edefd44b62a93ee1 [formerly 7c4e3a5e52223c630bc383cd48466a7cd62e9ea2]
    Former-commit-id: 4577d4f820b3d9c5a1088e1dd66b9f8611d0918f
    Former-commit-id: 16f1d94b9a69f18b2f6f3d65fe5d6e6488017a6a
    Former-commit-id: 170815aa4424097983c73d7a2528c6ae3ed64012
    Former-commit-id: cf698d9d9466fd09e9cead45cc80dc3210a72e0e

---
+++ scripts/i3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : i3.sh
+#
+# [] Creation Date : 18-11-2020
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: i3"
+}
+
+main() {
+        # Reset optind between calls to getopts
+        OPTIND=1
+
+        sudo pacman -Syu --noconfirm --needed i3-gaps i3-scrot i3status-rust
+
+        configfile "i3status" "" "i3"
+        configfile "i3" "" "i3"
+
+        sudo pacman -Syu --noconfirm --needed picom
+        sudo pacman -Syu --noconfirm --needed nitrogen
+}
