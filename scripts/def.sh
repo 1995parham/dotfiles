6f26a027944a4e24804d4554ab6dc688b353f5c9
Parham Alvani <parham.alvani@gmail.com>
2020-11-27 10:30:39 +0330

    feat: Install dictionaries
    Former-commit-id: fa924d0f76df612cf2941b6d2e5926fc86e50601 [formerly d8cf2d6ba1e81cb39f1f81724ad0b28f89387d69] [formerly cf43cca7f458a9941ecdc02ccddc114e5d147610 [formerly 49c4dcca91dbc0af608a9f416018828c0740e9bf [formerly 49c4dcca91dbc0af608a9f416018828c0740e9bf [formerly 49c4dcca91dbc0af608a9f416018828c0740e9bf [formerly 65b5f9acd6da350b8ba7bbb3a913553a183409f1]]]]]
    Former-commit-id: cc1fdb4f1df39424af77344b5b1fa0710689b181 [formerly 0ae9b3965f9ac043cce4ef2d4ea8baf5df9b5797]
    Former-commit-id: 789ccecddd06c2c5bc67d1742f89a2d19c968ed5
    Former-commit-id: b00b7141925d9559a483c105df1a3b4e432b7bd2
    Former-commit-id: 4ff52daf9377811c367b44adb41c51bd0ab5fd16
    Former-commit-id: 1cfcb76db13e7377d0a3075a30d7c4750ae81151

---
+++ scripts/def.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : sample.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+        echo "usage: def"
+}
+
+main() {
+        sudo pacman -Syu --needed --noconfirm sdcv
+        yay -Syu --needed --noconfirm stardict-oald
+}
