0f3b9dd297a553537e78c13f140b965c0f7c93d4
Parham Alvani <parham.alvani@gmail.com>
2020-12-11 17:57:42 +0330

    feat: Add Basic Configuration for Emails

---
+++ scripts/mail.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : mail.sh
+#
+# [] Creation Date : 11-12-2020
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+
+usage() {
+        echo "usage: mail"
+}
+
+main() {
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+                message "mail" "Darwin"
+
+                brew install offlineimap
+        else
+                message "mail" "Linux"
+                if [[ "$(command -v apt)" ]]; then
+                        echo "There is nothing that we can do"
+                elif [[ "$(command -v pacman)" ]]; then
+                        message "mail" "install offlineimap with pacman"
+                        sudo pacman -Syu --noconfirm --needed offlineimap
+                        yay -Syu --noconfirm --needed mu
+                fi
+        fi
+
+        configfile offlineimap "" mail
+
+        mkdir -p ~/mail/aut
+        mkdir -p ~/mail/main
+}
