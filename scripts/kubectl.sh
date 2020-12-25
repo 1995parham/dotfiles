234483705e2f339be31a81b1f2c95425ee12de3d
Parham Alvani <parham.alvani@gmail.com>
2020-12-25 23:00:54 +0330

    feat: Add Helmfile

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -20,6 +20,9 @@ kube-install() {
 		message "kubectl" "Install helm from brew"
 		brew install helm
 
+		message "kubectl" "Install helmfile from brew"
+                brew install helmfile
+
 		message "kubectl" "Install stern (Multi pod and container log tailing for Kubernetes) from brew"
 		brew install stern
 
@@ -29,8 +32,8 @@ kube-install() {
 		if [[ "$(command -v apt)" ]]; then
 			echo "There is nothing that we can do right now"
 		elif [[ "$(command -v pacman)" ]]; then
-			message "kubectl" "install kubectl helm with pacman"
-			sudo pacman -Syu --noconfirm --needed  kubectl helm
+			message "kubectl" "install kubectl/helm/helmfile with pacman"
+			sudo pacman -Syu --noconfirm --needed  kubectl helm helmfile
 
 			message "kubectl" "Install stern (Multi pod and container log tailing for Kubernetes) with yay"
 			yay -Syu --noconfirm --needed stern-bin
