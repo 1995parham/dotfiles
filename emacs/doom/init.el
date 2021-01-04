f47f795b1189416ecd4b903c2fac1f13c1ac269c
Parham Alvani <parham.alvani@gmail.com>
2021-01-04 11:24:57 +0330

    feat: Start Using Doom Modeline

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -37,7 +37,7 @@
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       ;;modeline          ; snazzy, Atom-inspired modeline, plus API
+       modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
