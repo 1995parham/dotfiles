03d7a30677db4960c648bf7e5aaadfcaf4765bfd
Parham Alvani <parham.alvani@gmail.com>
2021-01-11 10:35:05 +0330

    feat: Add Syncthing Installation Script
    Former-commit-id: a8789eed7bdd462cba53127de536bf355132edbc [formerly 57b678d7660d40d77fd80b9a958bc377f607160f] [formerly fdd5b2eb44cf1e7c2ecd8de8e3a63550339cb253 [formerly 5e4c711f871318ad658509ba9f309389fadf841e [formerly 5e4c711f871318ad658509ba9f309389fadf841e [formerly 5e4c711f871318ad658509ba9f309389fadf841e [formerly 99bbb6e72d3aadf0dc183e615000ac6ee4343251]]]]]
    Former-commit-id: d3d9e777ed52ab644025f379141a74f7eaf1fda1 [formerly b7cf542586acfb54f66816f67a94cb057f5fc0d9]
    Former-commit-id: 4237cd4a1f6d2efa7414e1a76b40927ecd4305c5
    Former-commit-id: 9f54317dd588fa9c8fce9151fc78ce1ef06d2283
    Former-commit-id: 04f41008e12a308bf7d76fe9d7acdb6cd88359e0
    Former-commit-id: cbfa7de93f4c9edb24e433e384cc0f114c2f12f0

---
+++ scripts/syncthing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : syncthing.sh
+#
+# [] Creation Date : 11-01-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+main() {
+        if [[ $OSTYPE == "linux-gnu" ]]; then
+                sudo pacman -Syu syncthing
+                sudo systemctl enable syncthing@parham
+        else
+                brew install syncthing
+                brew services start syncthing
+        fi
+
+        message "syncthing" "Please refer to its documentation for setup the mesh"
+}
