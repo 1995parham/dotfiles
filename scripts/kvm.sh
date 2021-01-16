ad0c6b15041b52bb4bab7c71b2ce09d40b913862
Parham Alvani <parham.alvani@gmail.com>
2021-01-16 13:37:48 +0330

    feat: Better KVM Installation Script
    Former-commit-id: 9146ce72d3d11014d0053eee47dc15d14988db3c [formerly 24b3da222e1fb1ab47a4b69779d921258904a328] [formerly f941fd0794ccfb92f4b5f4f6027d63d122dd371c [formerly 2c192447b42484e770488705435095c696db7956 [formerly 2c192447b42484e770488705435095c696db7956 [formerly 2c192447b42484e770488705435095c696db7956 [formerly dfe55288d66a899ba9e0c20180158483dff5c01c]]]]]
    Former-commit-id: 56d99d7bd25afc39a92fba1669d48049ed793914 [formerly 5de43979a8f0efabf512876fb26d803f436d6766]
    Former-commit-id: 16abaac9d3754be567b337f00d45f0e4fb1cb220
    Former-commit-id: 4d4e184b87aaa2ddf76b63f80f83bb58327e87dc
    Former-commit-id: 19a760277ca659bc3dca1191423f98e44781d105
    Former-commit-id: 553570cfa49eb07e170116843f2db31bba1a806c

--- scripts/kvm.sh
+++ scripts/kvm.sh
@@ -22,8 +22,12 @@ main() {
         mkdir -p $HOME/kvm/base
         message "kvm" "create virtual machine disk folder"
         mkdir -p $HOME/kvm/vm
-        # fetch ubuntu 20.04 LTS image
-        wget -P $HOME/kvm/base https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+        message "kvm" "fetch ubuntu 20.04 LTS image"
+        [ -f  $HOME/kvm/base/focal-server-cloudimg-amd64.img ] || wget -P $HOME/kvm/base https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
 
         sudo pacman -Syu --needed --noconfirm cloud-image-utils
+
+        message "kvm" "user access for kvm and libvirt"
+        sudo usermod -aG libvirt $USER
+        sudo usermod -aG kvm $USER
 }
