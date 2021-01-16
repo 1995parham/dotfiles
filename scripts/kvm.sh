b519d6477ec68e41816e42737cddf7e0301926b7
Parham Alvani <parham.alvani@gmail.com>
2021-01-16 13:30:00 +0330

    feat: Improve KVM Installation Script
    Former-commit-id: 61018db5ddd90695d5c18ff97f3f5aa08f16b884 [formerly e80ffa65be4f42ad45fbab37c838dd76d511f20d] [formerly 9b82b4016e6512d96401acef841d7ec5d35ff677 [formerly 5d2d38e734ea19c31000b892153a23c0b56be2e9 [formerly 5d2d38e734ea19c31000b892153a23c0b56be2e9 [formerly 5d2d38e734ea19c31000b892153a23c0b56be2e9 [formerly f15b60eb150e53f11e5e2657931bb6010c9f14df]]]]]
    Former-commit-id: c2ca7541e60208ec9b029f623b20539452d5cac8 [formerly 739c6c302b5f590dc31f68e71d36eaa8eae4f8ee]
    Former-commit-id: 8dbd770b6e213b8a231581abf0c40da54ba8d9ec
    Former-commit-id: 834be00fd54db9075446cede006b407799bb83c4
    Former-commit-id: 1c9e5d78210c76349e0147aed6db18c2971bd893
    Former-commit-id: 4a36ba15bf64764ead4f5c66113bbce628c8957c

--- scripts/kvm.sh
+++ scripts/kvm.sh
@@ -17,4 +17,13 @@ main() {
         sudo pacman -Syu --needed --noconfirm libvirt
         sudo pacman -Syu --needed --noconfirm ebtables dnsmasq bridge-utils
         sudo pacman -Syu --needed --noconfirm virt-manager
+
+        message "kvm" "create base images folder"
+        mkdir -p $HOME/kvm/base
+        message "kvm" "create virtual machine disk folder"
+        mkdir -p $HOME/kvm/vm
+        # fetch ubuntu 20.04 LTS image
+        wget -P $HOME/kvm/base https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+
+        sudo pacman -Syu --needed --noconfirm cloud-image-utils
 }
