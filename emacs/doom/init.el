26108398ec8515f6872baa12615e9aed56232d4b
Parham Alvani <parham.alvani@gmail.com>
2021-02-10 20:55:52 +0330

    feat: Comment out the lsp from latex
    Former-commit-id: dfefaced482b0cd72fd3cf77dc5a5d5254b37135 [formerly 3d519822b5b066841e2f78077435530a55842449] [formerly e3e462ab77ce41e1359b8766b3164f6fb801f593 [formerly 60d9b28b3152a7fbf55b0634e1c5d439db336e0e [formerly 60d9b28b3152a7fbf55b0634e1c5d439db336e0e [formerly 60d9b28b3152a7fbf55b0634e1c5d439db336e0e [formerly 9320cacad1fe9b4e9135b392bb1aac39b4b75fe8]]]]]
    Former-commit-id: 61f5e9fd6ff2d19bb7c4db54088a16ad276366bf [formerly 81e45d621c0387128c27245d9c4069ad9f9d8ca4]
    Former-commit-id: 379f0d393bfaeecd26ee429ca3b998b1eac7779d
    Former-commit-id: 2ac9660b34efd5d70b0106fb0343fb0e03df4fdf
    Former-commit-id: 4127f3da677e251b8ca888386d472a54849bdbb0
    Former-commit-id: da41a409f8d5fe3910d5f0d665c3e6d993a0353c

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -137,7 +137,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +latexmk +lsp)        ; writing papers in Emacs has never been so fun
+       (latex +latexmk)        ; writing papers in Emacs has never been so fun, add +lsp if you want slow lsp
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
