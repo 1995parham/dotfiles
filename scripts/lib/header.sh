92ae797f6c5bb2d25ec51c7411ec1dc9f2712572
Parham Alvani <parham.alvani@gmail.com>
2021-03-24 22:47:07 +0430

    feat: Move Header into Its Script
    Former-commit-id: f737de84994928d6efc1bade5b0217de8d02bc06 [formerly 4ab159a312d69b1ab51f5fe4b27c62fd8060de82] [formerly 2bc8c762a1ae4c68deb374556757e9825358da3f [formerly 2679d62b455bab767deabb2c1c5ec6b29d107499 [formerly 2679d62b455bab767deabb2c1c5ec6b29d107499 [formerly 2679d62b455bab767deabb2c1c5ec6b29d107499 [formerly 9995a3514d06ee43e8b41d531bc15242a90579bb]]]]]
    Former-commit-id: 666ae53a168ae29b2323b160d2db4c28b29dffa4 [formerly f11de9bb5b70eb7832cd4e525b86772dde192288]
    Former-commit-id: 4c66ae83f6223acc5c7f35622de0110c82689bd3
    Former-commit-id: 3534698f0c34bb01d63070d3253af3d0d0745970
    Former-commit-id: 4bde8d182c18414abded5cd755c1f49196ae554c
    Former-commit-id: bd6bd61100b03939c3dbcbdda9fdf420df0d1ed9

---
+++ scripts/lib/header.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : header.sh
+#
+# [] Creation Date : 24-03-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+echo '
+     _       _    __ _ _
+  __| | ___ | |_ / _(_) | ___  ___
+ / _` |/ _ \| __| |_| | |/ _ \/ __|
+| (_| | (_) | |_|  _| | |  __/\__ \
+ \__,_|\___/ \__|_| |_|_|\___||___/
+
+   ____  _  ___   ___  ____                   _
+  / __ \/ |/ _ \ / _ \| ___| _ __   __ _ _ __| |__   __ _ _ __ ___
+ / / _` | | (_) | (_) |___ \| |_ \ / _| | |__| |_ \ / _| | |_ | _ \
+| | (_| | |\__, |\__, |___) | |_) | (_| | |  | | | | (_| | | | | | |
+ \ \__,_|_|  /_/   /_/|____/| .__/ \__,_|_|  |_| |_|\__,_|_| |_| |_|
+  \____/                    |_|
+
+These scripts and repository created for persoanl use but I have being tried to generalized it as much as I can.
+Feel free to report issues at https://github.com/1995parham/dotfiles/issues or mailto:parham.alvani@gmail.com.
+
+Best
+Parham Alvani
+'
