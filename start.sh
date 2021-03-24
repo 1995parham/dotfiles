cb0352c4a206311e85c8fe5dc2773a3baf68c92d
Parham Alvani <parham.alvani@gmail.com>
2021-03-24 21:32:19 +0430

    feat: Linker Library
    Former-commit-id: d23404c861b9c9e7385ea770e5da00cd636add90 [formerly 16dbe8152cf663a2b40e476c592663c32fa35975] [formerly 07ac17e382ff7c2fe8e58545b977fe4e2c55c642 [formerly c1f3f23a758f5cbac87254f7a0804df55f6305ae [formerly c1f3f23a758f5cbac87254f7a0804df55f6305ae [formerly c1f3f23a758f5cbac87254f7a0804df55f6305ae [formerly 2bf9c6e56636396dea795f16ca186698055a1165]]]]]
    Former-commit-id: 2e959ada458df9b1d2700adc009694e6c0d8c02e [formerly 35e8e2959c9fbb92c4c9dd5959927cf3bfc268bc]
    Former-commit-id: 15ff6f8b0bb810f5075a0f68280fc3423af783e3
    Former-commit-id: 52a882b985d0c1d5732d565aad1ff0ce56263573
    Former-commit-id: 8a6d72e800980f66813646e40bc6622bf3781414
    Former-commit-id: 442291561ebead8baf04464b83cc9aa526cf0228

--- start.sh
+++ start.sh
@@ -14,123 +14,7 @@ set -e
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $current_dir/scripts/lib/message.sh
 source $current_dir/scripts/lib/proxy.sh
-
-# Creates a config file that resides in the `.config` directory, and provides a soft link for it.
-# for better organization of the repository, modules can be gathered into a directory, in these cases
-# the third parameter is used.
-# parameter 1: module name - string
-# parameter 2: file name - string - optional
-# parameter 3: directory - string - optional
-configfile() {
-        local module=$1
-        local src_file=$2
-        local src_dir=$3
-
-        if [ ! -e "$HOME/.config" ]; then
-                mkdir "$HOME/.config"
-        fi
-
-        if [ ! -z $src_file ]; then
-                local src_path="$current_dir${src_dir:+/$src_dir}/$module/$src_file"
-                local dst_file="$module/$src_file"
-
-                if [ ! -d "$HOME/.config/$module" ]; then
-                        mkdir "$HOME/.config/$module"
-                fi
-        else
-                src_file=$module
-                local src_path="$current_dir${src_dir:+/$src_dir}/$module"
-                local dst_file="$module"
-        fi
-        local dst_path="$HOME/.config/$dst_file"
-
-        linker $module $src_path $dst_path
-}
-
-# Creates a config file that resides in the `.config` directory, and provides a soft link for it.
-# for better organization of the repository, modules can be gathered into a directory, in these cases
-# the third parameter is used.
-# parameter 1: module name - string
-# parameter 2: file name - string
-# parameter 3: directory - string - optional
-configrootfile() {
-        local module=$1
-        local src_file=$2
-        local src_dir=$3
-
-        if [ ! -e "$HOME/.config" ]; then
-                mkdir "$HOME/.config"
-        fi
-
-        if [ ! -z $src_file ]; then
-                local src_path="$current_dir${src_dir:+/$src_dir}/$module/$src_file"
-                local dst_file="$src_file"
-        fi
-        local dst_path="$HOME/.config/$dst_file"
-
-        linker $module $src_path $dst_path
-}
-
-# Creates a systemd file that resides in the `.config` directory, and provides a soft link for it.
-# for better organization of the repository, modules can be gathered into a directory, in these cases
-# the third parameter is used.
-# parameter 1: module name - string
-# parameter 2: file name - string
-# parameter 3: directory - string - optional
-configsystemd() {
-        local module=$1
-        local src_file=$2
-        local src_dir=$3
-
-        if [ ! -e "$HOME/.config/systemd/user" ]; then
-                mkdir -p "$HOME/.config/systemd/user"
-        fi
-
-        if [ ! -z $src_file ]; then
-                local src_path="$current_dir${src_dir:+/$src_dir}/$module/$src_file"
-                local dst_file="$src_file"
-        fi
-        local dst_path="$HOME/.config/systemd/user/$dst_file"
-
-        linker $module $src_path $dst_path
-}
-
-
-# linker
-# parameter 1: module name - string
-# parameter 2: source path - string
-# parameter 3: destination path - string
-linker() {
-        local module=$1
-        local src_path=$2
-        local dst_path=$3
-
-        local create_link=true
-
-        if [ -e $dst_path ] || [ -L $dst_path ]; then
-                message "$module" "$dst_path already existed"
-
-                if [[ $src_path = $(readlink $dst_path) ]]; then
-                        message "$module" "$dst_path is a correct link"
-                        create_link=false
-                        return
-                fi
-
-                read -p "[$module] do you want to remove $dst_path ?[Y/n] " -n 1 delete_confirm; echo
-
-                if [[ $delete_confirm == "Y" ]]; then
-                        rm -R $dst_path
-                        message "$module" "$dst_path was removed successfully"
-                else
-                        create_link=false
-                fi
-        fi
-
-        if $create_link; then
-                ln -s $src_path $dst_path
-                message "$module" "Symbolic link created successfully from $src_path to $dst_path"
-        fi
-}
+source $current_dir/scripts/lib/linker.sh
 
 # start.sh
 program_name=$0
@@ -144,7 +28,6 @@ _end() {
 
 _usage() {
 	echo "usage: $program_name [-p] [-h] [-f] script [script options]"
-	echo "  -p   use parham-usvs proxy"
 	echo "  -h   display help"
         echo "  -f   force"
 }
