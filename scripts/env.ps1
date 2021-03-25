617ecb7f0bcd524a3c9a7d1fa2deef10a048e5a2
Parham Alvani <parham.alvani@gmail.com>
2021-03-25 18:38:03 +0430

    feat: Update Env Script
    Former-commit-id: 590cb6a40f69747e7bb14befac8101faa7a39bbc [formerly a969cff8045f9cc36d1cbcbb5ff4d8b9a7bb2508] [formerly 49c9165632f571c2b1966630191b0bed5446f081 [formerly 2227c0287c3af0436706dc342105f09d707e1169 [formerly 2227c0287c3af0436706dc342105f09d707e1169 [formerly 2227c0287c3af0436706dc342105f09d707e1169 [formerly 1d864fdc7ae136632a5e6eba8aa412aaf0ba96f5]]]]]
    Former-commit-id: ffe17e8e9881c0e7993d0e7aed59ee09ceacb565 [formerly f7999c33bb722bd7e1678ea979b9dfa7a86247ce]
    Former-commit-id: f7f0535dd3c73797f2904a45943a97b82d8feac5
    Former-commit-id: ad6a71a93278e5c0da451bf9d9eff06654063bf7
    Former-commit-id: e9059be9180a039cf209bd2566ea5debcea79c8c
    Former-commit-id: 0b244da6d13d393d9409337f3d689103c7461c59

--- scripts/env.ps1
+++ scripts/env.ps1
@@ -1,13 +1,13 @@
 Write-Output "up and running windows"
 
 if (-not(Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Output "install scoop"
-    iwr -useb get.scoop.sh | iex
+    Write-Output "install scoop without sudo"
+    Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
 }
 
 scoop install sudo pshazz 7zip git aria2
 
 if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Output "install chocolatey"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Output "install chocolatey with sudo"
+    sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
