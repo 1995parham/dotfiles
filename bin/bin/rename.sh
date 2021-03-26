37792f48cd58fc5257be9c37994f535c3916126b
Parham Alvani <parham.alvani@gmail.com>
2021-03-27 02:11:32 +0430

    fix: Handles Space in Rename Script
    Former-commit-id: a33c2738fcb0ac2c683055971a77ca2a9f1750ed [formerly 61126c6dbdcbe1c2c1011fc62b5cea24456f01c3] [formerly 9759404d69f9be9ff1fa8a0db41a25c50ff71040 [formerly 180cc9dd60d7c20f1dfd604bc154ab2a15c63cfb [formerly 180cc9dd60d7c20f1dfd604bc154ab2a15c63cfb [formerly 180cc9dd60d7c20f1dfd604bc154ab2a15c63cfb [formerly e2412975ac202a95c274a1aaaadf7603be4b5332]]]]]
    Former-commit-id: 7a1900618b943abd122b26049035e410978108ec [formerly 90e27a74b1953d0429266b667c6d739fcce614d9]
    Former-commit-id: c88482877892d31549b6509357cae6db39d2a210
    Former-commit-id: 892b40bd1d8b72ad0cfe9ff7ea4536fa920c98ca
    Former-commit-id: 20a6c2c5c8b13219e5337e5d35c9e6a5621ddac7
    Former-commit-id: 81c482a4c00dd27573c65f02c066244bb1ec729d

--- bin/bin/rename.sh
+++ bin/bin/rename.sh
@@ -12,39 +12,43 @@ echo "structure your tv-series, at least trying to"
 echo "please note that only .mp4 and .mkv are supported"
 
 rename() {
-        local name=$1      # name of the series
-        local season=$2    # season of the series
-        local index=$3     # base episode index
-        local movies=${!4} # list of episodes (mkv or mp4)
-        local dry_run=${5:-"true"}
+	local name=$1      # name of the series
+	local season=$2    # season of the series
+	local index=$3     # base episode index
+	local movies=${!4} # list of episodes (mkv or mp4)
+	local dry_run=${5:-"true"}
 
-        echo "$name $season $dry_run"
+	echo "$name $season $dry_run"
 
-        episode=$index
-        for movie in $movies; do
-                episode=$((episode + 1))
+	episode=$index
+	for movie in $movies; do
+		episode=$((episode + 1))
 
-                if [[ $dry_run == true ]]; then
-                        echo "$movie -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
-                else
-                        mkdir -p "Season $season/Episode $episode"
-                        mv $movie "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
-                fi
-        done
+		if [[ $dry_run == true ]]; then
+			echo "$movie -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
+		else
+			mkdir -p "Season $season/Episode $episode"
+			mv $movie "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
+		fi
+	done
 
-        subs=$(find . -maxdepth 1 -name '*.srt' -type f -printf %P\\n | sort)
+	subs=$(find . -maxdepth 1 -name '*.srt' -type f -printf %P\\n | sort)
 
-        episode=$index
-        for sub in $subs; do
-                episode=$((episode + 1))
+	# https://unix.stackexchange.com/questions/9496/looping-through-files-with-spaces-in-the-names
+	OIFS="$IFS"
+	IFS=$'\n'
+	episode=$index
+	for sub in $subs; do
+		episode=$((episode + 1))
 
-                if [[ $dry_run == true ]]; then
-                        echo "$sub -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
-                else
-                        mkdir -p "Season $season/Episode $episode"
-                        mv $sub "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
-                fi
-        done
+		if [[ $dry_run == true ]]; then
+			echo "$sub -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
+		else
+			mkdir -p "Season $season/Episode $episode"
+			mv $sub "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
+		fi
+	done
+	IFS="$OIFS"
 }
 
 # https://stackoverflow.com/questions/11426529/reading-output-of-a-command-into-an-array-in-bash
@@ -58,8 +62,9 @@ read -p "Base Episode Index: " -i "1" -e index
 
 rename "$name" $season $((index - 1)) movies[@] true
 
-read -p "are you ready ?[Y/n] " -n 1 delete_confirm; echo
+read -p "are you ready ?[Y/n] " -n 1 delete_confirm
+echo
 
 if [[ $delete_confirm == "Y" ]]; then
-        rename "$name" $season $((index - 1)) movies[@] false
+	rename "$name" $season $((index - 1)) movies[@] false
 fi
