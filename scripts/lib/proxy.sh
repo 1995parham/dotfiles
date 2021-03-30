b7cc2158a4d11bca5dff40c00a37e55e5eb868b0
Parham Alvani <parham.alvani@gmail.com>
2021-03-30 23:40:19 +0430

    feat: Add Prompt for Proxy
    Former-commit-id: 7a51da6a2a72aaaeea3eccd7d5e6ba431e3b62fd [formerly 0e3ccab5741b72a68539b13f6544286d543b546b] [formerly 7da1e7d3d626e24aeb89ab0d4075e1da239a23c2 [formerly 2a508f35f3122e63534ae8e68a6032bd8246ed65 [formerly 2a508f35f3122e63534ae8e68a6032bd8246ed65 [formerly 2a508f35f3122e63534ae8e68a6032bd8246ed65 [formerly 39c68049fb739e6138adce38bb708d5f1163b94e]]]]]
    Former-commit-id: 900ee107351aac4b11bf8a5a0cb1091862a06781 [formerly 67a3c7b99d82d4b5babfa4a53f235fe3e1638afd]
    Former-commit-id: 873a52e74464672299f5f90e3cfaa5d96fa3f004
    Former-commit-id: c4d94a0a4267af5c6bec845816cdbf31586f3655
    Former-commit-id: 591ee54865456c78b2f1bba700d4a1612a6c703b
    Former-commit-id: d6dde3d59fd3e70bc8c9a4f127a424fed43bc3b0

--- scripts/lib/proxy.sh
+++ scripts/lib/proxy.sh
@@ -9,27 +9,38 @@
 # =======================================
 
 proxy_start() {
-        echo
-        curl --max-time 5 ipinfo.io/ip
+	echo -e "\e[38;5;46m[proxy] \e[38;5;202msetup proxy based on local http proxy that is setup by v2ray\e[39m"
+	echo -n -e "\e[38;5;46m[proxy] \e[38;5;202mpress enter to continue?\e[39m"
+	read accept
 
-        if [ $? -ne 0 ]; then
-                return 1
-        fi
+	if [[ "$accept" != "" ]]; then
+		return 0
+	fi
 
-        export {http,https,ftp}_proxy="http://127.0.0.1:1080"
-        alias sudo='sudo -E'
+	echo
+	curl --max-time 5 ipinfo.io/ip
 
-        echo
-        curl --max-time 5 ipinfo.io/ip
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
 
-        if [ $? -ne 0 ]; then
-                proxy_stop
-                return 1
-        fi
+	export ftp_proxy="http://127.0.0.1:1080"
+	export http_proxy="http://127.0.0.1:1080"
+	export https_proxy="http://127.0.0.1:1080"
+	alias sudo='sudo -E'
+
+	echo
+	curl --max-time 5 ipinfo.io/ip
+
+	if [ $? -ne 0 ]; then
+		proxy_stop
+		return 1
+	fi
 }
 
 proxy_stop() {
-        unset {http,https,ftp}_proxy
-        unalias sudo 2> /dev/null
-        echo "[proxy] All proxy script configuration are removed"
+	unset {http,https,ftp}_proxy
+	unalias sudo 2>/dev/null
+
+	echo -e "\e[38;5;46m[proxy] \e[38;5;202mall proxy script configuration are removed\e[39m"
 }
