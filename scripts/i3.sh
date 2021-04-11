c6629d40fea47be300bdbd198d05cc6a1bcb29e4
Parham Alvani <parham.alvani@gmail.com>
2021-04-11 18:49:27 +0430

    feat: Add Rofi
    Former-commit-id: 720861e602b2def7bb208633a0191a8208018e54 [formerly dbcb108aab27e0998f5b7d219748f6b01a34c105] [formerly 13725ac7d9ef3fa16bbe5e6b1a206701f190a7f5 [formerly d5bca391b0fcbb067102eaae9b5ab1538437c7d6 [formerly d5bca391b0fcbb067102eaae9b5ab1538437c7d6 [formerly d5bca391b0fcbb067102eaae9b5ab1538437c7d6 [formerly 69f90f85981298b154653843c5a37affef7c57ea]]]]]
    Former-commit-id: ff645edea2472d27359f8aa30947d4885843e94d [formerly 2a67e2593530ad78210fcece4f11c4bcfef8512b]
    Former-commit-id: b1b1a7192ad226638643307868814ad4003aef08
    Former-commit-id: 6b876d71e7b653ab44dc9347c05943adc9fa6dca
    Former-commit-id: 850b316c84de54a901b3827e9e32b21407ace45d
    Former-commit-id: e0b6b547bf1555f741e76e843c6dd59c9fc30f78

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -46,6 +46,9 @@ main_pacman() {
 	linker dmenu $current_dir/i3/dmenurc $HOME/.dmenurc
 	chmod +x $HOME/.dmenurc
 
+	msg 'configure rofi another application luncher'
+	configfile rofi "" i3
+
 	linker gtk-2.0 $current_dir/i3/gtk-2.0/gtkrc-2.0 $HOME/.gtkrc-2.0
 	configfile gtk-3.0 settings.ini i3
 
