091dbfd8fc8eb461db5e285c08b8ea02d6cea0d1
Parham Alvani <parham.alvani@gmail.com>
2021-04-11 17:03:26 +0430

    feat: Update Owghat Script
    Former-commit-id: f1b667cec5504666c75d846bd68347d1e5a2b501 [formerly 7aaa189ea241f0cc85187a4b715853b8a878d235] [formerly ff8bb6288222717b64e2db8d285bc33e4f181726 [formerly 257a55b11f932e693c61bff56ca87a6305ae256c [formerly 257a55b11f932e693c61bff56ca87a6305ae256c [formerly 257a55b11f932e693c61bff56ca87a6305ae256c [formerly 1125d7b2ed65971b430485ff5f257f39d79dcd10]]]]]
    Former-commit-id: c1bbf3ae330523ad32b5dbe44c18f6238b585465 [formerly 488c4e428b009ff1494fa7d8014ab20a658de554]
    Former-commit-id: bd92eaf5bba0090357754801a09d7c709aed5583
    Former-commit-id: 11f50e09c8db2e656e5539e709a450153da8140e
    Former-commit-id: 7a546769daa3e200121fa78c9ed69812c047e8cc
    Former-commit-id: 1fe9191ab1ef6bca9cc13d5354c7b319e6491f47

--- bin/bin/owghat.sh
+++ bin/bin/owghat.sh
@@ -8,4 +8,12 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 
-curl -Ls https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86 | jq .result
+url=https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86
+
+result=$(curl -Ls $url | jq .result)
+
+sobh=$(echo $result | jq .azan_sobh --raw-output)
+zohr=$(echo $result | jq .azan_zohr --raw-output)
+maghreb=$(echo $result | jq .azan_maghreb --raw-output)
+
+echo "$sobh -> $zohr -> $maghreb"
