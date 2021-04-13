24e24f7867832e2e3dc6d3e6ce87b53fefd0521d
Parham Alvani <parham.alvani@gmail.com>
2021-04-14 03:13:18 +0430

    fix: Correct Couple Script
    Former-commit-id: 7076b02841962b5565bec9eb09795678614d368f [formerly 966de75ccd8e8048e12ba99d1b02add9d4931285] [formerly 9fdc01a16834b484b30df7d4636cac5c3ec5bf3e [formerly b2f56e4d8d3f27a40dd748a0faf1931d3444bc4b [formerly b2f56e4d8d3f27a40dd748a0faf1931d3444bc4b [formerly b2f56e4d8d3f27a40dd748a0faf1931d3444bc4b [formerly d95ac582f07c3262a68f3091a6d898d7f6e23039]]]]]
    Former-commit-id: a60e42dd86f14656b83cece1e3cd9e49b28cb8ff [formerly 1bed2a266012661216406a5a0721f560dcce64cc]
    Former-commit-id: fbb960ae9a8ce9f842d42c3d2d792e3ba4853217
    Former-commit-id: df20057c4da7a8402791f6aab3ea056ec3d2c632
    Former-commit-id: 7ec2835854d0adfd512572ca6036f7c480987862
    Former-commit-id: 5dd788437de469f0b3649cd9c723543da0dd2ae6

--- i3/polybar/scripts/couple.sh
+++ i3/polybar/scripts/couple.sh
@@ -9,47 +9,52 @@
 # =======================================
 
 in_relationship() {
-	local since=$(date -d "13 feb 2020 22:26:00" "+%s")
-	local now=$(date -d "now" "+%s")
+	local since
+	since=$(date -d "13 feb 2020 22:26:00" "+%s")
+	local now
+	now=$(date -d "now" "+%s")
 
 	local minute=$((60))
-	local hour=$(($minute * 60))
-	local day=$(($hour * 24))
+	local hour=$((minute * 60))
+	local day=$((hour * 24))
 
-	local diff=$(($now - $since))
+	local diff=$((now - since))
 
-	local days=$(($diff / $day))
-	diff=$(($diff - $days * $day))
-	local hours=$(($diff / $hour))
-	diff=$(($diff - $hours * $hour))
-	local minutes=$(($diff / $minute))
+	local days=$((diff / day))
+	diff=$((diff - days * day))
+	local hours=$((diff / hour))
+	diff=$((diff - hours * hour))
+	local minutes=$((diff / minute))
 
 	echo $days days $hours hours $minutes minutes
 }
 
 to_birthday() {
 	local minute=$((60))
-	local hour=$(($minute * 60))
-	local day=$(($hour * 24))
+	local hour=$((minute * 60))
+	local day=$((hour * 24))
 
-	local this_year=$(date "+%Y")
-	local next_year=$(($this_year + 1))
+	local this_year
+	this_year=$(date "+%Y")
+	local next_year=$((this_year + 1))
 
-	local to=$(date -d "12 oct $this_year 19:20:00" "+%s")
-	local now=$(date -d "now" "+%s")
+	local to
+	to=$(date -d "12 oct $this_year 19:20:00" "+%s")
+	local now
+	now=$(date -d "now" "+%s")
 
-	local diff=$(($to - $now))
+	local diff=$((to - now))
 
 	if [ $diff -lt 0 ]; then
 		to=$(date -d "12 oct $next_year 19:20:00" "+%s")
-		diff=$(($to - $now))
+		diff=$((to - now))
 	fi
 
-	local days=$(($diff / $day))
-	diff=$(($diff - $days * $day))
-	local hours=$(($diff / $hour))
-	diff=$(($diff - $hours * $hour))
-	local minutes=$(($diff / $minute))
+	local days=$((diff / day))
+	diff=$((diff - days * day))
+	local hours=$((diff / hour))
+	diff=$((diff - hours * hour))
+	local minutes=$((diff / minute))
 
 	echo $days days $hours hours $minutes minutes
 }
