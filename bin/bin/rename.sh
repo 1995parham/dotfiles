eba7916243157b967d219ce3a400bb179cfb729a
Parham Alvani <parham.alvani@gmail.com>
2021-04-14 01:27:08 +0430

    feat: Improve Rename Script
    Former-commit-id: b5a636a10f61211f5fa2693eae004ca5a7ab8520 [formerly 4ebc777c69225f397f72367d44bc1122aa7549dd] [formerly 64a57d212a15694ed6c2d58d2c2ffe7ea1ecb9fb [formerly 544dac5fcf1d4d3a0ae0c3116f44a1b955cd93d9 [formerly 544dac5fcf1d4d3a0ae0c3116f44a1b955cd93d9 [formerly 544dac5fcf1d4d3a0ae0c3116f44a1b955cd93d9 [formerly ef764b4cc9f9bbfa61a4fd8b5c383ae2c54e88cc]]]]]
    Former-commit-id: ac6a60a624389ea85eddadbe0ab6ec10fc070840 [formerly c32bc20e40dd02acdbb3e5671040551a813f0191]
    Former-commit-id: a15d389c954ea2879016eb2f7dd8a3d5c6b77ce5
    Former-commit-id: 23da7897d4ce603460a0ccc9ee4e13c2a08128ec
    Former-commit-id: 4d37391662695c4e25d752e80e522f5286eaa464
    Former-commit-id: 3d6a048da5e607e35a0379526382a04fb1e0b63b

--- bin/bin/rename.sh
+++ bin/bin/rename.sh
@@ -12,23 +12,28 @@ echo "structure your tv-series, at least trying to"
 echo "please note that only .mp4 and .mkv are supported"
 
 rename() {
-	local name=$1      # name of the series
-	local season=$2    # season of the series
-	local index=$3     # base episode index
-	local movies=${!4} # list of episodes (mkv or mp4)
+	local name=$1   # name of the series
+	local season=$2 # season of the series
+	local index=$3  # base episode index
 	local dry_run=${5:-"true"}
 
 	echo "$name $season $dry_run"
 
+	movies=$(find . -maxdepth 1 -name '*.mkv' -type f -printf %P\\n -o -name '*.mp4' -type f -printf %P\\n | sort)
+
+	# https://unix.stackexchange.com/questions/9496/looping-through-files-with-spaces-in-the-names
+	OIFS="$IFS"
+	IFS=$'\n'
+	episode=$index
 	episode=$index
 	for movie in $movies; do
 		episode=$((episode + 1))
 
 		if [[ $dry_run == true ]]; then
-			echo "$movie -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
+			echo "$movie -> Season $season/Episode $episode/$name S$(printf %02d "$season")E$(printf %02d "$episode").${movie##*.}"
 		else
 			mkdir -p "Season $season/Episode $episode"
-			mv $movie "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).${movie##*.}"
+			mv "$movie" "Season $season/Episode $episode/$name S$(printf %02d "$season")E$(printf %02d "$episode").${movie##*.}"
 		fi
 	done
 
@@ -42,29 +47,28 @@ rename() {
 		episode=$((episode + 1))
 
 		if [[ $dry_run == true ]]; then
-			echo "$sub -> Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
+			echo "$sub -> Season $season/Episode $episode/$name S$(printf %02d "$season")E$(printf %02d "$episode").srt"
 		else
 			mkdir -p "Season $season/Episode $episode"
-			mv $sub "Season $season/Episode $episode/$name S$(printf %02d $season)E$(printf %02d $episode).srt"
+			mv "$sub" "Season $season/Episode $episode/$name S$(printf %02d "$season")E$(printf %02d "$episode").srt"
 		fi
 	done
 	IFS="$OIFS"
 }
 
-# https://stackoverflow.com/questions/11426529/reading-output-of-a-command-into-an-array-in-bash
-mapfile -t movies < <(find . -maxdepth 1 -name '*.mkv' -type f -printf %P\\n -o -name '*.mp4' -type f -printf %P\\n | sort)
+sample=$(find . -maxdepth 1 -name '*.mkv' -type f -printf %P\\n -o -name '*.mp4' -type f -printf %P\\n | sort | head -1)
 
-read -p "TV Series Name: " -i "${movies[0]}" -e name
+read -r -p "TV Series Name: " -i "$sample" -e name
 
-read -p "TV Series Season: " season
+read -r -p "TV Series Season: " season
 
-read -p "Base Episode Index: " -i "1" -e index
+read -r -p "Base Episode Index: " -i "1" -e index
 
-rename "$name" $season $((index - 1)) movies[@] true
+rename "$name" "$season" $((index - 1)) true
 
-read -p "are you ready ?[Y/n] " -n 1 delete_confirm
+read -r -p "are you ready ?[Y/n] " -n 1 delete_confirm
 echo
 
 if [[ $delete_confirm == "Y" ]]; then
-	rename "$name" $season $((index - 1)) movies[@] false
+	rename "$name" "$season" $((index - 1)) false
 fi
