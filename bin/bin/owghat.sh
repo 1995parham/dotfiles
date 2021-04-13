28b226cb11ffc2ec69a56fbb8851f84dfe3c6e6f
Parham Alvani <parham.alvani@gmail.com>
2021-04-14 01:27:17 +0430

    fix: Correct Owghat Script
    Former-commit-id: 907bd8d83ef4dd808321c128f75a37b439a09bc1 [formerly b9e06a1ba82758d6cb24ed26507f4e5af48f1e5b] [formerly 8230d75be27d146c0c2c0be56298fc6a5983391b [formerly 3bf09b308dd5add65278797458cf11a3dfea5b2c [formerly 3bf09b308dd5add65278797458cf11a3dfea5b2c [formerly 3bf09b308dd5add65278797458cf11a3dfea5b2c [formerly 15eaadd0b6eb5804a37ae686e61bdd6b7735454c]]]]]
    Former-commit-id: 50e6fe7326fc7e3a6ba0cfb60379e3dcbf596bb1 [formerly b0e63f6072c0dbf1390153fbbe437b3474e3c7eb]
    Former-commit-id: 5d22cbdeb43ca60d466d0af91d78bc4d838a185b
    Former-commit-id: c697abcf6b4f01ddb3867ffc50b87bb1e6d79b7d
    Former-commit-id: a0c126123354bb57b97498d0b63d19bad62b298b
    Former-commit-id: 6e92a95de3798c1c926313113c1aac737b2d6a2e

--- bin/bin/owghat.sh
+++ bin/bin/owghat.sh
@@ -8,12 +8,12 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 
-url=https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86
+url="https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86"
 
-result=$(curl -Ls $url | jq .result)
+result=$(curl -Ls "$url" | jq .result)
 
-sobh=$(echo $result | jq .azan_sobh --raw-output)
-zohr=$(echo $result | jq .azan_zohr --raw-output)
-maghreb=$(echo $result | jq .azan_maghreb --raw-output)
+sobh=$(echo "$result" | jq .azan_sobh --raw-output)
+zohr=$(echo "$result" | jq .azan_zohr --raw-output)
+maghreb=$(echo "$result" | jq .azan_maghreb --raw-output)
 
 echo "$sobh -> $zohr -> $maghreb"
