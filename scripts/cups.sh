fab9b6503459ccdb435a7ef48d2fa7d05745cbcd
Parham Alvani <parham.alvani@gmail.com>
2021-04-18 18:27:32 +0430

    feat: Update CUPS Script
    Former-commit-id: ae6e4cb4549b3279cefb1ab2296486beaa7c0708 [formerly 7b1acdc176275b2fb5ce91d773a8da59b80a9be4] [formerly 9441c6e560009e4f76bbfc2c1846164a5f91b5f8 [formerly 00287aa4c7bfab4e1c06af113222c0a9ca40a1d9 [formerly 00287aa4c7bfab4e1c06af113222c0a9ca40a1d9 [formerly 00287aa4c7bfab4e1c06af113222c0a9ca40a1d9 [formerly 576e164a56efce62145d6396d15f7941264bf05a]]]]]
    Former-commit-id: 6d01739d202077d124091758e635bd858b9cabac [formerly 870d0b8f38f27fe669bce755ef7139de49b4f8e6]
    Former-commit-id: 921a6aea8b9b48ebbfd3a4055eff9f6f441d1254
    Former-commit-id: c9828c8bee158cca84679d5117b099fd6e9e1553
    Former-commit-id: ce3517af00e24b4487135c9964f3303daa9b2ec8
    Former-commit-id: 739f35473b684af4d99f874165b9fd4f769ff317

--- scripts/cups.sh
+++ scripts/cups.sh
@@ -15,6 +15,7 @@ usage() {
 main_pacman() {
 	sudo pacman -Syu --needed --noconfirm cups-pdf cups cups-filters cups-pk-helper
 	sudo sed 's#^\#Out.*#Out \${HOME}/Downloads#g' -i /etc/cups/cups-pdf.conf
+	sudo systemctl start cups
 	sudo lpadmin -p Virtual_PDF_Printer -v cups-pdf:/ -E -m CUPS-PDF_noopt.ppd
 }
 
