956df829cd1125d264974e9e108b8ddb2d65634e
Parham Alvani <parham.alvani@gmail.com>
2021-04-20 02:40:23 +0430

    feat: Add Texlive Installation for Windows

--- scripts/texlive.psm1
+++ scripts/texlive.psm1
@@ -1,8 +1,13 @@
 New-Module -Name texlive -ScriptBlock {
     function Install-Main {
         Write-Output "installing texlive on windows with its official installer"
-        New-Item -Type Directory texlive-install
-        Invoke-WebRequest http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe -OutFile texlive-install/install-tl-windows.exe
-        ./install-tl-windows.exe
+
+        
+        if (-not(Test-Path -Path ./texlive-installer)) {
+            New-Item -Type Directory texlive-installer
+            Invoke-WebRequest http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe -OutFile texlive-installer/install-tl-windows.exe
+        }
+
+        ./texlive-installer/install-tl-windows.exe
     }
 }
