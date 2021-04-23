9732941b43b0a89fcf1e84037306a05fc4912862
Parham Alvani <parham.alvani@gmail.com>
2021-04-23 19:36:28 +0430

    feat: Add Support for guake
    Former-commit-id: c25edcb0cef5eb4b62224be2ca3a8435e418abb9 [formerly 98b15a9cc6f63296c76fd7fac2b1c61507af1053] [formerly 3e54c34a9f3b3ce516051f5b6b0353cc9c78709f [formerly be857930514f86ebfd5fc712dff59c5349d72f05 [formerly be857930514f86ebfd5fc712dff59c5349d72f05 [formerly be857930514f86ebfd5fc712dff59c5349d72f05 [formerly 30a05f1f9794c64429f40764e9ce08a07fa3ddb8]]]]]
    Former-commit-id: bc1c35e6e03f4d691344f07a040ad3a9a114b2ac [formerly bbf7ee15be7cc0256bd9218461486416f946edd6]
    Former-commit-id: b5a865b5bcee0a6ab60cb3a1393f8bf998b3b413
    Former-commit-id: a8cf352bf0c76b9fe2afa8a4a74956ffb145a945
    Former-commit-id: 8fc15af7b382ff190c28b6eaa3067a502b6ec612
    Former-commit-id: a57cb40c0f2cba88e4f06714a054476a80230f89

---
+++ scripts/guake.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : guake.sh
+#
+# [] Creation Date : 23-04-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo -n "guake drop-in terminal for gnome-based environments"
+}
+
+main_apt() {
+	sudo apt install guake
+}
+
+main_pacman() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_brew() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main() {
+	configsystemd guake guake.service
+
+	systemctl --user enable guake.service --nwo
+}
