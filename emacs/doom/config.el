aa09bac42a691e3bfe985707c99fdca3e5fbc423
Parham Alvani <parham.alvani@gmail.com>
2021-04-27 02:52:14 +0430

    feat: Change Doom Font Size
    Former-commit-id: cca6ba6f310ba02a0b1816e6c0eac0096d0ecfb7 [formerly 4fbff51f0632ee7a96851f42bb489d9bc60ce05e] [formerly aa49e0d573b4879f96e6ddddc8950ec96a3c6996 [formerly a49e309b7156f54de4829692d96f85a31f3af55d [formerly a49e309b7156f54de4829692d96f85a31f3af55d [formerly a49e309b7156f54de4829692d96f85a31f3af55d [formerly ecf4d603a2216e70762e017a32c50d2472a88d3f]]]]]
    Former-commit-id: 039ae5fa9e2cb959eb1e8069e2bf3be6ea3209d3 [formerly 9bf594c94039f37db527837053e15506077ee98b]
    Former-commit-id: 2a69fdefebd2fcc3e42d4049f44ed16e9bbff37a
    Former-commit-id: 6aa68d7c3cccbe24173c721cae6413a500f70855
    Former-commit-id: fdb39268c1d05413e1986c2dea10fecb7d18b02a
    Former-commit-id: 696a75ad25183562219ab14f7370ba4edad92b55

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -30,8 +30,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :size 18)
-       doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 18))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 16)
+       doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
