6ed50dfd2c82d4116b2d2629d73f5a08fa6006e1
Parham Alvani <parham.alvani@gmail.com>
2021-04-27 09:25:47 +0430

    feat: Enable Unicode on Emacs
    Former-commit-id: dfdbae8587c8ff0ed50db8fb4073bb3102715cfd [formerly e48433e4b82b49fe48d1fc5ba599793393d8b245] [formerly ee6ed8447dd8d0a945559e79d4543f91e37180f6 [formerly 12aaa2f8bd0388e9a5a6e162e173838bc8a78412 [formerly 12aaa2f8bd0388e9a5a6e162e173838bc8a78412 [formerly 12aaa2f8bd0388e9a5a6e162e173838bc8a78412 [formerly 238b5fbaf0e6c0c0231e4f6ff64fce421529855d]]]]]
    Former-commit-id: 83070dc92353780dda826c1af9e76b247272d5cd [formerly b4b9ef2be1ddd8e85f5ecb3e405f4e93d8a7a552]
    Former-commit-id: 99fc2ab52b11800c202dc91b322459bf43c301a1
    Former-commit-id: 2caeb139a36367d472e73112be2daeb5ec079e4e
    Former-commit-id: 9366946e7d4880f6ad87e6910f77ca5b9bb1a3e0
    Former-commit-id: f30deb646c285f2b1407bb94834330e301ddc964

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -44,7 +44,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
