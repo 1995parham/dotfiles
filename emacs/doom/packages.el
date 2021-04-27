6108cb8fef196d2d1994287552f0333313173666
Parham Alvani <parham.alvani@gmail.com>
2021-04-27 12:01:20 +0430

    feat: Add emacs-bib Package
    Former-commit-id: a20a614bde2781ddcf691130d56f415df5a5f088 [formerly 0d54c31dc8a90e09da284fae5df206ac09733df9] [formerly d32a9eeda2954fa18edf3ccb1a0317b4c3f79003 [formerly e88cb26dee6b6c086ec4cd24d944f3cecaea31c3 [formerly e88cb26dee6b6c086ec4cd24d944f3cecaea31c3 [formerly e88cb26dee6b6c086ec4cd24d944f3cecaea31c3 [formerly 1377617fb46401c79e99696f45de3f4c36cd2b99]]]]]
    Former-commit-id: 33fe03efbe1bf5f9552a63542ab21ddf375f6b59 [formerly 3fa74ebd21db05f40a1b6b965b78f7a5cd1aff9a]
    Former-commit-id: 82c2192550aa56a87be8c4a7b68e1feb41657554
    Former-commit-id: 46e26d229049f931ae58f38311a27db7f46451ea
    Former-commit-id: 6d31f16a1aee1d1797052ec785fc853cd07e58dc
    Former-commit-id: d22ed52a4343d189cf9178a4a2f4242a7542692c

--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -8,6 +8,7 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
+(package! ivy-bibtex)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
