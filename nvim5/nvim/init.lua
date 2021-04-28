e11bda49665cf78f8cee934dbc7a60fa408463e5
Parham Alvani <parham.alvani@gmail.com>
2021-04-28 20:59:58 +0430

    feat: Format Lua Codes
    Former-commit-id: 49ac735276f90b12c68efa6a9875150412ceae2d [formerly 53a5ad0140e7dfadb146dbcecff2e1743bfd9eca] [formerly 08eeed85ca8c7d91b49a8a8e7e0c86cc5061d5e9 [formerly 467d0d81c66273ffd790c2d98f2ca5a28cda5091 [formerly 467d0d81c66273ffd790c2d98f2ca5a28cda5091 [formerly 467d0d81c66273ffd790c2d98f2ca5a28cda5091 [formerly e31f100a8d4a8cda0628595978b2e3e619f7df22]]]]]
    Former-commit-id: 34008d63c28e8158f6b21153c1f8c2b29ae05c19 [formerly 3eead80d8ee1706e3b4a766a0313de2ad86705af]
    Former-commit-id: d47b9f11fe821729ddf8351306a75d9d1fc85d54
    Former-commit-id: fec379131cf98a7f9d77028b883ea904de081307
    Former-commit-id: f6ac07a56cb78377938002ff31b59ce094457389
    Former-commit-id: e13354228f146a9b5bda20514e393201d0578717

--- nvim5/nvim/init.lua
+++ nvim5/nvim/init.lua
@@ -2,7 +2,6 @@ require('setting')
 require('plugin')
 require('key')
 
-vim.api.nvim_exec(
-[[
+vim.api.nvim_exec([[
 	autocmd BufNewFile,BufRead ~/.config/i3/config set filetype=i3config
 ]], false)
--- nvim5/nvim/lua/key.lua
+++ nvim5/nvim/lua/key.lua
@@ -17,24 +17,27 @@ vim.api.nvim_set_keymap('n', '<C-w>p', ':tabnext<CR>', {})
 vim.api.nvim_set_keymap('n', '<Leader>tc', ':tabnew<CR>', {})
 
 -- buffers
-vim.api.nvim_set_keymap('n', '<Leader>bn', ':bnext<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>bp', ':bprevious<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>bk', ':bdelete<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>bb', ':BufferPick<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>bn', ':bnext<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>bp', ':bprevious<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>bk', ':bdelete<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>bb', ':BufferPick<CR>', {silent = true})
 
 -- terminal
-vim.api.nvim_set_keymap('n', '<Leader>ot', ':terminal<CR>', { silent = true })
-vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ot', ':terminal<CR>', {silent = true})
+vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', {silent = true})
 
 -- fzf.vim
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':Files<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':Files<CR>', {silent = true})
 
 -- git
-vim.api.nvim_set_keymap('n', '<Leader>gg', ':Git<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>gg', ':Git<CR>', {silent = true})
 
 -- coc.vim
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gv', '<Plug>(coc-type-definition)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>cc', ':<C-u>CocList commands<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
+vim.api.nvim_set_keymap('n', 'gv', '<Plug>(coc-type-definition)',
+                        {silent = true})
+vim.api
+    .nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {silent = true})
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>cc', ':<C-u>CocList commands<CR>',
+                        {silent = true})
