c10a4e2d4fb077a2ab8af9725a1374260e93b036
Parham Alvani <parham.alvani@gmail.com>
2021-04-29 22:59:08 +0430

    feat: Use Doom biblio module.
    Former-commit-id: 6bc6a185c78aea2ce50910c2622880d5cac6e2ac [formerly a5f73242b19091a4754e2df1754a95d59f1336a8] [formerly 62364a0331cd7016263ca0266900a2a62cea62ed [formerly 3603ec4b628d7aa06b2cfc60326b246c47e0db84 [formerly 3603ec4b628d7aa06b2cfc60326b246c47e0db84 [formerly 3603ec4b628d7aa06b2cfc60326b246c47e0db84 [formerly 2c0534c17912c761e3f88844b3a24fb1cfe1a027]]]]]
    Former-commit-id: dc0320b6eab26546cc69f5eaa3625de6643e3897 [formerly 422930b79242532853e6a8f66b19c732cd1f63df]
    Former-commit-id: 5289e163c42eb347a461f25128a5f76f4d7f4ce1
    Former-commit-id: 3b2d4c10091d7ffbc820940d4febe90c83d8fa2b
    Former-commit-id: 5cdc19d176bfaad16387cd799bf6788c1448cc95
    Former-commit-id: b92be298863c4075b23f49d7fcbd422d663f2b4d

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -104,6 +104,7 @@
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
+       biblio
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
