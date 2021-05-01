d2419ad27bafb6cd58ce804c612df994139ddf97
Parham Alvani <parham.alvani@gmail.com>
2021-05-01 20:44:30 +0430

    feat: Add Tree Plugin
    Former-commit-id: 2290673114c5a4872be11dec4e2f0d0c25b0cd34 [formerly dec62fae4f2e2800e1340a5b645aad445b55411b] [formerly 075e0cc7d00b922277d13719f4c8f6855a1f2921 [formerly 217976861479658851c90f4f88460f7c7b4942a0 [formerly 217976861479658851c90f4f88460f7c7b4942a0 [formerly 217976861479658851c90f4f88460f7c7b4942a0 [formerly 040c40f900b77dabbf94a56db2ad6f834a3b0740]]]]]
    Former-commit-id: 9e3b0172e04f9fefc07ce298f444017af761b6c2 [formerly b9cd111e0f86eb356886bde942b7f40f1b42f576]
    Former-commit-id: bf0ae8b2f4f7f90c92d7d64875a71a5d5e9a1ae6
    Former-commit-id: ca2ffb38c8dcd33b0a7772708f98902c1b5a8c80
    Former-commit-id: 261a61b48615caa56faa6908565a10b681b1dcb0
    Former-commit-id: 370c95593e85b334ce6bd0125cdfebc7e68e8add

--- nvim5/nvim/lua/plugin.lua
+++ nvim5/nvim/lua/plugin.lua
@@ -101,6 +101,12 @@ return require('packer').startup(function()
 		after = { 'coc.nvim', 'nvim-treesitter' },
 	}
 
+	use {
+		'kyazdani42/nvim-tree.lua',
+		config = function() require('tree') end,
+		requires = {'kyazdani42/nvim-web-devicons'},
+	}
+
 	-- whichkey is a lua plugin for Neovim 0.5 that displays a popup with
 	-- possible keybindings of the command you started typing.
 	use {
