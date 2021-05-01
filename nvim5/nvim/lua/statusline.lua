284f3bb77e38b313ad480cf5e981af4972a6933a
Parham Alvani <parham.alvani@gmail.com>
2021-05-01 09:11:44 +0430

    feat: Show Treesitter into Statusline
    Former-commit-id: caa3b86ea0b36ddd031e22bce4f68fbfc64cfe4c [formerly d232f6a291715f55e32a1425b1056cad1ab27d45] [formerly 4d90a28a8ab281b9f22f6b07039dbeda947fdb64 [formerly ed8cc9fae0eeca10e9a2fd95a91818fdfbbc280b [formerly ed8cc9fae0eeca10e9a2fd95a91818fdfbbc280b [formerly ed8cc9fae0eeca10e9a2fd95a91818fdfbbc280b [formerly a97dab493266f31309d550ba5e564e20b86a4cfe]]]]]
    Former-commit-id: f3b28972ce216232cf35d5d22588af1536205295 [formerly 153161813ee9b2ce47577013ca06eef4f24e1f85]
    Former-commit-id: 03c097729664af93ee6a037b030f5b4a580bfb81
    Former-commit-id: 713751d8cea0b0a1bbd82f80235f9bf15209e40c
    Former-commit-id: 175781d6a3d92b0b7efb2040ca8a4ee1911807b4
    Former-commit-id: cb23d47696f61638b1489ccbb24d3c2785d7132e

--- nvim5/nvim/lua/statusline.lua
+++ nvim5/nvim/lua/statusline.lua
@@ -1,191 +1,209 @@
 local gl = require('galaxyline')
+local nt = require('nvim-treesitter')
 local gls = gl.section
 
 local colors = {
-    bg = '#525252',
-    black = '#2c2c2c',
-    yellow = '#fabd2f',
-    cyan = '#00e6e6',
-    darkblue = '#081633',
-    green = '#afd700',
-    orange = '#FF8800',
-    purple = '#5d4d7a',
-    magenta = '#d16d9e',
-    grey = '#c0c0c0',
-    blue = '#0087d7',
-    red = '#ec5f67'
+	bg = '#525252',
+	black = '#2c2c2c',
+	yellow = '#fabd2f',
+	cyan = '#00e6e6',
+	darkblue = '#081633',
+	green = '#afd700',
+	orange = '#FF8800',
+	purple = '#5d4d7a',
+	magenta = '#d16d9e',
+	grey = '#c0c0c0',
+	blue = '#0087d7',
+	red = '#ec5f67'
 }
 
 local buffer_not_empty = function()
-    if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then return true end
-    return false
+	if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then return true end
+	return false
 end
 
 gls.left[1] = {
-    FirstElement = {
-        provider = function() return '' end,
-        highlight = {colors.blue, colors.yellow}
-    }
+	FirstElement = {
+		provider = function() return '' end,
+		highlight = {colors.blue, colors.yellow}
+	}
 }
 
 gls.left[2] = {
-    ViMode = {
-        provider = function()
-            local alias = {
-                n = 'NORMAL',
-                i = 'INSERT',
-                c = 'COMMAND',
-                v = 'VISUAL',
-                V = 'VISUAL LINE',
-                [''] = 'VISUAL BLOCK'
-            }
-            return alias[vim.fn.mode()]
-        end,
-        separator = '|',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.orange, colors.bg, 'bold'}
-    }
+	ViMode = {
+		provider = function()
+			local alias = {
+				n = 'NORMAL',
+				i = 'INSERT',
+				c = 'COMMAND',
+				v = 'VISUAL',
+				V = 'VISUAL LINE',
+				[''] = 'VISUAL BLOCK'
+			}
+			return alias[vim.fn.mode()]
+		end,
+		separator = '|',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.orange, colors.bg, 'bold'}
+	}
 }
 
 gls.left[3] = {
-    FileIcon = {
-        provider = 'FileIcon',
-        condition = buffer_not_empty,
-        highlight = {
-            require('galaxyline.provider_fileinfo').get_file_icon_color,
-            colors.bg
-        }
-    }
+	FileIcon = {
+		provider = 'FileIcon',
+		condition = buffer_not_empty,
+		highlight = {
+			require('galaxyline.provider_fileinfo').get_file_icon_color,
+			colors.bg
+		}
+	}
 }
 
 gls.left[4] = {
-    FileName = {
-        provider = {'FileName', 'FileSize'},
-        condition = buffer_not_empty,
-        separator = '|',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.green, colors.bg}
-    }
+	FileName = {
+		provider = {'FileName', 'FileSize'},
+		condition = buffer_not_empty,
+		separator = '|',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.green, colors.bg}
+	}
 }
 
 gls.left[5] = {
-    GitIcon = {
-        provider = function() return '   ' end,
-        condition = buffer_not_empty,
-        highlight = {colors.orange, colors.purple}
-    }
+	GitIcon = {
+		provider = function() return '   ' end,
+		condition = buffer_not_empty,
+		highlight = {colors.orange, colors.purple}
+	}
 }
 
 gls.left[6] = {
-    GitBranch = {
-        provider = 'GitBranch',
-        condition = buffer_not_empty,
-        highlight = {colors.grey, colors.purple}
-    }
+	GitBranch = {
+		provider = 'GitBranch',
+		condition = buffer_not_empty,
+		highlight = {colors.grey, colors.purple}
+	}
 }
 
 local checkwidth = function()
-    local squeeze_width = vim.fn.winwidth(0) / 2
-    if squeeze_width > 40 then return true end
-    return false
+	local squeeze_width = vim.fn.winwidth(0) / 2
+	if squeeze_width > 40 then return true end
+	return false
 end
 
 gls.left[7] = {
-    DiffAdd = {
-        provider = 'DiffAdd',
-        condition = checkwidth,
-        icon = ' + ',
-        highlight = {colors.green, colors.purple}
-    }
+	DiffAdd = {
+		provider = 'DiffAdd',
+		condition = checkwidth,
+		icon = ' + ',
+		highlight = {colors.green, colors.purple}
+	}
 }
 
 gls.left[8] = {
-    DiffModified = {
-        provider = 'DiffModified',
-        condition = checkwidth,
-        icon = ' ~ ',
-        highlight = {colors.orange, colors.purple}
-    }
+	DiffModified = {
+		provider = 'DiffModified',
+		condition = checkwidth,
+		icon = ' ~ ',
+		highlight = {colors.orange, colors.purple}
+	}
 }
 
 gls.left[9] = {
-    DiffRemove = {
-        provider = 'DiffRemove',
-        condition = checkwidth,
-        icon = ' - ',
-        highlight = {colors.red, colors.purple}
-    }
+	DiffRemove = {
+		provider = 'DiffRemove',
+		condition = checkwidth,
+		icon = ' - ',
+		highlight = {colors.red, colors.purple}
+	}
 }
 
 gls.left[10] = {
-    LeftEnd = {
-        provider = function() return '|' end,
-        separator = '|',
-        separator_highlight = {colors.purple, colors.bg},
-        highlight = {colors.purple, colors.purple}
-    }
+	LeftEnd = {
+		provider = function() return '|' end,
+		separator = '|',
+		separator_highlight = {colors.purple, colors.bg},
+		highlight = {colors.purple, colors.purple}
+	}
 }
 
 gls.left[11] = {
-    DiagnosticError = {
-        provider = 'DiagnosticError',
-        icon = '  ',
-        highlight = {colors.red, colors.bg}
-    }
+	DiagnosticError = {
+		provider = 'DiagnosticError',
+		icon = '  ',
+		highlight = {colors.red, colors.bg}
+	}
 }
 
 gls.left[13] = {
-    DiagnosticWarn = {
-        provider = 'DiagnosticWarn',
-        icon = '  ',
-        highlight = {colors.blue, colors.bg}
-    }
+	DiagnosticWarn = {
+		provider = 'DiagnosticWarn',
+		icon = '  ',
+		highlight = {colors.blue, colors.bg}
+	}
 }
 
 gls.right[1] = {
-    FileFormat = {
-        provider = 'FileFormat',
-        separator = ' | ',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.cyan, colors.bg}
-    }
+	FileFormat = {
+		provider = 'FileFormat',
+		separator = ' | ',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.cyan, colors.bg}
+	}
 }
 
 gls.right[2] = {
-    LineInfo = {
-        provider = 'LineColumn',
-        separator = ' | ',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.grey, colors.bg}
-    }
+	LineInfo = {
+		provider = 'LineColumn',
+		separator = ' | ',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.grey, colors.bg}
+	}
 }
 
 gls.right[3] = {
-    PerCent = {
-        provider = 'LinePercent',
-        separator = '|',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.grey, colors.bg}
-    }
+	PerCent = {
+		provider = 'LinePercent',
+		separator = '|',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.grey, colors.bg}
+	}
 }
 gls.right[4] = {
-    ScrollBar = {provider = 'ScrollBar', highlight = {colors.yellow, colors.bg}}
+	ScrollBar = {provider = 'ScrollBar', highlight = {colors.yellow, colors.bg}}
 }
 
 gls.right[5] = {
-    BufferType = {
-        provider = 'FileTypeName',
-        separator = '|',
-        separator_highlight = {colors.black, colors.bg},
-        highlight = {colors.grey, colors.bg}
-    }
+	BufferType = {
+		provider = 'FileTypeName',
+		separator = '|',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.grey, colors.bg}
+	}
 }
 
+gls.right[6] = {
+	TreeSitter = {
+		provider = function()
+			return nt.statusline({
+				indicator_size = 100,
+				type_patterns = {'class', 'function', 'method'},
+				transform_fn = function(line) return line:gsub('%s*[%[%(%{]*%s*$', '') end,
+				separator = ' -> '
+			})
+		end,
+		separator = '|',
+		separator_highlight = {colors.black, colors.bg},
+		highlight = {colors.grey, colors.bg}
+	}
+}
+
+
 gls.short_line_right[1] = {
-    BufferIcon = {
-        provider = 'BufferIcon',
-        separator = '|',
-        separator_highlight = {colors.purple, colors.bg},
-        highlight = {colors.grey, colors.purple}
-    }
+	BufferIcon = {
+		provider = 'BufferIcon',
+		separator = '|',
+		separator_highlight = {colors.purple, colors.bg},
+		highlight = {colors.grey, colors.purple}
+	}
 }
