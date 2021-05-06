f5ed85272fb65031d23fa363fe81109af62fe1a9
Parham Alvani <parham.alvani@gmail.com>
2021-05-06 16:35:58 +0430

    feat: Add Ledger Installation Script
    Former-commit-id: 2cbcea6fabb71ae1e424cfe7ea7a28cc6c32967d [formerly faec94b8fb2313b15ebae84285cce4fb2d4eced4] [formerly 4d3255f36d0862653c30e1008f321fd65b54dc89 [formerly e61511a23cb009d907520ba589001abcb13bcaec [formerly e61511a23cb009d907520ba589001abcb13bcaec [formerly e61511a23cb009d907520ba589001abcb13bcaec [formerly 2922b9840c10bfbe23ab34dd651813cd6a57722d]]]]]
    Former-commit-id: 1a96d3557bda102cb2d1033da60f382149f9ed5f [formerly d8be7ecbc6eab312658f042d7a32f13a51b25b34]
    Former-commit-id: 57a82dccf97836b9ae6f8f7ab8d7d29d84c1d0af
    Former-commit-id: dbb55741e6d55ff759a314eca54dba2aea91fd10
    Former-commit-id: ea736ef7fd4a5c47477885e4a79af5244aa7d818
    Former-commit-id: 85f796b7a910a238a3966729a30186b7040fba4b

---
+++ scripts/ledger.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : sample.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo -n 'ledger hardware wallet management software and udev rules'
+}
+
+main_apt() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_pacman() {
+	yay -Syu --noconfirm --needed aur/ledger-live-bin
+}
+
+main_brew() {
+	brew install ledger-live
+}
