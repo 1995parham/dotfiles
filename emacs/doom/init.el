684756fbf4c23cd31a0b89938ad8672f98e65df8
Parham Alvani <parham.alvani@gmail.com>
2021-05-08 23:52:50 +0430

    feat: Enable new Plugins on Emacs.
    Former-commit-id: 93d43c553f302fc63ecb33e074f275c9f7ed1b09 [formerly f69d5f386c8076f1482ec8178664fa77862f0cce] [formerly 838788ff8f4ab6554e39c4dd9efe7e15fdcef1d9 [formerly 34ecb78e88f287e1baa96ffb5a06985a97542c6b [formerly 34ecb78e88f287e1baa96ffb5a06985a97542c6b [formerly 34ecb78e88f287e1baa96ffb5a06985a97542c6b [formerly 2441e9d166e7fbf9e34116969400baa3ddca3140]]]]]
    Former-commit-id: 5406c18acc6066498766539c225497e1f2fe3f2c [formerly 462be38b20d86635e65ae47a369111b7d74ad3ae]
    Former-commit-id: 7ed87c237719ad27d862d50f2d391dcebb8a7813
    Former-commit-id: b1ee4f47e794f9c9c5a5c9c13f0daa55e18dda16
    Former-commit-id: f41e9ffc11d5e2743f02c1f84be519fa84993cb9
    Former-commit-id: 8de240b6cbe06fa961d0568ff1eb7c828383de97

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -118,7 +118,7 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data                ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
@@ -133,7 +133,7 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       json                ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -168,7 +168,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       yaml                ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)
