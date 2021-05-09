92b6a6cc8adbd1820236ddc119a087251f72eb71
Parham Alvani <parham.alvani@gmail.com>
2021-05-09 10:22:51 +0430

    feat: Add coc-texlab

--- nvim/nvim5/nvim/lua/coc.lua
+++ nvim/nvim5/nvim/lua/coc.lua
@@ -2,7 +2,7 @@ vim.g.coc_global_extensions = {
     'coc-json', 'coc-html', 'coc-css', 'coc-rls', 'coc-tsserver',
     'coc-snippets', 'coc-python', 'coc-yaml', 'coc-pyright',
     'coc-tslint-plugin', 'coc-rust-analyzer', 'coc-vimtex', 'coc-angular',
-    'coc-prettier'
+    'coc-prettier', 'coc-texlab',
 }
 
 vim.g.coc_filetype_map = {['yaml.docker-compose'] = 'yaml'}
