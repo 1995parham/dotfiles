5d39e63e174fc2ee8b812d881fb331fe0b982f67
Parham Alvani <parham.alvani@gmail.com>
2021-05-10 21:49:58 +0430

    fix: Correct Shell Lint Issues
    Former-commit-id: 2497a23a5aa716908c3afa6b6b6de355ea533f95 [formerly 9da1da3e356b4d82bcb61d7080756ee3aee24ed6] [formerly 823ba4d4c0abdb1cb2a950033d174f32d3ccdd96 [formerly c9390a304698f6f7ac0c47de7d33a22353775ed3 [formerly c9390a304698f6f7ac0c47de7d33a22353775ed3 [formerly c9390a304698f6f7ac0c47de7d33a22353775ed3 [formerly 51ea6264bba7d327605154a9c969260e48a75ef1]]]]]
    Former-commit-id: 6349a5bff9a6390a4e0437159ddd75e456f36727 [formerly e24a41b16dbd3bbec24daaa4e9682416b531b9a8]
    Former-commit-id: e856e7f9047084bf6dc6d9e51f5734da036f617d
    Former-commit-id: 775b3180c95b5d935c29a4b820843e24447d3b09
    Former-commit-id: f3910ff2e7cb6d12c93caaacdcbb3d8de324602d
    Former-commit-id: b262dc029d567dc6405da943e330ed8d8873ec83

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -34,15 +34,15 @@ main() {
 	if [[ "$OSTYPE" == "linux"* ]]; then
 		msg "add homebrew to your profile"
 
-		test -d $HOME/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+		test -d "$HOME/.linuxbrew" && eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
 
 		test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 		brew_eval="eval \$($(brew --prefix)/bin/brew shellenv)"
 
-		if [ -r $HOME/.bash_profile ]; then
-			grep -i "$brew_eval" $HOME/.bash_profile || echo "$brew_eval" >>$HOME/.bash_profile
+		if [ -r "$HOME/.bash_profile" ]; then
+			grep -i "$brew_eval" "$HOME/.bash_profile" || echo "$brew_eval" >>"$HOME/.bash_profile"
 		fi
-		grep -i "$brew_eval" $HOME/.profile || echo "$brew_eval" >>$HOME/.profile
+		grep -i "$brew_eval" "$HOME/.profile" || echo "$brew_eval" >>"$HOME/.profile"
 	fi
 }
