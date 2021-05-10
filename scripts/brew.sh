ed7738491d81c3df939f04069f6c55f49a188a90
elahe dastan <elahe.dstn@gmail.com>
2021-05-10 21:47:30 +0430

    feat: Update Brew Installation Script
    Former-commit-id: 94649be63cfc6d0b466c419395051371a46b221e [formerly ba53935bcbd7b64b6986b1b4dfc1f5f2575ebd34] [formerly 4b0ed96787583ef1ab42ff57b4e7ac2dbcf844a3 [formerly 6b41b8f3b42c08f965803a676167113e10108146 [formerly 6b41b8f3b42c08f965803a676167113e10108146 [formerly 6b41b8f3b42c08f965803a676167113e10108146 [formerly bb4d1a6ff3fb4ae9e6760509ccedd975725ab241]]]]]
    Former-commit-id: 29a71695936fe2a1feb32e9b10932c678f582e64 [formerly 462f44da9a5c6362fb50d3131bc5a673a31be726]
    Former-commit-id: 8fe0175d69d9809be55d9fc256823950d5954ad3
    Former-commit-id: c44c30d70f69abee8041cf45b726bcb4ebbb999c
    Former-commit-id: 34351b747f4ca35ca472533385c17c8ddff90b53
    Former-commit-id: 933a04e40a460c5f08e768aea26744ec9893a459

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -22,38 +22,27 @@ usage() {
 }
 
 main() {
-	install_profile=false
-
-	# Reset optind between calls to getopts
-	OPTIND=1
-	while getopts "p" argv; do
-		case $argv in
-		p)
-			install_profile=true
-			;;
-		*) ;;
-		esac
-	done
-
 	if [[ "$OSTYPE" == "linux"* ]]; then
-		message "brew" "install the Homebrew dependencies:"
+		msg "install the Homebrew dependencies:"
 		if [[ "$(command -v apt)" ]]; then
 			sudo apt-get install build-essential file curl git
 		fi
-		if [[ "$(command -v pacman)" ]]; then
-			sudo pacman -Suy base-devel
-		fi
-
 	fi
 
 	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 	if [[ "$OSTYPE" == "linux"* ]]; then
-		if [ $install_profile = true ]; then
-			message "brew" "add Homebrew to your PATH:"
-			test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-			test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-			test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-			echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+		msg "add homebrew to your profile"
+
+		test -d $HOME/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+
+		test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+		brew_eval="eval \$($(brew --prefix)/bin/brew shellenv)"
+
+		if [ -r $HOME/.bash_profile ]; then
+			grep -i "$brew_eval" $HOME/.bash_profile || echo "$brew_eval" >>$HOME/.bash_profile
 		fi
+		grep -i "$brew_eval" $HOME/.profile || echo "$brew_eval" >>$HOME/.profile
 	fi
 }
