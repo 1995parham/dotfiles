e88fdb23ee0e332001597249c4ec25bff115a745
Parham <parham.alvani@gmail.com>
2021-05-16 00:01:42 +0430

    feat: Remove i3config
    Former-commit-id: 626cefbcbc92792f04d4d9536605cfe0caed2059 [formerly c38680778bea687d9c709f7360dce6c46590ba9a] [formerly 8d10150019e0b4514fd2ba1b69753a75649c89ec [formerly f448abe6be582d6f83dc950d9f353addc1d40398 [formerly f448abe6be582d6f83dc950d9f353addc1d40398 [formerly f448abe6be582d6f83dc950d9f353addc1d40398 [formerly 405813e68135bdc8eab66b551543b56f718b0ee5]]]]]
    Former-commit-id: 14c738dbaf7b68ec399f5cb36db99304fd0dc289 [formerly 2d4bef21944ea0495958b6f83d87c35bbce3bdf4]
    Former-commit-id: d56c5d0f392287b2025a8381b78ae7a762689172
    Former-commit-id: 4492736609b96a839a9396cdb97fbc6eb83b1f08
    Former-commit-id: 7e3485749120a9b65c5df64e17f0c21875eaa3e1
    Former-commit-id: dd0f7fc43282fdf52a9484f114cd84b53280ef80

--- nvim/nvim5/nvim/init.lua
+++ nvim/nvim5/nvim/init.lua
@@ -3,8 +3,6 @@ require('plugin')
 require('key')
 
 vim.api.nvim_exec([[
-	autocmd BufNewFile,BufRead ~/.config/i3/config set filetype=i3config
-
 	" close the location window automatically when quitting parent window
 	augroup qfclose
 		au!
