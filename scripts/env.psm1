a28fb4a31bc7515e77ac165446446d5bdf6389e3
Parham Alvani <parham.alvani@gmail.com>
2021-06-05 02:41:14 +0430

    feat: Improve Windows Env Script

--- scripts/env.psm1
+++ scripts/env.psm1
@@ -1,17 +1,19 @@
 New-Module -Name env -ScriptBlock {
     function Install-Main {
-        Write-Output "up and running windows"
+        Write-Output "up and running windows 10"
 
         if (-not(Get-Command scoop -ErrorAction SilentlyContinue)) {
             Write-Output "install scoop without sudo"
             Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
         }
 
-        scoop install sudo pshazz 7zip git aria2
+				scoop bucket add extras
+        scoop install sudo 7zip git aria2
 
         if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
             Write-Output "install chocolatey with sudo"
-            sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+            sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+						sudo Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
         }
     }
 }
