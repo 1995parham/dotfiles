1895a927bb4da5b04b0865a3ebcc04cbd7ace409
Parham Alvani <parham.alvani@gmail.com>
2021-06-18 09:13:49 +0430

    feat: Improve gpg Installation
    Former-commit-id: c33fcecd6e5d6fad03899f84c720eb38fb28dd5d [formerly 3317cbd603d6689b365f149cc60549ade654de6a] [formerly 125b271e056b1d024b72112824d0f5af1f55a06a [formerly 0ee5827364663f30feabffa916777ecd16fe2747 [formerly 0ee5827364663f30feabffa916777ecd16fe2747 [formerly 0ee5827364663f30feabffa916777ecd16fe2747 [formerly afefe82fa2e7499eaf0bcc88dcd5c0dd3550f26e]]]]]
    Former-commit-id: 39b2cf64d2d038a289837dc8423a65761484e48e [formerly 49f30a30c987d648b19ae25895e1469afb85d733]
    Former-commit-id: 4e718239764e57f94536646bbc084b195da996b5
    Former-commit-id: 6858c2886b016664b4ed3177f8bce1428640c441
    Former-commit-id: 81f28aa81d823e4f9960a228237fc6cb73c777c2
    Former-commit-id: c7a83e9d9a1ab0c87186c5ff9d9e242149bdfec9

--- scripts/gopass.psm1
+++ scripts/gopass.psm1
@@ -2,5 +2,6 @@ New-Module -Name gopass -ScriptBlock {
     function Install-Main {
         scoop install gopass
         winget install Gpg4win
+        Write-Output 'please install gopass-jsonapi from its source to configure gopass extension'
     }
 }
