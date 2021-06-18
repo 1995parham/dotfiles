2b6402800a0a3e0a400ee72b0705a415dea9f808
Parham Alvani <parham.alvani@gmail.com>
2021-06-18 19:05:10 +0430

    feat: Change Arabic Font to Vazir Code

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -176,7 +176,7 @@
 
 ;; Use font Vazir for Arabic (Farsi)
 (after! unicode-fonts
-  (push "Vazir" (cadr (assoc "Arabic" unicode-fonts-block-font-mapping))))
+  (push "Vazir-Code" (cadr (assoc "Arabic" unicode-fonts-block-font-mapping))))
 
 ;; Telling bibtex-completion where your bibliographies can be found:
 (setq bibtex-completion-bibliography
