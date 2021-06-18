bebb049f3f4bbc046ee062f3c8d4c2ba5f70873c
Parham Alvani <parham.alvani@gmail.com>
2021-06-18 18:59:21 +0430

    feat: Improve Font Installation Script

--- scripts/font.sh
+++ scripts/font.sh
@@ -28,17 +28,23 @@ main_pacman() {
 	sudo pacman -Syu --needed --noconfirm noto-fonts-emoji ttf-roboto ttf-jetbrains-mono ttf-font-awesome
 	yay -Syu --needed --noconfirm ttf-meslo
 	yay -Syu --needed --noconfirm vazir-fonts
+	yay -Syu --needed --noconfirm vazir-code-fonts
 	# yay -Syu --needed --noconfirm borna-fonts
 	yay -Syu --needed --noconfirm ttf-times-new-roman
 }
 
 main_apt() {
+	msg 'install roboto font from apt repository'
 	sudo apt-get install fonts-roboto
 
-	jb_version="2.225"
+	jbm_version="2.225"
+	msg "install jetbrains mono ($jbm_version) by dowloading its archive"
 
-	wget "https://download.jetbrains.com/fonts/JetBrainsMono-$jb_version.zip"
-	unzip "JetBrainsMono-$jb_version.zip" -d jb && rm "JetBrainsMono-$jb_version.zip"
+	rm "JetBrainsMono-$jbm_version.zip" || true
+	rm -Rf jb || true
+
+	wget "https://download.jetbrains.com/fonts/JetBrainsMono-$jbm_version.zip"
+	unzip "JetBrainsMono-$jbm_version.zip" -d jb && rm "JetBrainsMono-$jbm_version.zip"
 
 	mv jb/fonts/ttf/* "$HOME/.local/share/fonts/" && rm -Rf jb
 }
