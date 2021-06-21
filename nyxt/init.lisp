c95657c15d50102387537191010482efce4b1b89
Parham Alvani <parham.alvani@gmail.com>
2021-06-21 17:30:03 +0430

    feat: Update Nyxt Configuration
    Former-commit-id: 88ccab4b5cf94409b708e23bb85657fcb3bd7664 [formerly 8006e5c0c27731d0e0b9ebe70bf3076cac264043] [formerly f92ce6d6e3a67478808fa06e99bab8c6825f5bae [formerly 14a2e0495ff808957c7121947c7658107bc5aa9b [formerly 14a2e0495ff808957c7121947c7658107bc5aa9b [formerly 14a2e0495ff808957c7121947c7658107bc5aa9b [formerly 115d73ffcdce78494dce4afdd74e572000ae853f]]]]]
    Former-commit-id: 832e5ddd3d05e9e45e72daac649c128c5a0d8dd3 [formerly 359b3f306bd741f5a39f110d0d56ebce9d45b25f]
    Former-commit-id: 6fddf8da1c54ce2a7438295ed023e59de194c9f2
    Former-commit-id: 9ade2a1195d8d7d9d2ad41d31d03de8fb1ac203b
    Former-commit-id: 7c83358608bd68d40b26b3bdecb10092b649b991
    Former-commit-id: 6ebc4aeda77a01ad9aab672b8d4221507951362e

--- nyxt/init.lisp
+++ nyxt/init.lisp
@@ -1,2 +1,8 @@
-(define-configuration (buffer web-buffer)
-  ((default-modes (append '(nyxt::vi-normal-mode) %slot-default%))))
+(define-configuration nyxt/proxy-mode:proxy-mode
+                      ((proxy (make-instance 'proxy
+                                             :url (quri:uri "socks5://localhost:1086")
+                                             :allowlist '("localhost" "localhost:8080")
+                                             :proxied-downloads-p t))))
+
+(define-configuration buffer
+                      ((default-modes (append '(auto-mode vi-normal-mode) %slot-default%))))
