8ba0916fc7321e9d798b577a15d753f3eb44ed0f
Parham Alvani <parham.alvani@gmail.com>
2021-07-04 18:43:33 +0430

    fix: Correct Arch Installation Script
    Former-commit-id: 218edee0ada86a7f82c94c4f9191631ebcb65ca4 [formerly 3b39bd08ea62db15ea9f64cefe93c5f286f5500a] [formerly 6ee796d72f296c3d116602f5f2a0c4e8a9414480 [formerly 584ccee0ee7d07997d386e4ef8509267208ef0cb [formerly 584ccee0ee7d07997d386e4ef8509267208ef0cb [formerly 584ccee0ee7d07997d386e4ef8509267208ef0cb [formerly 87feacff32eabd735f781ab98348cdc83ae92d15]]]]]
    Former-commit-id: 4b06a13eded96ffc2cdaa5a44d3fb235edf9b2a5 [formerly 50e3c866bbbda542e0779e76bf1ba59814ce3735]
    Former-commit-id: 43d3ea40c279576fef70081ba96252e89b3d0ab6
    Former-commit-id: 9dae5a83879727e336dfcaa32f0e8f404a5f0f55
    Former-commit-id: 4891383a30ab1e8d95955985ecb65d993f7678af
    Former-commit-id: b335fc86082f7d73fed1dd23ba79a71603ac52b6

--- archinstall/install.py
+++ archinstall/install.py
@@ -194,21 +194,6 @@ def ask_user_questions():
             ] = archinstall.ask_for_main_filesystem_format()
         archinstall.arguments["harddrive"].keep_partitions = False
 
-    # Get disk encryption password (or skip if blank)
-    if (
-        archinstall.arguments["harddrive"]
-        and archinstall.arguments.get("!encryption-password", None) is None
-    ):
-        if passwd := archinstall.get_password(
-            prompt="Enter disk encryption password (leave blank for no encryption): "
-        ):
-            archinstall.arguments["!encryption-password"] = passwd
-            archinstall.arguments[
-                "harddrive"
-            ].encryption_password = archinstall.arguments[
-                "!encryption-password"
-            ]
-
     archinstall.arguments["bootloader"] = "grub-install"
 
     # Get the hostname for the machine
