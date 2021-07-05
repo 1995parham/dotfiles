df4ab0ec1dd0c94fe19e1943d7b547d421a730d0
Parham Alvani <parham.alvani@gmail.com>
2021-07-05 09:08:13 +0430

    feat: Add Sway Configuration Script
    Former-commit-id: 17a03fb2f7857f2db3d366345b40e5b1cd318164 [formerly 77dc9dcc1721c57e3848781d336dd6d89fcceb60] [formerly 684b97a7c2b46ae5e917f51169c527b833a4371b [formerly 6ef8bf4eb315299c6fcdd5ef65bd71914ee3fe70 [formerly 6ef8bf4eb315299c6fcdd5ef65bd71914ee3fe70 [formerly 6ef8bf4eb315299c6fcdd5ef65bd71914ee3fe70 [formerly 68e1bc4948b2c9901332449896db104a85cb2196]]]]]
    Former-commit-id: 1ee4a075ff2f462677d59e60b4134dfba295e6c1 [formerly 8256e403c3c6e58cf61f29b80f66557a3890980e]
    Former-commit-id: 76fa7e7243d274b52c2d1a46aafdf0e9207c678e
    Former-commit-id: 4218085cc8eafa57ab471190cdc7f809c5d569a3
    Former-commit-id: affd6825d31d30eea8fa3fa51d9ec61b7077a7a4
    Former-commit-id: c44743df93ec443ee1b722c23876266bd239ad78

---
+++ scripts/sway.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : i3.sh
+#
+# [] Creation Date : 18-11-2020
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo "sway window manager for arch, from scratch"
+	# shellcheck disable=1004
+	echo '
+ _____      ____ _ _   _
+/ __\ \ /\ / / _` | | | |
+\__ \\ V  V / (_| | |_| |
+|___/ \_/\_/ \__,_|\__, |
+                   |___/
+  '
+}
+
+main_brew() {
+	return 1
+}
+
+main_apt() {
+	return 1
+}
+
+main_pacman() {
+	current_dir=${current_dir:?"current_dir must be set"}
+
+	msg 'sway'
+	sudo pacman -Syu --noconfirm --needed sway swaylock swayidle waybar grim
+
+	sudo pacman -Syu --noconfirm --needed unclutter
+
+	msg 'notification with dunst'
+	sudo pacman -Syu --noconfirm --needed dunst
+	configfile dunst "" sway
+
+	msg 'backgrounds with swaybg'
+	msg 'setup a systemd timer to change background images each 5 minutes with swaybg'
+	sudo pacman -Syu --noconfirm --needed swaybg
+
+	configsystemd swaybg swaybg.timer sway
+	configsystemd swaybg swaybg.service sway
+	configfile swaybg gosimac.sh sway
+
+	systemctl --user enable swaybg.timer
+	systemctl --user start swaybg.timer
+
+	msg 'configure the dmenu, default application luncher on manjaro i3'
+	linker dmenu "$current_dir/sway/dmenurc" "$HOME/.dmenurc"
+	chmod +x "$HOME/.dmenurc"
+
+	msg 'configure rofi another application luncher'
+	sudo pacman -Syu --noconfirm --needed rofi
+	configfile rofi "" sway
+
+}
