167bf5a3cc747e904a37c453a0bab675887d57de
Parham Alvani <parham.alvani@gmail.com>
2021-07-05 15:26:46 +0000

    fix: Correct i3 Installation Script
    Former-commit-id: d0d3ffc202a98972012113d28b9b328f5a625384 [formerly d2e9d019ed0c0bfe2bbfa950c0ae7525cdd900d4] [formerly 0d8c5ba3a9e58b22ea0daafb3765bd489499e186 [formerly 9bf7aaa132ef251ac4e7f6d5d36abc0a3c5e40d7 [formerly 9bf7aaa132ef251ac4e7f6d5d36abc0a3c5e40d7 [formerly 9bf7aaa132ef251ac4e7f6d5d36abc0a3c5e40d7 [formerly 5e659a6c0aec52f096650dad46d857ce6f5732a2]]]]]
    Former-commit-id: 4a791cf3e1f673614d48fa11b40445f9780fe205 [formerly 7c62c6f73bee19075d306aff0f6b1ea29eb1b418]
    Former-commit-id: f4ae1dc73aacd0c6e2ef5645246fd0e4aa5929d1
    Former-commit-id: 74ea195c57b2d7fe7a4346cd83df6a710d8deb44
    Former-commit-id: 4c519f42e10cfc1d513269d99c7ea928ccff1973
    Former-commit-id: b958a889fbd9da2842789f695f4bd2ac7eeae187

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -32,18 +32,18 @@ main_pacman() {
 	current_dir=${current_dir:?"current_dir must be set"}
 
 	msg 'i3 with polybar'
-	sudo pacman -Syu --noconfirm --needed i3-gaps i3-scrot matcha-gtk-theme
+	sudo pacman -Syu --noconfirm --needed i3-gaps
+	yay -Syu --noconfirm --needed i3-scrot matcha-gtk-theme polybar i3exit
+	configfile gtk-3.0 settings.ini i3
 
 	configfile i3 "" i3
+	configfile polybar "" i3
 
 	msg 'x11/wayland image viewer'
 	sudo pacman -Syu --noconfirm --needed imv
 
 	msg 'pdf viewer'
 	sudo pacman -Syu --noconfirm --needed mupdf
-
-	msg 'jalali calender'
-	yay -Syu --noconfirm --needed jcal-git
 
 	msg 'picom'
 	sudo pacman -Syu --noconfirm --needed picom
@@ -62,14 +62,14 @@ main_pacman() {
 
 	msg 'backgrounds with feh'
 	sudo pacman -Syu --noconfirm --needed feh
-	msg 'setup a systemd timer to change background images each 5 minutes with nitrogen'
+	msg 'setup a systemd timer to change background images each 5 minutes with feh'
 	configsystemd feh feh.timer i3
 	configsystemd feh feh.service i3
 
 	systemctl --user enable feh.timer
 	systemctl --user start feh.timer
 
-	msg 'configure the dmenu, default application luncher on manjaro i3'
+	msg 'configure the dmenu, default application luncher on i3'
 	linker dmenu "$current_dir/i3/dmenurc" "$HOME/.dmenurc"
 	chmod +x "$HOME/.dmenurc"
 
@@ -77,13 +77,10 @@ main_pacman() {
 	sudo pacman -Syu --noconfirm --needed rofi
 	configfile rofi "" i3
 
-	sudo pacman -Syu --noconfirm --needed matcha-gtk-theme
-	linker gtk-2.0 "$current_dir/i3/gtk-2.0/gtkrc-2.0" "$HOME/.gtkrc-2.0"
-	configfile gtk-3.0 settings.ini i3
-
-	sudo pacman -Syu --noconfirm --needed perl-anyevent-i3
-
 	msg 'gnome-keyring setup with ~/.profile'
 	sudo pacman -Syu --noconfirm --needed gnome-keyring
 	dotfile i3 profile
+
+	msg 'pavucontrol, a panel for audio'
+	sudo pacman -Syu --noconfirm --needed pavucontrol
 }
