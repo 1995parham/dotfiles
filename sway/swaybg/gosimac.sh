8b61f5c8ccb57f67e105e25956366d2f3196e46d
Parham Alvani <parham.alvani@gmail.com>
2021-07-05 10:32:02 +0430

    fix: Correct SWAYSOCK in gosimac script
    Former-commit-id: e9948568504c779e5a5fe757105f6a4c72b7b2af [formerly d0850726c478b7cd689b4ea119c98c31eb8ad46f] [formerly 79ef1dd521719758b2c06b7b100ecc1123b1bdf1 [formerly 109f54334ab35bd75944cc53a6d1869992f01e0c [formerly 109f54334ab35bd75944cc53a6d1869992f01e0c [formerly 109f54334ab35bd75944cc53a6d1869992f01e0c [formerly df9939c74df9b33446cb33ca206f96c6e4192140]]]]]
    Former-commit-id: c2f94ae4d0faa966976de7ff77a4e09450b6cae6 [formerly fb9cc51ef4f51bf88c11616db30dbe9f294ac738]
    Former-commit-id: f56b902e09cd80a05cb6786df03e8dbab8a8ed72
    Former-commit-id: 8e46ce77b00ba7ffa9a8014c749c57073f24a389
    Former-commit-id: dd0a7e76bffe8e5eb2b0172f5b3ee42b538922c9
    Former-commit-id: ac7a2298e86096dd789e44f5c8e3137777836fce

--- sway/swaybg/gosimac.sh
+++ sway/swaybg/gosimac.sh
@@ -2,4 +2,6 @@
 
 new_wallpaper="$(find "$HOME/Pictures/GoSiMac" -maxdepth 1 -type f | shuf -n 1)"
 
+SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
+
 swaymsg -s "$SWAYSOCK" output '*' bg "$new_wallpaper" fill
