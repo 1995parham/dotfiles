a678da6447ead550c54ee18b1aaadcbea3ae4532
Parham Alvani <parham.alvani@gmail.com>
2021-07-12 22:55:07 +0430

    feat: Improve Installation
    Former-commit-id: a022371eeae54cdc0d849aef7ac9e73732d59a1b [formerly 7cd474d60941e114769d44d215513b8c207c1345] [formerly d727a2be4c1b1b0a6c65429ba0088495f6b8dc99 [formerly a12cdb05e5d61b52ac9f1d72f0ecdaf0f360a881 [formerly a12cdb05e5d61b52ac9f1d72f0ecdaf0f360a881 [formerly a12cdb05e5d61b52ac9f1d72f0ecdaf0f360a881 [formerly 56a1179fe0cd6150bf30ff7d17e6e787a8c72eed]]]]]
    Former-commit-id: d34837fe0bf3e55ae1765f33bdf9705afff38c20 [formerly af74cd542761c6b5ad9f60823debea265112dfcd]
    Former-commit-id: 6c61ef6a50645193f1132296db9b0b68aa7c0121
    Former-commit-id: 8544a1494a32d713be66e2a3272c9d7303530d0f
    Former-commit-id: 8ba041a630895c35ab0187ced84963637902e213
    Former-commit-id: 9d0cdb2fcd2f04f667a220b7baaa5e528e45e9f4

--- archinstall/install.sh
+++ archinstall/install.sh
@@ -8,4 +8,15 @@ fi
 sudo systemctl enable lightdm
 
 cd "$HOME/yay-bin" && makepkg -si
-cd "$HOME/dotfiles/" && ./start.sh i3
+
+read -r -p "do you want the minimal installation?[Y/n] " -n 1 accept
+echo
+if [[ $accept == "Y" ]]; then
+	echo "minimal installation"
+	cd "$HOME/dotfiles/" && ./start.sh mini3
+	sudo pacman -Syu netsurf
+else
+	echo "full installation"
+	cd "$HOME/dotfiles/" && ./start.sh i3
+	sudo pacman -Syu firefox
+fi
