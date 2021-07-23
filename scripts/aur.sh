e65a1d58c91b2df507b9228922556734d0d5f02c
Parham Alvani <parham.alvani@gmail.com>
2021-07-23 07:34:19 +0430

    fix: Correct aur Script
    Former-commit-id: 3b5174be57ca8722bc452e71cee4346978843b3f [formerly 3a820dba420523500b125801ca4703050a35ea2a] [formerly 3df4cbac209824013ad4556cc699689284fbacae [formerly 4c5cb2791827250ec843cb8f456dd2f5141fb15e [formerly 4c5cb2791827250ec843cb8f456dd2f5141fb15e [formerly 4c5cb2791827250ec843cb8f456dd2f5141fb15e [formerly cee1db6a77bb95b768e5593b9082dc95a1bf763d]]]]]
    Former-commit-id: a3b7e6d30c04d1c1cf5821db3886c639387c6c0e [formerly f5d9e79ed3afd6fee97a7f8eced02060432c5e5f]
    Former-commit-id: 2dcef59fcbfdc59d1b06b4e86713670ea1a2cc8a
    Former-commit-id: f52f24536aba08ebf86957043d35eaf80063991c
    Former-commit-id: 167f82f4e6eb27a743784efa8127d6c9fd650ba5
    Former-commit-id: 4a5a699be521addd80ac8e31807f7e4eaec63917

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -12,7 +12,7 @@ usage() {
 main() {
 	mkdir -p "$HOME/Documents/Git/parham/aur"
 
-	git clone git@aur.archlinux.org:natscli "$HOME/Documents/Git/parham/aur/natscli"
-	git clone git@aur.archlinux.org:okd-client-bin "$HOME/Documents/Git/parham/aur/okd-client-bin"
-	git clone git@aur.archlinux.org:gopass-jsonapi-bin "$HOME/Documents/Git/parham/aur/gopass-jsonapi-bin"
+	git clone aur@aur.archlinux.org:natscli "$HOME/Documents/Git/parham/aur/natscli"
+	git clone aur@aur.archlinux.org:okd-client-bin "$HOME/Documents/Git/parham/aur/okd-client-bin"
+	git clone aur@aur.archlinux.org:gopass-jsonapi-bin "$HOME/Documents/Git/parham/aur/gopass-jsonapi-bin"
 }
