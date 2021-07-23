0d4a9820951c735dc58f6a5fb9912ec15ee40d74
Parham Alvani <parham.alvani@gmail.com>
2021-07-23 07:51:32 +0430

    feat: Add new aur package
    Former-commit-id: 8612a2d43d4fe3e06617e76a6a8f05a979efefff [formerly 23cb8b1fc4e77b239769369561c74b73af508edf] [formerly 624b95fdf341d74f40378623995333f08b0ff22f [formerly ccf5cfaaf633d8a3601e570d78bde130d9c3779e [formerly ccf5cfaaf633d8a3601e570d78bde130d9c3779e [formerly ccf5cfaaf633d8a3601e570d78bde130d9c3779e [formerly c6649e7d9163e335480106f4f6844afb26221a08]]]]]
    Former-commit-id: cbc78a44a2da55cf1e50b0dcda71e7df07fb16e9 [formerly 5abfd2b4d1b314996e6f20346752a758dc6c0862]
    Former-commit-id: ac6a1c3e1ce0cdce2370d6b09e5a3343b8ef6cb4
    Former-commit-id: 9ac98c30f0271052500e5ea3cd5edc3b4ba58709
    Former-commit-id: 0852419e0afc3f0f884ea9289a5a7b35e5183025
    Former-commit-id: 0dbecd07f871e59d7b5b6084260e6e1b3e375555

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -15,4 +15,5 @@ main() {
 	git clone aur@aur.archlinux.org:natscli "$HOME/Documents/Git/parham/aur/natscli"
 	git clone aur@aur.archlinux.org:okd-client-bin "$HOME/Documents/Git/parham/aur/okd-client-bin"
 	git clone aur@aur.archlinux.org:gopass-jsonapi-bin "$HOME/Documents/Git/parham/aur/gopass-jsonapi-bin"
+	git clone aur@aur.archlinux.org:jwt-cli-bin "$HOME/Documents/Git/parham/aur/jwt-cli-bin"
 }
