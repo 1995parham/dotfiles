401a0b8d19a4472e510c8476f22cf1cc699a900e
Parham Alvani <parham.alvani@gmail.com>
2021-07-31 13:12:20 +0430

    feat: Add v2ray Installation
    Former-commit-id: 07e84107690cd98d176d1cf8ddd3ffa2783dcc4f [formerly cd37dba0bf0663a6c130cb1cabd2ef4b7abe4d54] [formerly cb100584e9a00555131c3a9aafc91665b93e296b [formerly de07a0f04dc627b5d507fe07c67fab2dde44a26b [formerly de07a0f04dc627b5d507fe07c67fab2dde44a26b [formerly de07a0f04dc627b5d507fe07c67fab2dde44a26b [formerly 860f3fa22e1b55cff18b12f2c1d314d6df16590d]]]]]
    Former-commit-id: aa54b5bd347e07042e598d0d3914acfb44422ec8 [formerly 7f72bc3e15f666406bb3f2d62e7b7c4ef1a25550]
    Former-commit-id: 1755830aedf9c7d71f268ddec245be0780f33057
    Former-commit-id: 45445f51e5cf862c41ac39760fe7493992055ac8
    Former-commit-id: 8a165e8a856bebfb0e6f2706faa48b08ddfc335a
    Former-commit-id: ad5110eaf41edf8683ae77c4b8e46fdb3511c727

---
+++ scripts/v2ray.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : v2ray.sh
+#
+# [] Creation Date : 31-07-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo -n -e "v2ray for u.s. unfair sanctions"
+
+	# shellcheck disable=1004,2016
+	echo '
+       ____
+__   _|___ \ _ __ __ _ _   _
+\ \ / / __) | |__/ _| | | | |
+ \ V / / __/| | | (_| | |_| |
+  \_/ |_____|_|  \__,_|\__, |
+                       |___/
+  '
+}
+
+main_apt() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_pacman() {
+	sudo pacman -Syu --needed --no-confirm v2ray
+}
+
+main_brew() {
+	brew install v2ray
+	brew services start v2ray
+}
+
+main() {
+	msg "there is nothing that we can do"
+	return 1
+}
