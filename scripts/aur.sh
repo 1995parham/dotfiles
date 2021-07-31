898e8dcd4a8975ee9cf07109c20205824202fae4
Parham Alvani <parham.alvani@gmail.com>
2021-07-31 06:21:52 +0430

    feat: Add New AUR Package

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -15,6 +15,7 @@ main() {
 	mkdir -p "$HOME/Documents/Git/parham/aur"
 
 	git clone aur@aur.archlinux.org:natscli "$HOME/Documents/Git/parham/aur/natscli"
+	git clone aur@aur.archlinux.org:natscli-bin "$HOME/Documents/Git/parham/aur/natscli-bin"
 	git clone aur@aur.archlinux.org:okd-client-bin "$HOME/Documents/Git/parham/aur/okd-client-bin"
 	git clone aur@aur.archlinux.org:gopass-jsonapi-bin "$HOME/Documents/Git/parham/aur/gopass-jsonapi-bin"
 	git clone aur@aur.archlinux.org:jwt-cli-bin "$HOME/Documents/Git/parham/aur/jwt-cli-bin"
