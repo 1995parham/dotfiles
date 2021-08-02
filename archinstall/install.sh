23f945c429f26e5c3f10f389049850f2f3ac727d
Parham Alvani <parham.alvani@gmail.com>
2021-08-02 23:31:39 +0430

    feat: Improve Archinstall Script
    Former-commit-id: 3c903e451dba8f6b48356f1c1b381b64c9487b7c [formerly 4946d7909c64bbc9288a33b0c0d63ddd2d21ac7e] [formerly 359c0d079e1eeadfbe0dc7e86adf991e5a4fef2c [formerly a31b4265e75342bda965b4496b7c487022317a64 [formerly a31b4265e75342bda965b4496b7c487022317a64 [formerly a31b4265e75342bda965b4496b7c487022317a64 [formerly 5cc9003b809972fc758fa0055f96c89fafe6dce0]]]]]
    Former-commit-id: c0ca80a6ca4d49b7bec3cecbc7bc06a1ff7df495 [formerly 4cbe37e434f8576779dd7902b4271ae86380befc]
    Former-commit-id: 4c4335bc5a1198c7c12263299fbc63bc7fe0b080
    Former-commit-id: 322a18d9138f8950ecce0f04e621372b011b2913
    Former-commit-id: 785c1840d706e1a0ff0b6d70b09e520aac4f4d0e
    Former-commit-id: fdc2e3cc92e54aadb9559bac6567043e23ad6cf3

--- archinstall/install.sh
+++ archinstall/install.sh
@@ -12,11 +12,11 @@ cd "$HOME/yay-bin" && makepkg -si
 read -r -p "do you want the minimal installation?[Y/n] " -n 1 accept
 echo
 if [[ $accept == "Y" ]]; then
-	echo "minimal installation"
+	echo "i3 minimal installation"
 	cd "$HOME/dotfiles/" && ./start.sh mini3
 	sudo pacman -Syu netsurf
 else
-	echo "full installation"
+	echo "i3 full installation"
 	cd "$HOME/dotfiles/" && ./start.sh i3
 	sudo pacman -Syu firefox
 fi
