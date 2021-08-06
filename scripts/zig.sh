c96af8f6ec4987664651ef977fa4a4397df2ab3d
Parham Alvani <parham.alvani@gmail.com>
2021-08-06 10:58:06 +0430

    feat: Add Zig lang Installer
    Former-commit-id: b451ccd6cb749708193bebd3f1d6c4c7f1dd39e1 [formerly c8273e34fedde2497ef43a622761e48cadf375c6] [formerly 99b9b2dbd06310dc4fcc2a33b5f7ce1cd1d44267 [formerly 6e4fe1f4d5e3d8a487085f0029d7fb36baf0d217 [formerly 6e4fe1f4d5e3d8a487085f0029d7fb36baf0d217 [formerly 6e4fe1f4d5e3d8a487085f0029d7fb36baf0d217 [formerly d5b0b13f47b05945c2c2cd7663d3a91587fc1902]]]]]
    Former-commit-id: b40019f8d17dda4d1f4544cec6f143aec4f0c262 [formerly d6edeeba697ddc89b941970cd7bea0724082a5a9]
    Former-commit-id: 617a7e15ab594cb5c2ac88eb3777150050de3728
    Former-commit-id: 1febacf29c5130a11c0fe95223969d43d95675e0
    Former-commit-id: 1cc741fd951608dce1c45f776abb25f10f9d9656
    Former-commit-id: f15c1483aa5726d21b0a851576f29af9f9738bcc

---
+++ scripts/zig.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : zig.sh
+#
+# [] Creation Date : 06-08-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo -n -e "Zig is a general-purpose programming language and toolchain for maintaining robust, optimal, and reusable software."
+
+	# shellcheck disable=1004,2016
+	echo '
+     _
+ ___(_) __ _
+|_  / |/ _` |
+ / /| | (_| |
+/___|_|\__, |
+       |___/
+  '
+}
+
+main_apt() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_pacman() {
+	sudo pacman -Syu --needed --noconfirm zig
+}
+
+main_brew() {
+	msg "there is nothing that we can do"
+	return 1
+}
