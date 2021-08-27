5e46af285bc93f0c96bfc6fb9faa614020662edb
Parham Alvani <parham.alvani@gmail.com>
2021-08-27 23:09:46 +0430

    feat: update font and theme of doom.
    Former-commit-id: 35b94d70fce6f241a14924b5d3c0d72a665c25d7 [formerly 873129d6a46ff8e62964850688983c14cd61ade1] [formerly 3bc9af408c491acd797b4e15724f0bb036e4aace [formerly 88bddf50205df84e4b296b7008afd3776864682e [formerly 88bddf50205df84e4b296b7008afd3776864682e [formerly 88bddf50205df84e4b296b7008afd3776864682e [formerly 9e0c9d6920628b5e0377a7dfeca9e3c1f41ebc6e]]]]]
    Former-commit-id: 1367b40410abb0b59419138ad052c63a4cd11e4b [formerly 46477ae94a77337a4fd3c09c68366b4e615464a8]
    Former-commit-id: 120751c254cb1545e5947e4a7134bee8f7b6938e
    Former-commit-id: c22e296776119f12905ed92d9b95b0bf08922acc
    Former-commit-id: 00ddef27cc89408e512083b471962c67f602f58e
    Former-commit-id: 7ff665a40e751427e89039a637299d458372dfce

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -30,13 +30,15 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :height 1.0)
-      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :height 1.3))
+;; (setq doom-font (font-spec :family "JetBrains Mono" :height 1.0)
+;;     doom-variable-pitch-font (font-spec :family "JetBrains Mono" :height 1.3))
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 100)
+
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-monokai-pro)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
