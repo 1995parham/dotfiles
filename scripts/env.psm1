b7f31857946470e492ade01201e48e8c84caa298
Parham Alvani <parham.alvani@gmail.com>
2021-09-05 20:56:20 +0430

    feat: update windows installation
    Former-commit-id: fe18c36f09e98e0fa878791e3dd6fedcc5a39c87 [formerly d653f04d1936e77ff5dec9067e3130a947312a3e] [formerly 36f54d4c1edbbc0756ec2b421007570d55dc174a [formerly d538a2edfc29a6a909b4f8b18caed0098a8739de [formerly d538a2edfc29a6a909b4f8b18caed0098a8739de [formerly d538a2edfc29a6a909b4f8b18caed0098a8739de [formerly 1ec9a2d981549a231b3bb2b06ccfb2332c2e974c]]]]]
    Former-commit-id: 0df73b089fc6a83767c3754053a82bf178a44536 [formerly 79e635f85ba4c50aa509ad19ff7697e98c9955da]
    Former-commit-id: 8266edcdf3f1fd908b83c4ca454e59a9601b44d7
    Former-commit-id: 26910e69a9c99504b07995e5e3281a859dd6804f
    Former-commit-id: 6f8b9edb6cd6e0e139d3482b988cf7cb376da6cb
    Former-commit-id: a3d16af3246b77818c1959a3985acd4f12cb9c4f

--- scripts/env.psm1
+++ scripts/env.psm1
@@ -1,13 +1,13 @@
 New-Module -Name env -ScriptBlock {
-    function Install-Main {
-        Write-Output "up and running windows 10"
+  function Install-Main {
+    Write-Output "up and running windows 10"
 
-        if (-not(Get-Command scoop -ErrorAction SilentlyContinue)) {
-            Write-Output "install scoop without sudo"
-            Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-        }
+      if (-not(Get-Command scoop -ErrorAction SilentlyContinue)) {
+        Write-Output "install scoop without sudo"
+          Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+      }
 
-	scoop bucket add extras
-        scoop install sudo 7zip git aria2
-    }
+    scoop bucket add extras
+      scoop install sudo 7zip aria2
+  }
 }
