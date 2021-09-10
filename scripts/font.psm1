f08a731303dc5b9a20776e5e9ccc7ac45b7d8564
Parham Alvani <1995parham@tuta.io>
2021-09-10 05:58:42 +0000

    feat: remove windows modules
    Former-commit-id: e293f489bd4556db2cfd06e8e99cb196f66cc9f8 [formerly 956cb2bd7f872aa865906ef388a9e752ef406133] [formerly fab0f21108604bd0586250ee9c1b1ac8cbae3d1f [formerly f004889141cac606cc63ac2d3a802800ddb0684a [formerly f004889141cac606cc63ac2d3a802800ddb0684a [formerly f004889141cac606cc63ac2d3a802800ddb0684a [formerly f3f8eb4b341f7b18cd246c8d4ca4b59a42881aa1]]]]]
    Former-commit-id: a5918e6c57fb6e0bef798a29870ddbdb40a04438 [formerly c8d2d648cc1f090e3502242c5cf2b90ca8f3703f]
    Former-commit-id: 5b1b47a2c70079811c653d9ffcdd0a8799bb0e3d
    Former-commit-id: 15c3b1a5da295c63d1b4ec1dd76ba3fb680452f5
    Former-commit-id: 30aa56f2b96feaf3832e7edc2e91659452c4b8ef
    Former-commit-id: e560438d0cbd056e1a9b792b5926997d8c4527f3

--- scripts/font.psm1
+++
@@ -1,8 +0,0 @@
-New-Module -Name font -ScriptBlock {
-    function Install-Main {
-        Write-Output "install fonts"
-
-        scoop bucket add nerd-fonts
-        sudo scoop install Meslo-NF JetBrains-Mono Font-Awesome
-    }
-}
