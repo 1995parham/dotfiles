58e0881c930ac056ee5a9018ec882c6f323e7d2c
Parham Alvani <parham.alvani@gmail.com>
2021-09-10 13:09:43 +0000

    Revert "feat: Remove sway"
    This reverts commit 70d8d6932c6c7af5da10c9c84752219346a83558 [formerly 2a2fbebab41fc61c7fef9c421d9aa63398aed565] [formerly 17271b96c3988f3b34478080c395878325044625] [formerly ab5872beeb1884f8a89ee5171a8d186a7b8a89c7] [formerly 2fca2ae5355b0d1e4f57ae1725a93c1010257215] [formerly b57df4e692a233b91a08be3b113f1f80541540e6 [formerly 7ae43f727b824d8fb327b6c02dc9fa1115c10165]] [formerly beb20d96196bf7defe671729d68a91709733fffa [formerly dfec23c24a7588d153456a410f80aeae8aeebceb] [formerly 11c9769813b454e56206528a66c1bcc3b1df1bb9 [formerly c80ed377ad5b8b7d2ca9f5393ad3c621c0f49aed [formerly c80ed377ad5b8b7d2ca9f5393ad3c621c0f49aed [formerly c80ed377ad5b8b7d2ca9f5393ad3c621c0f49aed [formerly cd1548827d61c714285bedd36b5a9b38b9e60638]]]]]].
    
    
    Former-commit-id: 9c90b4701a24330eaabafaf5702f762584d62fc2 [formerly d707f16566abee1f5af4fe412377005ab23ebd4e] [formerly 574e9f62bca8be0666d66e09c8ac3727c35900de [formerly 8f6c2491932dba2d4d6f842277e5030f099a71e3 [formerly 8f6c2491932dba2d4d6f842277e5030f099a71e3 [formerly 8f6c2491932dba2d4d6f842277e5030f099a71e3 [formerly 742657d14f9bd0fb960a5d049a738aebd5392b58]]]]]
    Former-commit-id: 7e0aedb669142fcd8aa6b17d215c8558fe482f42 [formerly 722fc229d5f556582b1915364bc4e3684c274dd9]
    Former-commit-id: 403a76c10aa5dbd7fe06706ad36ed37f966b0b43
    Former-commit-id: d9817244db4c622eeeaafcc3d6390f4499297a2d
    Former-commit-id: bd4380c8cfb557072538734facc5f6db883357c0
    Former-commit-id: b722115534d7099f89637bb0f04c0ede80a017c1

---
+++ sway/swaybg/gosimac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pid="$(pidof swaybg)"
+
+new_wallpaper="$(find "$HOME/Pictures/GoSiMac" -maxdepth 1 -type f | shuf -n 1)"
+
+export SWAYSOCK
+SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
+
+swaybg -i "$new_wallpaper" -m fill &
+sleep 1
+kill "$pid"
