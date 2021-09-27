d20290c0e32f4ae450b0d47d97cf670f0e4dd1e1
Elahe Dastan <elahe.dstn@gmail.com>
2021-09-27 20:59:38 +0330

    feat: add sbt for scala in java.sh
    Former-commit-id: 8d9338ea69b5c4f40d17dec9d01cfe35fcf8275d [formerly 935dca10640d569b2c0b0cfe47d7daf3f4e12cb9] [formerly 615b91e669b7afb7edadf58946da2ae3d56d9d4f [formerly 40a4aee01e90b33e92df503ac1bb59b75dbba54e [formerly 40a4aee01e90b33e92df503ac1bb59b75dbba54e [formerly 40a4aee01e90b33e92df503ac1bb59b75dbba54e [formerly 9e95ee867d3c24bda128ac8fe5b6b937c80ff7cb]]]]]
    Former-commit-id: 7378548790ecacab3bdfc104a7cf7be571e87057 [formerly 161e1519d18286f35a04efeb5fef630405628f4c]
    Former-commit-id: d1aed4fcc7a39ba2f6b4ba04cd8e00271f491e79
    Former-commit-id: f06c453bc4ac8c761c03d102e7f1dfc7eabf9af9
    Former-commit-id: 7384220db8d14e8b1183505d1a12d10cc2ffc8bb
    Former-commit-id: 71de8a8f0dcce338852529f47e714f7117d3e21d

--- scripts/java.sh
+++ scripts/java.sh
@@ -24,6 +24,12 @@ usage() {
 
 main_apt() {
 	sudo apt install openjdk-16-jdk
+
+	msg "install scala because of the queen"
+	echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+	curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+	sudo apt-get update
+	sudo apt-get install sbt
 }
 
 main_pacman() {
