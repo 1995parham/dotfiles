884fe0931eb2d476808370a0a9718408af7fab71
Parham Alvani <parham.alvani@gmail.com>
2021-10-02 20:22:17 +0330

    feat: add new neovim plugin
    Former-commit-id: da3b4570f618c7016e731098c3427fab42db55c0 [formerly 3446b1ac08aa3ac1ca86aa55d6759b31dc64e26f] [formerly caec45903c1f3daacd8ef13dd469f060b0be2556 [formerly 3fd652c65d09db27473f51ec5780b837703ef4e8 [formerly 3fd652c65d09db27473f51ec5780b837703ef4e8 [formerly 3fd652c65d09db27473f51ec5780b837703ef4e8 [formerly a4c0ea8843f235cfae53d72fe15d35d1efa9dbcb]]]]]
    Former-commit-id: 75219a21330dfd93abeedfeda75a3e44bc0fd724 [formerly fedc1e4641f58c6ca8e1354fd0a12a24a187c1cc]
    Former-commit-id: 0a0c1b543f55e0b525ff4ca14e12bce5eb007fef
    Former-commit-id: d4d8ccff6e0194e8ba6cb9be9b626078c854bc60
    Former-commit-id: d0c034bdd3f21aa8ced0b35a14995039114f5475
    Former-commit-id: 078f21a704c13cf69d0c4557a5daf1241a23de5f

--- nvim/nvim5/nvim/lua/plugin.lua
+++ nvim/nvim5/nvim/lua/plugin.lua
@@ -5,22 +5,22 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({
-		'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-		install_path
-	})
-	execute 'packadd packer.nvim'
+  fn.system({
+    'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  execute 'packadd packer.nvim'
 end
 
 return require('packer').startup(function()
-	-- packer can manage itself
-	use {'wbthomason/packer.nvim'}
+  -- packer can manage itself
+  use {'wbthomason/packer.nvim'}
 
-	-- an alternative sudo.vim for vim and neovim
-	use {'lambdalisue/suda.vim'}
+  -- an alternative sudo.vim for vim and neovim
+  use {'lambdalisue/suda.vim'}
 
-	-- vim-kubernetes
-	use {'andrewstuart/vim-kubernetes'}
+  -- vim-kubernetes
+  use {'andrewstuart/vim-kubernetes'}
 
   -- vim syntax highlighting plugin for json with c-style line (//) and block (/* */) comments.
   use {'kevinoid/vim-jsonc'}
@@ -34,118 +34,126 @@ return require('packer').startup(function()
   -- erlang indentation and syntax for vim
   use 'vim-erlang/vim-erlang-runtime'
 
-	-- naz neovim theme
-	use {
-		'1995parham/naz.vim',
-		branch = 'main',
-		config = function() require('colorbuddy').colorscheme('naz') end,
-		requires = {'tjdevries/colorbuddy.nvim'}
-	}
-
-	-- wakatime
-	use {'wakatime/vim-wakatime'}
-
-	-- markdown vim mode
-	use {
-		'plasticboy/vim-markdown',
-		config = function()
-			vim.g.vim_markdown_folding_disabled = 1
-			vim.g.vim_markdown_math = 1
-		end,
-		requires = { 'godlygeek/tabular' }
-	}
-
-	-- nvim Treesitter configurations and abstraction layer
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		config = function() require('treesitter') end
-	}
-
-	-- find, filter, preview, pick. all lua, all the time.
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-	}
-
-	-- an extension for telescope.nvim that allows you to switch between projects.
+  -- naz neovim theme
   use {
-    'nvim-telescope/telescope-project.nvim',
-		config = function() require'telescope'.load_extension'project' end,
+    '1995parham/naz.vim',
+    branch = 'main',
+    config = function() require('colorbuddy').colorscheme('naz') end,
+    requires = {'tjdevries/colorbuddy.nvim'}
   }
 
-	-- check syntax in vim asynchronously and fix files, with language server protocol (lsp) support
-	use {
-		'w0rp/ale',
-		setup = function() require('ale') end
-	}
+  -- wakatime
+  use {'wakatime/vim-wakatime'}
 
-	-- vim plugin for shfmt
-	use {
-		'z0mbix/vim-shfmt',
-		ft = {'sh', 'zsh', 'bash'},
-		setup = function() vim.g.shfmt_fmt_on_save = 1 end
-	}
+  -- markdown vim mode
+  use {
+    'plasticboy/vim-markdown',
+    config = function()
+      vim.g.vim_markdown_folding_disabled = 1
+      vim.g.vim_markdown_math = 1
+    end,
+    requires = { 'godlygeek/tabular' }
+  }
 
-	-- vim dashboard
-	use {'glepnir/dashboard-nvim', config = function() require('dashboard') end}
+  -- nvim Treesitter configurations and abstraction layer
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    config = function() require('treesitter') end
+  }
 
-	-- neovim statusline plugin written in lua
-	use {
-		'romgrk/barbar.nvim',
-		config = function() require('tabline') end,
-		requires = {'kyazdani42/nvim-web-devicons'}
-	}
+  -- find, filter, preview, pick. all lua, all the time.
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
 
-	-- vim syntax highlighting for i3 config
-	use {'mboughaba/i3config.vim'}
+  -- an extension for telescope.nvim that allows you to switch between projects.
+  use {
+    'nvim-telescope/telescope-project.nvim',
+    config = function() require'telescope'.load_extension'project' end,
+  }
 
-	-- intellisense engine for vim8 & neovim, full language server protocol support as vscode
-	use {
-		'neoclide/coc.nvim',
-		branch = 'release',
-		config = function() require('coc') end
-	}
+  -- check syntax in vim asynchronously and fix files, with language server protocol (lsp) support
+  use {
+    'w0rp/ale',
+    setup = function() require('ale') end
+  }
 
-	-- go development plugin for vim
-	use {'fatih/vim-go'}
+  -- vim plugin for shfmt
+  use {
+    'z0mbix/vim-shfmt',
+    ft = {'sh', 'zsh', 'bash'},
+    setup = function() vim.g.shfmt_fmt_on_save = 1 end
+  }
 
-	-- a vim plugin which shows a git diff in the gutter (sign column) and stages/undoes hunks.
-	use {'airblade/vim-gitgutter'}
-	-- a powerful git log viewer
-	use {'cohama/agit.vim'}
-	-- fugitive.vim: a git wrapper so awesome, it should be illegal
-	use {'tpope/vim-fugitive'}
+  -- vim dashboard
+  use {'glepnir/dashboard-nvim', config = function() require('dashboard') end}
 
-	-- vim plugin for .tmux.conf.
-	use {'tmux-plugins/vim-tmux'}
+  -- neovim statusline plugin written in lua
+  use {
+    'romgrk/barbar.nvim',
+    config = function() require('tabline') end,
+    requires = {'kyazdani42/nvim-web-devicons'}
+  }
 
-	-- neovim statusline plugin written in lua
-	use {
-		'glepnir/galaxyline.nvim',
-		branch = 'main',
-		-- use statusline.lua to setup statusline
-		config = function() require('statusline') end,
-		requires = {'kyazdani42/nvim-web-devicons'},
-		after = { 'coc.nvim', 'nvim-treesitter' },
-	}
+  -- vim syntax highlighting for i3 config
+  use {'mboughaba/i3config.vim'}
+
+  -- intellisense engine for vim8 & neovim, full language server protocol support as vscode
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release',
+    config = function() require('coc') end
+  }
+
+  -- go development plugin for vim
+  use {'fatih/vim-go'}
+
+  -- a vim plugin which shows a git diff in the gutter (sign column) and stages/undoes hunks.
+  use {'airblade/vim-gitgutter'}
+  -- a powerful git log viewer
+  use {'cohama/agit.vim'}
+  -- fugitive.vim: a git wrapper so awesome, it should be illegal
+  use {'tpope/vim-fugitive'}
+
+  -- vim plugin for .tmux.conf.
+  use {'tmux-plugins/vim-tmux'}
+
+  -- neovim statusline plugin written in lua
+  use {
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    -- use statusline.lua to setup statusline
+    config = function() require('statusline') end,
+    requires = {'kyazdani42/nvim-web-devicons'},
+    after = { 'coc.nvim', 'nvim-treesitter' },
+  }
 
   -- a file explorer tree for neovim written in lua
-	use {
-		'kyazdani42/nvim-tree.lua',
-		config = function() require('tree') end,
-		requires = {'kyazdani42/nvim-web-devicons'},
-	}
+  use {
+    'kyazdani42/nvim-tree.lua',
+    config = function() require('tree') end,
+    requires = {'kyazdani42/nvim-web-devicons'},
+  }
 
   -- vimtex: a modern vim and neovim filetype plugin for latex files.
   use 'lervag/vimtex'
 
-	-- whichkey is a lua plugin for Neovim 0.5 that displays a popup with
-	-- possible keybindings of the command you started typing.
-	use {
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup {
-			}
-		end
-	}
+  --- pop-up menu for code actions to show meta-information and diff preview
+  --[[
+  use({
+    'weilbith/nvim-code-action-menu',
+    cmd = 'CodeActionMenu',
+  })
+  --]]
+
+  -- whichkey is a lua plugin for Neovim 0.5 that displays a popup with
+  -- possible keybindings of the command you started typing.
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+      }
+    end
+  }
 end)
