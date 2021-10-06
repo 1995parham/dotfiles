38d694c81f23d50aa036df3f82155d1dd9a009f2
Parham Alvani <1995parham@tuta.io>
2021-10-06 15:35:35 +0330

    Revert "Removes executable permissions and update platformio."
    This reverts commit 32b47533bf72e099ec52a6457e56ec9a77587ce4 [formerly 5b547c1f197c7e2d8d3f2e1a23d4f342410e9183] [formerly fd1f89b25ed6e859f186ca2ae01efebff7ef0a7d] [formerly e925d2a7744da36fca0fdbc19a87c10f54197abe] [formerly 4b167029f8337b2af2a910b444573a22bb647157] [formerly 2d342114cdb361642fb2bb690c6b0a6cca319537 [formerly e22c2d07ebf0e040b7354ad4e3e25cf02bffad7a]] [formerly 2c8bee2f14312ff84feb2fd0d55397859d5e8a76 [formerly 9e9d5255340f6c69041de64e3b0794b8a1dbab0b] [formerly b0156622592228feb1332ea46a7883ab14eef8b3 [formerly ca370cd2ef5d122abec1d8e1dfaaa65227dbf50a [formerly ca370cd2ef5d122abec1d8e1dfaaa65227dbf50a [formerly ca370cd2ef5d122abec1d8e1dfaaa65227dbf50a [formerly 280677784c11d9ba1c67b167f8b2d05cdde1063f]]]]]].
    
    
    Former-commit-id: b57cb5bd5bfe30aedab78e00196fc8d94ab70d58 [formerly d6331f290c43b248a2326c48f48874df71a0a318] [formerly e15772c7116f3937500923fe731050122525b9e7 [formerly 44b8b9f6c466383a44ab8f64eaead3a3ead9417f [formerly 44b8b9f6c466383a44ab8f64eaead3a3ead9417f [formerly 44b8b9f6c466383a44ab8f64eaead3a3ead9417f [formerly 2c6e45aa2ac6111c903a5d4d1add80a87a674314]]]]]
    Former-commit-id: e42a58be50700d2abec8057d162218e3575b4eab [formerly 99be756f963cbaa16ad0ae46a3d236d2fa869c6c]
    Former-commit-id: 130a6e5507cf90b758ce0bd217ce5be0507f38f9
    Former-commit-id: cf1f993ae5c3ef2845c9720f051b2c0d489d1ff5
    Former-commit-id: b7a594a72e0f97484289ace4650aaf3d51b12db1
    Former-commit-id: 473129bd6368d86cf5fd42703b24d19b78bc75c8

---
+++ scripts/platformio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : platformio.sh
+#
+# [] Creation Date : 13-12-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+if [[ $EUID -ne 0 ]]; then
+	echo "[platformio] This script must be run as root"
+	exit 1
+fi
+
+echo "[platformio] Installing PlatformIO"
+pip2 install -U platformio
+
+echo "[platformio] Installing udev rules"
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/scripts/99-platformio-udev.rules >/etc/udev/rules.d/99-platformio-udev.rules
+service udev restart
