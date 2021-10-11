18f7fdd122fac3e73bfa61a82ab14f628b6fc002
Parham Alvani <1995parham@tuta.io>
2021-10-11 20:52:23 +0330

    feat: add stm installation script
    Former-commit-id: 39e5e23830526dff2c3a5a3561a37210800fa078 [formerly f556c3d918ef741e0f050396d5aea00ad1632caa] [formerly 4694345ec85f3a2ced76e634df5f4f450a029bf7 [formerly 4c12fbd74a26c18b434a1a8f16402d51f2aa3329 [formerly 4c12fbd74a26c18b434a1a8f16402d51f2aa3329 [formerly 4c12fbd74a26c18b434a1a8f16402d51f2aa3329 [formerly 48914925766230d6a1043d9a6518c5a69b402979]]]]]
    Former-commit-id: 1da55a92e545b5b950a97b6d6405bf6d20ed381b [formerly cd20377f83513b9ac95d79555c396d2170722be6]
    Former-commit-id: 1964e1af6775ec01b9000232c436d641aa15fd0a
    Former-commit-id: 2c8255b7daf84c0f86d12acb61654f7c883f64d1
    Former-commit-id: 165105484e1b848ed18ec2afbd5d3fb56e8c90a3
    Former-commit-id: 21a50ef5330c550859edf43993ac9b9561ab9b07

---
+++ scripts/stm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : stm.sh
+#
+# [] Creation Date : 25-05-2021
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+# shellcheck disable=2034
+dependencies="rust"
+
+usage() {
+	echo -n -e "have fun with stm discovery board and the lovely rust"
+
+	# shellcheck disable=1004,2016
+	echo '
+     _
+ ___| |_ _ __ ___
+/ __| __| |_ ` _ \
+\__ \ |_| | | | | |
+|___/\__|_| |_| |_|
+
+  '
+}
+
+main_pacman() {
+	sudo pacman -Syu --needed --noconfirm arm-none-eabi-gdb \
+		minicom \
+		openocd \
+		bluez \
+		bluez-utils \
+		rfkill
+
+	rustup component add llvm-tools-preview
+	rustup target add thumbv7em-none-eabihf
+	cargo install cargo-binutils
+
+	lsusb | grep ST-LINK
+}
