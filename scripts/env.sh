1dd5b6cbff975326618de80814632a40b7f55bd9
Parham Alvani <1995parham@tuta.io>
2021-10-13 03:05:25 +0330

    feat: add new package
    Former-commit-id: b53b94d8b28d29ae195b7c0544bd14782ee5354d [formerly a144cdbc6445e7e74c8ac66c56332c4b02ee835c] [formerly fafce36a20a4f9a7b5b192bfa3f6237b73d4b764 [formerly b34cb797bbacdf2865fbab01e3a46b3f253d9d59 [formerly b34cb797bbacdf2865fbab01e3a46b3f253d9d59 [formerly b34cb797bbacdf2865fbab01e3a46b3f253d9d59 [formerly 7d17a09336dbd303a91420ec44b436c464609874]]]]]
    Former-commit-id: 7e9c30341de639fb8610b0f60795893e56fec3a0 [formerly 2acbd791dd0a4d9b8b29e7572ac2185abe79754f]
    Former-commit-id: ab17255ce8df5d02c523069b594a1be5aedb0b21
    Former-commit-id: 4a1a07e203bd544808a5216a629a8a9ae8010072
    Former-commit-id: a0fdce185eea188a5e4401419aef6cd7f73d089f
    Former-commit-id: 9ede3a2358c118a03cf0818337027b080898370e

--- scripts/env.sh
+++ scripts/env.sh
@@ -25,7 +25,7 @@ packages=(zsh tmux htop aria2 curl bat vim jq yamllint fzf mosh figlet)
 
 brew_packages=(coreutils k6 inetutils inxi fontconfig wget)
 apt_packages=(bmon atop)
-pacman_packages=(mtr atop github-cli inetutils websocat fuse2 dog yq man-pages usbutils)
+pacman_packages=(mtr atop github-cli inetutils websocat fuse2 dog yq man-pages usbutils exfat-utils)
 yay_packages=(jcal jwt-cli-bin actionlint-bin)
 
 main_apt() {
