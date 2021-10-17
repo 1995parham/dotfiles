f114bb96bc5a34e560d6cbed281f6a0b7d4dffa4
Parham Alvani <parham.alvani@gmail.com>
2021-10-17 22:17:09 +0330

    feat: update docker script
    Former-commit-id: 648670ba4b97eb9583cbaf0f7fa96b01c08d0660 [formerly b6c0d657a8561de766670c076661102011531160] [formerly 646041db248028df528f210a6fce80b38463a76f [formerly 8e7087db1472d2d34d69f6945db142deb533d19e [formerly 8e7087db1472d2d34d69f6945db142deb533d19e [formerly 8e7087db1472d2d34d69f6945db142deb533d19e [formerly 809a9fe4b1a20bf1b87e464e476b6f03df5aa788]]]]]
    Former-commit-id: 97f5e8a04acd23e5d01a8552b77a7d13313022c9 [formerly 87279eb39335ebf08a715207dab2f85fa3cb768c]
    Former-commit-id: 67c8a026970c600980e3c145458c962b1c0910c0
    Former-commit-id: f2e8f0c5bafb15e668b26504d12464b246f90253
    Former-commit-id: f2902d4fbde5fcc76cb4aead45adf81f1735bea7
    Former-commit-id: 502f039860030729faef2ecdc93dd23d0a3e532e

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -72,7 +72,7 @@ main_brew() {
 
 main_pacman() {
 	msg "install podman-compose / podman with pacman"
-	sudo pacman -Syu --noconfirm --needed podman podman-docker podman-compose slirp4netns podman-dnsname
+	sudo pacman -Syu --noconfirm --needed podman podman-docker podman-compose docker-compose slirp4netns podman-dnsname
 
 	msg "install hadolint/hadolint with yay"
 	yay -Syu --needed --noconfirm hadolint-bin
