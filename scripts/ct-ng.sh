e41ad6e027ca87fe89855846dc9f7a9ed7e9f79e
Parham Alvani <parham.alvani@gmail.com>
2021-10-27 23:23:06 +0330

    feat: add installaer for crosscompile tool
    Former-commit-id: ea8c1bf0b5cdf35caa7f278ef9edfa1a66e08afb [formerly d0ff5b5159f013c110af3f3a2188b6b103aad3d6] [formerly 568b2a1fc157be9ec70f08f9a526603b8f72713c [formerly 951b52eb287ff888252fe5af77af0974a86556a2 [formerly 951b52eb287ff888252fe5af77af0974a86556a2 [formerly 951b52eb287ff888252fe5af77af0974a86556a2 [formerly 7322dd7cb736bfeda2705b191187c758f3e2803a]]]]]
    Former-commit-id: 3ec58f4b8e548dbdf1b624256838025239f5a81f [formerly ca5cfc64e2ba7473593f108c63ef80f315f5e9cc]
    Former-commit-id: cd665a086507b80cf757fb6d33dbec8157f943f7
    Former-commit-id: 26e62f9ec05fa4b403109981a9b810bc1cdfd4b3
    Former-commit-id: 95bf6e66042f8e0c54c3e142df527b2d7c89c484
    Former-commit-id: e78fe92cc1e95bb0c1f95f3b380457bfd5cc6325

---
+++ scripts/ct-ng.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : ct-ng.sh
+#
+# [] Creation Date : 17-07-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+usage() {
+	echo -n -e "crosstool-ng to compile what we needs as embedded developer"
+
+	# shellcheck disable=1004,2016
+	echo '
+      _
+  ___| |_      _ __   __ _
+ / __| __|____| |_ \ / _` |
+| (__| ||_____| | | | (_| |
+ \___|\__|    |_| |_|\__, |
+                     |___/
+  '
+}
+
+main_apt() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_pacman() {
+	yay -Rsu --noconfirm --needed crosstool-ng
+}
+
+main_brew() {
+	msg "there is nothing that we can do"
+	return 1
+}
