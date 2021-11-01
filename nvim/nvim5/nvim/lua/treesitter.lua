0346ecf2c072c4fd7630dab86d04841bff057e99
Parham Alvani <parham.alvani@gmail.com>
2021-11-01 11:11:03 +0330

    fix: correct yaml highlighting for helm charts
    Former-commit-id: eaab31fde13d2fbaf2c36e2788f946d36c7cfccc [formerly 71dcd8bb7b4ea83506df6472fcfe15b33d54d912] [formerly d9636b7d39b4760c74fe574508ab61dd074ff1c9 [formerly 5685ad15723d1f79e98a27f3d958906e65d270f4 [formerly 5685ad15723d1f79e98a27f3d958906e65d270f4 [formerly 5685ad15723d1f79e98a27f3d958906e65d270f4 [formerly 2503d29e1db1939bfd85b0fa12784c222a5200a9]]]]]
    Former-commit-id: 8e0ff7f8513bfb1f25a36582c8cb9911f498c379 [formerly ce77b644190251493c8fb67df60623df9ce5a7cf]
    Former-commit-id: 07e5f45dc783f1aa0fe8018a77fa889fef1730d8
    Former-commit-id: 57ea09715878434f216a08413b2ff0be4eea9ccf
    Former-commit-id: 29c8477402a1ac1a6ea98265f7147e0a9f49ea3f
    Former-commit-id: 50b3d86e83cba6564b107e02d78891f63833660b

--- nvim/nvim5/nvim/lua/treesitter.lua
+++ nvim/nvim5/nvim/lua/treesitter.lua
@@ -1,11 +1,11 @@
 require('nvim-treesitter.configs').setup {
     -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = {"lua", "go", "rust", "c", "bash", "json", "yaml", "python", "javascript"},
+    ensure_installed = { "lua", "go", "rust", "c", "bash", "json", "yaml", "python", "javascript" },
     -- list of parsers to ignore installing
     ignore_install = {},
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
+        disable = { "yaml" }, -- list of language that will be disabled
     },
     incremental_selection = {enable = true},
     indent = {enable = true}
