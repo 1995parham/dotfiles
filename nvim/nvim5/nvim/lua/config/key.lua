cfad0b73cd63d39b0ac447cf140f239b7c57a036
Parham Alvani <parham.alvani@gmail.com>
2021-12-25 08:33:47 +0330

    chore: remove snip engine
    Former-commit-id: b956e5628132ff575951253e5854b6ba40bb2650 [formerly f396668e3fd02ff4e32552d1e7088f453399b5de] [formerly f487771ef990ea0a476c7c28734d1f5370e373cb [formerly c4fa99cd3d1260e9cec630cf1c2c4508c3f46d94 [formerly c4fa99cd3d1260e9cec630cf1c2c4508c3f46d94 [formerly c4fa99cd3d1260e9cec630cf1c2c4508c3f46d94 [formerly 2e984294b51aed44ad6006f3b0ddbabce017b338]]]]]
    Former-commit-id: 6b63ce5944be58ef71ab4d9061d510a3d79cbe3a [formerly 7eea9977caaba35fff4dccb345d5d9e35fec0d23]
    Former-commit-id: be11fe3d38823bb690a2ce5127882da79ae948d5
    Former-commit-id: 5dce6fc024f01b5b898f34d32c2a1bffdfc99cc2
    Former-commit-id: 853198e64db6444b8d23f0daca6814cbe0c436d1
    Former-commit-id: f441a04b4c2c72c2a6e1c4a0d96e27fda8d1bd39

--- nvim/nvim5/nvim/lua/config/key.lua
+++ nvim/nvim5/nvim/lua/config/key.lua
@@ -85,13 +85,3 @@ vim.api.nvim_set_keymap('n', 'gv', '<Plug>(coc-type-definition)',
 vim.api
 .nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {silent = true})
 vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
-
--- snippets.nvim
-
--- <c-k> will either expand the current snippet at the word or try to jump to
--- the next position for the snippet.
-vim.api.nvim_set_keymap('i', '<c-k>', "<cmd>lua return require'snippets'.expand_or_advance(1)<CR>", {})
-
--- <c-j> will jump backwards to the previous field.
--- If you jump before the first field, it will cancel the snippet.
-vim.api.nvim_set_keymap('i', '<c-j>', "<cmd>lua return require'snippets'.advance_snippet(-1)<CR>", {})
