025831894bf897a108ae34de5a49029ac7841cfd
Parham Alvani <parham.alvani@gmail.com>
2022-01-10 11:34:17 +0330

    feat: update company-mode setting
    Former-commit-id: d371cc477451a750afaa93fe40161c8a18199f09 [formerly 2b205b8511318cebacf3f6cf1cfafe0d1e613fb1] [formerly dfc6e765b06c470b5ef9e56bb0f093d7fa7a4f54 [formerly 0bf36edcccc33fb4a997c1a1f07f6c58518bad47 [formerly 0bf36edcccc33fb4a997c1a1f07f6c58518bad47 [formerly 0bf36edcccc33fb4a997c1a1f07f6c58518bad47 [formerly e00dc3ce14aa44eca09ae95c88e67a8784643ba2]]]]]
    Former-commit-id: 4e8c9add6429a9894752e198fcfc417f622a0832 [formerly 92467d1f619f62796098cb789a80ae965e2d2981]
    Former-commit-id: 68c7c247711708e7d1384615765b4f2b5ee06ee2
    Former-commit-id: 340143acb1f618bf82490c756101685b8c6318f3
    Former-commit-id: f129aef28dc81c1e27d44d695a214f5516dc4b07
    Former-commit-id: 3182943594b9a716e0f6bd4bb94b2564be391d19

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -20,10 +20,10 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company             ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
-       ivy                 ; a search engine for love and life
+       (company +childframe) ; the ultimate code completion backend
+       ;;helm                ; the *other* search engine for love and life
+       ;;ido                 ; the other *other* search engine...
+       ivy                   ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
