64b183ee17983af2119a827c631f0010a0f14d91
Parham Alvani <parham.alvani@gmail.com>
2022-02-16 19:37:11 +0330

    feat: update doom configuration
    Former-commit-id: 14105f957ebcc4ac0f3eab882f7b9daf3a7b015a [formerly bc7dfa9b7c41df0a687dcbe96844c9f8c9cad13f] [formerly 0582dd393f70a9ab3d7ea59f201cf27efdd92fc9 [formerly 4ee9fef15983fa9f4812120d52501a154559290d [formerly 4ee9fef15983fa9f4812120d52501a154559290d [formerly 4ee9fef15983fa9f4812120d52501a154559290d [formerly 55310b4b57270097c8c67bf6cae195439011a107]]]]]
    Former-commit-id: 27fea16d0e217c75070cc4251e4e37bee6c900bf [formerly ae4e8bff8edc2fe7e73483e8c0eab3bf3cdd470f]
    Former-commit-id: 178f09c10065575109ed82eda13434d22c851229
    Former-commit-id: c688edf2323ce4f4b1044bb2433e54aa9729e9ae
    Former-commit-id: 4780cbb3b0d6b31c6c77c97a86ed7aef293aabf7
    Former-commit-id: e26db13c76091ffb8c09afba87b831c96b53a13c

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -37,14 +37,14 @@
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       modeline            ; snazzy, Atom-inspired modeline, plus API
+       ;;modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        neotree             ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
-       unicode             ; extended unicode support for various languages
+       ;;unicode             ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
@@ -70,7 +70,7 @@
        electric            ; smarter, keyword-based electric-indent
        ;;ibuffer           ; interactive buffer management
        undo                ; persistent, smarter undo for your inevitable mistakes
-       vc                  ; version-control and Emacs, sitting in a tree
+       ;;vc                  ; version-control and Emacs, sitting in a tree
 
        :term
        eshell              ; the elisp shell that works everywhere
