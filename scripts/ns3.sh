27f5fd301a4a8afa0dc382f760834927c49b0320
Parham Alvani <parham.alvani@gmail.com>
2022-02-18 09:10:41 +0330

    feat: improve ns-3 installation script
    Former-commit-id: ec6011e57ff9d6140b8a40311e477f037a8c6ea2 [formerly 115ea669a8751c456cf916d2c50d13292fb13750] [formerly 0171fcbc6c4f9c3589017c8216963bca9d628a5e [formerly 2f738f3051d7cdfe0e18730490b4172ad9d4b198 [formerly 2f738f3051d7cdfe0e18730490b4172ad9d4b198 [formerly 2f738f3051d7cdfe0e18730490b4172ad9d4b198 [formerly 31ce8f65e9e12c2fa870942b06305412f13ad3c9]]]]]
    Former-commit-id: f33877ed719fb639ff6de6ec2d130b03f4937376 [formerly b3b64b0595d75f5496d5934f933c78d54a77d887]
    Former-commit-id: adb0b1f7899766927ded2446c9578353a731cb70
    Former-commit-id: e184abf4896b5e8910d8ff5ada402cd8b302eefc
    Former-commit-id: d8ef34927cf4762142964b2b35fdf0062866dc04
    Former-commit-id: 27ca32796ebda55fb5ab74d58f0f9f6e2d37c368

--- scripts/ns3.sh
+++ scripts/ns3.sh
@@ -8,31 +8,34 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 
+version="3.35"
+
 usage() {
 	echo -n -e "research on [at citado] for wireless network"
 
 	# shellcheck disable=1004,2016
-	echo '
+	echo "
            _____
  _ __  ___|___ /
 | |_ \/ __| |_ \
 | | | \__ \___) |
 |_| |_|___/____/
 
-  '
+version: $version
+  "
 }
 
 main() {
 	mkdir -p "$HOME/Documents/Git/parham/citado/netsim"
 	cd "$HOME/Documents/Git/parham/citado/netsim" || exit
 
-	if [ ! -d "ns-allinone-3.35" ]; then
-		aria2c https://www.nsnam.org/releases/ns-allinone-3.35.tar.bz2
-		tar xjf ns-allinone-3.35.tar.bz2
-		rm ns-allinone-3.35.tar.bz2
+	if [ ! -d "ns-allinone-$version" ]; then
+		aria2c "https://www.nsnam.org/releases/ns-allinone-$version.tar.bz2"
+		tar xjf "ns-allinone-$version.tar.bz2"
+		rm "ns-allinone-$version.tar.bz2"
 	fi
 
-	cd ns-allinone-3.35/ns-3.35 || exit
+	cd "ns-allinone-$version/ns-$version" || exit
 	./waf configure --enable-examples --enable-tests --disable-python
 	./waf build
 	./test.py
