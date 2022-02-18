fecb8ae035b8d006c6b62848cf977a80da43c70c
Parham Alvani <parham.alvani@gmail.com>
2022-02-18 09:13:39 +0330

    feat: disable more settings on doom to increase its speed

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -45,7 +45,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-molokai)
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
