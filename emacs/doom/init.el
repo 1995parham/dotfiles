7746be171d4456c6f98b7613ff957bb1c242b8c1
Parham Alvani <parham.alvani@gmail.com>
2022-02-18 09:13:39 +0330

    feat: disable more settings on doom to increase its speed
    Former-commit-id: 58aaaec958914042aeb300031a5a11acd0a1dc54 [formerly 411de96af94ab10cb0c99c34e1d390e713f3652a] [formerly 8c88667eef6724d9f62ff5dc172b4a6c3c8c527b [formerly e518bdc7f3b33d18dfe2dcd1dab8bc29e62833dd [formerly e518bdc7f3b33d18dfe2dcd1dab8bc29e62833dd [formerly e518bdc7f3b33d18dfe2dcd1dab8bc29e62833dd [formerly fecb8ae035b8d006c6b62848cf977a80da43c70c]]]]]
    Former-commit-id: 7ac9a0ef5b63adb858c6051081987e62b8a6bf31 [formerly 07fcbf232d9c5c80f54cd343c0c566059c3b2488]
    Former-commit-id: b965a5cd98e5ed0a429ba697d7740eebaeafce40
    Former-commit-id: 52edbbb75007361595f87dcbe5a72ca19835734f
    Former-commit-id: b382b12ac70e7da5f899f4341c5515caf1ddd7e6
    Former-commit-id: bf1864e93641ef5786abe7d6d8f7c3905133e7f9

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -138,7 +138,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex )            ; writing papers in Emacs has never been so fun, add +lsp if you want slow lsp
+       ;;(latex )          ; writing papers in Emacs has never been so fun, add +lsp if you want slow lsp
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
