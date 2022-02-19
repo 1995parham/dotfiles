f7ea63c50ba37cedf3f04c48faaf2f368b12f614
Parham Alvani <parham.alvani@gmail.com>
2022-02-19 07:11:43 +0330

    chore: add unused auctex packages
    Former-commit-id: 651cdca0a28ade070dd4c5839b3953f8bfb35592 [formerly e39a043bf5d91ff7ab0c63f3005d06cac7a433cc] [formerly 2e3acff18432df56f04088165f2933d63ec879c3 [formerly 9e5cc92dd673ae75ff2b746ffd2976368a01eb5f [formerly 9e5cc92dd673ae75ff2b746ffd2976368a01eb5f [formerly 9e5cc92dd673ae75ff2b746ffd2976368a01eb5f [formerly 43eacfd2d03f0a57aea8200e8db8a621ea7594f0]]]]]
    Former-commit-id: 892a955f177e92a1a165036f57a01ee42f1e1bcd [formerly bbc5b3856c00d84a1d036847a7fc000d1535f037]
    Former-commit-id: 64133416443b9e098307ddc744d133f3194ed45e
    Former-commit-id: 19d0b8e4e501f30dbc60107718264d29fe5b0a6c
    Former-commit-id: e647269d6bb9546153f664ec40f4dcc639696731
    Former-commit-id: fb92016cf51fb10e30ca666353967f10b48f7d1a

--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -7,7 +7,8 @@
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+;;(package! auctex)
+;;(package! evil-tex)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
