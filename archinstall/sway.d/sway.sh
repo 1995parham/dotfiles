cebfc0751acd46a2d6cb5a1a3523bdcbad9fda91
Parham Alvani <parham.alvani@gmail.com>
2022-03-06 02:32:16 +0330

    feat: improve sway lunch script
    Former-commit-id: 33480f858cb49b5cad20ea77cfb303624a8270f8 [formerly c1345be4a00a5347b3e0a58e3a0eb26af5df789c] [formerly f9773fcac26cb3c78583c10fba420819148ee76f [formerly 4c54a86bf1a232934c00466aa072f8d8756b9d28 [formerly 4c54a86bf1a232934c00466aa072f8d8756b9d28 [formerly 4c54a86bf1a232934c00466aa072f8d8756b9d28 [formerly 58f67351ecb0f5eb261b418a92ac7e0794d70d9c]]]]]
    Former-commit-id: 151918474d17b46550d5af3f4876534c0e2cc56a [formerly c711c5c6d01e1f29c2fbb728cc94f6bab9a7460d]
    Former-commit-id: 8f50ea7e4df57ac9e0ae8e7e37f645e5121ee60f
    Former-commit-id: 813707fdb4dfbe10d3485e42c462d9f0039b79bb
    Former-commit-id: 53675e82756c2be1c53671606e23a67bd807ecfe
    Former-commit-id: 7dfcc71624ea9b94a84310f8890aa12f202580af

--- archinstall/sway.d/sway.sh
+++ archinstall/sway.d/sway.sh
@@ -5,8 +5,9 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORM=wayland
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
+export MOZ_ENABLE_WAYLAND=1
 
-eval $(gnome-keyring-daemon --start)
+eval "$(gnome-keyring-daemon --start)"
 export SSH_AUTH_SOCK
 
 exec sway --unsupported-gpu
