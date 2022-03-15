784d81d28a950463021a17d205034fb53e5ee2f2
Parham Alvani <parham.alvani@gmail.com>
2022-03-16 00:13:23 +0330

    feat: update sway luncher script

--- archinstall/sway.d/sway.sh
+++ archinstall/sway.d/sway.sh
@@ -3,9 +3,13 @@
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
+# to enable wayland on firefox
 export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
 
 eval "$(gnome-keyring-daemon --start)"
 export SSH_AUTH_SOCK
