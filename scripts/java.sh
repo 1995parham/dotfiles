09cb69180978acde847c298d78b3fe6fa40306c6
Parham Alvani <parham.alvani@gmail.com>
2022-03-31 19:29:48 +0430

    Revert "feat: add jdtls"
    This reverts commit 95e6dd7692a9c8efcbc1f9ececf725426732fd87 [formerly 2f02f52e17f1687ec457603ae419552eccc954c0] [formerly 47c1a2cadffd634bc9b5bbec894d1d005f1e3097] [formerly 489ca8f6a6b3516235694243be798eeea0f79b71] [formerly 11590610a24a4748a4388e9d059ac5756645509a] [formerly f93983da8eda49d603afa134356c4923f9550024 [formerly 43b89959d42e2608ecc3104a0e11622eda748272]] [formerly a61119f8e961020d231c402da4993209dd83c6b2 [formerly f8576de69ce1cd4ad99aeeff334db57297444760] [formerly 104b1d00e733a83c20f5d63f8eec5648344d47ff [formerly 5b12bd1d2ced73432370e92852c7ed0209e36c50 [formerly 5b12bd1d2ced73432370e92852c7ed0209e36c50 [formerly 5b12bd1d2ced73432370e92852c7ed0209e36c50 [formerly d7847a827823732a75d711426ce161342fcff7ed]]]]]].
    
    
    Former-commit-id: c705d941824c88319cd11515815492181aec957d [formerly 26885d114e07a14eb50d7b4166963d00436cfdd5] [formerly bef827b3bdf34ae8c20788582f6979be2734da7e [formerly 809701886a47507275a637965b3bb80aead4c4d9 [formerly 809701886a47507275a637965b3bb80aead4c4d9 [formerly 809701886a47507275a637965b3bb80aead4c4d9 [formerly fcd5171815d96ec5a3d8f5ae4cbb224cde07e87e]]]]]
    Former-commit-id: b6f90b31fdec5b390400f6b768dddce323f30eff [formerly e120d70321f406ed63ea65d7cfd44ed7734a6de9]
    Former-commit-id: c4b2a531559ad9b76c5308900c901abd4456dfca
    Former-commit-id: b74e13620a5de49d3a9fd1f303234f7a80119f51
    Former-commit-id: 380982831428befd5f86dedea669274cb25d01ba
    Former-commit-id: 2b315d90a39c32b9bee9be38be77ac45b7f3fc3d

--- scripts/java.sh
+++ scripts/java.sh
@@ -36,8 +36,6 @@ main_pacman() {
 	sudo pacman -Syu --needed --noconfirm jdk-openjdk gradle maven
 	msg "install scala because of the queen"
 	sudo pacman -Syu --needed --noconfirm sbt
-	msg 'install eclipse jdt language server'
-	proxy_start && yay -Syu jdtls && proxy_stop
 }
 
 main_brew() {
