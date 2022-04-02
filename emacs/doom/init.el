3d7f752cce3ed3703d66e36ad30644c72e1cef81
Parham Alvani <parham.alvani@gmail.com>
2022-04-02 09:33:10 +0430

    feat: switch to term because there is no compile
    Former-commit-id: 643d5477b81a3931bd514504c648cda3c5ec4e65 [formerly 4d84f546fca660cacd74423bb69872fc35a75bdb] [formerly 58f8ece009d891f23e8d4435456da2568711f17a [formerly 8b4e51d4ea99ed06d2230bfb5bfe89e5e2b116c7 [formerly 8b4e51d4ea99ed06d2230bfb5bfe89e5e2b116c7 [formerly 8b4e51d4ea99ed06d2230bfb5bfe89e5e2b116c7 [formerly d83b458780005e25de2b4abc9af79c8d1a44ef3a]]]]]
    Former-commit-id: fb72c48844908d757cd4d4aa053a4994c60719c2 [formerly bd61fcc6bb958d3793be258e248b783ec6b8b64f]
    Former-commit-id: 773678903035519486e6185da2a4734b1939d37d
    Former-commit-id: e55f870582dfb7cd3bb3465c4bcba4ee44acb195
    Former-commit-id: ca51e2f57c375f467e85629da018dec0c8d5ac27
    Former-commit-id: 9f33f477a61744f35a3c54848cb380932b617a4b

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -75,8 +75,8 @@
        :term
        eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
-       ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       term              ; basic terminal emulator for Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        ;;syntax            ; tasing you for every semicolon you forget
