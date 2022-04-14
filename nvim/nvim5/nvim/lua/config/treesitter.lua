678e2da61b0adf77f525fc3df56ae75ce14e0b83
Parham Alvani <parham.alvani@gmail.com>
2022-04-14 21:11:04 +0430

    Revert "feat: update neorg support"
    This reverts commit 99ede725eaed4392574a884530a73b4020bc979d [formerly 3e6c8cae16993b8b0f550360080b886b702e5f15] [formerly cf5468da4f2b7fd734980afe50a41e64c2ff83cc] [formerly e3c037b4ca6f89a12ad001d95e2fec63386480bc] [formerly a49ce2272c3dff1da7ea6bc0565d95de07194568] [formerly c53a75e76f34aace379b9a047490038142a683f2 [formerly a17119d312adf12038b1c8d66f40fd9890dfd01f]] [formerly 51eacc98d2eb721c5d9a04a2df3d0d6d7b4c1f76 [formerly e1ed733bbd5ba8fd8a0a64e291847adafb2d4e07] [formerly 38c376ceb09b3946ca07acf6bb1b7abc5c532636 [formerly ca09960d84e554739e279e9cb041e597a38d2acc [formerly ca09960d84e554739e279e9cb041e597a38d2acc [formerly ca09960d84e554739e279e9cb041e597a38d2acc [formerly b6957c089ecebf86ce571358d8177b44246a2398]]]]]].
    
    
    Former-commit-id: da7dd321def93c2b53b07f418712a2ca92b9ca0c [formerly ba1b72e5b126f48f823315990878f9dfa27cea59] [formerly 2d79c3287bcd2c44848385f238e39cefa1574451 [formerly 79d0af606ea8ed756689c0ba53ac81eb6bcc5235 [formerly 79d0af606ea8ed756689c0ba53ac81eb6bcc5235 [formerly 79d0af606ea8ed756689c0ba53ac81eb6bcc5235 [formerly 0083ddf6d7a762fa9e050dfa49df7ac165f05a4c]]]]]
    Former-commit-id: 8e61ca72c91a4b0f16f56184c4318f3a30ac9e10 [formerly 81cc50ec5b8528c11b3f4e8d2753e8200ba19511]
    Former-commit-id: 55e4c6ad0d960b19e4f50368ea19d05b7d2cf79d
    Former-commit-id: 8b399bc66f60a1b14af9e140efbe73d0859afb47
    Former-commit-id: 21ac106ac265ee27feb27d535ac73e9e7bce0f3d
    Former-commit-id: a62c8fbf67b544020a649ae15675bc87f8ea258f

--- nvim/nvim5/nvim/lua/config/treesitter.lua
+++ nvim/nvim5/nvim/lua/config/treesitter.lua
@@ -1,26 +1,6 @@
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
--- These two are optional and provide syntax highlighting
--- for Neorg tables and the @document.meta tag
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
 require('nvim-treesitter.configs').setup {
     -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = { "lua", "go", "rust", "c", "bash", "json", "yaml", "python", "javascript", "cpp", "norg", "norg_meta", "norg_table" },
+    ensure_installed = { "lua", "go", "rust", "c", "bash", "json", "yaml", "python", "javascript", "cpp", "norg" },
     -- list of parsers to ignore installing
     ignore_install = {},
     highlight = {
