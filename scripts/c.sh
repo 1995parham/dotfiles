49285ea9bd7d0928ae6cada7a0d3eceb38842dfc
Parham Alvani <parham.alvani@gmail.com>
2022-05-02 21:16:11 +0430

    feat: add new tool for c
    Former-commit-id: 428f74a76f3569893428e4d5cbb7592fe7380eb9 [formerly e173bb201fa9094a1cc168fe347f3a0bd5ae8812] [formerly c0b6318b55fa1b5b5a52672188924d8de0e71a96 [formerly b537c76478c944560a4a80ed646c1977a436f6eb [formerly b537c76478c944560a4a80ed646c1977a436f6eb [formerly b537c76478c944560a4a80ed646c1977a436f6eb [formerly 56818172a8dab9da38aef9ddfd0cc1cde2bf76b7]]]]]
    Former-commit-id: 14ebb126f2a1a896b193ffacb6a29b34ba2b791c [formerly 5548aacba9df61c6bc267164ee9b240a5dd827f4]
    Former-commit-id: 284935dddfe8ce52989c253943ee6271674dfa1c
    Former-commit-id: 5cb4b4c2e7c9783c9e4fb21608de01cfc4fb75b7
    Former-commit-id: 3b84eda03171de5b03edbf649afbded5c739cabd
    Former-commit-id: 032411ca2aeadfdcc3c2566cc9be6ae539e9eb67

--- scripts/c.sh
+++ scripts/c.sh
@@ -21,7 +21,7 @@ usage() {
 
 main_pacman() {
 	msg 'install clang and clangd'
-	sudo pacman -Syu --needed --noconfirm clang cmake valgrind
+	sudo pacman -Syu --needed --noconfirm clang cmake valgrind meson
 }
 
 main_brew() {
