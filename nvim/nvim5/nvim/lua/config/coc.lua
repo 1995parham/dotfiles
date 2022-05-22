260c69329116ec6ef27e3e396f7c9d79c243d9ca
Parham Alvani <parham.alvani@gmail.com>
2022-05-22 23:55:31 +0430

    feat: add support for coc floating windows

--- nvim/nvim5/nvim/lua/config/coc.lua
+++ nvim/nvim5/nvim/lua/config/coc.lua
@@ -10,6 +10,12 @@ vim.g.coc_global_extensions = {
 vim.g.coc_filetype_map = {['yaml.docker-compose'] = 'yaml'}
 
 -- highlight the symbol and its references when holding the cursor.
+-- scroll in floating window
 vim.api.nvim_exec([[
 autocmd CursorHold * silent call CocActionAsync('highlight')
+
+inoremap <silent><expr> <c-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<c-j>"
+inoremap <silent><expr> <c-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<c-k>"
+vnoremap <silent><expr> <c-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<c-j>"
+vnoremap <silent><expr> <c-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<c-k>"
 ]], false)
