51b972ce366715a7b6d452f5dd301b241a60c9b8
Parham Alvani <parham.alvani@gmail.com>
2022-05-31 01:19:08 +0430

    feat: update brew installation script
    Former-commit-id: 19b131038dd7f3b94530ff41042ab6b736be0b00 [formerly 4887f8833b8d29eaf7b0bcb86372175788e1ef9d] [formerly 5ffced806a67fa5717fa3a21e05d06784cc93073 [formerly 96f230bdae173979753116fb6b8630df481b9a40 [formerly 96f230bdae173979753116fb6b8630df481b9a40 [formerly 96f230bdae173979753116fb6b8630df481b9a40 [formerly 755ee1fa9ea0cab5f322c83a970bb4aac8fb3be4]]]]]
    Former-commit-id: ea3ca818683c62731e4da3527be9b6de689e61a0 [formerly 290dd9deb0fb43430c3ec35c90a7305089375bee]
    Former-commit-id: 1f62872085d0ebf24e76e0abb1abeb44d27c7a96
    Former-commit-id: f6d02d277aa30319233ab62eb785f4fe9510c164
    Former-commit-id: 4dd8e5159f8cfa62b05aca841e358d4703620abb
    Former-commit-id: 79c5123a690c7ad52327d212634cd8833fda45cd

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # In The Name of God
 # ========================================
-# [] File Name : sample.sh
+# [] File Name : brew.sh
 #
 # [] Creation Date : 17-07-2018
 #
@@ -9,8 +9,7 @@
 # =======================================
 
 usage() {
-	echo "usage: brew [-p]"
-	echo "-p: install .profile or .bash_profile for using brew"
+	echo "The missing package manager for macOS (or Linux)"
 	echo '
  _
 | |__  _ __ _____      __
@@ -21,26 +20,21 @@ usage() {
   '
 }
 
-#main_apt() {
-#	msg "install the Homebrew dependencies"
-#	sudo apt-get install build-essential file curl git
-#}
-
 main() {
-	yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-	if [[ "$OSTYPE" == "linux"* ]]; then
-		msg "add homebrew to your profile"
+	msg "add homebrew to your profile"
 
-		test -d "$HOME/.linuxbrew" && eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
+	test -d "$HOME/.linuxbrew" && eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
 
-		test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-		brew_eval="eval \$($(brew --prefix)/bin/brew shellenv)"
+	test -d /opt/homebrew/bin/ && eval "$(/opt/homebrew/bin/brew shellenv)"
 
-		if [ -r "$HOME/.bash_profile" ]; then
-			grep -i "$brew_eval" "$HOME/.bash_profile" || echo "$brew_eval" >>"$HOME/.bash_profile"
-		fi
-		grep -i "$brew_eval" "$HOME/.profile" || echo "$brew_eval" >>"$HOME/.profile"
+	brew_eval="eval \$($(brew --prefix)/bin/brew shellenv)"
+
+	if [ -r "$HOME/.bash_profile" ]; then
+		grep -i "$brew_eval" "$HOME/.bash_profile" || echo "$brew_eval" >>"$HOME/.bash_profile"
 	fi
+	grep -i "$brew_eval" "$HOME/.profile" || echo "$brew_eval" >>"$HOME/.profile"
 }
