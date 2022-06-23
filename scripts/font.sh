6591d77ff37b53e3dff33939fa9c86db1144a172
Parham Alvani <parham.alvani@gmail.com>
2022-06-24 01:42:39 +0430

    feat: update fonts
    Former-commit-id: fe15d8661e7dd3d7abf01dcab886e58dca5d960a [formerly 625276108d3f6999f0ffb30ab17a67653879ce84] [formerly e0de0a14fb0a8e17e7d30c0e711a214604a5d425 [formerly 546f25bfe625d77001ba4a72ff3ec50c52aef076 [formerly 546f25bfe625d77001ba4a72ff3ec50c52aef076 [formerly 546f25bfe625d77001ba4a72ff3ec50c52aef076 [formerly eaae61b21c47ed68a57655209e858724a373feee]]]]]
    Former-commit-id: 0e57aa7d409d5d18eae7b8329f0a6ee7cac576a4 [formerly e6bf5b1d2b517c6973c31a61ae03579f431b1674]
    Former-commit-id: 100a623049d21ac6ce8560e67a40e708bbd1a7db
    Former-commit-id: 271d15f45890f8bfd13c7b1c742e899339f03b95
    Former-commit-id: ed7045574a6dc14515b55a9b79403fbf1f21e551
    Former-commit-id: 961044830ca85a03739078c534b8e7e853d00e6b

--- scripts/font.sh
+++ scripts/font.sh
@@ -20,66 +20,10 @@ usage() {
   '
 }
 
-if [[ $(uname) == 'Darwin' ]]; then
-	# MacOS
-	font_dir="$HOME/Library/Fonts"
-else
-	# Linux
-	font_dir="$HOME/.local/share/fonts"
-	if [ ! -d "$HOME/.local/share/fonts" ]; then
-		mkdir -p "$font_dir"
-	fi
-fi
-
-_install_jetbrains() {
-	if fc-list -q 'JetBrains Mono'; then
-		msg "you have the jetbrains mono installed"
-	else
-		jbm_version="2.225"
-		msg "install jetbrains mono ($jbm_version) by dowloading its archive"
-
-		rm "JetBrainsMono-$jbm_version.zip" || true
-		rm -Rf jb || true
-
-		wget "https://download.jetbrains.com/fonts/JetBrainsMono-$jbm_version.zip"
-		unzip "JetBrainsMono-$jbm_version.zip" -d jb && rm "JetBrainsMono-$jbm_version.zip"
-
-		mv jb/fonts/ttf/* "$font_dir" && rm -Rf jb
-	fi
-}
-
-_install_vazir_code() {
-	if fc-list -q 'Vazir Code'; then
-		msg "you have the vazir code installed"
-	else
-		vzc_version="1.1.2"
-		msg "install vazir code ($vzc_version) by downloading its archive"
-
-		rm "vazir-code-font-v$vzc_version.zip" || true
-		rm -Rf vzc || true
-
-		wget "https://github.com/rastikerdar/vazir-code-font/releases/download/v$vzc_version/vazir-code-font-v$vzc_version.zip"
-		unzip "vazir-code-font-v$vzc_version.zip" -d vzc && rm "vazir-code-font-v$vzc_version.zip"
-
-		mv vzc/Vazir-Code.ttf "$font_dir" && rm -Rf vzc
-	fi
-}
-
-_install_vazir_thin() {
-	if fc-list -q 'Vazir Thin'; then
-		msg "you have the vazir thin installed"
-	else
-		v_version="29.1.0"
-		wget "https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v$v_version/dist/Vazir-Thin.ttf"
-		mv Vazir-Thin.ttf "$font_dir"
-	fi
-}
-
 main_brew() {
 	brew install --cask homebrew/cask-fonts/font-jetbrains-mono
 	brew install --cask homebrew/cask-fonts/font-jetbrains-mono-nerd-font
-
-	_install_vazir_thin
+	brew install --cask font-vazirmatn
 }
 
 main_pacman() {
@@ -90,12 +34,7 @@ main_pacman() {
 }
 
 main_apt() {
-	msg 'install roboto font from apt repository'
-	sudo apt-get install fonts-roboto
+	msg 'sorry we cannot do anything right now'
 
-	_install_jetbrains
-
-	_install_vazir_thin
-
-	_install_vazir_code
+	return 1
 }
