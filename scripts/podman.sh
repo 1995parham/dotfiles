82964f2e935dcaf75b1c55ad039102e5483a2f4d
Parham Alvani <parham.alvani@gmail.com>
2022-07-01 21:34:49 +0430

    feat: add new podman network stack
    Former-commit-id: 2df68196c12b9ad938c77389d5b3e214a415eedf [formerly a67ee992d4c89145f93951a6957f7370798aac33] [formerly 08a877dfaf11a357e6ad6f85d07f9d0d37cfd234 [formerly 9094298a0dc279abc35514aba005070a85d79698 [formerly 9094298a0dc279abc35514aba005070a85d79698 [formerly 9094298a0dc279abc35514aba005070a85d79698 [formerly 53795dfa5bee5ea466c56496c78f4ff1c108714f]]]]]
    Former-commit-id: 73b9dedc571680231dba8fc222be67dc164b609b [formerly 436d80c03b63d010a70771636ace36fe02d18b0d]
    Former-commit-id: e594618a797f73fa4a75cb345e174150de3cac6d
    Former-commit-id: bf3b503b4775220b9dea7cf576fea0db3c97223b
    Former-commit-id: 5e6388740d7ef25df90ddf899fdaee1e6e7097ee
    Former-commit-id: 1e6928e2a87b79a6bd96dc861edeacb4a13a16da

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -32,7 +32,7 @@ main_brew() {
 
 main_pacman() {
 	msg "install podman-compose / podman with pacman"
-	sudo pacman -Syu --noconfirm --needed podman podman-docker podman-compose docker-compose slirp4netns podman-dnsname fuse-overlayfs
+	sudo pacman -Syu --noconfirm --needed podman podman-docker podman-compose docker-compose slirp4netns podman-dnsname fuse-overlayfs netavark
 
 	msg "install hadolint/hadolint with yay"
 	yay -Syu --needed --noconfirm hadolint-bin
