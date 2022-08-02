9189fd915a0d8c452c35ddbbbd13fe260e80ba1c
Parham Alvani <parham.alvani@gmail.com>
2022-08-02 10:11:02 +0430

    feat: add blocky installation script
    Former-commit-id: 35586f95ebb8ddf8000c795ab0c6cd757fb13e26
    Former-commit-id: 3096f6faa2b7fda75fc63da96f0d5324fada81e3 [formerly 70d1e3360538b1b2515553b9d84c168e8f12b782]
    Former-commit-id: a98b9e93fa07434006589cbe9dbb35c5a98c22e6
    Former-commit-id: 3f0dd1aa11ddb8d77715b03df9403030ef648abc
    Former-commit-id: fd5ba2a25b16286ec068302ba43387603892b9f0
    Former-commit-id: b6ca667d5bd8ae5340df61f9dfe37a2e907090a6

---
+++ scripts/blocky.sh
@@ -0,0 +1,35 @@
+usage() {
+	echo -e "Fast and lightweight DNS proxy as ad-blocker for local network with many features"
+
+	# shellcheck disable=1004,2016
+	echo '
+ _     _            _
+| |__ | | ___   ___| | ___   _
+| |_ \| |/ _ \ / __| |/ / | | |
+| |_) | | (_) | (__|   <| |_| |
+|_.__/|_|\___/ \___|_|\_\\__, |
+                         |___/
+  '
+}
+
+main_apt() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main_pacman() {
+	current_dir=${current_dir:?"current_dir must be set"}
+
+	yay -Syu blocky
+}
+
+main_brew() {
+	msg "there is nothing that we can do"
+	return 1
+}
+
+main() {
+	sudo cp "$current_dir/blocky/blocky.yml" /etc/blocky.yml
+
+	sudo git clone "https://github.com/StevenBlack/hosts.git" /opt/hosts || (cd "/opt/hosts" && sudo git pull)
+}
