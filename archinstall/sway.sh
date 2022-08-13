dc7f9c8fedace1b4ee86da4c4decb214637e00f8
Parham Alvani <parham.alvani@gmail.com>
2022-08-13 20:46:36 +0430

    feat: re-structure the dotfiles
    Former-commit-id: 40e87f22bbb94ecfc43a224229edab3d8b4caa17
    Former-commit-id: 50005c648561c5beb41eac00cd5f7f2cec25c1a5 [formerly 564ec5c7f34f7bbf91cd96ab8943f154e917f166]
    Former-commit-id: 31a239a074171187e3357fe22abde3469d7ab5dd
    Former-commit-id: 1e9c8fe474cbee047058a258b24c022c910e82d6
    Former-commit-id: 380782c4b6cba21d5d009a109d32270690e72861
    Former-commit-id: 3d3cdfcf4d428621fa3539901de68fca060000dc

--- archinstall/sway.sh
+++ archinstall/sway.sh
@@ -6,30 +6,30 @@ if [[ $USER != parham ]]; then
 	exit
 fi
 
-echo "install mesa-vdpau for better performance on ATI graphic cards"
-echo "https://wiki.archlinux.org/title/Improving_performance"
-
-# echo "because of iran sanctions maybe you need ghermezi for downloading from github"
-# cd "$HOME/dotfiles/" && ./start.sh v2ray
-# sudo vim /etc/v2ray/config.json
-# sudo systemctl enable --now v2ray
-
-# global variable that points to dotfiles root directory
+# global variable that points to `dotfiles/archinstall` root directory
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$HOME/yay-bin" && makepkg -si
+# shellcheck source=scripts/lib/message.sh
+source "$current_dir/../scripts/lib/message.sh"
+# shellcheck source=scripts/lib/require.sh
+source "$current_dir/../scripts/lib/require.sh"
 
-echo "lets use greetd as dm, so wait for r...ust"
-cd "$HOME/dotfiles/" && ./start.sh rust
-yay -Syu --needed --noconfirm greetd greetd-tuigreet-bin
+# install yay-bin to have yay for installing from
+if ! pacman -Qi yay >/dev/null 2>&2; then
+	cd "$HOME/yay-bin" && makepkg -si
+fi
+
+message "archinstall" "lets use greetd as desktop manager, so wait for rust"
+cd "$current_dir/.." && ./start.sh rust
+require_aur greetd greetd-tuigreet-bin
 
 sudo cp "$current_dir/sway.d/sway.sh" /usr/local/bin/sway.sh
 sudo cp "$current_dir/sway.d/sway.desktop" /usr/share/wayland-sessions/sway.desktop
 sudo cp "$current_dir/sway.d/greetd" /etc/pam.d/greetd
 sudo cp "$current_dir/sway.d/config.toml" /etc/greetd/config.toml
 
+message "archinstall" "enable greetd"
 sudo systemctl enable greetd.service
 
-echo "have fun with your sway"
-cd "$HOME/dotfiles/" && ./start.sh sway
-sudo pacman -Syu --needed --noconfirm firefox
+message "archinstall" "setup sway and required softwares"
+cd "$current_dir/.." && ./start.sh sway
