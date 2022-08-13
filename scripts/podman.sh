913b6e4197a02f3e883056137db0441fceedb2b5
Parham Alvani <parham.alvani@gmail.com>
2022-08-13 11:12:25 +0430

    feat: re-structure the dotfiles
    Former-commit-id: 7b9d54a157757164a8bd75cc253bf8e513de5d8f
    Former-commit-id: 202ba7a4574cf8dd36224c5e52a04c76212a2b23 [formerly 5069846140c08fb4b0ca69a9ea769c33e5437834]
    Former-commit-id: 3093909b796fdb3c0b13844de266b894f3069cb5
    Former-commit-id: 225e51df9c902cca34fe2bf610ffba6099766cd6
    Former-commit-id: 26a6a913cdebed06b77b9179548456b7a5221acc
    Former-commit-id: a709ff24053ff85ed8e440ac4e5f13f7b4464247

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# In The Name Of God
-# ========================================
-# [] File Name : podman.sh
-#
-# [] Creation Date : 27-01-2022
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
 
 usage() {
-	echo "install podman useable at snapp"
+	echo "install podman instead of docker"
 
 	# shellcheck disable=1004,2016
 	echo '
@@ -40,16 +32,9 @@ main_brew() {
 
 main_pacman() {
 	msg "install podman-compose / podman with pacman"
-	sudo pacman -Syu --noconfirm --needed podman podman-docker podman-compose docker-compose slirp4netns podman-dnsname fuse-overlayfs netavark aardvark-dns
+	require_pacman podman podman-docker podman-compose docker-compose slirp4netns podman-dnsname fuse-overlayfs netavark aardvark-dns
 
-	msg "install hadolint/hadolint with yay"
-	yay -Syu --needed --noconfirm hadolint-bin
-
-	msg "install dive with yay"
-	yay -Syu --needed --noconfirm dive-bin
-
-	msg "install lazydocker with yay"
-	yay -Syu --needed --noconfirm lazydocker-bin
+	require_aur hadolint-bin dive-bin lazydocker-bin
 
 	msg "podman and dive configuration"
 	configfile dive "" podman
@@ -64,13 +49,7 @@ main_pacman() {
 	systemctl --user enable --now podman.service
 	systemctl --user enable --now podman.socket
 
-	msg 'remember arch has cgroup 2 by default'
-
-	msg 'check /etc/hosts to contain the localhost'
-	cat <<EOF
-127.0.0.1 localhost
-::1 localhost
-EOF
+	msg 'archlinux uses cgroup v2 by default'
 }
 
 main() {
