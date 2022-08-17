d8b3cb038d1d89e11b79c549fb184eb4631d8be8
Parham Alvani <parham.alvani@gmail.com>
2022-08-17 10:28:10 +0430

    feat: update navi installation
    Former-commit-id: 368e830b69940d88f99c1e421846de163a2585d8
    Former-commit-id: 3de532dc9a752da582843fd8192b24f4232118ac [formerly 4141988613c1de1add0c972ed3d74f3f5c45fb75]
    Former-commit-id: 8fe1dd856589348a8a12f38f77e068db034280d3
    Former-commit-id: 48b3d38d2d179aa78902ff3caa646a8743e891a4
    Former-commit-id: 548dc2c36e222ec6208410816beef5a7fe2b11ec
    Former-commit-id: 23a00da29955482653913e664cc5cadeb1d30dd8

--- scripts/navi.sh
+++ scripts/navi.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# In The Name of God
-# ========================================
-# [] File Name : sample.sh
-#
-# [] Creation Date : 17-07-2018
-#
-# [] Created By : Parham Alvani <parham.alvani@gmail.com>
-# =======================================
 
 usage() {
 	echo "navi cheatsheet"
@@ -30,7 +22,7 @@ main_apt() {
 }
 
 main_pacman() {
-	yay -Syu --noconfirm --needed navi-bin
+	require_aur navi
 }
 
 main() {
