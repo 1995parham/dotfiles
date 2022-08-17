f0bca2f08ac79faaf16f71567164e3bda04137fb
Parham Alvani <parham.alvani@gmail.com>
2022-08-17 22:35:53 +0430

    feat: switch waybar to its git version
    Former-commit-id: 38876567ccb8d852a9982c03621e1f10b3da87ab
    Former-commit-id: d862ea935a2fd054e3a0d9b840233fb6213c0c97 [formerly f80bb9a87f0869d2ead67bee3470299ad3eb4e7e]
    Former-commit-id: 1be548a59598511424bd9f6d3c222c591d8ffd66
    Former-commit-id: 18788e20246bb92843fee05616b53c65c5e6b95e
    Former-commit-id: 03f51744399bac627ae0b821e2893d14ef734978
    Former-commit-id: c77d69b88f7497044851f7b7f21a5d938c61b630

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -25,49 +25,50 @@ main_pacman() {
 	dotfiles_root=${dotfiles_root:?"dotfiles_root must be set"}
 
 	msg 'hyprland'
-	yay -Syu hyprland-git
-	sudo pacman -Syu --noconfirm --needed waybar grim xdg-user-dirs wl-clipboard
+	require_aur hyprland-git
+	require_pacman grim xdg-user-dirs wl-clipboard
+	require_aur waybar-hyprland-git
 	configfile hypr "" hyprland
-	configfile waybar "" sway
+	configfile waybar "" hyprland
 
 	msg 'better hyprland with more keys [brightnessctl]'
-	sudo pacman -Syu --noconfirm --needed brightnessctl
+	require_pacman brightnessctl
 
 	msg 'required freedesktop services'
-	sudo pacman -Syu --noconfirm --needed upower rtkit
+	require_pacman upower rtkit
 
 	msg 'featurerich screenshot tool'
-	sudo pacman -Syu --noconfirm --needed flameshot
+	require_pacman flameshot
 
 	msg 'gtk theme'
-	yay -Syu --noconfirm --needed matcha-gtk-theme
+	require_aur matcha-gtk-theme
 	configfile gtk-3.0 settings.ini sway
 
 	msg 'qt support'
-	sudo pacman -Syu --noconfirm --needed qt5-wayland
+	require_pacman qt5-wayland
 
 	msg 'x11/wayland image viewer'
-	sudo pacman -Syu --noconfirm --needed imv
+	require_pacman imv
 
 	msg 'pdf viewer'
-	sudo pacman -Syu --noconfirm --needed mupdf
+	require_pacman mupdf
 
 	msg 'notification with dunst'
-	sudo pacman -Syu --noconfirm --needed dunst libnotify
+	require_pacman dunst libnotify
 	configfile dunst "" sway
 
 	msg 'configure fuzzel as an another application luncher'
-	sudo pacman -Syu --noconfirm --needed fuzzel
+	require_pacman fuzzel
 
 	msg 'we are going to have sound'
-	sudo pacman -Syu --noconfirm --needed pulsemixer easyeffects
+	require_pacman pulsemixer easyeffects
 
 	msg 'configure the dmenu, default application luncher on manjaro i3 days'
 	linker dmenu "$dotfiles_root/sway/dmenurc" "$HOME/.dmenurc"
 	chmod +x "$HOME/.dmenurc"
 
 	msg 'gnome-keyring/seahorse setup with ~/.profile'
-	sudo pacman -Syu --noconfirm --needed gnome-keyring seahorse
+	require_pacman gnome-keyring seahorse
 	dotfile sway profile
 	mkdir -p "$HOME/.gnupg"
 	linker gnupg "$dotfiles_root/sway/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
