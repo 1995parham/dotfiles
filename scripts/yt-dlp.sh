c3fc2bcb40f857a931bddb01457b41c9006b76dc
Parham Alvani <parham.alvani@gmail.com>
2022-08-26 10:54:16 +0430

    feat: update yt-dlp
    Former-commit-id: 79fa57f0f55eb18d81996f7adb5c16fa12305006
    Former-commit-id: 3c3fd410ff7503550ce37c970b5197519e796465 [formerly 6bfa9c8683c3c11f720c2c14136f3104a9d494f9]
    Former-commit-id: f2d71390a02baa7fe06851f9d4d30461453b4510
    Former-commit-id: f98225da0b9d751170d52580158d5d924fb93084
    Former-commit-id: 232934c0cef092d1ab56ab055bc0a9c308af9c92
    Former-commit-id: a44e43515ba848a09b670845ff94568e5bd4fea4

--- scripts/yt-dlp.sh
+++ scripts/yt-dlp.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# In The Name of God
-# ========================================
-# [] File Name : yt-dlp.sh
-#
-# [] Creation Date : 12-07-2022
-#
-# [] Created By : Parham Alvani <parham.alvani@gmail.com>
-# =======================================
 
 usage() {
 	echo -n "A youtube-dl fork with additional features and fixes"
@@ -21,18 +13,8 @@ usage() {
 	'
 }
 
-main_brew() {
-	msg "there is nothing that we can do"
-	return 1
-}
-
 main_pacman() {
-	sudo pacman -Syu --noconfirm --needed yt-dlp
-}
-
-main_apt() {
-	msg "there is nothing that we can do"
-	return 1
+	require_pacman yt-dlp
 }
 
 main() {
