8912cbdc95e69d68f40c9d27bbf88fca4e7a08fc
Parham Alvani <parham.alvani@gmail.com>
2022-08-29 19:53:11 +0430

    feat: use html-based colors
    Former-commit-id: 3060065f5354403e5c4faa96871122da99ad3e95
    Former-commit-id: 5fde538618157a6eec42d54786b0a3819a13bae6 [formerly 9da609a7625842a27f736fa4227dfda75f0d934c]
    Former-commit-id: 84ff11a7bacc7cbc8b90d731d0a3c473965f7706
    Former-commit-id: 714e8527b8626d2dcd2bf1156b540c1b50234907
    Former-commit-id: dd87e2ae777c7ae2d8d236411c9d75f17ac51937
    Former-commit-id: 257a2912844913e67eded6a2173c5a10d3e36298

--- scripts/lib/message.sh
+++ scripts/lib/message.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-function yes_or_no {
+F_CYAN="\033[38;2;0;255;255m"
+F_GREEN="\033[38;2;127;230;127m"
+F_RED="\033[38;2;255;127;127m"
+F_ORANGE="\033[38;2;255;165;0m"
+F_YELLOW="\033[38;2;255;255;25m"
+F_RESET="\033[39m"
+
+function yes_or_no() {
 	yes_to_all=${yes_to_all:-0}
 	if $yes_to_all; then
 		return 0
@@ -10,11 +17,11 @@ function yes_or_no {
 	shift
 
 	while true; do
-		read -r -p "$(echo -e "\033[38;5;226m[$module] \033[38;5;202m$*\033[39m [\033[38;5;46my\033[39m/\033[38;5;196mn\033[39m]: ")" yn
+		read -r -p "$(echo -e "\033[38;5;226m[$module] ${F_ORANGE}$*${F_RESET} [${F_GREEN}y${F_RESET}/${F_RED}n${F_RESET}]: ")" yn
 		case $yn in
 		[Yy]*) return 0 ;;
 		[Nn]*)
-			echo -e "\033[38;5;196mAborted \033[39m"
+			echo -e "${F_YELLOW}Aborted${F_RESET}"
 			return 1
 			;;
 		esac
@@ -25,26 +32,26 @@ function message() {
 	local module=$1
 	shift
 
-	echo -e "\033[38;5;45m[$module] \033[38;5;202m$*\033[39m"
+	echo -e "${F_CYAN}[$module] ${F_ORANGE}$* $F_RESET"
 }
 
 function running() {
 	local module=$1
 	shift
 
-	echo -e "\033[38;5;226m[$module] \033[38;5;202m⇒ $* \033[39m"
+	echo -e "${F_YELLOW}[$module] ${F_ORANGE}⇒ $* $F_RESET"
 }
 
 function action() {
 	local module=$1
 	shift
 
-	echo -e "\033[38;5;197m[$module] \033[38;5;202m⇒ $* \033[39m"
+	echo -e "${F_RED}[$module] ${F_ORANGE}⇒ $* $F_RESET"
 }
 
 function ok() {
 	local module=$1
 	shift
 
-	echo -e "\033[38;5;46m[$module] \033[38;5;202m⇒ $* \033[39m"
+	echo -e "${F_GREEN}[$module] ${F_ORANGE}⇒ $* $F_RESET"
 }
