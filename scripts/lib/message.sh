87fff30b36b0c43452b941f6ff0d24298dd37b90
Parham Alvani <parham.alvani@gmail.com>
2022-08-29 19:31:49 +0430

    fix: correct colors on different shells
    Former-commit-id: cec158a2e74214c5d8fa0277cefbd1d48098f56d
    Former-commit-id: b9b856425b542b3dde5487a0d60d1ae5e01b48aa [formerly 11cfe85cdbc78f650a108d1fadd882ff94e0d985]
    Former-commit-id: 518bd15e21be795f7a71b216728b049fe00fb3e0
    Former-commit-id: 222c3148dc73a9357a31eb6c873ab1d30f6ecc2e
    Former-commit-id: 5becbaf8fb11151bee4901777be0b2dc098759e1
    Former-commit-id: 6017e7704f622eee0ce281c5cf920a0c9a307fa6

--- scripts/lib/message.sh
+++ scripts/lib/message.sh
@@ -10,11 +10,11 @@ function yes_or_no {
 	shift
 
 	while true; do
-		read -r -p "$(echo -e "\e[38;5;226m[$module] \e[38;5;202m$*\e[39m [\e[38;5;46my\e[39m/\e[38;5;196mn\e[39m]: ")" yn
+		read -r -p "$(echo -e "\033[38;5;226m[$module] \033[38;5;202m$*\033[39m [\033[38;5;46my\033[39m/\033[38;5;196mn\033[39m]: ")" yn
 		case $yn in
 		[Yy]*) return 0 ;;
 		[Nn]*)
-			echo -e "\e[38;5;196mAborted \e[39m"
+			echo -e "\033[38;5;196mAborted \033[39m"
 			return 1
 			;;
 		esac
@@ -25,26 +25,26 @@ function message() {
 	local module=$1
 	shift
 
-	echo -e "\e[38;5;45m[$module] \e[38;5;202m$*\e[39m"
+	echo -e "\033[38;5;45m[$module] \033[38;5;202m$*\033[39m"
 }
 
 function running() {
 	local module=$1
 	shift
 
-	echo -e "\e[38;5;226m[$module] \e[38;5;202m⇒ $* \e[39m"
+	echo -e "\033[38;5;226m[$module] \033[38;5;202m⇒ $* \033[39m"
 }
 
 function action() {
 	local module=$1
 	shift
 
-	echo -e "\e[38;5;197m[$module] \e[38;5;202m⇒ $* \e[39m"
+	echo -e "\033[38;5;197m[$module] \033[38;5;202m⇒ $* \033[39m"
 }
 
 function ok() {
 	local module=$1
 	shift
 
-	echo -e "\e[38;5;46m[$module] \e[38;5;202m⇒ $* \e[39m"
+	echo -e "\033[38;5;46m[$module] \033[38;5;202m⇒ $* \033[39m"
 }
