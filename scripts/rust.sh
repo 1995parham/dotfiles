0a05debb7271ec18b02cbda5eff66c4d0739a450
Parham Alvani <parham.alvani@gmail.com>
2022-10-06 09:20:05 +0330

    feat: add cargo configuration
    Former-commit-id: 2136c9fa3d1e8f9e993c32db999303882686c4e9
    Former-commit-id: 201a128efce906a18de7d9ee372374d574a9bf32 [formerly ba00c1478611ef3668d16649cde56e417bb0dcd0]
    Former-commit-id: 4cb8c79dd5f7741ffc9f6a6b5895291208b29dda
    Former-commit-id: ac24116d3f9236f700e6ab4dc8991e267b7eda3a
    Former-commit-id: a11b39a63c40691d8c7fddef548f0612734981ac
    Former-commit-id: c8df44f606647e2d56aa45f90d7eb663fa78bfdc

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -34,9 +34,13 @@ main_brew() {
 }
 
 main() {
+	dotfiles_root=${dotfiles_root:?"dotfiles_root must be set"}
+
 	msg "install the stable toolchain and select it as default"
 	rustup toolchain install stable
 	rustup default stable
+
+	cp "$dotfiles_root/rust/config.toml" "$HOME/.cargo/config.toml"
 
 	msg 'install cargo plugins'
 	cargo install cargo-edit cargo-expand
