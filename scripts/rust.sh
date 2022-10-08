32afaaf013438ee2ea90bb066433fc9a4ff906a8
Parham Alvani <parham.alvani@gmail.com>
2022-10-08 13:44:35 +0330

    chore: use rust instead of rustup
    Former-commit-id: 173b8b894ec78fdd3771761c67fb32a0a410fecd
    Former-commit-id: c06c8eb5987f396e639266fc50422b3d2d36e0f0 [formerly b14cfd9d360262c1ca12b4118e17c62356e3a7d1]
    Former-commit-id: 6f8b0530cf1b55c52fcda6a5536b06c9d0f42741
    Former-commit-id: 51c4d1de0fd25c4302d85b6730394413514baa08
    Former-commit-id: f54065b79e4c97d8f13dfad19956b707a65d99c1
    Former-commit-id: 88a0be4cbb8f94c377d6abc385f0b4ef3f3047ec

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -13,7 +13,7 @@ usage() {
 }
 
 main_pacman() {
-	require_pacman rustup
+	require_pacman rust
 }
 
 main_brew() {
@@ -37,16 +37,17 @@ main() {
 	dotfiles_root=${dotfiles_root:?"dotfiles_root must be set"}
 
 	msg "install the stable toolchain and select it as default"
-	rustup toolchain install stable
-	rustup default stable
+	# rustup toolchain install stable
+	# rustup default stable
 
+	mkdir -p "$HOME/.cargo" || true
 	cp "$dotfiles_root/rust/config.toml" "$HOME/.cargo/config.toml"
 
 	msg 'install cargo plugins'
 	cargo install cargo-edit cargo-expand
 
 	msg 'install rustup plugins'
-	rustup component add clippy
-	rustup component add rustfmt
-	rustup component add rls rust-analysis rust-src
+	# rustup component add clippy
+	# rustup component add rustfmt
+	# rustup component add rls rust-analysis rust-src
 }
