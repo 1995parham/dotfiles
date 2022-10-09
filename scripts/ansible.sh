cd99e6ba35f9e5b5f2b6f5e1206049f01199781a
Parham Alvani <parham.alvani@gmail.com>
2022-10-09 22:39:56 +0330

    feat: add ansible installation laguage
    Former-commit-id: c4f456c195f5172722cc97cbf3810083a3e18d20
    Former-commit-id: 8f22a99efc68eac468f89067f5feac3cea7dc000 [formerly 777beda9b240b39ab8dfeef57af04b4e2741d410]
    Former-commit-id: 477bea94fc3a04612587fe23b7ccdae01faca85c
    Former-commit-id: 67fc82fc3c0ae3a38bc03df4faa48530ce2ed5bc
    Former-commit-id: c0889d2336dbd1267bf10d3df6e9638e90297323
    Former-commit-id: f965a3764ffc1619d6a559a60525cbc8922246cd

---
+++ scripts/ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+usage() {
+	echo -n -e "ansible is a radically simple IT automation platform that makes your applications and systems easier to deploy and maintain."
+
+	# shellcheck disable=1004,2016
+	echo '
+                 _ _     _
+  __ _ _ __  ___(_) |__ | | ___
+ / _| | |_ \/ __| | |_ \| |/ _ \
+| (_| | | | \__ \ | |_) | |  __/
+ \__,_|_| |_|___/_|_.__/|_|\___|
+  '
+}
+
+main_pacman() {
+	require_pacman ansible ansible-lint
+}
+
+main() {
+	nvim '+MasonInstall ansible-language-server' --headless +qall
+}
