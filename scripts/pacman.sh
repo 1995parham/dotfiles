49e25a5049fa2e54bfc75405bb07475c17add31f
Parham Alvani <parham.alvani@gmail.com>
2022-10-18 04:35:17 +0330

    feat: add pacman configuration script
    Former-commit-id: c123e0bf65f0179a2365cc7e5abc2e8a22163cd2
    Former-commit-id: 8ecd825e40c47dc9dd0baef81cb01d189ce99858 [formerly 15246f6d16a9f8d25c431e898b7954910bde2230]
    Former-commit-id: b64a6ec79f3e86e6a2035219806a1562f596ca0b
    Former-commit-id: 027c2f68e4341e1899e2e087ff3f2a1e6e3837fd
    Former-commit-id: 4e40acf5a56895526d792b0c75b2745407ddc0dc
    Former-commit-id: 502606a7d6144f28698bee30e6640ea0d4548798

---
+++ scripts/pacman.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+usage() {
+	echo 'pacman configuraiton for arch here and now'
+
+	echo '
+ _ __   __ _  ___ _ __ ___   __ _ _ __
+| |_ \ / _| |/ __| |_ | _ \ / _| | |_ \
+| |_) | (_| | (__| | | | | | (_| | | | |
+| .__/ \__,_|\___|_| |_| |_|\__,_|_| |_|
+|_|
+  '
+}
+
+main_pacman() {
+	configfile pacman
+}
