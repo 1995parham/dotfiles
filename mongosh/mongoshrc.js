7c027bb14a2266c4023a39b8459b48fdfaa02c6f
Parham Alvani <parham.alvani@gmail.com>
2022-10-24 17:31:26 +0330

    feat: remove scope from mongosh configuration
    Former-commit-id: 56eb4363b84f491c4f3a462ae5978749391e8901
    Former-commit-id: 688d674fa1be750bc3f9b23fd5c0554eee0d138a [formerly bb773fee2bc12fcb78595509e9da6872d3a0caf9]
    Former-commit-id: 26fe91404cdcad5885f73b9a920cdf49ce679faf
    Former-commit-id: e79fd14348f1a21e5e7ee2a61610285cbbf7a3b9
    Former-commit-id: 4d7ef983c499d4e5fc5dbbbee4d4281010fbac38
    Former-commit-id: e9133bf52bbd5c1ee9c5b36df6cecc93ec83f1ab

--- mongosh/mongoshrc.js
+++ mongosh/mongoshrc.js
@@ -1,13 +1,11 @@
-{
-  config.set("editor", "nvim");
+config.set("editor", "nvim");
 
-  const hostnameSymbol = Symbol("hostname");
-  const uptimeSymbol = Symbol("uptime");
+const hostnameSymbol = Symbol("hostname");
+const uptimeSymbol = Symbol("uptime");
 
-  prompt = () => {
-    if (!db[hostnameSymbol]) db[hostnameSymbol] = db.serverStatus().host;
-    if (!db[uptimeSymbol]) db[uptimeSymbol] = db.serverStatus().uptime;
+prompt = () => {
+  if (!db[hostnameSymbol]) db[hostnameSymbol] = db.serverStatus().host;
+  if (!db[uptimeSymbol]) db[uptimeSymbol] = db.serverStatus().uptime;
 
-    return `${db.getName()}@${db[hostnameSymbol]} [${db[uptimeSymbol]}]> `;
-  };
-}
+  return `${db.getName()}@${db[hostnameSymbol]} [${db[uptimeSymbol]}]> `;
+};
