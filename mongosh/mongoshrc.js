a2101bf5ae43e1e54f08ff55b389a53ce64fffc2
Parham Alvani <parham.alvani@gmail.com>
2022-10-24 12:32:49 +0330

    feat: add mongosh configuration
    Former-commit-id: ac219576b577ea06b024320529a9c398ca7f293c
    Former-commit-id: 605728451285a8049b9b440bb962c1b5c681024f [formerly c694a9a716d8484747d3aab1f170d22e6dee5470]
    Former-commit-id: bcc433634964f892d330ee974a16971bc2bc3a90
    Former-commit-id: 8f049e479fa19042ff0aff2b569ea6c5c96c38a2
    Former-commit-id: 1f4750ed4314ee48c146de81508aa434504b2e89
    Former-commit-id: fa6b7ac12bdd9d192bff0452f5db963eb0df1936

---
+++ mongosh/mongoshrc.js
@@ -0,0 +1,13 @@
+{
+  config.set("editor", "nvim");
+
+  const hostnameSymbol = Symbol("hostname");
+  const uptimeSymbol = Symbol("uptime");
+
+  prompt = () => {
+    if (!db[hostnameSymbol]) db[hostnameSymbol] = db.serverStatus().host;
+    if (!db[uptimeSymbol]) db[uptimeSymbol] = db.serverStatus().uptime;
+
+    return `${db.getName()}@${db[hostnameSymbol]} [${db[uptimeSymbol]}]> `;
+  };
+}
