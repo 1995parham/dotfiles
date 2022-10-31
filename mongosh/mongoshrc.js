4cb5d836892ae3f6f5d1156d1c8e321f1fdcf7ca
Parham Alvani <parham.alvani@gmail.com>
2022-11-01 00:56:11 +0330

    feat: add scripts into mongosh
    Former-commit-id: 1656135e0186c8477d843295d385d4980fcdc988
    Former-commit-id: de4a735e82d201cdc20218845a8e3e43d676fcf3 [formerly 11861efe9c4b8b4afb773234d4b98e3e1c7ede73]
    Former-commit-id: b0fcd47afe57ef4ef93d56c9d5f56aec821b612f
    Former-commit-id: f12ca6a2bd27f48ac9ad97e8109123109f41ffb6
    Former-commit-id: 1244b9081ab050b3b542b291eafcfcf46966a114
    Former-commit-id: 964c19030ca9d9e058b94318d418c0800c2503a4

--- mongosh/mongoshrc.js
+++ mongosh/mongoshrc.js
@@ -9,3 +9,109 @@ prompt = () => {
 
   return `${db.getName()}@${db[hostnameSymbol]} [${db[uptimeSymbol]}]> `;
 };
+
+let mongoTuning = {};
+
+/*
+ * Query Profiler helper functions for the Apress book "MongoDB Performance Tuning"
+ *
+ * @Authors: Michael Harrison (Michael.J.Harrison@outlook.com) and Guy Harrison (Guy.A.Harrison@gmail.com).
+ * @Date:   2020-09-03T17:54:50+10:00
+ * @Last modified by:   Michael Harrison
+ * @Last modified time: 2021-04-08T10:50:52+10:00
+ *
+ */
+mongoTuning.profileQuery = () => {
+  const profileQuery = db.system.profile.aggregate([
+    {
+      $group: {
+        _id: { cursorid: "$cursorid" },
+        count: { $sum: 1 },
+        "queryHash-max": { $max: "$queryHash" },
+        "millis-sum": { $sum: "$millis" },
+        "ns-max": { $max: "$ns" },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          queryHash: "$queryHash-max",
+          collection: "$ns-max",
+        },
+        count: { $sum: 1 },
+        millis: { $sum: "$millis-sum" },
+      },
+    },
+    { $sort: { millis: -1 } },
+    { $limit: 10 },
+  ]);
+  return profileQuery;
+};
+
+/**
+ * Get details of a query from system.profile using the queryhash
+ *
+ * @param {string} queryHash - The queryHash of the query of interest.
+ *
+ * @returns {queryDetails} query ns, command and basic statistics
+ */
+
+mongoTuning.getQueryByHash = function (queryHash) {
+  return db.system.profile.findOne(
+    { queryHash },
+    { ns: 1, command: 1, docsExamined: 1, millis: 1, planSummary: 1 }
+  );
+};
+
+/**
+ * Fetch simplified profiling info for a given database and namespace.
+ *
+ * @param {string} dbName - The name of the database to fetch profiling data for.
+ * @param {string} collectionName - The name of the collection to fetch profiling data for.
+ *
+ * @returns {ProfilingData} Profiling data for the given namespace (queries only), grouped and simplified.
+ */
+
+mongoTuning.getProfileData = function (dbName, collectionName) {
+  var mydb = db.getSiblingDB(dbName); // eslint-disable-line
+  const ns = dbName + "." + collectionName;
+  const profileData = mydb
+    .getSiblingDB(dbName)
+    .getCollection("system.profile")
+    .aggregate([
+      {
+        $match: {
+          ns,
+          op: "query",
+        },
+      },
+      {
+        $group: {
+          _id: {
+            filter: "$query.filter",
+          },
+          count: {
+            $sum: 1,
+          },
+          "millis-sum": {
+            $sum: "$millis",
+          },
+          "nreturned-sum": {
+            $sum: "$nreturned",
+          },
+          "planSummary-first": {
+            $first: "$planSummary",
+          },
+          "docsExamined-sum": {
+            $sum: "$docsExamined",
+          },
+        },
+      },
+      {
+        $sort: {
+          "millis-sum": -1,
+        },
+      },
+    ]);
+  return profileData;
+};
