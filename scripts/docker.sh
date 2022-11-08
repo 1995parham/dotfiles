371796f9a5fe981ba9eabe3edc078c3fbf1815fb
Parham Alvani <parham.alvani@gmail.com>
2022-11-08 08:29:52 +0330

    feat: update docker installation script
    Former-commit-id: ac2c412b3bd01b7dbbd3a36f60823a5777d533c3
    Former-commit-id: 4642e1cdd2f5ac2f247dd73c061b2e62beb6c04e [formerly 3c9eb3f742817ad070fd272fd95dd37b57390612]
    Former-commit-id: e0f3d745d91f296523b767f6ca7ca03ae97a98d3
    Former-commit-id: eb6aa09c209a9f89702daecbc601edfbb4edd6c8
    Former-commit-id: 85bd77c1fbd3d547c6d506a7d5f981b687781905
    Former-commit-id: 39c1dc3fc11c861e8c530bfdce26766691be8e92

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -19,8 +19,8 @@ main_apt() {
 }
 
 main_brew() {
-	brew install --cask docker
-	brew install docker-compose lazydocker
+	require_brew_cask docker
+	require_brew docker-compose lazydocker
 
 	mkdir -p ~/.docker/cli-plugins
 	ln -sfn /opt/homebrew/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
@@ -37,6 +37,17 @@ main_pacman() {
 	msg "manage docker as a non-root user"
 	sudo groupadd -f docker
 	sudo usermod -aG docker "$USER"
+
+	if yes_or_no "docker" "do you want to use v2ray as a docker proxy?"; then
+		sudo mkdir -p /etc/systemd/system/docker.service.d/
+		echo '
+[Service]
+Environment="HTTP_PROXY=http://127.0.0.1:1080/"
+Environment="HTTPS_PROXY=http://127.0.0.1:1080/"
+    ' | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf
+		sudo systemctl daemon-reload
+	fi
+
 	newgrp docker
 }
 
