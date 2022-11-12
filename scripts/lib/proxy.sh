bc0d45c07141643d50ed5648b4bf4a7c756cf42b
Parham Alvani <parham.alvani@gmail.com>
2022-11-12 20:37:55 +0330

    feat: add socks proxy configuration script
    Former-commit-id: 423d402460731de9fb5165b0368e9c82b7505c58
    Former-commit-id: a9abe18f9f6542784463e2593dd2da297e388fb2 [formerly 20616394338b9c8d144c7149f78813d03a331b45]
    Former-commit-id: 08fefec53d5f0ab46863309c1feac04e528b4841
    Former-commit-id: d1e0519a9b93d72f34ad223ebb299a13c4831d64
    Former-commit-id: ef0eef1c7f428e260e527f2b1504db74e9008e95
    Former-commit-id: 9a45fb967db401541bdc3cd8b6cd0c3dba7ba0cf

--- scripts/lib/proxy.sh
+++ scripts/lib/proxy.sh
@@ -28,3 +28,32 @@ proxy_stop() {
 
 	echo -e "\033[38;5;46m[proxy] \033[38;5;202mall proxy script configurations are removed\033[39m"
 }
+
+socks_start() {
+	echo -e "\033[38;5;46m[socks] \033[38;5;202msetup proxy based on local socks proxy which is setup by v2ray\033[39m"
+	echo -e "\033[38;5;46m[socks] \033[38;5;202mpress enter to continue or anything else to disable it\033[39m"
+	read -r accept
+
+	if [[ "$accept" != "" ]]; then
+		return 0
+	fi
+
+	echo
+	curl --max-time 10 ifconfig.io/country_code || return 1
+
+	export ftp_proxy="http://127.0.0.1:1080"
+	export http_proxy="socks5://127.0.0.1:1086"
+	export https_proxy="socks5://127.0.0.1:1086"
+	alias sudo='sudo -E'
+
+	echo
+	curl --max-time 10 ifconfig.io/country_code || socks_stop
+	echo
+}
+
+socks_stop() {
+	unset {http,https,ftp}_proxy || true
+	unalias sudo 2>/dev/null || true
+
+	echo -e "\033[38;5;46m[socks] \033[38;5;202mall proxy script configurations are removed\033[39m"
+}
