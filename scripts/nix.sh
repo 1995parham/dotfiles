142bac0d4046b13589dcd0a8d2b38f4d5f1d5d41
Parham Alvani <parham.alvani@gmail.com>
2022-11-21 09:35:57 +0330

    feat: add nix installation script
    Former-commit-id: 02ced9f7ad36e480acccc06275975c9a2d439a4f
    Former-commit-id: 26491d51176d2bdf50dde7cef040af526249abb9 [formerly 2236bf0bfd81af4f9b703931633b9eba8926e396]
    Former-commit-id: 3a8660733ae53ae31d0bf9ad9d625cb2ce09f42c
    Former-commit-id: 49b91c06fda5aff37fb08f73b0a7fdf85449ced2
    Former-commit-id: 291c60c54a54cc04a6e54b1b2acd44b722042955
    Former-commit-id: 22857d150e78a4bc3f0700c0b3cf62eb755edbd9

---
+++ scripts/nix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage() {
+	echo "Nix is a purely functional package manager that aims to make package management reliable and reproducible. "
+
+	# shellcheck disable=1004,2016
+	echo '
+       _
+ _ __ (_)_  __
+| |_ \| \ \/ /
+| | | | |>  <
+|_| |_|_/_/\_\
+
+  '
+}
+
+main_pacman() {
+	require_pacman nix
+
+	msg 'nix-daemon service with systemd'
+	sudo systemctl enable --now nix-daemon.service
+
+	msg "add required users to the nix-users group in order to access the daemon socket"
+	sudo usermod -aG nix-users "$USER"
+
+	msg 'manually add current user into nix-users group'
+	# newgrp nix-users
+}
+
+main() {
+	msg 'add channels and update it'
+	nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+	nix-channel --update
+}
