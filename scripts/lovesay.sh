8f57cfd431fb27c362fdd5484aea650c436f3285
Parham Alvani <parham.alvani@gmail.com>
2022-11-27 16:12:44 +0330

    feat: add lovesay installation script
    Former-commit-id: ab2f368c011abb01beed212af7a06a882a02efdf
    Former-commit-id: e0b7ac376d3e5df0099b66d3f455a069e7e7de32 [formerly 8fb38c8de224f3916f0e8434d409baf030f6e6f7]
    Former-commit-id: 078b5787701b2058d0e71fca6acf50d8af8e42b6
    Former-commit-id: 546694398f6d1d4e88d7d2a0bf3a678dd5eb7fe8
    Former-commit-id: 3ba91288bf3dcba1dd1d4e2c16e0471386f419bc
    Former-commit-id: 4d50cd550b2c699fb9b79ad1f4dabfe41975ba74

---
+++ scripts/lovesay.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export dependencies=(python)
+
+usage() {
+	echo -n "cowsay, but full of love ♡"
+
+	# shellcheck disable=1004
+	echo '
+ _
+| | _____   _____  ___  __ _ _   _
+| |/ _ \ \ / / _ \/ __|/ _` | | | |
+| | (_) \ V /  __/\__ \ (_| | |_| |
+|_|\___/ \_/ \___||___/\__,_|\__, |
+                             |___/
+  '
+}
+
+main_apt() {
+	return 0
+}
+
+main_brew() {
+	return 0
+}
+
+main_pacman() {
+	return 0
+}
+
+main() {
+	python -mpip install --user --pre -U lovesay
+}
