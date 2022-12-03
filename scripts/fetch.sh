2be53186240c78434254afc4786298d43d6fbba6
Parham Alvani <parham.alvani@gmail.com>
2022-12-03 10:00:18 +0330

    feat: add fetch installation script
    Former-commit-id: db63d8b6c96cc7c1053ddd217f12cdd09f1c9a5e
    Former-commit-id: b24efa149c0a113303d8e9f092d3880502a2470c [formerly e40a005104c9841ce95ee53e626309116f1f194a]
    Former-commit-id: 0cd21cc8c84a6732fa4726db5c69346a98a28493
    Former-commit-id: 95624b91b6bace3f83d9387c3043d2c7843dd131
    Former-commit-id: 470424fd4bd1794bc19d2b23f85dbf7a5bb5a15e
    Former-commit-id: 9be3c50dd0c85936d042a3710d0bcc0f073e32e5

---
+++ scripts/fetch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+usage() {
+	echo -n "a command-line system/repository information tool"
+	# shellcheck disable=2016
+	echo '
+  __      _       _
+ / _| ___| |_ ___| |__
+| |_ / _ \ __/ __| |_ \
+|  _|  __/ || (__| | | |
+|_|  \___|\__\___|_| |_|
+	'
+}
+
+main_pacman() {
+	require_pacman neofetch onefetch
+}
+
+main() {
+	configfile neofetch
+}
