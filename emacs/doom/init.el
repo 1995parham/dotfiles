1109abf5955486728e9dacd09ac1b58f5ae2eeaa
Parham Alvani <parham.alvani@gmail.com>
2023-01-01 19:59:37 +0330

    feat: add markdown plugin into emacs

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -147,7 +147,7 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       ;;markdown          ; writing docs for people to ignore
+       markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
