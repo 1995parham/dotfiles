931058850d17f349e803c7328324583e0c7ed865
Parham Alvani <parham.alvani@gmail.com>
2023-01-27 07:13:31 +0330

    feat: change doom theme to see it is working or not

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -42,7 +42,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-monokai-spectrum)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
