2a3d325343201ee4342949fd59b9476e8aa177fd
Parham Alvani <parham.alvani@gmail.com>
2023-03-23 04:49:32 +0330

    feat: update owghat script

--- bin/bin/owghat.sh
+++ bin/bin/owghat.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# In The Name of God
-# ========================================
-# [] File Name : owghat.sh
-#
-# [] Creation Date : 03-11-2020
-#
-# [] Created By : Parham Alvani <parham.alvani@gmail.com>
-# =======================================
 
 url="https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86"
 
