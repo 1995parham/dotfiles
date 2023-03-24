9692fa04693dbc814c696f4b21ef0eb227ab6fd2
Parham Alvani <parham.alvani@gmail.com>
2023-03-24 17:03:02 +0330

    feat: update 30nama script

--- scripts/30nama.sh
+++ scripts/30nama.sh
@@ -22,7 +22,7 @@ main() {
 	if [ ! -d /opt/30nama ]; then
 		sudo mkdir /opt/30nama
 		sudo chown "$USER:$USER" /opt/30nama
-		aria2c "https://github.com/Mr30nama/30nama-desktop/releases/download/v$version/30nama-$version.AppImage" -d /opt/30nama
+		aria2c "https://github.com/Mr30nama/30nama-Hybrid/releases/download/v$version/30nama-$version.AppImage" -d /opt/30nama
 		aria2c "https://github.com/Mr30nama/30nama/raw/main/logo.png" -d /opt/30nama
 		chmod +x "/opt/30nama/30nama-$version.AppImage"
 	fi
