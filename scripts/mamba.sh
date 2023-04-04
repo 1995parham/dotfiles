7241caaf9e46180999eac9f6f3cf62615a47bd10
Parham Alvani <parham.alvani@gmail.com>
2023-04-04 15:38:43 +0330

    feat: switch to mamba from conda

---
+++ scripts/mamba.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+usage() {
+	echo -n -e "u-mamba for machine learning and data science"
+
+	# shellcheck disable=1004,2016
+	echo '
+                           _
+ _ __ ___   __ _ _ __ ___ | |__   __ _
+| |_ | _ \ / _| | |_ | _ \| |_ \ / _| |
+| | | | | | (_| | | | | | | |_) | (_| |
+|_| |_| |_|\__,_|_| |_| |_|_.__/ \__,_|
+
+  '
+}
+
+main_pacman() {
+	require_aur micromamba
+}
+
+main_apt() {
+	return 0
+}
+
+main_brew() {
+	return 0
+}
+
+main() {
+	dotfile mamba condarc
+}
