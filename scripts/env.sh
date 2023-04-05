d76b2e8eb012afbee0ab52615fa0e621f470e80f
Parham Alvani <parham.alvani@gmail.com>
2023-04-05 21:07:06 +0430

    feat: install git on mac

--- scripts/env.sh
+++ scripts/env.sh
@@ -18,7 +18,7 @@ export dependencies=("fetch")
 
 packages=(zsh tmux htop aria2 curl bat vim jq yamllint fzf mosh figlet)
 
-brew_packages=(coreutils k6 inetutils inxi fontconfig wget tmuxp dua-cli)
+brew_packages=(coreutils k6 inetutils inxi fontconfig wget tmuxp dua-cli git)
 apt_packages=(bmon atop)
 pacman_packages=(
 	perl-image-exiftool
