fecec00cf04437f7c78f8ad0b80450398f7b48b4
Parham Alvani <parham.alvani@gmail.com>
2023-04-06 15:03:07 +0330

    feat: add figlet into dns

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -2,9 +2,18 @@
 
 usage() {
 	echo -n 'my dns, my rules'
+	echo '
+     _
+  __| |_ __  ___
+ / _| | |_ \/ __|
+| (_| | | | \__ \
+ \__,_|_| |_|___/
+  '
 }
 
 main_pacman() {
+	dotfiles_root=${dotfiles_root:?"dotfiles_root must be set"}
+
 	msg "systemd-resolved will work out of the box with a network manager using /etc/resolv.conf."
 	sudo systemctl enable --now systemd-resolved
 
