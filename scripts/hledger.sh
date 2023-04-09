34ee5475a977a9969176fe90f344975ad6f6b4a3
Parham Alvani <parham.alvani@gmail.com>
2023-04-09 08:21:44 +0330

    feat: add hledger installation

---
+++ scripts/hledger.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+usage() {
+	echo -n "Robust, fast, intuitive plain text accounting tool with CLI, TUI and web interfaces."
+	# shellcheck disable=2016,2028
+	echo '
+ _     _          _
+| |__ | | ___  __| | __ _  ___ _ __
+| |_ \| |/ _ \/ _| |/ _| |/ _ \ |__|
+| | | | |  __/ (_| | (_| |  __/ |
+|_| |_|_|\___|\__,_|\__, |\___|_|
+                    |___/
+	'
+}
+
+main_pacman() {
+	require_pacman hledger
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	msg "hello parham, clone your accounting private repository"
+
+	if [ ! -d "$HOME/Documents/Git/parham/parham-alvani" ]; then
+		mkdir "$HOME/Documents/Git/parham/parham-alvani"
+	fi
+
+	cd "$HOME/Documents/Git/parham/parham-alvani" || return
+	clone parham-alvani/ledger git@github.com:
+	cd - || return
+
+	cd "$HOME/Documents/Git/parham/parham-alvani/ledger" || return
+	linker "hledger" "$(pwd)/2023.journal" "$HOME/.hledger.journal"
+	cd - || return
+}
