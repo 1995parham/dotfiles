0c5f31ec92242b96ec969496dbbc9a0b7b6cf510
Parham Alvani <parham.alvani@gmail.com>
2023-04-09 15:10:16 +0330

    feat: add a python script for owghat

--- bin/bin/owghat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-url="https://api.keybit.ir/owghat/?city=%D8%AA%D9%87%D8%B1%D8%A7%D9%86"
-
-curl -Ls "$url" | jq '.result | .azan_sobh, .azan_zohr, .azan_maghreb' -r | xargs printf ' %s  %s  %s\n'
