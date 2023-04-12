7b9aa9bec86d96ce20c001e20d7166b72bf98b01
Parham Alvani <parham.alvani@gmail.com>
2023-04-12 15:34:39 +0330

    feat: update bg script to create another process

--- sway/sway/scripts/bg.sh
+++ sway/sway/scripts/bg.sh
@@ -1,15 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu
 
 # kill the current swaybg process
-pkill swaybg
+pkill -e swaybg || true
 
-while true; do
-	swaybg -i "$(find "$HOME/Pictures/GoSiMac" -type f -name '*.png' -or -name '*.jpg' -not -name lock.jpg | shuf -n1)" -m fill &
-	NEW_PID=$!
-	sleep 5
-	if [ -n "$OLD_PID" ]; then
-		kill "$OLD_PID"
-	fi
-	OLD_PID=$NEW_PID
-	sleep 50
-done
+_end() {
+	echo "see you in a better tomorrow [you signal start.sh execuation]"
+	pkill swaybg
+	exit
+}
+
+main() {
+	trap '_end' INT
+	shift
+
+	while true; do
+		swaybg -i "$(find "$HOME/Pictures/GoSiMac" -type f -name '*.png' -or -name '*.jpg' -not -name lock.jpg | shuf -n1)" -m fill &
+		local new_pid=$!
+		sleep 5
+		if [ -n "$old_pid" ]; then
+			kill "$old_pid"
+		fi
+		local old_pid=$new_pid
+		sleep 50
+	done
+}
+
+fork() {
+	echo 'creating a new fork'
+	# based on https://unix.stackexchange.com/a/435582/538843
+	nohup setsid "$0" daemon "$@" &>/dev/null &
+}
+
+if [ $# -ge 1 ] && [ "$1" == "daemon" ]; then
+	main "$@"
+else
+	fork "$@"
+fi
