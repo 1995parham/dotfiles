3fea90643a8ef2a4c9a5bf7960ceb3c63df629b4
Parham Alvani <parham.alvani@gmail.com>
2023-04-12 15:48:39 +0330

    feat: update bg script

--- sway/sway/scripts/bg.sh
+++ sway/sway/scripts/bg.sh
@@ -15,19 +15,31 @@ main() {
 	trap '_end' INT
 	shift
 
+	local old_pid=""
+	local new_pid=""
+
 	while true; do
 		swaybg -i "$(find "$HOME/Pictures/GoSiMac" -type f -name '*.png' -or -name '*.jpg' -not -name lock.jpg | shuf -n1)" -m fill &
-		local new_pid=$!
+		new_pid=$!
 		sleep 5
 		if [ -n "$old_pid" ]; then
 			kill "$old_pid"
 		fi
-		local old_pid=$new_pid
+		old_pid=$new_pid
 		sleep 50
 	done
 }
 
 fork() {
+	echo 'is there anyother bg living there?'
+	bg_process_count=$(pgrep -c bg.sh)
+	while [ "$bg_process_count" -gt 1 ]; do
+		echo "there are more that 1 bg process is living there, lets killing them"
+
+		pkill -eo bg.sh
+		bg_process_count=$(pgrep -c bg.sh)
+	done
+
 	echo 'creating a new fork'
 	# based on https://unix.stackexchange.com/a/435582/538843
 	nohup setsid "$0" daemon "$@" &>/dev/null &
