515e7b593d4f5dd21e1ff4aa54a0c4396755ddee
Parham Alvani <parham.alvani@gmail.com>
2023-04-15 00:01:56 +0330

    feat: remove google chrome flags for now

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -34,10 +34,6 @@ main_pacman() {
 		msg 'chrome, the worst browser ever but sometime we need that shit'
 		require_aur google-chrome-beta
 
-		dotfiles_root=${dotfiles_root:?"dotfiles_root must be set"}
-		msg 'providing chrome-beta-flags.conf'
-		linker "browser" "$dotfiles_root/chrome/chrome-beta-flags.conf" "$HOME/.config/chrome-beta-flags.conf"
-
 		gopass-jsonapi configure --browser chrome --path ~/.config/google-chrome-beta --manifest-path ~/.config/google-chrome-beta/NativeMessagingHosts/com.justwatch.gopass.json
 	fi
 }
