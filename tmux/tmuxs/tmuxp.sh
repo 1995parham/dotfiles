5c7ed04f956c6198b676c3970069480146e64a5b
Parham Alvani <parham.alvani@gmail.com>
2023-04-16 14:29:15 +0330

    feat: complete an awesome session selector

--- tmux/tmuxs/tmuxp.sh
+++ tmux/tmuxs/tmuxp.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -e
 
-# shellcheck disable=2046
+# shellcheck disable=2046,2016
 layout=$(
-	fd .\.yaml "$HOME/.config/tmuxp" |
+	grep 'session_name:' $(fd .\.yaml "$HOME/.config/tmuxp") | cut -d':' -f3 |
 		fzf --color=fg:#ffa500,hl:#a9a9a9,prompt:#adff2f,separator:#ffe983,info:#ffe2ec \
-			--preview="cat {}"
+			--preview='cat $(grep -l {} $(fd .\.yaml "$HOME/.config/tmuxp"))'
 )
 
-tmuxp load "$layout"
+if [ -z "$layout" ]; then
+	exit 1
+fi
+
+# shellcheck disable=2046
+path=$(grep -l "^session_name:$layout\$" $(fd .\.yaml "$HOME/.config/tmuxp"))
+
+if [ ! -f "$path" ]; then
+	exit 1
+fi
+
+tmuxp load "$path"
