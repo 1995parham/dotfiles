52410dc81941a5320189a1e8826005d51595b3a5
Parham Alvani <parham.alvani@gmail.com>
2023-04-16 14:30:15 +0330

    fix: correct issue with rofi selector

--- sway/rofi/tmuxp.sh
+++ sway/rofi/tmuxp.sh
@@ -4,12 +4,12 @@ layout_yamls="$(fd .\.yaml "$HOME/.config/tmuxp")"
 
 if [ $# -gt 0 ]; then
 	# shellcheck disable=2086
-	layout_yaml=$(grep "^session_name: $*\$" $layout_yamls -l)
+	layout_yaml=$(grep "^session_name:$*\$" $layout_yamls -l)
 
 	layout_yaml=$(basename "$layout_yaml")
 	layout_name=${layout_yaml%.*}
 
-	coproc alacritty --hold -e tmuxp load "$layout_name" >/dev/null 2>&1
+	coproc alacritty -e tmuxp load "$layout_name" >/dev/null 2>&1
 elif [ $# = 0 ]; then
 	# rofi first calls script without arguments to have the
 	# entries.
