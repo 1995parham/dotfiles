d7c706c9e091d1cf21a24eace6af865e376e4a9f
Parham Alvani <parham.alvani@gmail.com>
2023-04-18 23:39:16 +0330

    feat: add starship prompt

---
+++ scripts/startship.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+usage() {
+	echo -n "the minimal, blazing-fast, and infinitely customizable prompt for any shell!"
+	echo '
+     _                 _     _
+ ___| |_ __ _ _ __ ___| |__ (_)_ __
+/ __| __/ _| | |__/ __| |_ \| | |_ \
+\__ \ || (_| | |  \__ \ | | | | |_) |
+|___/\__\__,_|_|  |___/_| |_|_| .__/
+                              |_|
+  '
+}
+
+main_pacman() {
+	require_pacman starship
+}
+
+main() {
+	configrootfile starship starship.toml ""
+}
