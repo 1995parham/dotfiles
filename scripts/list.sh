948ee62bb799825c533b6fb838383d65a36fbce3
Parham Alvani <parham.alvani@gmail.com>
2023-07-09 11:34:22 +0330

    fix: correct lint issues in the list script

--- scripts/list.sh
+++ scripts/list.sh
@@ -27,11 +27,8 @@ main_brew() {
 }
 
 main() {
-	local scripts
-	scripts=$(fd -e .sh -d 1 . "$dotfiles_root/scripts" -x basename | sed -e "s/.sh$//")
-
 	local script
-	for script in ${scripts[*]}; do
+	for script in $(fd -e .sh -d 1 . "$dotfiles_root/scripts" -x basename | sed -e "s/.sh$//"); do
 		# shellcheck disable=1090
 		description=$(source "$dotfiles_root/scripts/$script.sh" && usage | head -1)
 		msg "$script: $description"
