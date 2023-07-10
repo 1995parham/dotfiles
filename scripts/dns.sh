a939a3424793fa4d4d6977ad6dcec754fecfad33
Parham Alvani <parham.alvani@gmail.com>
2023-07-10 09:44:59 +0330

    feat: improve dns configuration to use it without shecan

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -31,6 +31,7 @@ main_pacman() {
 	kinds=(
 		"shecan: public and free version of shecan accessible from everywhere"
 		"shecan-pro: private and pro version of shecan accessible from home only"
+		"none: no shecan"
 	)
 
 	select kind in "${kinds[@]}"; do
@@ -43,31 +44,35 @@ main_pacman() {
 		sudo rm '/etc/NetworkManager/dnsmasq.d/shecan.conf'
 	fi
 
-	root=${root:?"root must be set"}
-	for f in "$root/dns/domains-$kind".*; do
-		mapfile -t domains <<<"$(cat "$f")"
-		domains_dnsmasq="$(printf '/%s' "${domains[@]}")"
-		echo -e "server=$domains_dnsmasq/${primary_shecan_dns[$kind]}\nserver=$domains_dnsmasq/${secondary_shecan_dns[$kind]}" |
-			sudo tee -a /etc/NetworkManager/dnsmasq.d/shecan.conf
-	done
+	if [ "$kind" != none ]; then
+		root=${root:?"root must be set"}
+		for f in "$root/dns/domains-$kind".*; do
+			mapfile -t domains <<<"$(cat "$f")"
+			domains_dnsmasq="$(printf '/%s' "${domains[@]}")"
+			echo -e "server=$domains_dnsmasq/${primary_shecan_dns[$kind]}\nserver=$domains_dnsmasq/${secondary_shecan_dns[$kind]}" |
+				sudo tee -a /etc/NetworkManager/dnsmasq.d/shecan.conf
+		done
+	fi
 
 	copycat "dns" dns/1995parham.conf /etc/NetworkManager/dnsmasq.d/1995parham.conf
 
 	dnsmasq --test --conf-file=/dev/null --conf-dir=/etc/NetworkManager/dnsmasq.d
 	sudo nmcli general reload
 
-	if [ "$(curl -s check.shecan.ir)" = '0' ]; then
-		msg "shecan is ready"
-	else
-		msg 'shecan is not working' 'error'
-		return 1
-	fi
+	if [ "$kind" != none ]; then
+		if [ "$(curl -s check.shecan.ir)" = '0' ]; then
+			msg "shecan is ready"
+		else
+			msg 'shecan is not working' 'error'
+			return 1
+		fi
 
-	if [ -d "/etc/docker" ]; then
-		sudo touch /etc/docker/daemon.json
+		if [ -d "/etc/docker" ]; then
+			sudo touch /etc/docker/daemon.json
 
-		msg 'merge dns configuration for docker with system current configuration'
-		r=$(jq -s '.[0] * (.[1] // {})' "$root/dns/$kind-docker.json" "/etc/docker/daemon.json")
-		echo "$r" | sudo tee "/etc/docker/daemon.json"
+			msg 'merge dns configuration for docker with system current configuration'
+			r=$(jq -s '.[0] * (.[1] // {})' "$root/dns/$kind-docker.json" "/etc/docker/daemon.json")
+			echo "$r" | sudo tee "/etc/docker/daemon.json"
+		fi
 	fi
 }
