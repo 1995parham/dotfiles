6372160859bb564a7d320b8490d43bf58ed3a6fa
Parham Alvani <parham.alvani@gmail.com>
2023-07-13 11:39:27 +0330

    feat: add discord installation script

---
+++ scripts/discord.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+usage() {
+	echo "Imagine a place..."
+
+	# shellcheck disable=1004,2016
+	echo '
+     _ _                       _
+  __| (_)___  ___ ___  _ __ __| |
+ / _` | / __|/ __/ _ \| |__/ _` |
+| (_| | \__ \ (_| (_) | | | (_| |
+ \__,_|_|___/\___\___/|_|  \__,_|
+  '
+}
+
+main_pacman() {
+	require_aur webcord
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
