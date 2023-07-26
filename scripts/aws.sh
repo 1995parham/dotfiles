125e1e31d53802e92d571c01b27fdd3c8df73a13
Parham Alvani <parham.alvani@gmail.com>
2023-07-26 18:22:39 +0330

    feat: add aws tools script

---
+++ scripts/aws.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+usage() {
+	echo -n "AWS CLI with required tools like terraform"
+	# shellcheck disable=2016
+	echo '
+
+  __ ___      _____
+ / _` \ \ /\ / / __|
+| (_| |\ V  V /\__ \
+ \__,_| \_/\_/ |___/
+
+	'
+}
+
+main_pacman() {
+	require_pacman aws-cli-v2 aws-vault terraform
+	require_aur cw-cloudwatch
+}
+
+main() {
+	return 0
+}
