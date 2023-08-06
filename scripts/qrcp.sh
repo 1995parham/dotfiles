cb77e6eea6fa7640a108e9640d9a72f055619127
Parham Alvani <parham.alvani@gmail.com>
2023-08-06 13:06:30 -0400

    feat: add script for qrcp installation

---
+++ scripts/qrcp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+usage() {
+	echo "Transfer files over wifi from your computer to your mobile device by scanning a QR code without leaving the terminal"
+
+	# shellcheck disable=1004,2016
+	echo '
+
+  __ _ _ __ ___ _ __
+ / _` | |__/ __| |_ \
+| (_| | | | (__| |_) |
+ \__, |_|  \___| .__/
+    |_|        |_|
+  '
+}
+
+main_pacman() {
+	require_aur qrcp
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
