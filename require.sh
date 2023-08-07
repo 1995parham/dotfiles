0d146cf88871819f882a7c24c717677d99d3e81a
Parham Alvani <parham.alvani@gmail.com>
2023-08-07 00:12:06 -0400

    Squashed 'scripts/lib/' changes from f04e484e..c0261fb5
    c0261fb5 feat: add a function for comparing semantic versions
    bd1ed383 feat: update readme
    
    git-subtree-dir: scripts/lib
    git-subtree-split: c0261fb5d7863b7bbcc44c138c19c6dc59684f09

--- require.sh
+++ require.sh
@@ -183,3 +183,50 @@ function clone() {
 		cd - &>/dev/null || return
 	fi
 }
+
+function semver_compare() {
+	# version1 > version2 -> 0
+	# version1 == version2 -> 1
+	# version1 < version2 -> 2
+
+	version1=${1:?"version-1 required"}
+	version2=${2:?"version-2 required"}
+
+	# first, replace the dots by blank spaces:
+
+	version1=${version1//./ }
+	version2=${version2//./ }
+
+	# can get rid of staring 'v':
+
+	version1=${version1//v/}
+	version2=${version2//v/}
+
+	patch1="$(echo "$version1" | awk '{print $3}')"
+	minor1="$(echo "$version1" | awk '{print $2}')"
+	major1="$(echo "$version1" | awk '{print $1}')"
+
+	patch2="$(echo "$version2" | awk '{print $3}')"
+	minor2="$(echo "$version2" | awk '{print $2}')"
+	major2="$(echo "$version2" | awk '{print $1}')"
+
+	if [ "$major1" -gt "$major2" ]; then
+		return 0
+	elif [ "$major1" -lt "$major2" ]; then
+		return 2
+	fi
+
+	if [ "$minor1" -gt "$minor2" ]; then
+		return 0
+	elif [ "$minor1" -lt "$minor2" ]; then
+		return 2
+	fi
+
+	if [ "$patch1" -gt "$patch2" ]; then
+		return 0
+	elif [ "$patch1" -lt "$patch2" ]; then
+		return 2
+	fi
+
+	return 1
+}
