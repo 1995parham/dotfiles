9c5aa3fd7f167a6c8e97179816281b24c3ef2726
Parham Alvani <parham.alvani@gmail.com>
2023-08-07 00:11:58 -0400

    feat: update 30nama installation script

--- scripts/30nama.sh
+++ scripts/30nama.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="0.12.4"
+version="1.0.0"
 
 usage() {
 	echo -n "You will not get bored with 30nama ($version)"
@@ -22,8 +22,22 @@ main() {
 	if [ ! -d /opt/30nama ]; then
 		sudo mkdir /opt/30nama
 		sudo chown "$USER:$USER" /opt/30nama
-		aria2c "https://github.com/Mr30nama/30nama-Hybrid/releases/download/v$version/30nama-$version.AppImage" -d /opt/30nama
+	fi
+
+	if [ ! -f /opt/30nama/logo.png ]; then
 		aria2c "https://github.com/Mr30nama/30nama/raw/main/logo.png" -d /opt/30nama
+	fi
+
+	current_version="$(find /opt/30nama -iname "30nama-*.AppImage" -exec sh -c 'basename $1 .AppImage | cut -d- -f2' sh {} \;)"
+	if [ -n "$current_version" ]; then
+		if [ "$(semver_compara "$current_version" "$version")" -eq 2 ]; then
+			rm "/opt/30nama/30nama-$current_version.AppImage"
+
+			aria2c "https://github.com/Mr30nama/30nama-Hybrid/releases/download/v$version/30nama-$version.AppImage" -d /opt/30nama
+			chmod +x "/opt/30nama/30nama-$version.AppImage"
+		fi
+	else
+		aria2c "https://github.com/Mr30nama/30nama-Hybrid/releases/download/v$version/30nama-$version.AppImage" -d /opt/30nama
 		chmod +x "/opt/30nama/30nama-$version.AppImage"
 	fi
 
