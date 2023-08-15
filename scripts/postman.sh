12c4e2ea268778be1ba7213cc0a4ae3a81315938
Parham Alvani <parham.alvani@gmail.com>
2023-08-15 05:18:02 -0400

    feat: add postman installation script

---
+++ scripts/postman.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+usage() {
+	echo "Postman is an API platform for building and using APIs"
+
+	# shellcheck disable=1004,2016
+	echo '
+                 _
+ _ __   ___  ___| |_ _ __ ___   __ _ _ __
+| |_ \ / _ \/ __| __| |_ ` _ \ / _` | |_ \
+| |_) | (_) \__ \ |_| | | | | | (_| | | | |
+| .__/ \___/|___/\__|_| |_| |_|\__,_|_| |_|
+|_|
+  '
+}
+
+main_pacman() {
+	require_aur postman-bin
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
