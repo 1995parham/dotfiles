94a5d5c416fafaf958299548df9bcda2cdf593a5
Parham Alvani <parham.alvani@gmail.com>
2023-08-15 15:07:06 -0400

    feat: add another package for nvidia

--- scripts/gpu-nvidia.sh
+++ scripts/gpu-nvidia.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 usage() {
-	echo 'Nvidia GPU based on its open-source kernel module, for desktop environments and also remembering Iman Tabrizian'
-	echo -n 'based on https://github.com/elFarto/nvidia-vaapi-driver'
+	echo -n 'Nvidia GPU based on its open-source kernel module, for desktop environments and also remembering Iman Tabrizian'
 	echo '
                                     _     _ _
   __ _ _ __  _   _       _ ____   _(_) __| (_) __ _
@@ -16,8 +15,7 @@ usage() {
 
 main_pacman() {
 	require_pacman linux-headers linux-zen-headers
-	require_pacman nvidia-open-dkms
-	# require_aur libva-nvidia-driver
+	require_pacman nvidia-open-dkms libva-vdpau-driver
 	require_pacman nvidia-prime nvtop
 
 	msg 'please set the nvidia-drm.modeset=1 as kernel parameter in /boot/loader/entries/*.conf' 'notice'
