41a93f90b5fc57145a7f14eafd52ca791c5179dd
Parham Alvani <parham.alvani@gmail.com>
2023-08-15 14:51:43 -0400

    feat: improve scripts for using gpu

---
+++ scripts/gpu-intel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+usage() {
+	echo "Intel GPU based on its open-source kernel module, for desktop environments"
+
+	# shellcheck disable=1004,2016
+	echo '
+                         _       _       _
+  __ _ _ __  _   _      (_)_ __ | |_ ___| |
+ / _` | |_ \| | | |_____| | |_ \| __/ _ \ |
+| (_| | |_) | |_| |_____| | | | | ||  __/ |
+ \__, | .__/ \__,_|     |_|_| |_|\__\___|_|
+ |___/|_|
+  '
+}
+
+main_pacman() {
+	require_pacman intel-media-driver
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
---
+++ scripts/gpu-nvidia.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage() {
+	echo 'Nvidia GPU based on its open-source kernel module, for desktop environments and also remembering Iman Tabrizian'
+	echo -n 'based on https://github.com/elFarto/nvidia-vaapi-driver'
+	echo '
+                                    _     _ _
+  __ _ _ __  _   _       _ ____   _(_) __| (_) __ _
+ / _| | |_ \| | | |_____| |_ \ \ / / |/ _| | |/ _| |
+| (_| | |_) | |_| |_____| | | \ V /| | (_| | | (_| |
+ \__, | .__/ \__,_|     |_| |_|\_/ |_|\__,_|_|\__,_|
+ |___/|_|
+
+  '
+}
+
+main_pacman() {
+	require_pacman linux-headers linux-zen-headers
+	require_pacman nvidia-open-dkms
+	# require_aur libva-nvidia-driver
+	require_pacman nvidia-prime nvtop
+
+	msg 'please set the nvidia-drm.modeset=1 as kernel parameter in /boot/loader/entries/*.conf' 'notice'
+}
