9100c96498fdac8f43d1a488bc932a86990090f4
Parham Alvani <parham.alvani@gmail.com>
2023-08-19 02:25:29 -0400

    feat: add amd gpu script

---
+++ scripts/gpu-amd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+usage() {
+	echo "AMD GPU based on its open-source kernel module, for desktop environments"
+
+	# shellcheck disable=1004,2016
+	echo '
+                                             _
+  __ _ _ __  _   _        __ _ _ __ ___   __| |
+ / _` | |_ \| | | |_____ / _` | |_ ` _ \ / _` |
+| (_| | |_) | |_| |_____| (_| | | | | | | (_| |
+ \__, | .__/ \__,_|      \__,_|_| |_| |_|\__,_|
+ |___/|_|
+  '
+}
+
+main_pacman() {
+	require_pacman xf86-video-amdgpu vulkan-radeon libva-mesa-driver mesa-vdpau
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
