e3cf61d6f54852d3a9bb8c7166f85c4f72cc0897
Parham Alvani <parham.alvani@gmail.com>
2023-09-06 04:31:13 -0400

    Squashed 'scripts/lib/' changes from 70d1aa9a..bc473be7
    bc473be7 fix: correct semver compare function and start writing unit tests
    e9a87472 Merge pull request #1 from 1995parham/dependabot/github_actions/actions/checkout-4
    7eaeeb97 chore(deps): bump actions/checkout from 3 to 4
    e125f679 fix: correct a typo
    ce1c9a15 feat: add more documents on linking
    
    git-subtree-dir: scripts/lib
    git-subtree-split: bc473be74efd2fa1aebc2dc9b29caab4277e6f56

---
+++ unit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+assert_equals() {
+	if [ "$#" -ne 2 ]; then
+		message "assert" "assert_equal requires 2 arguments" "error"
+		return 1
+	fi
+
+	value="$1"
+	expect="$2"
+
+	if [ "$value" != "$expect" ]; then
+		message "assert" "expects $expect but found $value" "error"
+		return 1
+	fi
+
+	return 0
+}
+
+set -eu
+set -o pipefail
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$root/message.sh"
+
+declare -F | while read -r unit_test; do
+	unit_test="${unit_test#"declare -f "}"
+
+	if [[ "$unit_test" =~ test_* ]]; then
+		"$unit_test" || (
+			message "$unit_test" 'failed' 'error'
+			return 1
+		)
+
+		message "$unit_test" 'passed'
+	fi
+done
