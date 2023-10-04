e1e851a067608eb39197d78dd9551f7b4b95771c
Parham Alvani <parham.alvani@gmail.com>
2023-10-04 05:02:48 +0000

    chore: remove insomnia configuration

--- scripts/insomnia.sh
+++ scripts/insomnia.sh
@@ -27,38 +27,3 @@ main_brew() {
 main() {
 	return 0
 }
-
-main_parham() {
-	mkdir -p "$HOME/.config/Insomnia/" || true
-
-	if [ -d "$HOME/.config/Insomnia/version-control" ]; then
-		cd "$HOME/.config/Insomnia/version-control" || return
-
-		url=$(git remote get-url origin 2>/dev/null)
-		if [[ "$url" =~ .*github.com[:/]1995parham-me/insomnia ]]; then
-			msg 'valid repository, so fetching it'
-			git pull origin main
-
-			return 0
-		else
-			msg "invalid repository $url"
-
-			if yes_or_no "insomnia" "do you want to remove current insomnia configuration?"; then
-				msg 'removing current configuration to replace it with new configuration'
-				rm -Rf "$HOME/.config/Insomnia/version-control"
-			else
-				return 1
-			fi
-		fi
-	fi
-
-	if [ -e "$HOME/.config/Insomnia/version-control" ] || [ -L "$HOME/.config/Insomnia/version-control" ]; then
-		if yes_or_no "insomnia" "do you want to remove current insomnia configuration?"; then
-			msg 'removing current configuration to replace it with new configuration'
-			rm -Rf "$HOME/.config/Insomnia/version-control"
-		else
-			return 1
-		fi
-	fi
-	git clone git@github.com:1995parham-me/insomnia "$HOME/.config/Insomnia/version-control"
-}
