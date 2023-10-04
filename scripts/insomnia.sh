eac261f400ee2cdc93ec7aba6d7c3f3463a9cdde
Parham Alvani <parham.alvani@gmail.com>
2023-10-04 05:02:25 +0000

    fix: correct insomnia configuration path

--- scripts/insomnia.sh
+++ scripts/insomnia.sh
@@ -31,8 +31,8 @@ main() {
 main_parham() {
 	mkdir -p "$HOME/.config/Insomnia/" || true
 
-	if [ -d "$HOME/.config/Insomnia/version_control" ]; then
-		cd "$HOME/.config/Insomnia/version_control" || return
+	if [ -d "$HOME/.config/Insomnia/version-control" ]; then
+		cd "$HOME/.config/Insomnia/version-control" || return
 
 		url=$(git remote get-url origin 2>/dev/null)
 		if [[ "$url" =~ .*github.com[:/]1995parham-me/insomnia ]]; then
@@ -45,20 +45,20 @@ main_parham() {
 
 			if yes_or_no "insomnia" "do you want to remove current insomnia configuration?"; then
 				msg 'removing current configuration to replace it with new configuration'
-				rm -Rf "$HOME/.config/Insomnia/version_control"
+				rm -Rf "$HOME/.config/Insomnia/version-control"
 			else
 				return 1
 			fi
 		fi
 	fi
 
-	if [ -e "$HOME/.config/Insomnia/version_control" ] || [ -L "$HOME/.config/Insomnia/version_control" ]; then
+	if [ -e "$HOME/.config/Insomnia/version-control" ] || [ -L "$HOME/.config/Insomnia/version-control" ]; then
 		if yes_or_no "insomnia" "do you want to remove current insomnia configuration?"; then
 			msg 'removing current configuration to replace it with new configuration'
-			rm -Rf "$HOME/.config/Insomnia/version_control"
+			rm -Rf "$HOME/.config/Insomnia/version-control"
 		else
 			return 1
 		fi
 	fi
-	git clone git@github.com:1995parham-me/insomnia "$HOME/.config/Insomnia/version_control"
+	git clone git@github.com:1995parham-me/insomnia "$HOME/.config/Insomnia/version-control"
 }
