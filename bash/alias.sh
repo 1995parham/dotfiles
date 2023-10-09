dc743819069b079eea4fa862ee89ed176a10cd1c
Parham Alvani <parham.alvani@gmail.com>
2023-10-09 08:21:43 +0000

    feat: add more alias into bash

--- bash/alias.sh
+++ bash/alias.sh
@@ -2,14 +2,14 @@
 
 # check the weather using wttr.in
 function wea() {
-	local request="wttr.in/${1-tehran}?Fqm"
+	local request="wttr.in/${1-Tehran}?Fqm"
 	[ "$(tput cols)" -lt 125 ] && request+='n'
 	curl -H "Accept-Language: en" --compressed "$request"
 }
 
 # tehran weather in one line
-alias wea1='curl -s "wttr.in/TEHRAN?format=3&m"'
+alias wea1='curl -s "wttr.in/{Miami,Austin,Tehran}?format=3&m"'
 # current weather in tehran
-alias weac='curl -s "wttr.in/TEHRAN?F0m"'
+alias weac='curl -s "wttr.in/Tehran?F0m"'
 # 3 day forecast in tehran
-alias weaf='curl -s "wttr.in/TEHRAN?Fqm"'
+alias weaf='curl -s "wttr.in/Tehran?Fqm"'
