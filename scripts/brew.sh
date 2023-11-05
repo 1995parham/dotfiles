6a2894c59fb1541712c20ea45be09f6c0257df36
Parham Alvani <parham.alvani@gmail.com>
2023-11-05 06:22:36 +0000

    feat: improve brew installation

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 usage() {
-	echo "The missing package manager for macOS (or Linux)"
+	echo "The missing package manager for macOS (not Linux)"
 	echo '
  _
 | |__  _ __ _____      __
@@ -13,20 +13,26 @@ usage() {
 }
 
 main_brew() {
-	xcode-select --install
+	if [[ ! "$(command -v brew)" ]]; then
+		xcode-select --install
 
-	/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
 
 	# shellcheck disable=2016
-	(
-		echo
-		echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-	) >>/Users/parham/.zprofile
+	if ! grep -q -F 'eval "$(/opt/homebrew/bin/brew shellenv)"' \
+		"$HOME/.zprofile"; then
+
+		# shellcheck disable=2016
+		(
+			echo
+			echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+		) |
+			tee -a "$HOME/.zprofile"
+	fi
+
+	# provides brew in the current shell
 	eval "$(/opt/homebrew/bin/brew shellenv)"
-
-	# shellcheck disable=2016
-	echo 'path=("/opt/homebrew/bin" $path)' >>~/.zshrc
-	echo 'export PATH' >>~/.zshrc
 
 	msg 'add cask-versions for finding alternative versions of Casks'
 	brew tap homebrew/cask-versions
@@ -37,22 +43,5 @@ main_pacman() {
 }
 
 main_apt() {
-	require_apt build-essential procps curl file git
-
-	/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-	msg "add homebrew to your profile"
-
-	test -d "$HOME/.linuxbrew" && eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
-
-	test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-	test -d /opt/homebrew/bin/ && eval "$(/opt/homebrew/bin/brew shellenv)"
-
-	brew_eval="eval \$($(brew --prefix)/bin/brew shellenv)"
-
-	if [ -r "$HOME/.bash_profile" ]; then
-		grep -i "$brew_eval" "$HOME/.bash_profile" || echo "$brew_eval" >>"$HOME/.bash_profile"
-	fi
-	grep -i "$brew_eval" "$HOME/.profile" || echo "$brew_eval" >>"$HOME/.profile"
+	return 1
 }
