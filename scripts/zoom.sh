3bacdcb19846c2fef6e3b61a2984deabc8d62781
Parham Alvani <parham.alvani@gmail.com>
2023-11-07 21:46:54 +0000

    feat: add zoom installation script

---
+++ scripts/zoom.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+usage() {
+	echo "Zoom is a communications platform that allows users to connect with video, audio, phone, and chat."
+
+	# shellcheck disable=1004,2016
+	echo '
+
+ _______   ___  _ __ ___
+|_  / _ \ / _ \| |_ ` _ \
+ / / (_) | (_) | | | | | |
+/___\___/ \___/|_| |_| |_|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_brew() {
+	require_brew_cask zoom
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
