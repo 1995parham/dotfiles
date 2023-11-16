8b34a4ba72c68073c4408dc1cdb3d17a53a5075e
Parham Alvani <parham.alvani@gmail.com>
2023-11-16 22:04:09 +0000

    feat: add minikube installation script

---
+++ scripts/minikube.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+usage() {
+	echo "minikube quickly sets up a local Kubernetes cluster on macOS, Linux, and Windows."
+
+	# shellcheck disable=1004,2016,2028
+	echo '
+           _       _ _          _
+ _ __ ___ (_)_ __ (_) | ___   _| |__   ___
+| |_ | _ \| | |_ \| | |/ / | | | |_ \ / _ \
+| | | | | | | | | | |   <| |_| | |_) |  __/
+|_| |_| |_|_|_| |_|_|_|\_\\__,_|_.__/ \___|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	require_pacman minikube
+}
+
+main_brew() {
+	require_bew minikube
+}
+
+main() {
+	minikube config set driver docker
+}
+
+main_parham() {
+	return 0
+}
