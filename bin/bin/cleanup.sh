23b517d8985e1bbcef0909eaf3f529d60004c849
Parham Alvani <parham.alvani@gmail.com>
2023-11-19 18:25:15 +0000

    feat: update cleanup script

--- bin/bin/cleanup.sh
+++ bin/bin/cleanup.sh
@@ -16,11 +16,13 @@ done
 while read -r folder; do
 	# shellcheck disable=2076
 	if [[ ! " ${stable_folders[*]} " =~ " ${folder} " ]]; then
+		echo "removing $folder..."
 		rm -rf "$folder"
 	fi
-done < <(find "$HOME/Downloads" -maxdepth 1 -mtime +1)
+done < <(find "$HOME/Downloads" -maxdepth 1 -mtime +12h)
 
 while read -r file; do
+	echo "removing $file..."
 	# shellcheck disable=2076
 	rm -rf "$file"
 done < <(find "$HOME/Downloads/Telegram Desktop" -mindepth 1 -mtime +15m)
