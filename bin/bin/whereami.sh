f185c3f125dd22b48a223e9bf90a8ec074661d2a
Parham Alvani <parham.alvani@gmail.com>
2023-11-23 05:57:16 +0000

    feat: update whereami script

--- bin/bin/whereami.sh
+++ bin/bin/whereami.sh
@@ -2,10 +2,8 @@
 
 set -eu
 
-echo '
-Sometimes we lose ourselves to success, gaining approval, meeting the expectations of others.
-Sometimes the lost part of ourselves is faith, hope, a dream.
-It is so easy to lose a piece of ourselves and it can happen in a thousand different ways.
-'
+# Sometimes we lose ourselves to success, gaining approval, meeting the expectations of others.
+# Sometimes the lost part of ourselves is faith, hope, a dream.
+# It is so easy to lose a piece of ourselves and it can happen in a thousand different ways.
 
-curl -sL http://ipconfig.io/json | jq '.ip, .country , .asn_org'
+curl -sL http://ipconfig.io/json | jq -j '"\(.ip) - \(.country) (\(.asn_org))"'
