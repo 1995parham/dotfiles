4ac17a7f1ed9ffcd726b05e55dbbc95542fb4280
Parham Alvani <parham.alvani@gmail.com>
2023-11-29 19:43:09 +0000

    feat: enable ssh on pegasus with love

---
+++ pegasus/scripts/ssh.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+usage() {
+	echo "Enable ssh-access to Pegasus"
+
+	# shellcheck disable=1004,2016
+	echo '
+         _
+ ___ ___| |__
+/ __/ __| |_ \
+\__ \__ \ | | |
+|___/___/_| |_|
+  '
+}
+
+export dependencies=("ssh-2fa")
+
+pre_main() {
+	if [ ! -f "$HOME/.profile" ]; then
+		echo "#!/usr/bin/env bash" >"$HOME/.profile"
+	fi
+}
+
+main_pacman() {
+	return 0
+}
+
+main() {
+	# shellcheck disable=2016
+	if ! grep -q -F 'eval "$(gnome-keyring-daemon --start)" && export SSH_AUTH_SOCK' \
+		"$HOME/.profile"; then
+
+		echo 'eval "$(gnome-keyring-daemon --start)" && export SSH_AUTH_SOCK' |
+			tee -a "$HOME/.profile"
+	fi
+}
