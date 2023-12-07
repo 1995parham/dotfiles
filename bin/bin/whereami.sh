2311084040808b8279326a080121905ef4f0fc79
Parham Alvani <parham.alvani@gmail.com>
2023-12-07 14:31:09 +0000

    chore: try to use cache in case of whereami command failure

--- bin/bin/whereami.sh
+++ bin/bin/whereami.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://stackoverflow.com/questions/3822621/how-to-exit-if-a-command-failed
-set -eu
-set -o pipefail
+# set -eu
+# set -o pipefail
 
 # Sometimes we lose ourselves to success, gaining approval, meeting the expectations of others.
 # Sometimes the lost part of ourselves is faith, hope, a dream.
@@ -10,4 +10,8 @@ set -o pipefail
 
 curl -sL http://ipconfig.io/json |
 	jq -j '"\(.ip) - \(.country) (\(.asn_org))"' 2>/dev/null |
-	tee /tmp/whereami.sh || cat /tmp/whereami.sh
+	tee /tmp/whereami.sh
+
+if [ "${PIPESTATUS[0]}" != "0" ]; then
+	cat /tmp/whereami.sh
+fi
