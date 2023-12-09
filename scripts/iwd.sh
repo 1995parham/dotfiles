4d2da4e680939c4d84fa6ccf8f784cd95f53e5fa
Parham Alvani <parham.alvani@gmail.com>
2023-12-09 22:21:39 +0000

    feat: add iwd installation script

---
+++ scripts/iwd.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+usage() {
+	echo "iNet wireless daemon, is a wireless daemon for Linux written by Intel"
+
+	# shellcheck disable=1004,2016
+	echo '
+ _             _
+(_)_      ____| |
+| \ \ /\ / / _` |
+| |\ V  V / (_| |
+|_| \_/\_/ \__,_|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	require_pacman iwd
+}
+
+main() {
+	copycat "iwd" "iwd/wifi-backend.conf" "/etc/NetworkManager/conf.d/wifi-backend.conf"
+}
