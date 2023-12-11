71489443f1bceedbcb236c768d40c63c56c06a5a
Parham Alvani <parham.alvani@gmail.com>
2023-12-11 06:00:01 +0000

    feat: add new installation script

---
+++ scripts/warp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+usage() {
+	echo "The free app that makes your Internet safer (even when you are using speedify)"
+
+	# shellcheck disable=1004,2016
+	echo '
+
+__      ____ _ _ __ _ __
+\ \ /\ / / _` | |__| |_ \
+ \ V  V / (_| | |  | |_) |
+  \_/\_/ \__,_|_|  | .__/
+                   |_|
+  '
+}
+
+main_brew() {
+	require_brew_cask cloudflare-warp
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
