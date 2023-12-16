42dfa4bc01f617e0e6360b001f77aff077b49d70
Roozbeh Sharifnasab <roozbeh.sharifnasab@snapp.cab>
2023-12-16 12:44:39 +0330

    revert indentation mess

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 usage() {
-    echo -n "installs required packages for having a working system"
-    echo '
+	echo -n "installs required packages for having a working system"
+	echo '
   ___ _ ____   __
  / _ \ |_ \ \ / /
 |  __/ | | \ V /
@@ -19,85 +19,85 @@ export dependencies=("fetch" "zsh" "bash")
 packages=(tmux htop aria2 curl bat vim jq yamllint fzf mosh figlet lolcat)
 
 brew_packages=(
-    coreutils
-    k6
-    inetutils
-    inxi
-    fontconfig
-    wget
-    tmuxp
-    dua-cli
-    git
-    bash
-    ripgrep
-    fd
-    glab
-    gh
-    just
-    bat-extras
-    wakatime-cli
-    muzzle
-    jcal
-    speedtest-cli
-    chafa
-    mtr
-    dog
-    yq
-    watch
-    mike-engel/jwt-cli/jwt-cli
+	coreutils
+	k6
+	inetutils
+	inxi
+	fontconfig
+	wget
+	tmuxp
+	dua-cli
+	git
+	bash
+	ripgrep
+	fd
+	glab
+	gh
+	just
+	bat-extras
+	wakatime-cli
+	muzzle
+	jcal
+	speedtest-cli
+	chafa
+	mtr
+	dog
+	yq
+	watch
+	mike-engel/jwt-cli/jwt-cli
 )
 
 apt_packages=(bmon atop jcal)
 
 pacman_packages=(
-    perl-image-exiftool
-    ripgrep
-    mtr
-    git-delta
-    fd
-    jless
-    chafa
-    dua-cli
-    github-cli glab
-    inetutils websocat fuse2
-    dog go-yq man-pages usbutils exfat-utils
-    openbsd-netcat
-    speedtest-cli
-    cpupower
-    reflector
-    jwt-cli
-    tokei
-    glow
-    wakatime
-    tmuxp
-    arch-wiki-lite arch-wiki-docs
-    pastel
-    man-db
-    bandwhich
-    lsof
-    vhs
-    just
-    bat-extras
-    tcpdump
+	perl-image-exiftool
+	ripgrep
+	mtr
+	git-delta
+	fd
+	jless
+	chafa
+	dua-cli
+	github-cli glab
+	inetutils websocat fuse2
+	dog go-yq man-pages usbutils exfat-utils
+	openbsd-netcat
+	speedtest-cli
+	cpupower
+	reflector
+	jwt-cli
+	tokei
+	glow
+	wakatime
+	tmuxp
+	arch-wiki-lite arch-wiki-docs
+	pastel
+	man-db
+	bandwhich
+	lsof
+	vhs
+	just
+	bat-extras
+	tcpdump
 )
 yay_packages=(jcal actionlint-bin cbonsai k6)
 
 main_apt() {
-    sudo apt update -yq
+	sudo apt update -yq
 
-    msg "install ${apt_packages[*]} + ${packages[*]} with apt"
-    require_apt "${apt_packages[@]}" "${packages[@]}"
+	msg "install ${apt_packages[*]} + ${packages[*]} with apt"
+	require_apt "${apt_packages[@]}" "${packages[@]}"
 }
 
 main_pacman() {
-    msg "install ${pacman_packages[*]} + ${packages[*]} with pacman"
-    require_pacman "${pacman_packages[@]}" "${packages[@]}"
+	msg "install ${pacman_packages[*]} + ${packages[*]} with pacman"
+	require_pacman "${pacman_packages[@]}" "${packages[@]}"
 
-    msg "install ${yay_packages[*]} with yay"
-    require_aur "${yay_packages[@]}"
+	msg "install ${yay_packages[*]} with yay"
+	require_aur "${yay_packages[@]}"
 }
 
 main_brew() {
-    msg "install ${brew_packages[*]} + ${packages[*]} with brew"
-    require_brew "${brew_packages[@]}" "${packages[@]}"
+	msg "install ${brew_packages[*]} + ${packages[*]} with brew"
+	require_brew "${brew_packages[@]}" "${packages[@]}"
 }
