56ca52d942c7594df457bfa58954fb78eae14ea5
Parham Alvani <parham.alvani@gmail.com>
2023-12-19 20:34:11 +0000

    feat: update tmux script

--- tmux/tmuxs/new.sh
+++ tmux/tmuxs/new.sh
@@ -13,7 +13,8 @@ tmuxs_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$tmuxs_root/../../scripts/lib/message.sh"
 
 project=$(
-	fd -tdirectory -H ^\.git$ ~/Documents/Git -x dirname |
+	# -H is not enough for having .git in your search, you need to have -I too.
+	fd -tdirectory -IH ^\.git$ ~/Documents/Git -x dirname |
 		fzf --color=fg:#ffa500,hl:#a9a9a9,prompt:#adff2f,separator:#ffe983,info:#ffe2ec \
 			--preview="onefetch {}; tokei {}"
 )
