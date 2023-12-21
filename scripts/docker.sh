dc7126f74658edd7f224d66e9b8a8d6bbd4ade5e
Parham Alvani <parham.alvani@gmail.com>
2023-12-21 16:49:31 +0000

    feat: update docker installation script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -28,7 +28,7 @@ main_brew() {
 main_pacman() {
 	require_pacman docker docker-compose dive docker-buildx docker-scan
 
-	require_aur hadolint-bin lazydocker-bin
+	require_aur hadolint-bin lazydocker
 
 	sudo mkdir /etc/docker || true
 	sudo touch /etc/docker/daemon.json
@@ -42,16 +42,6 @@ main_pacman() {
 	msg "manage docker as a non-root user"
 	sudo groupadd -f docker
 	sudo usermod -aG docker "$USER"
-
-	if yes_or_no "docker" "do you want to use v2ray as a docker proxy?"; then
-		sudo mkdir -p /etc/systemd/system/docker.service.d/ || true
-		echo '
-[Service]
-Environment="HTTP_PROXY=http://127.0.0.1:1080/"
-Environment="HTTPS_PROXY=http://127.0.0.1:1080/"
-    ' | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf
-		sudo systemctl daemon-reload
-	fi
 
 	sudo mkdir -p /etc/systemd/system/containerd.service.d/ || true
 	echo '
