0dc4f04048070f08195c2d163848daac18e26937
Parham Alvani <parham.alvani@gmail.com>
2024-01-05 16:18:56 +0000

    feat: add dvc installation script

---
+++ scripts/dvc.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+usage() {
+	echo "Data and model versioning"
+
+	# shellcheck disable=1004,2016
+	echo '
+     _
+  __| |_   _____
+ / _` \ \ / / __|
+| (_| |\ V / (__
+ \__,_| \_/ \___|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	require_aur dvc
+}
+
+main_brew() {
+	require_brew dvc
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
