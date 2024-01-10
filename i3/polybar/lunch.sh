8f1754ab62f2098460ae760b3382fac2b74541d5
Parham Alvani <parham.alvani@gmail.com>
2024-01-10 21:34:05 +0000

    feat: update polybar script for lunching it

--- i3/polybar/lunch.sh
+++ i3/polybar/lunch.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
-# In The Name of God
-# ========================================
-# [] File Name : lunch.sh
-#
-# [] Creation Date : 11-04-2021
-#
-# [] Created By : Parham Alvani <parham.alvani@gmail.com>
-# =======================================
+
 # https://github.com/polybar/polybar/wiki#launching-the-bar-in-your-wms-bootstrap-routine
 
-# terminate already running bar instances
+# Terminate already running bar instances:
 # killall -q polybar
-# if all your bars have ipc enabled, you can also use
+# If all your bars have IPC enabled, it is better to use:
 polybar-msg cmd quit
 
-# launch bars
+# Launch 2 bars
 bars=('top' 'bottom')
 
 for bar in "${bars[@]}"; do
@@ -24,7 +17,7 @@ for bar in "${bars[@]}"; do
 	disown
 done
 
-# wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 echo "bars launched..."
