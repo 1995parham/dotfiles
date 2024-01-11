ff0cd8bc8e8ccb84778395d0772ecd7de7b79afc
Parham Alvani <parham.alvani@gmail.com>
2024-01-11 14:31:58 +0000

    feat: update tex installation script

--- scripts/tectonic.sh
+++ scripts/tectonic.sh
@@ -14,8 +14,16 @@ usage() {
 }
 
 main_pacman() {
-	require_pacman texlab python-pygments graphviz tectonic
+	require_pacman texlab python-pygments graphviz tectonic texlive-binextra perl
 	require_aur libxcrypt-compat
+
+	# install the rquired perl modules (to use latexindent)
+	sudo cpan -i App::cpanminus
+	sudo cpanm YAML::Tiny
+	sudo cpanm File::HomeDir
+	sudo cpanm Unicode::GCString
+	sudo cpanm Log::Log4perl
+	sudo cpanm Log::Dispatch::File
 }
 
 main_brew() {
