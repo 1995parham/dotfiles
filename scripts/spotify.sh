aa6c3ce239c102d5a7b7b4f253665c47792095e6
Parham Alvani <parham.alvani@gmail.com>
2024-01-11 17:15:47 +0000

    feat: add spotify installation script

---
+++ scripts/spotify.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+usage() {
+  echo "Spotify is a digital music service that gives you access to millions of songs."
+
+  # shellcheck disable=1004,2016
+  echo '
+                 _   _  __
+ ___ _ __   ___ | |_(_)/ _|_   _
+/ __| |_ \ / _ \| __| | |_| | | |
+\__ \ |_) | (_) | |_| |  _| |_| |
+|___/ .__/ \___/ \__|_|_|  \__, |
+    |_|                    |___/
+  '
+}
+
+
+
+pre_main() {
+  return 0
+}
+
+main_pacman() {
+  return 1
+}
+
+main_apt() {
+  return 1
+}
+
+main_brew() {
+  return 1
+}
+
+main() {
+  return 0
+}
+
+main_parham() {
+  return 0
+}
