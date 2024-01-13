97d79ed11c8c6f8364bb89b7be9349f367437aa0
Parham Alvani <parham.alvani@gmail.com>
2024-01-13 18:39:28 +0000

    Squashed 'scripts/lib/' changes from 8531204e..0bf2be76
    0bf2be76 chore: change the way for finding the source of script
    
    git-subtree-dir: scripts/lib
    git-subtree-split: 0bf2be76628330c76c9c53a9af1e6b95249e21a6

--- main.sh
+++ main.sh
@@ -2,13 +2,15 @@
 
 # aggregate all scripts and load them in one place.
 
-dotfile_lib_root="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
+dotfile_lib_root="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
 # shellcheck source=message.sh
 source "$dotfile_lib_root/message.sh"
 # shellcheck source=proxy.sh
 source "$dotfile_lib_root/proxy.sh"
-# shellcheck source=linker.sh
-source "$dotfile_lib_root/linker.sh"
+if [ -n "$root" ]; then
+	# shellcheck source=linker.sh
+	source "$dotfile_lib_root/linker.sh"
+fi
 # shellcheck source=require.sh
 source "$dotfile_lib_root/require.sh"
