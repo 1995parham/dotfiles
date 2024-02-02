7c36b216ee587697daf10ed37a6e14c113b016ab
Parham Alvani <parham.alvani@gmail.com>
2024-02-02 05:29:07 +0000

    feat: update wezterm configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,6 +18,31 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action.ActivateTabRelative(-1),
 	},
+	{
+		key = "LeftArrow",
+		mods = "CTRL | SHIFT",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "RightArrow",
+		mods = "CTRL | SHIFT",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
 }
+
+wezterm.on("update-right-status", function(window, _)
+	local hostname = " " .. wezterm.nerdfonts.fa_laptop .. " " .. wezterm.hostname() .. " "
+
+	window:set_right_status(wezterm.format({
+		{ Foreground = { Color = "#ffffff" } },
+		{ Text = hostname },
+	}))
+end)
+
+-- machine specific configuration based on the hostname
+local hostname = wezterm.hostname()
+if hostname == "pegasus" then
+	config.font_size = 10
+end
 
 return config
