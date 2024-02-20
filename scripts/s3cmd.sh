bc1515131c9e2300bc8d5f10389c8afced8947e8
Parham Alvani <parham.alvani@gmail.com>
2024-02-20 05:07:03 +0000

    feat: add s3cmd script

---
+++ scripts/s3cmd.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+usage() {
+	echo "Command line tool for managing S3 compatible storage services (including Amazon S3 and CloudFront)"
+
+	# shellcheck disable=1004,2016
+	echo '
+     _____                    _
+ ___|___ /  ___ _ __ ___   __| |
+/ __| |_ \ / __| |_ ` _ \ / _` |
+\__ \___) | (__| | | | | | (_| |
+|___/____/ \___|_| |_| |_|\__,_|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	return 1
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	return 1
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
