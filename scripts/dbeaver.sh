79dc1dd4a8f4086041057624137a8d65291455d0
Parham Alvani <parham.alvani@gmail.com>
2024-02-21 22:27:46 +0000

    feat: add new database tool

---
+++ scripts/dbeaver.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+usage() {
+	echo "DBeaver Community is a free cross-platform database tool for developers, database administrators, analysts, and everyone working with data."
+
+	# shellcheck disable=1004,2016
+	echo '
+     _ _
+  __| | |__   ___  __ ___   _____ _ __
+ / _` | |_ \ / _ \/ _` \ \ / / _ \ |__|
+| (_| | |_) |  __/ (_| |\ V /  __/ |
+ \__,_|_.__/ \___|\__,_| \_/ \___|_|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	return 1
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	require_brew dbeaver-community
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
