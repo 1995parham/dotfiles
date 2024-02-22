3a5a9325cd800ddca8c06dcf25509688b858f6a1
Parham Alvani <parham.alvani@gmail.com>
2024-02-22 13:10:00 +0000

    feat: improve tmux main layout hello script

--- tmux/tmuxs/hello.sh
+++ tmux/tmuxs/hello.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+
 set -e
 
 # a global variable that points to tmuxs root directory.
 tmuxs_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck source=./scripts/lib/message.sh
-source "$tmuxs_root/../../scripts/lib/message.sh"
-# shellcheck source=./scripts/lib/require.sh
-source "$tmuxs_root/../../scripts/lib/require.sh"
+# shellcheck source=./scripts/lib/main.sh
+source "$tmuxs_root/../../scripts/lib/main.sh"
 
-not_require_country 'Iran'
+if ! not_require_country 'Iran'; then
+	if ! yes_or_no 'do you want to continue?'; then
+		exit 1
+	fi
+fi
