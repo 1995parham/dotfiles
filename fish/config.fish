19448f24e2dc7e575f9f73e315a2a6ebccc830cd
Parham Alvani <parham.alvani@gmail.com>
2024-02-28 08:41:32 +0000

    chore: add ls function

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,10 @@ starship init fish | source
 fish_add_path $HOME/bin
 fish_add_path $HOME/.local/bin
 
+function ls --description 'List contents of directory'
+    command ls -G $argv
+end
+
 # Setting PATH for Python 3.12
 # The original version is saved in /Users/elahe/.config/fish/config.fish.pysave
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
