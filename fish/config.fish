2d7363d736ebe1443d6fdbe2e6c3527f488d382b
Parham Alvani <parham.alvani@gmail.com>
2024-02-29 17:25:32 +0000

    feat: customize navi keybinding in fish

--- fish/config.fish
+++ fish/config.fish
@@ -18,8 +18,36 @@ function ls --description 'List contents of directory'
     command ls -G $argv
 end
 
+function _navi_smart_replace
+  set -l current_process (commandline -p | string trim)
+
+  if test -z "$current_process"
+      commandline -i (navi --print)
+  else
+      set -l best_match (navi --print --best-match --query "$current_process")
+
+      if not test "$best_match" >/dev/null
+          return
+      end
+
+      if test -z "$best_match"
+          commandline -p (navi --print --query "$current_process")
+      else if test "$current_process" != "$best_match"
+          commandline -p $best_match
+      else
+          commandline -p (navi --print --query "$current_process")
+      end
+  end
+
+  commandline -f repaint
+end
+
 if type -q navi
-  navi widget fish | source
+  if test $fish_key_bindings = fish_default_key_bindings
+      bind \ch _navi_smart_replace
+  else
+      bind -M insert \ch _navi_smart_replace
+  end
 end
 
 # Setting PATH for Python 3.12
