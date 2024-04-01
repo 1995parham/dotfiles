7cb684db32e1fd1803aa0d380ff4f6b1e5ef6323
Parham Alvani <parham.alvani@gmail.com>
2024-04-01 08:10:18 +0000

    feat: update wezterm configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -52,11 +52,65 @@ wezterm.on("update-right-status", function(window, _)
 	local _, clock, _ = wezterm.run_child_process({ "bash", "-lc", "TZ='Asia/Tehran' date +%H:%M:%S" })
 	clock = wezterm.nerdfonts.fa_clock_o .. " " .. clock:gsub("[\n\r]", " ")
 
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		local percentage = b.state_of_charge * 100
+
+		if b.state == "Charging" then
+			if 0 <= percentage and percentage < 10 then
+				bat = wezterm.nerdfonts.md_battery_charging_10 .. " "
+			elseif 10 <= percentage and percentage < 20 then
+				bat = wezterm.nerdfonts.md_battery_charging_20 .. " "
+			elseif 20 <= percentage and percentage < 30 then
+				bat = wezterm.nerdfonts.md_battery_charging_30 .. " "
+			elseif 30 <= percentage and percentage < 40 then
+				bat = wezterm.nerdfonts.md_battery_charging_40 .. " "
+			elseif 40 <= percentage and percentage < 50 then
+				bat = wezterm.nerdfonts.md_battery_charging_50 .. " "
+			elseif 50 <= percentage and percentage < 60 then
+				bat = wezterm.nerdfonts.md_battery_charging_60 .. " "
+			elseif 60 <= percentage and percentage < 70 then
+				bat = wezterm.nerdfonts.md_battery_charging_70 .. " "
+			elseif 70 <= percentage and percentage < 80 then
+				bat = wezterm.nerdfonts.md_battery_charging_80 .. " "
+			elseif 80 <= percentage and percentage < 90 then
+				bat = wezterm.nerdfonts.md_battery_charging_90 .. " "
+			else
+				bat = wezterm.nerdfonts.md_battery_charging_100 .. " "
+			end
+		else
+			if 0 <= percentage and percentage < 10 then
+				bat = wezterm.nerdfonts.md_battery_10 .. " "
+			elseif 10 <= percentage and percentage < 20 then
+				bat = wezterm.nerdfonts.md_battery_20 .. " "
+			elseif 20 <= percentage and percentage < 30 then
+				bat = wezterm.nerdfonts.md_battery_30 .. " "
+			elseif 30 <= percentage and percentage < 40 then
+				bat = wezterm.nerdfonts.md_battery_40 .. " "
+			elseif 40 <= percentage and percentage < 50 then
+				bat = wezterm.nerdfonts.md_battery_50 .. " "
+			elseif 50 <= percentage and percentage < 60 then
+				bat = wezterm.nerdfonts.md_battery_60 .. " "
+			elseif 60 <= percentage and percentage < 70 then
+				bat = wezterm.nerdfonts.md_battery_70 .. " "
+			elseif 70 <= percentage and percentage < 80 then
+				bat = wezterm.nerdfonts.md_battery_80 .. " "
+			elseif 80 <= percentage and percentage < 90 then
+				bat = wezterm.nerdfonts.md_battery_90 .. " "
+			else
+				bat = wezterm.nerdfonts.md_battery_100 .. " "
+			end
+		end
+
+		bat = bat .. string.format("%.0f%%", percentage)
+	end
+
 	window:set_right_status(wezterm.format({
 		{ Foreground = { Color = "#ffffff" } },
 		{ Text = "  " .. hostname .. "  " },
 		{ Text = "  " .. clock .. "  " },
 		{ Text = "  " .. jalali_date .. "  " },
+		{ Text = "  " .. bat .. "  " },
 	}))
 end)
 
