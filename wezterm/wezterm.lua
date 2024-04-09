fdf8b6e4ba7b95974330b3cace67e224d631833a
Parham Alvani <parham.alvani@gmail.com>
2024-04-09 07:50:24 +0000

    chore: update font size :monocle_face:

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -117,7 +117,7 @@ end)
 -- machine specific configuration based on the hostname
 local hostname = wezterm.hostname()
 if hostname == "millennium-falcon" or hostname == "tantive-iv" then
-	config.font_size = 10
+	config.font_size = 9
 end
 
 return config
