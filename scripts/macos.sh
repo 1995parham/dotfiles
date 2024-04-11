284eb3ed4c035ee73b44c07ca33075eaef276cfd
Parham Alvani <parham.alvani@gmail.com>
2024-04-11 05:39:03 +0000

    feat: update macos script

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -54,6 +54,7 @@ main_brew() {
 	if [[ "$USER" == "parham" ]]; then
 		defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:///Applications/Google Chrome Beta.app/</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>'
 		defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:///opt/homebrew/opt/emacs-plus@30/Emacs.app</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>'
+		defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:///Applications/Obsidian.app/</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>'
 	else
 		defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:///Applications/Google Chrome.app/</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>'
 	fi
