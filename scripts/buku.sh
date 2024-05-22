69b4dc12d1fd7eb9d89967168ea44cb5c3ec2db5
Parham Alvani <parham.alvani@gmail.com>
2024-05-22 13:39:11 +0000

    fix: correct lint issues

--- scripts/buku.sh
+++ scripts/buku.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 usage() {
-  echo -n "personal mini-web in text"
-  # shellcheck disable=2016,2028
-  echo '
+	echo -n "personal mini-web in text"
+	# shellcheck disable=2016,2028
+	echo '
  _           _
 | |__  _   _| | ___   _
 | |_ \| | | | |/ / | | |
@@ -15,24 +15,24 @@ usage() {
 export dependencies=('python' 'rust')
 
 main_pacman() {
-  require_pip buku
-  require_aur bukubrow
+	require_pip buku
+	require_aur bukubrow
 }
 
 main_brew() {
-  require_brew buku
+	require_brew buku
 }
 
 main() {
-  return 0
+	return 0
 }
 
 main_parham() {
-  msg "hello parham, clone your bookmarks private repository"
+	msg "hello parham, clone your bookmarks private repository"
 
-  clone git@github.com:parham-alvani/tabs "$HOME/Documents/Git/parham/parham-alvani"
+	clone git@github.com:parham-alvani/tabs "$HOME/Documents/Git/parham/parham-alvani"
 
-  cd "$HOME/Documents/Git/parham/parham-alvani/tabs" || return
-  mkdir -p "$HOME/.local/share/buku" && linker "buku" "$(pwd)/bookmarks.db" "$_/bookmarks.db"
-  cd - || return
+	cd "$HOME/Documents/Git/parham/parham-alvani/tabs" || return
+	mkdir -p "$HOME/.local/share/buku" && linker "buku" "$(pwd)/bookmarks.db" "$_/bookmarks.db"
+	cd - || return
 }
