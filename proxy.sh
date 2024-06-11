19ac164607e9e423dded94be238971eedc0a5a4a
Parham Alvani <parham.alvani@gmail.com>
2024-06-11 04:35:49 +0000

    Squashed 'scripts/lib/' changes from 5fcb1fd6..917c85bf
    917c85bf feat: improve proxy command
    415b87bf chore: remove unused output
    375c5a26 fix: correct clone function
    06f7b4c1 fix: correct lint issues
    
    git-subtree-dir: scripts/lib
    git-subtree-split: 917c85bf650837c8e741372a338eeca81152c00a

--- proxy.sh
+++ proxy.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 # https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy/
 
+# by default it checkes for 127.0.0.1:2081 to find a proxy
+# but you can also manually pass the proxy url.
+
 proxy_start() {
-	if [[ -n $(command -v ss) ]]; then
-		if ! (ss -tunl | grep :2081 &>/dev/null); then
-			return 0
-		fi
-	elif [[ -n $(command -v netstat) ]]; then
-		if ! (netstat -an | grep LISTEN | grep 2081 &>/dev/null); then
-			return 0
+	if [ $# -gt 1 ]; then
+		return 1
+	elif [ $# -eq 1 ]; then
+		url="$1"
+	else
+		url="http://127.0.0.1:2081"
+
+		if [[ -n $(command -v ss) ]]; then
+			if ! (ss -tunl | grep :2081 &>/dev/null); then
+				return 0
+			fi
+		elif [[ -n $(command -v netstat) ]]; then
+			if ! (netstat -an | grep LISTEN | grep 2081 &>/dev/null); then
+				return 0
+			fi
 		fi
 	fi
 
-	echo -e "\033[38;5;46m[proxy] \033[38;5;202msetup proxy based on local http proxy which is setup by v2ray\033[39m"
+	echo -e "\033[38;5;46m[proxy] \033[38;5;202msetup proxy based on http proxy on $url\033[39m"
 	echo -e "\033[38;5;46m[proxy] \033[38;5;202mpress enter to continue or anything else to disable it\033[39m"
 	read -r accept
 
@@ -23,9 +34,9 @@ proxy_start() {
 	echo
 	curl --max-time 10 https://ipconfig.io/country || return 1
 
-	export ftp_proxy="http://127.0.0.1:2081"
-	export http_proxy="http://127.0.0.1:2081"
-	export https_proxy="http://127.0.0.1:2081"
+	export ftp_proxy="$url"
+	export http_proxy="$url"
+	export https_proxy="$url"
 	alias sudo='sudo -E'
 
 	echo
@@ -38,33 +49,4 @@ proxy_stop() {
 	unalias sudo 2>/dev/null || true
 
 	echo -e "\033[38;5;46m[proxy] \033[38;5;202mall proxy script configurations are removed\033[39m"
-}
-
-socks_start() {
-	echo -e "\033[38;5;46m[socks] \033[38;5;202msetup proxy based on local socks proxy which is setup by v2ray\033[39m"
-	echo -e "\033[38;5;46m[socks] \033[38;5;202mpress enter to continue or anything else to disable it\033[39m"
-	read -r accept
-
-	if [[ "${accept}" != "" ]]; then
-		return 0
-	fi
-
-	echo
-	curl --max-time 10 https://ipconfig.io/country || return 1
-
-	export ftp_proxy="http://127.0.0.1:2081"
-	export http_proxy="socks5://127.0.0.1:2080"
-	export https_proxy="socks5://127.0.0.1:2080"
-	alias sudo='sudo -E'
-
-	echo
-	curl --max-time 10 https://ipconfig.io/country || socks_stop
-	echo
-}
-
-socks_stop() {
-	unset {http,https,ftp}_proxy || true
-	unalias sudo 2>/dev/null || true
-
-	echo -e "\033[38;5;46m[socks] \033[38;5;202mall proxy script configurations are removed\033[39m"
 }
