9fdc7bce50b9a38c357eb0270f5f1ab48529cd19
Parham Alvani <parham.alvani@gmail.com>
2024-07-22 03:58:34 +0000

    feat: add task warrior

---
+++ scripts/taskwarrior.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+usage() {
+	echo "Taskwarrior - Command line Task Management"
+
+	# shellcheck disable=1004,2016
+	echo '
+ _            _                             _
+| |_ __ _ ___| | ____      ____ _ _ __ _ __(_) ___  _ __
+| __/ _` / __| |/ /\ \ /\ / / _` | |__| |__| |/ _ \| |__|
+| || (_| \__ \   <  \ V  V / (_| | |  | |  | | (_) | |
+ \__\__,_|___/_|\_\  \_/\_/ \__,_|_|  |_|  |_|\___/|_|
+  '
+}
+
+pre_main() {
+	return 0
+}
+
+main_pacman() {
+	return 1
+}
+
+main_apt() {
+	return 1
+}
+
+main_brew() {
+	require_brew task
+}
+
+main() {
+	return 0
+}
+
+main_parham() {
+	return 0
+}
