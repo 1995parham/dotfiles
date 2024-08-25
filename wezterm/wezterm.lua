d1a642a6bc14811d3091e075a64f26161175761b
Parham Alvani <parham.alvani@gmail.com>
2024-08-25 13:23:03 +0000

    chore: my eyes are better than before?

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -117,7 +117,7 @@ end)
 -- machine specific configuration based on the hostname
 local hostname = wezterm.hostname()
 if hostname == "millennium-falcon" then
-    config.font_size = 9
+    config.font_size = 8.5
 end
 if hostname == "tantive-iv" then
     config.font_size = 8.5
