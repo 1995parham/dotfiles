29038a243ea80795490c83ff355c54cebfcd7a7f
Parham Alvani <parham.alvani@gmail.com>
2024-10-02 22:31:41 +0000

    feat: switch tmux to use alacritty

--- tmux/tmuxs/tmuxp.sh
+++ tmux/tmuxs/tmuxp.sh
@@ -22,16 +22,9 @@ fi
 # using commands are better because tmux can be share between different
 # terminal emulators and this will mess up the environment variables.
 if [[ "${OSTYPE}" == "darwin"* ]]; then
-    if [[ -n "$(command -v wezterm)" ]]; then
-        # TODO (parham): replace it with a better solution. it works only on macOS and it isn't tested enough.
-        socket="$HOME/.local/share/wezterm/default-org.wezfurlong.wezterm"
-        export WEZTERM_UNIX_SOCKET="$socket"
-
-        pane_id=$(wezterm cli spawn)
-        wezterm cli send-text --pane-id "${pane_id}" "$(printf "%s\r" "tmuxp load $path")"
-        wezterm cli activate-pane --pane-id "${pane_id}"
-    elif [[ -n "$(command -v kitty)" ]]; then
-        kitty @ launch --type=tab --hold --env PATH="$PATH" tmuxp load "$path"
+    bash="$(which bash)"
+    if [[ -n "$(command -v alacritty)" ]]; then
+        alacritty msg create-window -e "$bash" -lc "tmuxp load $path -y"
     fi
 else
     tmuxp load "$path"
