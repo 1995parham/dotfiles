803d2f842990fb4c8a1ed99992efc4583805f1a1
Parham Alvani <parham.alvani@gmail.com>
2024-10-31 23:27:27 +0000

    feat: disable siri in macos

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -21,6 +21,21 @@ main_brew() {
 
     sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Welcome to Parham and Elahe's Mac. Please treat our digital space with the same respect you'd show our home. For access, kindly text or call us."
 
+    msg 'Disable Siri'
+    defaults write com.apple.assistant.support 'Assistant Enabled' -bool false
+    defaults write com.apple.assistant.backedup 'Use device speaker for TTS' -int 3
+    launchctl disable "user/$UID/com.apple.assistantd"
+    launchctl disable "gui/$UID/com.apple.assistantd"
+    sudo launchctl disable 'system/com.apple.assistantd'
+    launchctl disable "user/$UID/com.apple.Siri.agent"
+    launchctl disable "gui/$UID/com.apple.Siri.agent"
+    sudo launchctl disable 'system/com.apple.Siri.agent'
+    defaults write com.apple.SetupAssistant 'DidSeeSiriSetup' -bool True
+    defaults write com.apple.systemuiserver 'NSStatusItem Visible Siri' 0
+    defaults write com.apple.Siri 'StatusMenuVisible' -bool false
+    defaults write com.apple.Siri 'UserHasDeclinedEnable' -bool true
+    defaults write com.apple.assistant.support 'Siri Data Sharing Opt-In Status' -int 2
+
     msg "General UI/UX"
 
     # Close any open System Preferences panes, to prevent them from overriding
