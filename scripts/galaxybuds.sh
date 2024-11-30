836a567fa72f585dfd518d07b91c15e9de1a8f55
Parham Alvani <parham.alvani@gmail.com>
2024-11-30 13:46:18 +0000

    feat: add new script to manage your galaxy buds

---
+++ scripts/galaxybuds.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo "The Samsung Galaxy Buds are a line of wireless Bluetooth earbuds designed by Samsung Electronics. They were first released on March 9, 2019, as the successor to the Gear IconX."
+
+    # shellcheck disable=1004,2016,2028
+    echo '
+             _                  _               _
+  __ _  __ _| | __ ___  ___   _| |__  _   _  __| |___
+ / _` |/ _` | |/ _` \ \/ / | | | |_ \| | | |/ _` / __|
+| (_| | (_| | | (_| |>  <| |_| | |_) | |_| | (_| \__ \
+ \__, |\__,_|_|\__,_/_/\_\\__, |_.__/ \__,_|\__,_|___/
+ |___/                    |___/
+  '
+}
+
+pre_main() {
+    return 0
+}
+
+main_pacman() {
+    return 1
+}
+
+main_xbps() {
+    return 1
+}
+
+main_apt() {
+    return 1
+}
+
+main_brew() {
+    require_brew_cask galaxybudsclient
+}
+
+main() {
+    return 0
+}
+
+main_parham() {
+    return 0
+}
