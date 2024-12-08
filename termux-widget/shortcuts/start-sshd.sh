78180296e4ceacd2491ea7521bdb3a8874007003
Parham Alvani <parham.alvani@gmail.com>
2024-12-08 10:35:08 +0000

    feat: move termux widget into dotdroid

--- termux-widget/shortcuts/start-sshd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# https://stackoverflow.com/questions/3822621/how-to-exit-if-a-command-failed
-set -eu
-set -o pipefail
-
-sshd
--- termux-widget/shortcuts/tasks/org-sync.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# https://stackoverflow.com/questions/3822621/how-to-exit-if-a-command-failed
-set -eu
-set -o pipefail
-
-cd /sdcard/Notes/org
-git pull origin main
-git push origin main
