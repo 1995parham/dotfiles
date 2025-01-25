471bcf5473014a6ad608e177414bbce72896d2c8
Parham Alvani <parham.alvani@gmail.com>
2025-01-25 21:36:47 +0000

    chore: start using wezterm to see how it goes

--- tmux/tmuxs/tmuxp.sh
+++ tmux/tmuxs/tmuxp.sh
@@ -3,7 +3,7 @@ set -e
 
 # shellcheck disable=2046,2016
 layout=$(
-    grep 'session_name:' $(fd .\.yaml "$HOME/.config/tmuxp") | cut -d':' -f3 |
+    grep 'session_name:' $(fd .\.yaml "$HOME/.config/tmuxp") | cut -d':' -f3 | awk '{$1=$1;print}' |
         fzf --color=fg:#ffa500,hl:#a9a9a9,prompt:#adff2f,separator:#ffe983,info:#ffe2ec \
             --preview='bat -f $(grep -l {} $(fd .\.yaml "$HOME/.config/tmuxp"))'
 )
@@ -13,7 +13,7 @@ if [ -z "$layout" ]; then
 fi
 
 # shellcheck disable=2046
-path=$(grep -l "^session_name:$layout\$" $(fd .\.yaml "$HOME/.config/tmuxp"))
+path=$(grep -l "^session_name: $layout\$" $(fd .\.yaml "$HOME/.config/tmuxp"))
 
 if [ ! -f "$path" ]; then
     exit 1
@@ -23,11 +23,10 @@ fi
 # terminal emulators and this will mess up the environment variables.
 if [[ "${OSTYPE}" == "darwin"* ]]; then
     bash="$(which bash)"
-    if [[ -n "$(command -v alacritty)" ]]; then
-        alacritty msg create-window -e "$bash" -lc "tmuxp load $path -y"
-    fi
-    if [ -e "/Applications/Alacritty.app/Contents/MacOS/alacritty" ]; then
-        "/Applications/Alacritty.app/Contents/MacOS/alacritty" msg create-window -e "$bash" -lc "tmuxp load $path -y"
+
+    if [[ -n "$(command -v wezterm)" ]]; then
+        echo "tmuxp load $path -y -d && tmux a -t '$layout'"
+        wezterm cli spawn -- "$bash" -ilc "tmuxp load $path -y -d && tmux a -t '$layout'"
     fi
 else
     tmuxp load "$path"
