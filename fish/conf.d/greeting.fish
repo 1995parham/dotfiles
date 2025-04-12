e2ab2d265d0932528120eb26d7c56dd81bbe8f0b
Parham Alvani <parham.alvani@gmail.com>
2025-04-12 05:55:07 +0000

    Squashed 'scripts/lib/' changes from 007b7b07..74da322c
    74da322c fix: handle timeout in detecting country
    
    git-subtree-dir: scripts/lib
    git-subtree-split: 74da322cd14ca0e4c0e122b35d95b7caf6c3190e

--- fish/conf.d/greeting.fish
+++
@@ -1,4 +0,0 @@
-function fish_greeting
-    echo "Hello 🐼!"
-    echo "Sometimes when you're in a dark place you think you've been buried, but you've actually been planted."
-end
--- scripts/tex.sh
+++
@@ -1,1 +0,0 @@
-tectonic.sh
