88f66939e8d8f3f0e6e5e7f7ddb1acb77a7cdb02
Parham Alvani <parham.alvani@gmail.com>
2025-05-17 16:50:14 +0000

    chore: remove efp from vscode

--- scripts/code.sh
+++ scripts/code.sh
@@ -26,7 +26,7 @@ main_pacman() {
 
 main_brew() {
     require_brew_cask visual-studio-code visual-studio-code@insiders
-    require_brew devcontainer efm-langserver
+    require_brew devcontainer
 }
 
 main() {
