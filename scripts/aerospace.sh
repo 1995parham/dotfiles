7d0391200804e558952fadc17dd2d1b5fc4f885f
Parham Alvani <parham.alvani@gmail.com>
2025-06-26 10:51:54 +0000

    feat: add aerospace installation

---
+++ scripts/aerospace.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+usage() {
+    echo "AeroSpace is an i3-like tiling window manager for macOS"
+
+    # shellcheck disable=1004,2016
+    echo '
+
+  __ _  ___ _ __ ___  ___ _ __   __ _  ___ ___
+ / _` |/ _ \ |__/ _ \/ __| |_ \ / _` |/ __/ _ \
+| (_| |  __/ | | (_) \__ \ |_) | (_| | (_|  __/
+ \__,_|\___|_|  \___/|___/ .__/ \__,_|\___\___|
+                         |_|
+  '
+}
+
+pre_main() {
+    return 0
+}
+
+main_pacman() {
+    return 1
+}
+
+main_xbps() {
+    return 1
+}
+
+main_apt() {
+    return 1
+}
+
+main_pkg() {
+    return 1
+}
+
+main_brew() {
+    require_brew_cask nikitabobko/tap/aerospace
+}
+
+main() {
+    return 0
+}
+
+main_parham() {
+    return 0
+}
