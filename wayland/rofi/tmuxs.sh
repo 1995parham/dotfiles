370e04c9b09afd5bed8011ae3329c589b1287890
Parham Alvani <parham.alvani@gmail.com>
2025-07-03 05:25:44 +0000

    Squashed 'scripts/lib/' changes from 94ebfb2a..03799d71
    03799d71 feat: use another domain to have ip address information
    
    git-subtree-dir: scripts/lib
    git-subtree-split: 03799d71b9091e1ca4006982c919aeaa6bca8ed1

--- wayland/rofi/tmuxs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# = 1 ]; then
-    coproc alacritty -e tmux attach -t "${1%%:*}"
-elif [ $# = 0 ]; then
-    # rofi first calls script without arguments to have the
-    # entries.
-    tmux list-sessions
-fi
