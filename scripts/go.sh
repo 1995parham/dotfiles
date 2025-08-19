41a1a02f87ba5e5142e85922c9f201d9cc1cf36f
Parham Alvani <parham.alvani@gmail.com>
2025-08-19 16:21:26 +0000

    feat: remove wire from go required packages

--- scripts/go.sh
+++ scripts/go.sh
@@ -61,7 +61,7 @@ go-install-packages() {
     require_go golang.org/x/tools/gopls
     require_go golang.org/dl/gotip
     require_go github.com/go-delve/delve/cmd/dlv
-    require_go github.com/google/wire/cmd/wire
+    # require_go github.com/google/wire/cmd/wire
     require_go github.com/abice/go-enum
     require_go github.com/swaggo/swag/cmd/swag
     require_go golang.org/x/tools/cmd/gonew
