5c7daf9d36ae315bb9bf6920148e13642f84297c
Parham Alvani <parham.alvani@gmail.com>
2025-10-07 20:23:27 +0000

    feat: update neovim installation script

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -32,39 +32,65 @@ main_brew() {
     require_pip 'nvim-remote'
 }
 
+remove_nvim_config() {
+    if yes_or_no "neovim" "do you want to remove current neovim configuration?"; then
+        msg 'removing current configuration to replace it with new configuration'
+        rm -rf "$HOME/.config/nvim"
+        return 0
+    else
+        return 1
+    fi
+}
+
 main() {
-    if [ -d "$HOME/.config/nvim" ]; then
-        cd "$HOME/.config/nvim" || return
+    # Check if nvim config exists
+    if [ -e "$HOME/.config/nvim" ]; then
+        # If it's a git repository, check if it's the right one
+        if [ -d "$HOME/.config/nvim/.git" ]; then
+            pushd "$HOME/.config/nvim" >/dev/null || return 1
 
-        url=$(git remote get-url origin 2>/dev/null)
-        if [[ "$url" =~ .*github.com[:/]1995parham/elievim ]]; then
-            msg 'valid repository, so fetching it'
-            git pull origin main
+            url=$(git remote get-url origin 2>/dev/null)
+            if [[ "$url" =~ .*github.com[:/]1995parham/elievim ]]; then
+                msg 'valid repository, fetching latest changes'
+                if ! git pull origin main; then
+                    msg 'failed to pull latest changes' 'error'
+                    popd >/dev/null
+                    return 1
+                fi
+                popd >/dev/null
+                lsp_servers
 
-            return 0
-        else
-            msg "invalid repository $url"
+                msg 'syncing neovim plugins with Lazy'
+                if ! nvim --headless "+Lazy! sync" +qa; then
+                    msg 'failed to sync neovim plugins' 'error'
+                    return 1
+                fi
 
-            if yes_or_no "neovim" "do you want to remove current neovim configuration?"; then
-                msg 'removing current configuration to replace it with new configuration'
-                rm -Rf ~/.config/nvim
+                return 0
             else
-                return 1
+                msg "invalid repository $url"
+                popd >/dev/null
             fi
         fi
+
+        # Config exists but is not the right repo, ask to remove
+        remove_nvim_config || return 1
     fi
 
-    if [ -e "$HOME/.config/nvim" ] || [ -L "$HOME/.config/nvim" ]; then
-        if yes_or_no "neovim" "do you want to remove current neovim configuration?"; then
-            msg 'removing current configuration to replace it with new configuration'
-            rm -Rf ~/.config/nvim
-        else
-            return 1
-        fi
+    # Clone the repository
+    msg 'cloning elievim repository'
+    if ! git clone https://github.com/1995parham/elievim "$HOME/.config/nvim"; then
+        msg 'failed to clone elievim repository' 'error'
+        return 1
     fi
-    git clone https://github.com/1995parham/elievim ~/.config/nvim
 
     lsp_servers
 
-    nvim --headless "+Lazy! sync" +qa
+    msg 'syncing neovim plugins with Lazy'
+    if ! nvim --headless "+Lazy! sync" +qa; then
+        msg 'failed to sync neovim plugins' 'error'
+        return 1
+    fi
+
+    msg 'neovim configuration installed successfully'
 }
