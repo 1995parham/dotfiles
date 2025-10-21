4145ebbb6657048ca9dbfa50bdfeaf505a2c48ec
Parham Alvani <parham.alvani@gmail.com>
2025-10-21 14:26:53 +0000

    feat: switch gopass installation to require

--- scripts/gopass.sh
+++ scripts/gopass.sh
@@ -27,20 +27,11 @@ main_pacman() {
 }
 
 gopass-upstall() {
-    msg "upstall gppass from github"
-    gopass_vr=$(curl -s https://api.github.com/repos/gopasspw/gopass/releases/latest | grep 'tag_name' | cut -d\" -f4)
-    gopass_vl=''
-    if hash gopass &>/dev/null; then
-        gopass_vl=$(gopass version | grep gopass | cut -d' ' -f2 | sed 's/\+.*//')
-    fi
+    msg "installing gopass from github"
 
-    msg "github: ${gopass_vr#v}, local: $gopass_vl"
-    if [[ ${gopass_vr#v} != "$gopass_vl" ]]; then
-        msg "dowloading from github"
-        curl -L "https://github.com/gopasspw/gopass/releases/download/${gopass_vr}/gopass_${gopass_vr#v}_linux_amd64.deb" >gopass.deb
-        sudo dpkg -i gopass.deb
-        rm gopass.deb
-    fi
+    # Use the github.sh library to install from deb release
+    # Note: This will skip if gopass is already installed
+    require_github_release "gopasspw/gopass" "gopass" "gopass_\${version}_linux_amd64" "deb"
 
     msg "$(gopass version)"
 }
