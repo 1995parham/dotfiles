ad633c102106d78c9359007eda06b9301b07745d
Parham Alvani <parham.alvani@gmail.com>
2026-01-27 18:27:05 +0000

    chore: remove unused android packages

--- scripts/android.sh
+++ scripts/android.sh
@@ -25,42 +25,43 @@ main_pacman() {
 
 main_brew() {
     require_brew_cask openmtp android-platform-tools
+    # require_brew_cask openmtp
 
-    if [ ! -f /usr/local/bin/heimdall ]; then
-        msg 'installing heimdall to upgrade recovery.img on Galaxy Tab S6 Lite (Wi-Fi)'
-
-        # Using specific version from 2020-01-22 (last known working version)
-        local image=Heimdall-macOS-master-012220
-
-        msg 'downloading heimdall disk image'
-        if ! curl -fsSL "https://blob.lineageos.org/downloads/heimdall/$image.dmg" -o "$image.dmg"; then
-            msg 'failed to download heimdall' 'error'
-            return 1
-        fi
-
-        msg 'mounting disk image'
-        if ! hdiutil attach "$image.dmg"; then
-            msg 'failed to attach disk image' 'error'
-            rm -f "$image.dmg"
-            return 1
-        fi
-
-        msg 'copying heimdall binary to /usr/local/bin'
-        if ! sudo cp /Volumes/Heimdall/heimdall /usr/local/bin/heimdall; then
-            msg 'failed to copy heimdall binary' 'error'
-            hdiutil detach /Volumes/Heimdall 2>/dev/null
-            rm -f "$image.dmg"
-            return 1
-        fi
-
-        msg 'unmounting disk image'
-        if ! hdiutil detach /Volumes/Heimdall; then
-            msg 'failed to detach disk image' 'error'
-        fi
-
-        rm -f "$image.dmg"
-        msg 'heimdall installed successfully'
-    else
-        msg 'heimdall already installed'
-    fi
+    # if [ ! -f /usr/local/bin/heimdall ]; then
+    #     msg 'installing heimdall to upgrade recovery.img on Galaxy Tab S6 Lite (Wi-Fi)'
+    #
+    #     # Using specific version from 2020-01-22 (last known working version)
+    #     local image=Heimdall-macOS-master-012220
+    #
+    #     msg 'downloading heimdall disk image'
+    #     if ! curl -fsSL "https://blob.lineageos.org/downloads/heimdall/$image.dmg" -o "$image.dmg"; then
+    #         msg 'failed to download heimdall' 'error'
+    #         return 1
+    #     fi
+    #
+    #     msg 'mounting disk image'
+    #     if ! hdiutil attach "$image.dmg"; then
+    #         msg 'failed to attach disk image' 'error'
+    #         rm -f "$image.dmg"
+    #         return 1
+    #     fi
+    #
+    #     msg 'copying heimdall binary to /usr/local/bin'
+    #     if ! sudo cp /Volumes/Heimdall/heimdall /usr/local/bin/heimdall; then
+    #         msg 'failed to copy heimdall binary' 'error'
+    #         hdiutil detach /Volumes/Heimdall 2>/dev/null
+    #         rm -f "$image.dmg"
+    #         return 1
+    #     fi
+    #
+    #     msg 'unmounting disk image'
+    #     if ! hdiutil detach /Volumes/Heimdall; then
+    #         msg 'failed to detach disk image' 'error'
+    #     fi
+    #
+    #     rm -f "$image.dmg"
+    #     msg 'heimdall installed successfully'
+    # else
+    #     msg 'heimdall already installed'
+    # fi
 }
